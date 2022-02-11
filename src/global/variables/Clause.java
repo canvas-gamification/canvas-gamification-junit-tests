f59fe542889053a6e4e0df4051433bc3a97e7db2
opeyem1a <opeyadeyemi@gmail.com>
2022-02-11 09:14:29 -0800

    + clauses have internal validate() method + clauses have invalid message defined internally + default message is returned if clause invalid message is not defined

--- src/global/variables/Clause.java
+++ src/global/variables/Clause.java
@@ -1,6 +1,9 @@
 package global.variables;
 
+import java.util.Objects;
+
 public abstract class Clause implements Regexable {
+    String INVALID_MESSAGE;
     String name;
 
     public Clause(String name) {
@@ -13,5 +16,14 @@ public abstract class Clause implements Regexable {
 
     public String getName() {
         return this.name;
+    }
+
+    public boolean validate(int matchGroupNum) {
+        return true;
+    }
+
+    public String getInvalidMessage() {
+        // TODO: return "Invalid ClauseClassName" if INVALID_MESSAGE == null
+        return Objects.requireNonNullElse(this.INVALID_MESSAGE, "Invalid Clause");
     }
 }
