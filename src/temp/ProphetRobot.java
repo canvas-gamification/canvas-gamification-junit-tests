6f3d0a207c3dfe05f96b069c18a02fe207ce2848
opeyem1a <opeyadeyemi@gmail.com>
2022-02-11 10:56:29 -0800

    + create Logger class so we can print messages to console for devs

--- src/temp/ProphetRobot.java
+++ src/temp/ProphetRobot.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class ProphetRobot {
     public static void main(String[] args) {
         Random gen = new Random();
-        int prediction = gen.nextInt(100);
+        int prediction = gen.nextInt(95);
         char fingersHeldUp = (char) (65 + gen.nextInt(5));
-        double num = 12 + gen.nextDouble() * 50;
+        double num = 12 + gen.nextDouble() * 49;
         boolean same = fingersHeldUp == prediction;
         System.out.println("The prediction by Simon was: " + prediction + ", num: " + num + " and the number of fingers you held up was: " + fingersHeldUp);
 //        if (same) {
