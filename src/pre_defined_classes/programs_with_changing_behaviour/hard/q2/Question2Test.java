306d58ce77cc6131b51f9215922b07baeb5d74af
Hedgemon4 <sethlinc4@gmail.com>
2022-02-24 16:39:55 -0700

    Finished pre defined classes-programs with changing behaviour-hard-q2 now that IntegerLiteral supports ranges

--- src/pre_defined_classes/programs_with_changing_behaviour/hard/q2/Question2Test.java
+++ src/pre_defined_classes/programs_with_changing_behaviour/hard/q2/Question2Test.java
@@ -1,23 +1,25 @@
 package pre_defined_classes.programs_with_changing_behaviour.hard.q2;
 
 import global.BaseRandomTest;
+import global.BaseTest;
 import global.variables.Clause;
-import global.variables.RandomInteger;
-import global.variables.StringLiteral;
+import global.variables.clauses.IntegerLiteral;
+import global.variables.clauses.RandomInteger;
+import global.variables.clauses.StringLiteral;
 
 public class Question2Test extends BaseRandomTest {
 
     public Clause[] testSentence() {
         return new Clause[]{
-                new RandomInteger(10000000 , 100000000),
+                new RandomInteger(10000000, 100000000),
                 new StringLiteral(" has been decrypted using "),
                 new RandomInteger(1, 10),
                 new StringLiteral(" into "),
-                //can't finish this test until Integer Literal is updated
+                new IntegerLiteral(1000000, 100000000)
         };
     }
 
     public void runMain() {
-        Question2.main(new String[0]);}
-
+        Question2.main(new String[0]);
+    }
 }
