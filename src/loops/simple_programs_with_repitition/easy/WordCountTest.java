00d0f4522a16cd10c94404728d9e205fd289ade4
Le Quan <minhquanle_2002@yahoo.com>
2022-02-25 00:34:14 -0800

    loops_simple_programs_with_repitition_easy_q5

---
+++ src/loops/simple_programs_with_repitition/easy/WordCountTest.java
@@ -0,0 +1,35 @@
+package loops.simple_programs_with_repitition.easy;
+
+import global.variables.*;
+import global.variables.clauses.*;
+import global.BaseTest;
+
+public class WordCountTest extends BaseTest {
+
+    String input = "This is just for testing. Do not copy!";
+
+    public Clause[] testSentence() {
+
+        Clause[] output = new Clause[3];
+        output[0] = new StringLiteral("Enter a sentence: ");
+        output[1] = new NewLine();
+        output[2] = new StringLiteral("The number of words in the sentence is " + countingWords(input) + " words.");
+
+        return output;
+
+    }
+
+    public void runMain() {
+        provideInput(input);
+        WordCount.main(new String[0]);
+    }
+
+    public static int countingWords(String message) {
+        message.toLowerCase();
+        int count = 0;
+        for (int i = 0; i < message.length(); i++)
+            if (message.charAt(i) == ' ')
+                count++;
+        return count + 1;
+    }
+}
