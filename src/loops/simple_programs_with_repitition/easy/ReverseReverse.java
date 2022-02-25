880e42da2a7e9034ce60d282e037f2b86652fe36
Le Quan <minhquanle_2002@yahoo.com>
2022-02-25 00:32:03 -0800

    loops_simple_programs_with_repitition_easy_q4

---
+++ src/loops/simple_programs_with_repitition/easy/ReverseReverse.java
@@ -0,0 +1,23 @@
+package loops.simple_programs_with_repitition.easy;
+
+/*
+Write a program to reverse a number.
+*/
+
+import java.util.Scanner;
+public class ReverseReverse
+{
+   public static void main( String[] args )
+   {
+      Scanner input = new Scanner( System.in );
+      System.out.println( "Enter a number : " );
+      int n = input.nextInt();
+      int reverse = 0;
+      while( n > 0 ) 
+      {
+         reverse = ( reverse * 10 ) + ( n % 10 );
+         n = n / 10;
+      }
+      System.out.println( "Reversed Number: " + reverse );
+   }
+}
