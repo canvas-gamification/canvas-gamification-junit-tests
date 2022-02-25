46232840bec86c485578f5a932f3a2d0f407c4f4
Le Quan <minhquanle_2002@yahoo.com>
2022-02-25 00:40:44 -0800

    loops_simple_programs_with_repitition_easy_q7

---
+++ src/loops/simple_programs_with_repitition/easy/AlTeRnAtInGcAsETest.java
@@ -0,0 +1,41 @@
+package loops.simple_programs_with_repitition.easy;
+
+import global.variables.*;
+import global.variables.clauses.*;
+import global.BaseTest;
+
+public class AlTeRnAtInGcAsETest extends BaseTest {
+
+    String input = "This is a sentence. This is another sentence.";
+
+    public Clause[] testSentence() {
+
+        Clause[] output = new Clause[3];
+
+        output[0] = new StringLiteral("Enter a sentence: ");
+        output[1] = new NewLine();
+        output[2] = new StringLiteral("YoUr SeNtEnCe In AlTeRnAtInG cAsE iS: " + alternate(input));
+
+        return output;
+
+    }
+
+    public void runMain() {
+        provideInput(input);
+        AlTeRnAtInGcAsE.main(new String[0]);
+    }
+
+    public static String alternate(String sentence) {
+        String result = "";
+        int i = 0;
+        while (i < sentence.length()) {
+            if (i % 2 == 0)
+                result += Character.toUpperCase(sentence.charAt(i));
+            else
+                result += Character.toLowerCase(sentence.charAt(i));
+            i++;
+        }
+        return result;
+    }
+
+}
