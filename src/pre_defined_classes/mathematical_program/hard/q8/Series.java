8ad114900c6a4bd562fe5739f484e55d505e4a84
Samantha H <hodge1214@gmail.com>
2022-02-26 14:16:13 -0500

    Wrote out clause for Series but for some reason it isn't working. Waiting on double literal to write a test that checks the math.

---
+++ src/pre_defined_classes/mathematical_program/hard/q8/Series.java
@@ -0,0 +1,20 @@
+package pre_defined_classes.mathematical_program.hard.q8;
+
+/*
+A geometric sequence is a sequence of numbers where each term is determined by multiplying the previous term using the general formula:
+Nth _term = first_term(r)^(n-1)
+where r is a constant and n is an integer representing the nth term in the sequence.
+As an example, let's suppose that the first term is 3 and r=2. Then, the second term is calculated with n=2 as follows:
+second_term = 3(2)^(2-1) = 3(2) = 6. Similarly, the fourth term is calculated with n=4 as follows: fourth_term = 3(2)^(4-1) = 3(8) = 24.
+Following this approach, make a Java program that computes the seventh term in a sequence where r=1/4 and the first_term=1.
+ */
+public class Series {
+    public static void main(String[] args) {
+        int firstTerm = 1;
+        double r = 0.25;
+        int n = 7;
+        double nthTerm = firstTerm * Math.pow(r, n - 1);
+        System.out.println("The 7th term in the geometric sequence is: " + nthTerm);
+    }
+
+}
---
+++ src/pre_defined_classes/mathematical_program/hard/q8/SeriesTest.java
@@ -0,0 +1,24 @@
+package pre_defined_classes.mathematical_program.hard.q8;
+
+import global.BaseTest;
+import global.variables.Clause;
+import global.variables.StringLiteral;
+import org.junit.jupiter.api.Test;
+import temp.Example;
+
+public class SeriesTest extends BaseTest {
+    public Clause[] testSentence() {
+        return new Clause[]{
+                new StringLiteral("The 7th term in the geometric sequence is: 2.44140625E-4")
+        };
+    }
+
+    public void runMain() {
+        Example.main(new String[]{});
+    }
+
+    @Test
+    public void mathTest() {
+        //this test will check if the math is wrong, not just the formatting, to provide a more accurate error message
+    }
+}
