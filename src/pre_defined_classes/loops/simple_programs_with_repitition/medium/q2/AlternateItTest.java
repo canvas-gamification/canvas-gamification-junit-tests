14bc649e99176fc59e92b760c2c80eb8de787be2
Le Quan <minhquanle_2002@yahoo.com>
2022-03-01 15:12:43 -0800

    loops_simple_programs_with_repitition_medium_q2

---
+++ src/pre_defined_classes/loops/simple_programs_with_repitition/medium/q2/AlternateItTest.java
@@ -0,0 +1,31 @@
+package pre_defined_classes.loops.simple_programs_with_repitition.medium.q2;
+
+import global.variables.*;
+import global.variables.clauses.*;
+import global.BaseTest;
+
+public class AlternateItTest extends BaseTest {
+
+    int input1 = 20;
+
+    public Clause[] testSentence() {
+        return new Clause[] {
+            new StringLiteral("Enter a number for the upper bound of the series: "),
+            new NewLine(),
+            new StringLiteral("The sum of the series is "),
+            new IntegerLiteral(seriesSum(input1)),
+        };
+    }
+
+    public void runMain() {
+        provideInput("" + input1);
+        AlternateIt.main(new String[0]);
+    }
+
+    public int seriesSum(int n) {
+        int sum  = 0; 
+        for(int i = 0; i < n; i++) 
+           sum += Math.pow( -1, i ) * ( i + 1 );
+        return(sum);
+    }
+}
