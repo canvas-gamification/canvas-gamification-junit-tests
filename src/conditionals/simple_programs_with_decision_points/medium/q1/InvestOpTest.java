ee64c20e5f73f271728e235e911185fb22bb7699
Hedgemon4 <sethlinc4@gmail.com>
2022-03-02 15:54:13 -0700

    Need multiple scanner inputs to finish

--- src/conditionals/simple_programs_with_decision_points/medium/q1/InvestOpTest.java
+++ src/conditionals/simple_programs_with_decision_points/medium/q1/InvestOpTest.java
@@ -10,20 +10,28 @@ import global.variables.clauses.IntegerLiteral;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.RandomInteger;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
+
 
 public class InvestOpTest extends BaseTest {
+    public Random r = new Random();
 
     public Clause[] testSentence() {
         return new Clause[]{
-                new StringLiteral("What is your investment in $ (rounded to the nearest dollar)? "),
+                new StringLiteral("What is your investment in \\$ \\(rounded to the nearest dollar\\)\\? "),
                 new NewLine(),
-                new StringLiteral("Investment : "),
-                new IntegerLiteral(5000)
+                new StringLiteral("Investment \\: \\$"),
+                new IntegerLiteral(10002)
         };
     }
 
     public void runMain() {
-        provideInput("" + 5000);
+        provideInput(5001 +  "");
         InvestOp.main(new String[0]);
     }
+
 }
