36c2aa32b3297b4ef1c05c356f9e92aa9db1d407
Hedgemon4 <sethlinc4@gmail.com>
2022-03-02 16:41:03 -0700

    Updated code using DoubleLiteral to test for correct output

--- src/pre_defined_classes/mathematical_program/hard/q1/Javademic.java
+++ src/pre_defined_classes/mathematical_program/hard/q1/Javademic.java
@@ -4,6 +4,13 @@ package pre_defined_classes.mathematical_program.hard.q1;
 Make a program to calculate the growth in the number of cases (xt) for a pandemic at a given time <tt>t</tt> using the
 equation: xt= (1+r)t x0, where r = 0.3 is the rate of growth, the initial number of people infected is x0= 5. Your
 program will calculate how many people are going to be infected in t = 30 days.
+Distractions:
+double xt = (( 1 + r ) * t ) + x0;
+import java.util.Math;
+int r = 0.3;
+double xt = Math.pow( quantity, x0 );
+double quantity = Math.exp( 1+r, t );
+double xt = Math.exp(( 1 + r ), t) * x0;
  */
 
 public class Javademic {
--- src/pre_defined_classes/mathematical_program/hard/q1/JavademicTest.java
+++ src/pre_defined_classes/mathematical_program/hard/q1/JavademicTest.java
@@ -2,22 +2,33 @@ package pre_defined_classes.mathematical_program.hard.q1;
 
 import global.BaseTest;
 import global.variables.Clause;
-import global.variables.IntegerLiteral;
-import global.variables.StringLiteral;
-import pre_defined_classes.mathematical_program.hard.q1.Javademic;
+import global.variables.clauses.DoubleLiteral;
+import global.variables.clauses.IntegerLiteral;
+import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JavademicTest extends BaseTest {
+    final double xtExpected = 13099.97821824974;
 
     public Clause[] testSentence() {
         return new Clause[]{
                 new StringLiteral("Num cases of infections in "),
                 new IntegerLiteral(30),
                 new StringLiteral(" days is "),
-                new StringLiteral("13099.97821824974"),
+                //These are the possible range of values if they select on of the parsons distractors, and the proper
+                //value is tested for below
+                new DoubleLiteral(44, 1.23453340530816128E17, "xt"),
                 new StringLiteral(" people")
         };
     }
 
     public void runMain() {
-        Javademic.main(new String[0]);}
+        Javademic.main(new String[0]);
+    }
+
+    @Test
+    public void xtTest(){
+        assertEquals(Double.parseDouble(getItemByName("xt")) , xtExpected, "Calculated value of xt is incorrect");
+    }
 }
