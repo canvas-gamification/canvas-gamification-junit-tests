ab3350b31b880fe3c13400a590e00e20f432db56
Hedgemon4 <sethlinc4@gmail.com>
2022-03-02 17:24:07 -0700

    Updated code using DoubleLiteral to test calculated area value

--- src/pre_defined_classes/mathematical_program/medium/q2/HeronTest.java
+++ src/pre_defined_classes/mathematical_program/medium/q2/HeronTest.java
@@ -2,17 +2,28 @@ package pre_defined_classes.mathematical_program.medium.q2;
 
 import global.BaseTest;
 import global.variables.Clause;
+import global.variables.clauses.DoubleLiteral;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HeronTest extends BaseTest {
+    final double area = 8.317902379807064;
 
     public Clause[] testSentence() {
         return new Clause[]{
-                new StringLiteral("The area of the triangle is: 8.317902379807064 units squared.")
+                new StringLiteral("The area of the triangle is: "),
+                new DoubleLiteral(8, 9, "area"),
+                new StringLiteral(" units squared.")
         };
     }
 
     public void runMain() {
         Heron.main(new String[0]);
+    }
+
+    @Test
+    public void areaTest(){
+        assertEquals(area + "", getItemByName("area"), "Calculated area is inccorect, try again.");
     }
 }
