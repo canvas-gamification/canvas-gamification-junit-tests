687c9bbcd2009d0ca5e96033fe3c20aec633ce60
Hedgemon4 <sethlinc4@gmail.com>
2022-03-02 17:18:13 -0700

    Updated code using DoubleLiteral to test calculated BMI value

--- src/pre_defined_classes/mathematical_program/medium/q1/BMITest.java
+++ src/pre_defined_classes/mathematical_program/medium/q1/BMITest.java
@@ -2,19 +2,31 @@ package pre_defined_classes.mathematical_program.medium.q1;
 
 import global.BaseTest;
 import global.variables.Clause;
+import global.variables.clauses.DoubleLiteral;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.StringLiteral;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BMITest extends BaseTest {
 
     public Clause[] testSentence() {
         return new Clause[]{
-                new StringLiteral("The BMI of a person that weighs 70.5 kilograms and is 1.63 metres tall is: 26.53468327750386")
+                new StringLiteral("The BMI of a person that weighs "),
+                new DoubleLiteral(70.5, 70.5),
+                new StringLiteral(" kilograms and is "),
+                new DoubleLiteral(1.63, 1.63),
+                new StringLiteral(" metres tall is: "),
+                new DoubleLiteral(26, 27, "BMI")
         };
     }
 
     public void runMain() {
         BMI.main(new String[0]);
+    }
+
+    @Test
+    public void BMIValueTest(){
+        assertEquals(26.53468327750386 + "", getItemByName("BMI").trim(), "Calculated value of BMI is incorrect");
     }
 }
