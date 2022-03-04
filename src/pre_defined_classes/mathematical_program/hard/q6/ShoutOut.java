c6dbc1eee78078f9e370a24d57b92d51549493ed
Samantha H <hodge1214@gmail.com>
2022-03-03 20:01:56 -0500

    Added distractors to question code, added incorrect math message.

--- src/pre_defined_classes/mathematical_program/hard/q6/ShoutOut.java
+++ src/pre_defined_classes/mathematical_program/hard/q6/ShoutOut.java
@@ -7,6 +7,17 @@ of the sound to be measured in W/m^2 ( Watts per metre squared), I0 is the minim
 by the human ear (which is known to be 10^-12 W/m^2). Make a Java program to find the ratio between the decibel value
 of a person speaking if the intensity of their voice is 10^-5 W/m^2  and a person shouting which has the intensity of
 10^-4 W/m^2 .
+
+Distractors:
+int dB = 10 * ( Math.log( IpShout / I0 ) );
+double dB = ( Math.log( IpShout / I0 ) );
+int dB =   ( Math.log( IpShout / 10 ) );
+double dBRatio = ( 10 * ( Math.log( IpSpeak / I0 ) ) ) / dB;
+dB = ( 10 * ( Math.log( IpSpeak / I0 ) ) ) ;
+dBRatio = ( 10 * ( Math.Log( IpSpeak / I0 ) ) ) / dB;
+dBSpeak = 10 * ( log( IpSpeak / I0 ) );
+dBShout = 10 * ( log( IpShout / I0 ) );
+dBRatio = dBShout  / dBSpeak;
  */
 public class ShoutOut {
     public static void main(String[] args) {
--- src/pre_defined_classes/mathematical_program/hard/q6/ShoutOutTest.java
+++ src/pre_defined_classes/mathematical_program/hard/q6/ShoutOutTest.java
@@ -2,20 +2,26 @@ package pre_defined_classes.mathematical_program.hard.q6;
 
 import global.BaseTest;
 import global.variables.Clause;
-import global.variables.StringLiteral;
+import global.variables.clauses.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShoutOutTest extends BaseTest {
     public Clause[] testSentence() {
         return new Clause[]{
-                new StringLiteral("The ratio of the decibel values between a person speak and a person shouting is 0.8749999999999999"),
+                new StringLiteral("The ratio of the decibel values between a person speak and a person shouting is "),
+                new DoubleLiteral(0.8749999999999999, 0.8749999999999999, "math")
         };
     }
+
 
     public void runMain() {
         ShoutOut.main(new String[]{});
     }
 
-    public void mathTest(){
-        //this test will be to check if the math is wrong to give a clearer error message
+    @Test
+    public void mathTest() {
+        assertEquals("0.8749999999999999", getItemByName("math"), "Your math is incorrect");
     }
 }
