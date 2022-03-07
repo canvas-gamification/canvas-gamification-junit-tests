8706d3e6c980764bd82d8155ef55a3e143453501
opeyem1a <opeyadeyemi@gmail.com>
2022-03-06 21:00:34 -0800

    + better TestOption + TestOption validation

--- src/global/tools/TestOption.java
+++ src/global/tools/TestOption.java
@@ -3,14 +3,18 @@ package global.tools;
 import global.exceptions.InvalidTestOptionException;
 
 public class TestOption {
-    public static boolean runMainBeforeEach = true;
+    public static boolean isInputTest = false;
+    public static String defaultInput = null;
 
     public static void validate() throws InvalidTestOptionException {
+        // TODO: catch these errors somewhere and make sure they are only logged to the devs and not returned to the user.
         // validate that options are valid
-//        throw new InvalidTestOptionException("This is a test.");  // for example
-    }
+        if(isInputTest && defaultInput == null) {
+            throw new InvalidTestOptionException("Input tests must define a TestOption.defaultInput.");
+        }
 
-    public static boolean shouldRunMainBeforeEach() {
-        return runMainBeforeEach;
+        if(defaultInput != null && !isInputTest) {
+            throw new InvalidTestOptionException("You have defined a defaultInput without setting TestOption.isInputTest = true.");
+        }
     }
 }
