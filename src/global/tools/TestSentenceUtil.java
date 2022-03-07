4a505e7a9f6f35189e8cf8af5f207aa54bfcc31a
opeyem1a <opeyadeyemi@gmail.com>
2022-03-07 14:41:58 -0800

    + hasRandom util

--- src/global/tools/TestSentenceUtil.java
+++ src/global/tools/TestSentenceUtil.java
@@ -1,9 +1,18 @@
 package global.tools;
 
 import global.variables.Clause;
+import global.variables.RandomClause;
 import global.variables.clauses.PlaceHolder;
 
 public class TestSentenceUtil {
+    public static boolean hasRandom(Clause[] testSentence) {
+        // does the sentence contain random clauses
+        for(Clause clause: testSentence) {
+            if(clause instanceof RandomClause) return true;
+        }
+        return false;
+    }
+
     public static boolean hasPlaceHolders(Clause[] testSentence) {
         // TODO: does the sentence contain placeholders
         return false;
