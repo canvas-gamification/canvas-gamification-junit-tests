3863bfed0b9d251cb9a25710ea8c165d139f92e0
Hedgemon4 <sethlinc4@gmail.com>
2022-03-10 13:56:17 -0700

    Renamed test to MainTest and inserted Parsons label

--- src/pre_defined_classes/programs_with_changing_behaviour/hard/q2/MainTest.java
+++ src/pre_defined_classes/programs_with_changing_behaviour/hard/q2/MainTest.java
@@ -1,15 +1,15 @@
 package pre_defined_classes.programs_with_changing_behaviour.hard.q2;
 
 import global.BaseRandomTest;
-import global.BaseTest;
 import global.variables.Clause;
 import global.variables.clauses.IntegerLiteral;
 import global.variables.clauses.RandomInteger;
 import global.variables.clauses.StringLiteral;
+
 import static org.junit.jupiter.api.Assertions.fail;
 
-public class DeCrypticTest extends BaseRandomTest {
-
+public class MainTest extends BaseRandomTest {
+    //Parsons with Distractors
     public Clause[] testSentence() {
         return new Clause[]{
                 new RandomInteger(10000000, 100000000),
@@ -21,11 +21,7 @@ public class DeCrypticTest extends BaseRandomTest {
     }
 
     public void runMain() {
-        try {
-            DeCryptic.main(new String[0]);
-        }catch(ArithmeticException e){
-            fail("Tried to divide by 0. Make sure your key is larger than 0");
-        }
+        DeCryptic.main(new String[0]);
     }
 
 }
