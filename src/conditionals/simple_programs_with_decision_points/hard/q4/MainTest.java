5f6fd8297faab646e1f7de73a41ad3d898a4bc4b
Samantha H <hodge1214@gmail.com>
2022-03-11 15:44:19 -0500

    Set up structure, waiting on input stuff

---
+++ src/conditionals/simple_programs_with_decision_points/hard/q4/MainTest.java
@@ -0,0 +1,24 @@
+package conditionals.simple_programs_with_decision_points.hard.q4;
+
+import global.BaseTest;
+import global.variables.Clause;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.RandomInteger;
+import global.variables.clauses.StringLiteral;
+import global.variables.wrappers.Optional;
+import temp.Example;
+
+public class MainTest extends BaseTest {
+    //Parsons
+    public Clause[] testSentence() {
+        return new Clause[]{
+                new StringLiteral("Is the door open or closed right now? (Enter true for open and false for closed)"),
+                new NewLine(),
+                new StringLiteral("")//either I will close the door or Time to open the door
+        };
+    }
+
+    public void runMain() {
+        DoorBoy.main(new String[]{});
+    }
+}
