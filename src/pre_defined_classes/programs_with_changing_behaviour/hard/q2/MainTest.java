22b0148c831cd1de9185a29ee41f0151ac430418
Hedgemon4 <sethlinc4@gmail.com>
2022-03-10 19:23:44 -0700

    Added a test to make sure encrypt = crypt/key

--- src/pre_defined_classes/programs_with_changing_behaviour/hard/q2/MainTest.java
+++ src/pre_defined_classes/programs_with_changing_behaviour/hard/q2/MainTest.java
@@ -5,18 +5,20 @@ import global.variables.Clause;
 import global.variables.clauses.IntegerLiteral;
 import global.variables.clauses.RandomInteger;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MainTest extends BaseRandomTest {
     //Parsons with Distractors
     public Clause[] testSentence() {
         return new Clause[]{
-                new RandomInteger(10000000, 100000000),
+                new RandomInteger(10000000, 100000000, "crypt"),
                 new StringLiteral(" has been decrypted using "),
-                new RandomInteger(1, 10),
+                new RandomInteger(1, 10, "key"),
                 new StringLiteral(" into "),
-                new IntegerLiteral(1000000, 100000000)
+                new IntegerLiteral(1000000, 100000000, "encrypt")
         };
     }
 
@@ -24,4 +26,11 @@ public class MainTest extends BaseRandomTest {
         DeCryptic.main(new String[0]);
     }
 
+    @Test
+    public void outputTest(){
+        int crypt = Integer.parseInt(getItemByName("crypt"));
+        int key = Integer.parseInt(getItemByName("key"));
+        int encrypt = Integer.parseInt(getItemByName("encrypt"));
+        assertEquals(crypt/key, encrypt, "Value of encrypt is wrong");
+    }
 }
