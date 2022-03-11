6a0deb708b2ca32b885cf1ff5e5cd7f23a654a22
Hedgemon4 <sethlinc4@gmail.com>
2022-03-10 20:26:20 -0700

    Renamed test, added Parsons label, and updated asserts

--- src/pre_defined_classes/mathematical_program/medium/q5/MainTest.java
+++ src/pre_defined_classes/mathematical_program/medium/q5/MainTest.java
@@ -8,14 +8,14 @@ import global.variables.clauses.StringLiteral;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TrigValTest extends BaseTest {
-
+public class MainTest extends BaseTest {
+    //Parsons
     public Clause[] testSentence() {
         return new Clause[]{
-                new StringLiteral("The value of tan\\^2 is\\: "),
+                new StringLiteral("The value of tan\\^2 is: "),
                 new DoubleLiteral(2, 4, "tan2"),
                 new NewLine(),
-                new StringLiteral("The value of sec\\^2 is\\: "),
+                new StringLiteral("The value of sec\\^2 is: "),
                 new DoubleLiteral(3, 5, "sec2"),
                 new NewLine(),
                 new StringLiteral("Their sum is\\: "),
@@ -29,8 +29,8 @@ public class TrigValTest extends BaseTest {
 
     @Test
     public void trigDoubleValueTest(){
-        assertEquals(2.9999999999999982 + "", getItemByName("tan2"), "Calculated value of tan^2 is incorrect");
-        assertEquals(3.9999999999999982 + "", getItemByName("sec2"), "Calculated value of sec^2 is incorrect");
-        assertEquals(6.9999999999999964 + "", getItemByName("sum"), "Calculated sum of tan^2 + sec^2 is incorrect");
+        assertEquals(3.0, Double.parseDouble(getItemByName("tan2")), 0.1,  "Calculated value of tan^2 is incorrect");
+        assertEquals(4.0, Double.parseDouble(getItemByName("sec2")), 0.1,  "Calculated value of sec^2 is incorrect");
+        assertEquals(7.0, Double.parseDouble(getItemByName("sum")), 0.1,"Calculated sum of tan^2 + sec^2 is incorrect");
     }
 }
--- src/pre_defined_classes/mathematical_program/medium/q5/TrigVal.java
+++ src/pre_defined_classes/mathematical_program/medium/q5/TrigVal.java
@@ -2,23 +2,25 @@ package pre_defined_classes.mathematical_program.medium.q5;
 
 /*
 Java's Math class has the ability to calculate trigonometric values by using methods like <tt>cos()</tt> for the cosine
-function and <tt>tan()</tt> for the tangent function. Using these functions, we can verify some well known trigonometric
-identities. Make a Java program that verifies the following trigonometric identity: sec2() + tan2() = 1 where(theta) is
-the degree in radians and sec is a mathematical formula that calculates the length of the hypotenuse divided by the
-length of the adjacent side. Note, the formula to obtain sec2()is : sec2()=1cos2()
+function and <tt>tan()</tt> for the tangent function. Using these functions, we can verify some well known
+trigonometric identities. Make a Java program that verifies the following trigonometric identity: sec2() + tan2() = 1
+where(theta) is the degree in radians and sec is a mathematical formula that calculates the length of the hypotenuse
+divided by the length of the adjacent side. Note, the formula to obtain sec2()is : sec2()=1cos2()
+Sample Output:
+	The value of tan^2 is: 1
+	The value of sec^2 is: 1
+	Their sum is: 2
  */
 
-public class TrigVal
-{
-    public static void main( String[] args )
-    {
+public class TrigVal {
+    public static void main(String[] args) {
         double degrees = 60.0;
-        double theta = Math.toRadians( degrees );
-        double tan2 = Math.pow( Math.tan( theta ) , 2 );
-        double cos2 = Math.pow(Math.cos( theta ), 2 );
+        double theta = Math.toRadians(degrees);
+        double tan2 = Math.pow(Math.tan(theta), 2);
+        double cos2 = Math.pow(Math.cos(theta), 2);
         double sec2 = 1 / cos2;
-        System.out.println( "The value of tan^2 is: " + tan2 );
-        System.out.println( "The value of sec^2 is: " + sec2 );
-        System.out.println( "Their sum is: " + ( tan2 + sec2 ) );
+        System.out.println("The value of tan^2 is: " + tan2);
+        System.out.println("The value of sec^2 is: " + sec2);
+        System.out.println("Their sum is: " + (tan2 + sec2));
     }
 }
