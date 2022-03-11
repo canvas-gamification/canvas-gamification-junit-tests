3e0fede3d779f3ebeafd29cd5808eed08bb6bbf1
Hedgemon4 <sethlinc4@gmail.com>
2022-03-10 20:35:28 -0700

    Renamed test, added Parsons label, and updated asserts

--- src/pre_defined_classes/mathematical_program/medium/q7/MainTest.java
+++ src/pre_defined_classes/mathematical_program/medium/q7/MainTest.java
@@ -1,6 +1,5 @@
 package pre_defined_classes.mathematical_program.medium.q7;
 
-import global.BaseRandomTest;
 import global.BaseTest;
 import global.variables.Clause;
 import global.variables.clauses.DoubleLiteral;
@@ -8,8 +7,8 @@ import global.variables.clauses.StringLiteral;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FastAndWateryTest extends BaseTest {
-
+public class MainTest extends BaseTest {
+    //Parsons
     public Clause[] testSentence() {
         return new Clause[]{
                 new StringLiteral("The speed of a tsunami starting at a depth of 5000 is "),
@@ -24,6 +23,6 @@ public class FastAndWateryTest extends BaseTest {
 
     @Test
     public void waterSpeedMathTest(){
-        assertEquals(221.35943621178654 + "", getItemByName("waterSpeed"), "Your calculated water speed is incorrect");
+        assertEquals(221.4 , Double.parseDouble(getItemByName("waterSpeed")), 0.2,  "Your calculated water speed is incorrect");
     }
 }
