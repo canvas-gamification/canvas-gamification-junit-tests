13e3850336edebade33c0c825809b62bd04a2e7d
Hedgemon4 <sethlinc4@gmail.com>
2022-03-10 20:09:05 -0700

    Renamed test, added Parsons label, and updated asserts

--- src/pre_defined_classes/mathematical_program/medium/q4/BasketballGames.java
+++ src/pre_defined_classes/mathematical_program/medium/q4/BasketballGames.java
@@ -3,21 +3,22 @@ package pre_defined_classes.mathematical_program.medium.q4;
 /*
 You are a scorekeeper for your high school basketball team. It’s the end of the season and you have to calculate your
 team’s participation in all of the tournaments. You decide to do this using a Java program. Your job is to get the
-program to calculate the total score across all the games, find the highest score, and find the lowest score. Here is
-the game statistics:
-            Game #1 - Your school lost (Your coach recorded the score as -36)
-            Game #2 - Your school won (Your coach recorded the scored as 59)
-            Game #3 - Your school won (Your coach recorded the scored as 32)
-As you can see, whenever a game was lost, the score was recorded as a negative number. So the actual score for Game #1
-is 36 points. Your program will need to take this into consideration.
+ program to calculate the total score across all the games, find the highest score, and find the lowest score.
+ Here are the game statistics:
+            Game #1 - Your school scored 36 points
+            Game #2 - Your school scored 59 points
+            Game #3 - Your school scored 32 points
+	Sample Output:
+		Our team scored 100 points in the season!
+		The highest score was 50 points
+		The lowest score was 20 points
  */
 
 public class BasketballGames {
     public static void main(String[] args) {
-        int game1 = -36;
+        int game1 = 36;
         int game2 = 59;
         int game3 = 32;
-        game1 = Math.abs(game1);
         int max = Math.max(Math.max(game1, game2), game3);
         int min = Math.min(Math.min(game1, game2), game3);
         System.out.println("Our team scored " + (game1 + game2 + game3) + " points in the season!");
--- src/pre_defined_classes/mathematical_program/medium/q4/MainTest.java
+++ src/pre_defined_classes/mathematical_program/medium/q4/MainTest.java
@@ -5,26 +5,36 @@ import global.variables.Clause;
 import global.variables.clauses.IntegerLiteral;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.api.Test;
 
-public class BasketballGamesTest extends BaseRandomTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class MainTest extends BaseRandomTest {
+    //Parsons
     public Clause[] testSentence() {
         return new Clause[]{
                 new StringLiteral("Our team scored "),
-                new IntegerLiteral(127),
+                new IntegerLiteral(120, 130, "totalPoints"),
                 new StringLiteral(" points in the season!"),
                 new NewLine(),
                 new StringLiteral("The highest score was "),
-                new IntegerLiteral(59),
+                new IntegerLiteral(55, 65, "highScore"),
                 new StringLiteral(" points."),
                 new NewLine(),
                 new StringLiteral("The lowest score was "),
-                new IntegerLiteral(32),
+                new IntegerLiteral(30, 35, "lowScore"),
                 new StringLiteral(" points."),
         };
     }
 
     public void runMain() {
         BasketballGames.main(new String[0]);
+    }
+
+    @Test
+    public void outputTest(){
+        assertEquals(127, Integer.parseInt(getItemByName("totalPoints")),  "Calculated number of points is incorrect");
+        assertEquals(59, Integer.parseInt(getItemByName("highScore")), "Calculated high score is incorrect");
+        assertEquals(32, Integer.parseInt(getItemByName("lowScore")), "Calculated lowest score is incorrect");
     }
 }
