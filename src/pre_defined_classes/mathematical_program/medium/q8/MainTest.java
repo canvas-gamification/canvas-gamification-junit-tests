4e847c92e9b7faf264b21d1bd9ad52ab3671991a
Hedgemon4 <sethlinc4@gmail.com>
2022-03-11 14:47:15 -0700

    Added space before Parsons

--- src/pre_defined_classes/mathematical_program/medium/q8/MainTest.java
+++ src/pre_defined_classes/mathematical_program/medium/q8/MainTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
-    //Parsons
+    // Parsons
     public Clause[] testSentence() {
         return new Clause[]{
                 new StringLiteral("The magnitude of an earthquake with intensity "),
