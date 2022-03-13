462070d7da46906f7623846d804f649cb8133975
Hedgemon4 <sethlinc4@gmail.com>
2022-03-13 17:23:12 -0600

    Wrote test for conditionals-simple_programs_with_decision_points-medium-q2

---
+++ src/global/conditionals/simple_programs_with_decision_points/medium/q2/NumberJumble.java
@@ -0,0 +1,21 @@
+package global.conditionals.simple_programs_with_decision_points.medium.q2;
+import java.util.Scanner;
+
+/*Write a program that asks the user for a number and divides the number by 10 if it is greater than 10, and multiplies
+ it by 10 if it is less than 10.
+*/
+
+public class NumberJumble
+{
+    public static void main( String [] args )
+    {
+        Scanner input = new Scanner( System.in );
+        System.out.println("Enter a number: ");
+        int n = input.nextInt();
+        if( n > 10)
+            n = n / 10;
+        else
+            n = n * 10;
+        System.out.println("Number = " + n);
+    }
+}
