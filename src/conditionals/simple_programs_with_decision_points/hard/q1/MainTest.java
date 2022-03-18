218d88791b4c96cb31f57f370c36a25ae6fbd87a
Samantha H <hodge1214@gmail.com>
2022-03-18 15:49:43 -0400

    Switched over to input testing

--- src/conditionals/simple_programs_with_decision_points/hard/q1/MainTest.java
+++ src/conditionals/simple_programs_with_decision_points/hard/q1/MainTest.java
@@ -1,36 +1,47 @@
 package conditionals.simple_programs_with_decision_points.hard.q1;
 
 import global.BaseTest;
+import global.tools.TestOption;
 import global.variables.Clause;
 import global.variables.clauses.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest extends BaseTest {
     // Parsons with distractors
-    int input = 5;
-    String negativeInput = "-46";
 
     public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "5";
         return new Clause[]{
                 new StringLiteral("Enter a number: "),
                 new NewLine(),
-                new IntegerLiteral(input, "returnedValue")
+                new IntegerLiteral("absoluteValue")
         };
     }
 
     public void runMain() {
-        provideInput(String.valueOf(input));
         AbsolutelySure.main(new String[]{});
     }
 
-    @Test
-    public void negativeValueTest() {
-        provideInput(negativeInput);
-        AbsolutelySure.main(new String[]{});
-        String output = getOutput();
-        int contains = output.indexOf(negativeInput);
-        assertEquals(-1, contains, "Your program does not change negative numbers to positive numbers");
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(Arguments.of(5,5), Arguments.of(-46, 46), Arguments.of(0, 0));
     }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void testWithInput(int input, int output) {
+        String error = "The absolute value was calculated incorrectly";
+        if(input<0 )
+            error += ", negative values were no handled properly";
+        runWithInput(input + "");
+        assertEquals(Integer.parseInt(getItemByName("absoluteValue")), output, error);
+    }
+
 }
