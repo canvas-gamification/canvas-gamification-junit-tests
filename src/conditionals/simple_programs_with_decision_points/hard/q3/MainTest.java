503912bc4a27b136843f83b62cacb351bbf58a0b
Samantha H <hodge1214@gmail.com>
2022-03-18 15:36:23 -0400

    Changed parsons notation

--- src/conditionals/simple_programs_with_decision_points/hard/q3/MainTest.java
+++ src/conditionals/simple_programs_with_decision_points/hard/q3/MainTest.java
@@ -5,7 +5,7 @@ import global.variables.Clause;
 import global.variables.clauses.StringLiteral;
 
 public class MainTest extends BaseTest {
-    //Parsons
+    // Parsons
 
     //test if greater than 21 but not greater than 30 mins
     //test if not greater than 21 but greater than 30 mins
