7d838db4336e29475a0b4ca5e3fb83e7bd789470
Samantha H <hodge1214@gmail.com>
2022-03-19 21:08:41 -0400

    changed delta

--- src/pre_defined_classes/mathematical_program/hard/q8/MainTest.java
+++ src/pre_defined_classes/mathematical_program/hard/q8/MainTest.java
@@ -23,7 +23,7 @@ public class MainTest extends BaseTest {
 
     @Test
     public void testMath(){
-        assertEquals(2.44140625, Double.parseDouble(getItemByName("7thTerm")), 0.1, "Your math calculation is incorrect. Try referring back to the question for what the correct calculation looks like.");
+        assertEquals(2.44140625, Double.parseDouble(getItemByName("7thTerm")), 0.00001, "Your math calculation is incorrect. Try referring back to the question for what the correct calculation looks like.");
     }
 
 }
