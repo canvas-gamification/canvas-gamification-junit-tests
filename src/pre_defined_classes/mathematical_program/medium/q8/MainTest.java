dab34c7fa7ef5b1dde6669a68c15ce44aa54d801
Hedgemon4 <sethlinc4@gmail.com>
2022-03-23 21:30:34 -0600

    Updated error messages and delta values

--- src/pre_defined_classes/mathematical_program/medium/q8/MainTest.java
+++ src/pre_defined_classes/mathematical_program/medium/q8/MainTest.java
@@ -5,6 +5,7 @@ import global.variables.Clause;
 import global.variables.clauses.DoubleLiteral;
 import global.variables.clauses.StringLiteral;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
@@ -12,9 +13,9 @@ public class MainTest extends BaseTest {
     public Clause[] testSentence() {
         return new Clause[]{
                 new StringLiteral("The magnitude of an earthquake with intensity "),
-                new DoubleLiteral(23200.0, 23200.0, "intensity"),
+                new DoubleLiteral("intensity"),
                 new StringLiteral(" is "),
-                new DoubleLiteral( "magnitude"),
+                new DoubleLiteral("magnitude"),
                 new StringLiteral(" on the Richter scale.")
         };
     }
@@ -24,7 +25,7 @@ public class MainTest extends BaseTest {
     }
 
     @Test
-    public void magnitudeTest(){
-        assertEquals(10.05, Double.parseDouble(getItemByName("magnitude")), 0.05,"The calculated magnitude value is incorrect");
+    public void magnitudeTest() {
+        assertEquals(10.05, Double.parseDouble(getItemByName("magnitude")), 0.001, "The calculated magnitude value is incorrect.");
     }
 }
