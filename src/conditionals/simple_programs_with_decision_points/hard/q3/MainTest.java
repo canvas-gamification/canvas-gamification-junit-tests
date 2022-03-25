04b5025f05272d83a411fd3104441e54c2a7f6c9
Samantha H <hodge1214@gmail.com>
2022-03-25 19:36:09 -0400

    Changed over to injectors

--- src/conditionals/simple_programs_with_decision_points/hard/q3/MainTest.java
+++ src/conditionals/simple_programs_with_decision_points/hard/q3/MainTest.java
@@ -1,19 +1,15 @@
 package conditionals.simple_programs_with_decision_points.hard.q3;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
 import global.tools.TestOption;
 import global.variables.Clause;
 import global.variables.clauses.*;
-import global.variables.wrappers.Optional;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest extends BaseTest {
     // Parsons with distractors
@@ -27,8 +23,7 @@ public class MainTest extends BaseTest {
                 new NewLine(),
                 new StringLiteral("Current Time Minutes:  "),
                 new NewLine(),
-                new Optional(new StringLiteral("Please go to sleep.")),
-                new Optional(new StringLiteral("The night is still young!"))
+                new PlaceHolder()
         };
     }
 
@@ -36,34 +31,16 @@ public class MainTest extends BaseTest {
         SleepyTime.main(new String[0]);
     }
 
-    static Stream<Arguments> inputProviderLate() {
-        return Stream.of(Arguments.of(22, 31), Arguments.of(22, 24));
-    }
-
-    static Stream<Arguments> inputProviderEarly() {
-        return Stream.of(Arguments.of(20, 56), Arguments.of(18, 1));
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(Arguments.of(22, 31, "Please go to sleep."), Arguments.of(22, 24, "Please go to sleep."), Arguments.of(20, 56, "The night is still young!"), Arguments.of(18, 1, "The night is still young!"));
     }
 
     @ParameterizedTest
-    @MethodSource("inputProviderLate")
-    void testWithInputLate(int hours, int minutes) {
-        runWithInput((hours + "") + " " + (minutes + ""));
-        String output = getOutput();
-        assertTrue(output.contains("Please go to sleep."), "Your program tells the user to stay up when they should be going to sleep.");
-    }
+    @MethodSource("inputProvider")
+    void testWithInput(int hours, int minutes, String message) throws InvalidClauseException {
+        runWithInput((hours + " ") + (minutes + ""), new Clause[]{
+                new StringLiteral(message, "message")
+        });
 
-    @ParameterizedTest
-    @MethodSource("inputProviderEarly")
-    void testWithInputEarly(int hours, int minutes) {
-        runWithInput((hours + "") + " " + (minutes + ""));
-        String output = getOutput();
-        assertTrue(output.contains("The night is still young!"), "Your program tells the user to go to sleep when they should be staying up.");
-    }
-
-    @Test
-    void doubleMessageTest(){
-        runWithInput("15 21");
-        String output = getOutput();
-        assertTrue(output.contains("The night is still young!") ^ output.contains("Please go to sleep."), "Your program prints both possible responses to the input.");
     }
 }
