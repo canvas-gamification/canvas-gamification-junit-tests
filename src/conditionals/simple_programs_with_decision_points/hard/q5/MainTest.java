2640b4555f09090c0a7e9993755a88f244ad973d
Samantha H <hodge1214@gmail.com>
2022-03-25 00:34:39 -0400

    Changed template to fit the question

--- src/conditionals/simple_programs_with_decision_points/hard/q5/MainTest.java
+++ src/conditionals/simple_programs_with_decision_points/hard/q5/MainTest.java
@@ -5,6 +5,7 @@ import global.tools.TestOption;
 import global.variables.Clause;
 import global.variables.clauses.*;
 import global.variables.wrappers.Optional;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -33,15 +34,21 @@ public class MainTest extends BaseTest {
     }
 
     static Stream<Arguments> inputProvider() {
-        return Stream.of(Arguments.of("a", 31), Arguments.of(22, 24));
-    }
-//test that both outputs aren't occuring
-    @ParameterizedTest
-    @MethodSource("inputProvider")
-    void testWithInputLate(int hours, int minutes) {
-        runWithInput((hours + "") + " " + (minutes + ""));
-        String output = getOutput();
-        assertTrue(output.contains("Please go to sleep."), "Your program tells the user to stay up when they should be going to sleep.");
+        return Stream.of(Arguments.of("a", "Vowel"), Arguments.of("e", "Vowel"), Arguments.of("i", "Vowel"), Arguments.of("o", "Vowel"), Arguments.of("u", "Vowel"), Arguments.of("k", "Not a vowel"), Arguments.of("b", "Not a vowel"), Arguments.of("x", "Not a vowel"));
     }
 
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void testWithInputLate(String letter, String message) {
+        runWithInput(letter);
+        String output = getOutput();
+        assertTrue(output.contains(message), "Your program does not correctly identify if " + letter + " is a vowel or consonant.");
+    }
+
+    @Test
+    void doubleMessageTest() {
+        runWithInput("a");
+        String output = getOutput();
+        assertTrue(output.contains("Vowel") ^ output.contains("Not a vowel"), "Your program prints both possible responses to the input.");
+    }
 }
