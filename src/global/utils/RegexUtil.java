dd74ba1680423a2d787b967a9097989b1d33ef18
opeyem1a <opeyadeyemi@gmail.com>
2022-03-25 15:36:35 -0700

    + fix definition of getRegexInt

--- src/global/utils/RegexUtil.java
+++ src/global/utils/RegexUtil.java
@@ -30,8 +30,22 @@ public class RegexUtil {
         if (lower == Integer.MIN_VALUE && upper == Integer.MAX_VALUE) {
             return "\\d+";
         }
+
         int minDigits = String.valueOf(Math.abs(lower)).length();
         int maxDigits = String.valueOf(Math.abs(upper)).length();
+
+        if(parity(lower) == -1 && parity(upper) == -1) {  // both negative parity
+            // when both lower and upper are negative, the lower value has more digits, so we swap
+            int tempMinDigits = minDigits;
+            minDigits = maxDigits;
+            maxDigits = tempMinDigits;
+        }
+
+        if (parity(lower) != parity(upper)) {  // if range spans from negative to positive values
+            // 1 value is always possible around 0, and the largest digits possible on either side becomes the max
+            maxDigits = Math.max(minDigits, maxDigits);
+            minDigits = 1;
+        }
 
         return String.format("\\d{%d,%d}", minDigits, maxDigits);
     }
@@ -39,5 +53,9 @@ public class RegexUtil {
     public static int[] getSplitDecimal(double num) {
         String[] lowSplit = String.valueOf(Math.abs(num)).split("\\.");
         return new int[]{Integer.parseInt(lowSplit[0]), Integer.parseInt(lowSplit[1])};
+    }
+
+    public static int parity(int value) {
+        return value >= 0 ? 1 : -1;  // returns -1 if value is negative, 1 if value is positive or zero
     }
 }
