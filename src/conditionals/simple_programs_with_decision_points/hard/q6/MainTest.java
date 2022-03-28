6702984dbd967c522736c6757ea4d8d155cedb51
Samantha H <hodge1214@gmail.com>
2022-03-28 02:58:02 -0400

    changed over to injections

--- src/conditionals/simple_programs_with_decision_points/hard/q6/MainTest.java
+++ src/conditionals/simple_programs_with_decision_points/hard/q6/MainTest.java
@@ -1,18 +1,15 @@
 package conditionals.simple_programs_with_decision_points.hard.q6;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
 import global.tools.TestOption;
 import global.variables.Clause;
 import global.variables.clauses.*;
-import global.variables.wrappers.Optional;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest extends BaseTest {
     // Parsons with distractors
@@ -24,8 +21,7 @@ public class MainTest extends BaseTest {
         return new Clause[]{
                 new StringLiteral("Enter your current temperature \\( in fahrenheit \\): "),
                 new NewLine(),
-                new Optional(new StringLiteral("You have a fever. Stay at home and rest.")),
-                new Optional(new StringLiteral("You don't have a fever. Have fun out there!"))
+                new PlaceHolder()
         };
     }
 
@@ -39,16 +35,9 @@ public class MainTest extends BaseTest {
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void testWithInputLate(String temperature, String message) {
-        runWithInput(temperature);
-        String output = getOutput();
-        assertTrue(output.contains(message), "Your program does not correctly identify when a person does or does not have a fever. Check your if statement.");
-    }
-
-    @Test
-    void doubleMessageTest() {
-        runWithInput("40");
-        String output = getOutput();
-        assertTrue(output.contains("You have a fever. Stay at home and rest.") ^ output.contains("You don't have a fever. Have fun out there!"), "Your program prints both possible responses to the input.");
+    void testWithInputLate(String temperature, String message) throws InvalidClauseException {
+        runWithInput(temperature, new Clause[]{
+                new StringLiteral(message)
+        });
     }
 }
