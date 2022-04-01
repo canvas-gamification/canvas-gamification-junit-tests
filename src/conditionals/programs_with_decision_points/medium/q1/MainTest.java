7c0ab4461655e6a71bbc99f4e5768ff178201179
Samantha H <hodge1214@gmail.com>
2022-04-01 15:41:48 -0400

    Changed to injectors

--- src/conditionals/programs_with_decision_points/medium/q1/MainTest.java
+++ src/conditionals/programs_with_decision_points/medium/q1/MainTest.java
@@ -1,18 +1,15 @@
 package conditionals.programs_with_decision_points.medium.q1;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
 import global.tools.TestOption;
 import global.variables.Clause;
 import global.variables.clauses.*;
-import global.variables.wrappers.Optional;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest extends BaseTest {
     // Parsons
@@ -24,10 +21,7 @@ public class MainTest extends BaseTest {
         return new Clause[]{
                 new StringLiteral("Enter a character: "),
                 new NewLine(),
-                new StringLiteral(TestOption.defaultInput),
-                new Optional(new StringLiteral(" is a lowercase letter")),
-                new Optional(new StringLiteral(" is an uppercase letter")),
-                new Optional(new StringLiteral(" is neither uppercase not lowercase"))
+                new PlaceHolder()
         };
     }
 
@@ -42,27 +36,19 @@ public class MainTest extends BaseTest {
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void testWithInputLate(String input, int version) {
-        runWithInput(input);
-        String message, error;
-        if (version == 1) {
-            message = " is a lowercase letter";
-            error = "Your program did not correctly identify a lowercase letter. Check your if statement.";
-        } else if (version == 2) {
-            message = " is an uppercase letter";
-            error = "Your program did not correctly identify an uppercase letter. Check your if statement.";
-        } else {
-            message = " is neither uppercase not lowercase";
-            error = "Your program did not correctly identify that the input was not uppercase or lowercase. Check your if statement.";
-        }
-        String output = getOutput();
-        assertTrue(output.contains(message), error);
-    }
+    void testWithInputLate(String input, int version) throws InvalidClauseException {
 
-    @Test
-    void doubleMessageTest() {
-        runWithInput("a");
-        String output = getOutput();
-        assertTrue(output.contains(" is a lowercase letter") ^ output.contains(" is an uppercase letter") ^ output.contains(" is neither uppercase not lowercase"), "Your program prints both possible responses to the input.");
+        String message;
+        if (version == 1) {
+            message = input + " is a lowercase letter";
+        } else if (version == 2) {
+            message = input + " is an uppercase letter";
+        } else {
+            message = input + " is neither uppercase not lowercase";
+        }
+
+        runWithInput(input, new Clause[]{
+                new StringLiteral(message)
+        });
     }
 }
