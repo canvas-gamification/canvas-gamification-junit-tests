613701d4cf33c4a3e9432df496cff0e217957674
Hedgemon4 <sethlinc4@gmail.com>
2022-04-01 08:56:08 -0600

    Finished writing test

--- src/conditionals/simple_programs_with_decision_points/medium/q5/MainTest.java
+++ src/conditionals/simple_programs_with_decision_points/medium/q5/MainTest.java
@@ -8,22 +8,26 @@ import global.variables.clauses.DoubleLiteral;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
+import global.variables.wrappers.Optional;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
 
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
-        TestOption.defaultInput = "10000";
+        TestOption.defaultInput = "12000.0";
         return new Clause[]{
-                new StringLiteral("What is your annual salary in $?"),
+                new StringLiteral("What is your annual salary in \\$\\?"),
                 new NewLine(),
                 new PlaceHolder(),
-                new StringLiteral("Annual Salary: $"),
+                new PlaceHolder(),
+                new StringLiteral("Annual Salary\\: \\$"),
                 new DoubleLiteral("salary")
         };
     }
@@ -33,24 +37,25 @@ public class MainTest extends BaseTest {
     }
 
     static Stream<Arguments> inputProvider() {
-        return Stream.of(Arguments.of(10000, "Congratulations!, you received a bonus of 15%!", 11500), Arguments.of(
-                12000, "", 12000)
+        return Stream.of(Arguments.of(10000.0, "Congratulations!, you received a bonus of 15%!", 11500.0), Arguments.of(
+                12000.0, "", 12000.0)
         );
     }
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void testWithInput(int input, String message, int outputSalary) throws InvalidClauseException {
-        Clause [] c = new Clause[]{
+    void testWithInput(double input, String message, double outputSalary) throws InvalidClauseException {
+        Clause[] c = new Clause[]{
+                new StringLiteral(""),
                 new StringLiteral("")
         };
-        if(message.equals("Congratulations!, you received a bonus of 15%!")){
+        if (message.equals("Congratulations!, you received a bonus of 15%!")) {
             c = new Clause[]{
                     new StringLiteral(message),
                     new NewLine()
             };
         }
         runWithInput(input + "", c);
-        assertEquals(Double.parseDouble(getItemByName("salary")), outputSalary, "The calculated salary values was incorrect.");
+        assertEquals(Double.parseDouble(getItemByName("salary")), outputSalary, 0.01, "The calculated salary values was incorrect.");
     }
 }
