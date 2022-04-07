22bfc473d300ac3cfa2fa827f3e1ade3870f1a4c
Seth <sethlinc4@gmail.com>
2022-04-07 11:03:09 -0700

    Changed test error messages and formatted file text

--- src/conditionals/simple_programs_with_decision_points/medium/q4/MainTest.java
+++ src/conditionals/simple_programs_with_decision_points/medium/q4/MainTest.java
@@ -10,11 +10,14 @@ import global.variables.wrappers.Optional;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
     // Parsons
+
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
         TestOption.defaultInput = "20 40";
@@ -34,15 +37,17 @@ public class MainTest extends BaseTest {
         TheBiggerInt.main(new String[0]);
     }
 
-    static Stream<Arguments> inputProvider(){
-        return Stream.of(Arguments.of(-20, -40, 20, -40), Arguments.of(49, 10, 39, 10), Arguments.of(20, 50, 20, 50), Arguments.of(500, 0, 500, 0), Arguments.of(-40, -20, -40, -20));
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(Arguments.of(-20, -40, 20, -40), Arguments.of(49, 10, 39, 10), Arguments.of(20, 50, 20, 50),
+                Arguments.of(500, 0, 500, 0), Arguments.of(-40, -20, -40, -20));
     }
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void inputTest(int inputA, int inputB, int outputA, int outputB){
+    void correctNumberOutput(int inputA, int inputB, int outputA, int outputB) {
         runWithInput(inputA + System.lineSeparator() + inputB);
-        assertEquals(Integer.parseInt(getItemByName("a")), outputA, "Calculated value of A is incorrect. Make sure a = a - b if a > b.");
-        assertEquals(Integer.parseInt(getItemByName("b")), outputB, "Calculated value of B is incorrect.");
+        assertEquals(Integer.parseInt(getItemByName("a")), outputA, "The calculated value of A is incorrect.");
+        assertEquals(Integer.parseInt(getItemByName("b")), outputB, "The calculated value of B is incorrect.");
     }
+
 }
--- src/conditionals/simple_programs_with_decision_points/medium/q4/TheBiggerInt.java
+++ src/conditionals/simple_programs_with_decision_points/medium/q4/TheBiggerInt.java
@@ -1,26 +1,29 @@
 package conditionals.simple_programs_with_decision_points.medium.q4;
+
 import java.util.Scanner;
 
 /*
 Write a program that will take in two numbers <tt>a</tt> and <tt>b</tt>, and subtract <tt>b</tt> from <tt>a</tt> only
-if <tt>a</tt> is greater than <tt>b</tt>.\
+if <tt>a</tt> is greater than <tt>b</tt>.
+
 Sample Output:
 	Enter the first number: 40
 	Enter the second number: 20
 	a = 20, b = 20
+
  */
 
-public class TheBiggerInt
-{
-    public static void main( String [] args )
-    {
-        Scanner input = new Scanner( System.in );
+public class TheBiggerInt {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter the first number: ");
         int a = input.nextInt();
         System.out.println("Enter the second number: ");
         int b = input.nextInt();
-        if( a > b )
+        if (a > b)
             a = a - b;
         System.out.println("a = " + a + ", b = " + b);
     }
+
 }
