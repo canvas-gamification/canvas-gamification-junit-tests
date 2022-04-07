c404f5001915d6f3bdabbba09e60a41862a8282b
Seth <sethlinc4@gmail.com>
2022-04-07 10:54:03 -0700

    Added more tests

--- src/conditionals/simple_programs_with_decision_points/medium/q3/MainTest.java
+++ src/conditionals/simple_programs_with_decision_points/medium/q3/MainTest.java
@@ -1,4 +1,4 @@
-package pre_defined_classes.conditionals.simple_programs_with_decision_points.medium.q3;
+package conditionals.simple_programs_with_decision_points.medium.q3;
 
 import global.BaseTest;
 import global.exceptions.InvalidClauseException;
@@ -10,12 +10,14 @@ import global.variables.clauses.StringLiteral;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import java.io.InvalidClassException;
+
 import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
     // Parsons
+
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
         TestOption.defaultInput = "19";
@@ -30,8 +32,10 @@ public class MainTest extends BaseTest {
         VirtualBouncer.main(new String[0]);
     }
 
-    static Stream<Arguments> inputProvider(){
-        return Stream.of(Arguments.of(19,  "Step right in sir."), Arguments.of(18, "Whoa there kiddo, it's past your bedtime."));
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(Arguments.of(19, "Step right in sir."), Arguments.of(18, "Whoa there kiddo, it's past your bedtime."),
+                Arguments.of(66, "Step right in sir."), Arguments.of(1, "Whoa there kiddo, it's past your bedtime."),
+                Arguments.of(16, "Whoa there kiddo, it's past your bedtime."), Arguments.of(37, "Step right in sir."));
     }
 
     @ParameterizedTest
@@ -40,9 +44,10 @@ public class MainTest extends BaseTest {
         runWithInput(input + "", new Clause[]{
                 new StringLiteral(output, "underAge")
         });
-        String errorMessage = "Output string incorrect for age 18 or below." ;
-        if(input >= 19)
+        String errorMessage = "Output string incorrect for age 18 or below.";
+        if (input >= 19)
             errorMessage = "Output string incorrect for age 19 or above.";
         assertEquals(getItemByName("underAge"), output, errorMessage);
     }
+
 }
--- src/conditionals/simple_programs_with_decision_points/medium/q3/VirtualBouncer.java
+++ src/conditionals/simple_programs_with_decision_points/medium/q3/VirtualBouncer.java
@@ -1,4 +1,4 @@
-package pre_defined_classes.conditionals.simple_programs_with_decision_points.medium.q3;
+package conditionals.simple_programs_with_decision_points.medium.q3;
 import java.util.Scanner;
 
 /*
@@ -6,16 +6,16 @@ Write a program to ask the user for their age and check whether they are of lega
 province, where the drinking age is 19. You don’t need to account for negative numbers.
  */
 
-public class VirtualBouncer
-{
-    public static void main( String [] args )
-    {
-        Scanner input = new Scanner( System.in );
+public class VirtualBouncer {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("What is your age? ");
         int age = input.nextInt();
-        if( age >= 19)
+        if (age >= 19)
             System.out.println("Step right in sir.");
         else
             System.out.println("Whoa there kiddo, it's past your bedtime.");
     }
+
 }
