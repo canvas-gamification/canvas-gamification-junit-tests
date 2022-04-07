d2b1ad6f1c30421e8487f8c15ad2bcdbebb66692
Samantha H <hodge1214@gmail.com>
2022-04-07 15:54:28 -0400

    Gramatical fixes

--- src/pre_defined_classes/mathematical_program/hard/q6/ShoutOut.java
+++ src/pre_defined_classes/mathematical_program/hard/q6/ShoutOut.java
@@ -2,7 +2,7 @@ package pre_defined_classes.mathematical_program.hard.q6;
 
 /*
 Figuring out the intensity of sound in the decibel (dB) scale is an interesting application of logarithms.
-To calculate the deciBel values of a particular sound, we use the formula: dB = 10 log(Ip/I0), Where Ip is the intensity
+To calculate the decibel values of a particular sound, we use the formula: dB = 10 log(Ip/I0), Where Ip is the intensity
 of the sound to be measured in W/m^2 ( Watts per metre squared), I0 is the minimum intensity of sound that can be heard
 by the human ear (which is known to be 10^-12 W/m^2). Make a Java program to find the ratio between the decibel value
 of a person speaking if the intensity of their voice is 10^-5 W/m^2  and a person shouting which has the intensity of
@@ -28,7 +28,7 @@ public class ShoutOut {
         dBSpeak = 10 * (Math.log(IpSpeak / I0));
         dBShout = 10 * (Math.log(IpShout / I0));
         dBRatio = dBSpeak / dBShout;
-        System.out.println("The ratio of the decibel values between a person speak and a person shouting is " + dBRatio);
+        System.out.println("The ratio of the decibel values between a person speaking and a person who is shouting is " + dBRatio);
     }
 
 }
