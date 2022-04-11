0ed8c925ecf40be1bcb4568738e2adcadf4d132a
Le Quan <minhquanle_2002@yahoo.com>
2022-04-11 16:38:13 -0700

    period on the error message

--- src/loops/simple_programs_with_repitition/easy/q4/MainTest.java
+++ src/loops/simple_programs_with_repitition/easy/q4/MainTest.java
@@ -37,7 +37,7 @@ public class MainTest extends BaseTest {
     @MethodSource("inputProvider")
     void testWithInput(int input, int output) throws InvalidClauseException{
         runWithInput(input + "");
-        assertEquals(Integer.parseInt(getItemByName("numberOutput")), output, "Incorrect reversed value");
+        assertEquals(Integer.parseInt(getItemByName("numberOutput")), output, "Incorrect reversed value.");
     }
 
 }
