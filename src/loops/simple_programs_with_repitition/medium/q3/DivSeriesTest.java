5712c9b0115d24078c3806e6fbafe8b64064fe3b
Le Quan <minhquanle_2002@yahoo.com>
2022-04-11 17:09:05 -0700

    merge main, new input test

--- src/loops/simple_programs_with_repitition/medium/q3/DivSeriesTest.java
+++ src/loops/simple_programs_with_repitition/medium/q3/DivSeriesTest.java
@@ -2,33 +2,47 @@ package loops.simple_programs_with_repitition.medium.q3;
 
 import global.variables.*;
 import global.variables.clauses.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
 
 public class DivSeriesTest extends BaseTest {
-
-    int input1 = 20;
+    // Parsons
 
     public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "10";
         return new Clause[] {
                 new StringLiteral("Enter a number for the upper bound of the series: "),
                 new NewLine(),
                 new StringLiteral("The sum of the series is "),
-                new IntegerLiteral(seriesDivSum(input1)),
+                new IntegerLiteral("numberOutput"),
         };
     }
 
     public void runMain() {
-        provideInput("" + input1);
         DivSeries.main(new String[0]);
     }
 
-    public int seriesDivSum(int n) {
-        int sum = 0;
-        int i = 1;
-        while (i <= n) {
-            sum += 20 / i;
-            i++;
-        }
-        return(sum);
+    static Stream<Arguments> inputProvider(){
+        return Stream.of(
+            Arguments.of(0, 0),
+            Arguments.of(15, 61),
+            Arguments.of(456, 66),
+            Arguments.of(45646, 66)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void testWithInput(int input, int output) throws InvalidClauseException {
+        runWithInput("" + input);
+        assertEquals(Integer.parseInt(getItemByName("numberOutput")), output, "Series sum value is incorrect.");
     }
 }
