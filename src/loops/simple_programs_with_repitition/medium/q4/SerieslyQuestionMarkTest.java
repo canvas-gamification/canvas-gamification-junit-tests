1e62f0a17e17c24f784e9907b528f8e5d9046de0
Le Quan <minhquanle_2002@yahoo.com>
2022-04-11 17:16:02 -0700

    merge main + new input test

--- src/loops/simple_programs_with_repitition/medium/q4/SerieslyQuestionMarkTest.java
+++ src/loops/simple_programs_with_repitition/medium/q4/SerieslyQuestionMarkTest.java
@@ -2,30 +2,47 @@ package loops.simple_programs_with_repitition.medium.q4;
 
 import global.variables.*;
 import global.variables.clauses.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
 
 public class SerieslyQuestionMarkTest extends BaseTest {
-
-    int input1 = 20;
+    // Parsons
 
     public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "10";
         return new Clause[] {
                 new StringLiteral("Enter a number for the upper bound of the series: "),
                 new NewLine(),
                 new StringLiteral("The sum of the series is "),
-                new IntegerLiteral(seriesQuestionMark(input1)),
+                new IntegerLiteral("numberOutput"),
         };
     }
 
     public void runMain() {
-        provideInput("" + input1);
         SerieslyQuestionMark.main(new String[0]);
     }
 
-    public int seriesQuestionMark(int n) {
-        int sum = 0;
-        for (int i = 1; i < n; i++)
-            sum += i / n;
-        return(sum);
+    static Stream<Arguments> inputProvider(){
+        return Stream.of(
+            Arguments.of(0, 0),
+            Arguments.of(15, 0),
+            Arguments.of(456, 0),
+            Arguments.of(45646, 0)
+        );
     }
-} 
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void testWithInput(int input, int output) throws InvalidClauseException {
+        runWithInput("" + input);
+        assertEquals(Integer.parseInt(getItemByName("numberOutput")), output, "Division series sum value is incorrect.");
+    }
+}
