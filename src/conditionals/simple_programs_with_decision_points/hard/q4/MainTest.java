1a37db2813cc93e64ef1e77ef5f8423a318e0673
Samantha H <hodge1214@gmail.com>
2022-04-16 14:09:32 -0400

    Turned paramaterized test into two seperate tests

--- src/conditionals/simple_programs_with_decision_points/hard/q4/MainTest.java
+++ src/conditionals/simple_programs_with_decision_points/hard/q4/MainTest.java
@@ -4,14 +4,8 @@ import global.BaseTest;
 import global.exceptions.InvalidClauseException;
 import global.tools.TestOption;
 import global.variables.Clause;
-import global.variables.clauses.NewLine;
-import global.variables.clauses.PlaceHolder;
-import global.variables.clauses.StringLiteral;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
+import global.variables.clauses.*;
+import org.junit.jupiter.api.Test;
 
 public class MainTest extends BaseTest {
     // Parsons with distractors
@@ -30,15 +24,19 @@ public class MainTest extends BaseTest {
         DoorBoy.main(new String[0]);
     }
 
-    static Stream<Arguments> inputProvider() {
-        return Stream.of(Arguments.of("true", "I will close the door"), Arguments.of("false", "Time to open the door"));
+    @Test
+    public void testOpen() throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program should have closed the door, it was already open.";
+        runWithInput(String.valueOf(true), new Clause[]{
+                new StringLiteral("I will close the door")
+        });
     }
 
-    @ParameterizedTest
-    @MethodSource("inputProvider")
-    void testWithInput(String open, String message) throws InvalidClauseException {
-        runWithInput(open, new Clause[]{
-                new StringLiteral(message)
+    @Test
+    public void testClosed() throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program should have opened the door, it was already closed.";
+        runWithInput(String.valueOf(false), new Clause[]{
+                new StringLiteral("Time to open the door")
         });
     }
 }
