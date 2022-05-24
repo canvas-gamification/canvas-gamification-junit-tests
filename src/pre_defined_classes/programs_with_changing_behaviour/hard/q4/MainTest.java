5291c5413eb12f20379a0b62513beb48db23f1d8
Hedgemon4 <sethlinc4@gmail.com>
2022-05-24 09:38:08 -0600

    Wrote test for pre_defined_classes-programs_with_changing_behaviors-hard-q4

---
+++ src/pre_defined_classes/programs_with_changing_behaviour/hard/q4/MainTest.java
@@ -0,0 +1,51 @@
+package pre_defined_classes.programs_with_changing_behaviour.hard.q4;
+
+import global.BaseRandomTest;
+import global.variables.Clause;
+import global.variables.clauses.RandomInteger;
+import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.api.RepeatedTest;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MainTest extends BaseRandomTest {
+    // Parsons with Distractors
+    static int independentNumbers;      // Keeps track of how many times all digits of the passcode are equal to each other
+
+    public Clause[] testSentence() {
+        return new Clause[]{
+                new StringLiteral("Random Passcode: "),
+                new RandomInteger(0, 10, "n1"),
+                new StringLiteral(" "),
+                new RandomInteger(0, 10, "n2"),
+                new StringLiteral(" "),
+                new RandomInteger(0, 10, "n3"),
+                new StringLiteral(" "),
+                new RandomInteger(0, 10, "n4"),
+                new StringLiteral(" "),
+                new RandomInteger(0, 10, "n5"),
+                new StringLiteral(" "),
+                new RandomInteger(0, 10, "n6")
+        };
+    }
+
+    public void runMain() {
+        PassCode.main(new String[0]);
+    }
+
+    @RepeatedTest(50)
+    public void generatedNumbersAreIndependent(){           // Independent Numbers Workaround
+        int n1 = Integer.parseInt(getItemByName("n1"));
+        int n2 = Integer.parseInt(getItemByName("n2"));
+        int n3 = Integer.parseInt(getItemByName("n3"));
+        int n4 = Integer.parseInt(getItemByName("n4"));
+        int n5 = Integer.parseInt(getItemByName("n5"));
+        int n6 = Integer.parseInt(getItemByName("n6"));
+        if(n1 == n2 && n1 == n3 && n1 == n4 && n1 == n5 && n1 == n6) {
+            independentNumbers++;
+        }
+        if(independentNumbers >= 30) {
+            fail("Make sure your randomly generated numbers are independent (not always equal).");
+        }
+    }
+
+}
