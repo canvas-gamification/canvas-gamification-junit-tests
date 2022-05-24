cd889150640a57267fb7acc261b6827bdaec78c5
Hedgemon4 <sethlinc4@gmail.com>
2022-05-24 16:32:57 -0600

    Wrote test for pre_defined_classes-programs_with_changing_behaviors-medium-q1

---
+++ src/pre_defined_classes/programs_with_changing_behaviour/medium/q1/ProphetRobot.java
@@ -0,0 +1,20 @@
+package pre_defined_classes.programs_with_changing_behaviour.medium.q1;
+
+/*
+Simon the prophet claims that he can predict the future. An easy way to test his predictive power is to see whether he
+can guess correctly how many fingers you are holding up behind your back. Make a Java program that first generates a
+random number of fingers held up behind a person's back (i.e., a number between 0 and 10 inclusive), and then generates
+another number that corresponds to Simon's prediction. Then, print out a statement to the console in the following format:
+"The prediction by Simon was: (prediction value here), and the number of fingers you held up was: (number of fingers held up here)"
+ */
+
+import java.util.Random;
+
+public class ProphetRobot {
+    public static void main(String[] args) {
+        Random gen = new Random();
+        int fingersHeldUp = gen.nextInt(11);
+        int prediction = gen.nextInt(11);
+        System.out.println("The prediction by Simon was: " + prediction + ", and the number of fingers you held up was: " + fingersHeldUp);
+    }
+}
