97022c01397b16df954e7d0612e49b4dc675fa52
Hedgemon4 <sethlinc4@gmail.com>
2022-05-24 16:19:59 -0600

    Changed question and finished test, but waiting for randomness patch

--- src/pre_defined_classes/programs_with_changing_behaviour/hard/q3/MainTest.java
+++ src/pre_defined_classes/programs_with_changing_behaviour/hard/q3/MainTest.java
@@ -5,16 +5,19 @@ import global.variables.Clause;
 import global.variables.clauses.RandomChar;
 import global.variables.clauses.RandomInteger;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseRandomTest {
     public Clause[] testSentence() {
         return new Clause[]{
                 new StringLiteral("Random Postal Code: "),
-                new RandomChar('A', '[', "c1"),
+                new RandomChar('A', 'G', "c1"),
                 new RandomInteger(0, 10, "n1"),
-                new RandomChar('A', '[', "c2"),
+                new RandomChar('A', 'G', "c2"),
                 new RandomInteger(0, 10, "n2"),
-                new RandomChar('A', '[', "c3"),
+                new RandomChar('A', 'G', "c3"),
                 new RandomInteger(0, 10, "n3")
         };
     }
@@ -22,4 +25,5 @@ public class MainTest extends BaseRandomTest {
     public void runMain() {
         PostalCode.main(new String[0]);
     }
+
 }
