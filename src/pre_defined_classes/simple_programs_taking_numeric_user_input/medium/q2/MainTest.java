21da91d319f36bf445504876f992c79854dbcc6d
Hedgemon4 <sethlinc4@gmail.com>
2022-05-25 10:20:27 -0600

    completed JUnit for q2

---
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/medium/q2/MainTest.java
@@ -0,0 +1,52 @@
+package pre_defined_classes.simple_programs_taking_numeric_user_input.medium.q2;
+
+import global.BaseTest;
+import global.tools.TestOption;
+import global.variables.Clause;
+import global.variables.clauses.IntegerLiteral;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest extends BaseTest {
+    // Parsons
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "5";
+        return new Clause[]{
+                new StringLiteral("Enter the length of the side of a square: "),
+                new NewLine(),
+                new StringLiteral("The area is: "),
+                new IntegerLiteral("area"),
+                new NewLine(),
+                new StringLiteral("The perimeter is: "),
+                new IntegerLiteral("perimeter")
+        };
+    }
+
+    public void runMain() {
+        SquareSpace.main(new String[0]);
+    }
+
+    public static Stream<Arguments> inputProvider(){
+        return Stream.of(Arguments.of(5, 25, 20), Arguments.of(200, 40000, 800), Arguments.of(9, 81, 36), Arguments.of(15, 225, 60));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void calculatesAreaCorrectly(int length, int area, int perimeter){
+        runWithInput(System.lineSeparator() + length);
+        assertEquals(Integer.parseInt(getItemByName("area")), area, "Your program did not correctly calculate the area of a square.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void calculatesPerimeterCorrectly(int length, int area, int perimeter){
+        runWithInput(System.lineSeparator() + length);
+        assertEquals(Integer.parseInt(getItemByName("perimeter")), perimeter, "Your program did not correctly calculate the area of a square.");
+    }
+}
