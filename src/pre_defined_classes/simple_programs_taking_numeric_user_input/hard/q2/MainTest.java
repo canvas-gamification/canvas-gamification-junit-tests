51d084ff249b6f560ef60fbc27589e41a2df48a1
Hedgemon4 <sethlinc4@gmail.com>
2022-05-25 12:06:49 -0600

    completed JUnit for q2

---
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q2/MainTest.java
@@ -0,0 +1,41 @@
+package pre_defined_classes.simple_programs_taking_numeric_user_input.hard.q2;
+
+import global.BaseTest;
+import global.tools.TestOption;
+import global.variables.Clause;
+import global.variables.clauses.IntegerLiteral;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest extends BaseTest {
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "4";
+        return new Clause[]{
+                new StringLiteral("How many bags are you checking in\\? "),
+                new NewLine(),
+                new StringLiteral("The baggage fee is \\$"),
+                new IntegerLiteral("fee")
+        };
+    }
+
+    public void runMain() {
+        AirlineBaggage.main(new String[0]);
+    }
+
+    public static Stream<Arguments> inputProvider() {
+        return Stream.of(Arguments.of(4, 100), Arguments.of(69, 1725), Arguments.of(0, 0), Arguments.of(2, 50));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void correctlyCalculatesBaggageFee(int numBags, int fee) {
+        runWithInput(numBags + System.lineSeparator());
+        assertEquals(Integer.parseInt(getItemByName("fee")), fee, "Your program does not correctly calculate the baggage fee.");
+    }
+}
