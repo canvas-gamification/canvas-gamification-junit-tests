9f400017bb4f8a967be1e596034bc4b275f880e4
Hedgemon4 <sethlinc4@gmail.com>
2022-05-25 09:46:14 -0600

    formatted code

--- src/pre_defined_classes/simple_programs_taking_numeric_user_input/medium/q1/MainTest.java
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/medium/q1/MainTest.java
@@ -10,6 +10,7 @@ import global.variables.clauses.StringLiteral;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
