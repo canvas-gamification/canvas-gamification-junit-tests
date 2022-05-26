351dfa0b7a1da7811222b4062665488a9f75eaf7
Hedgemon4 <sethlinc4@gmail.com>
2022-05-25 20:11:04 -0600

    started JUnit for question 2

---
+++ src/pre_defined_classes/simple_programs_taking_string_user_input/medium/q2/GuessCityName.java
@@ -0,0 +1,30 @@
+package pre_defined_classes.simple_programs_taking_string_user_input.medium.q2;
+/*
+Make a program that asks the user for a city name, then replace all the vowels (a, e, i, o u) with the letter y and
+display that changed name to see if the user can guess what the city name actually is.
+
+Sample Output:
+	Enter a city name:
+	Kelowna
+	What is the name of the city that is spelt like Kylywny
+
+
+ */
+
+import java.util.Scanner;
+
+public class GuessCityName {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a city name: ");
+        String city = input.nextLine();
+        String city2 = city;
+        city2 = city2.replace('a', 'y');
+        city2 = city2.replace('e', 'y');
+        city2 = city2.replace('i', 'y');
+        city2 = city2.replace('o', 'y');
+        city2 = city2.replace('u', 'y');
+        System.out.println("What is the name of the city that is spelt like " + city2 + "?");
+    }
+}
+
---
+++ src/pre_defined_classes/simple_programs_taking_string_user_input/medium/q2/MainTest.java
@@ -0,0 +1,48 @@
+package pre_defined_classes.simple_programs_taking_string_user_input.medium.q2;
+
+import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
+import global.variables.Clause;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.PlaceHolder;
+import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class MainTest extends BaseTest {
+    // Parsons
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "Kelowna ";
+        return new Clause[]{
+                new StringLiteral("Enter a city name: "),
+                new NewLine(),
+                new StringLiteral("What is the name of the city that is spelt like "),
+                new StringLiteral("Kylywny"),
+                //new PlaceHolder(),
+                new StringLiteral("\\?")
+        };
+    }
+
+    public void runMain() {
+        GuessCityName.main(new String[0]);
+    }
+
+    public static Stream<Arguments> inputProvider() {
+        return Stream.of(Arguments.of("Kelowna", "Kylywny"), Arguments.of("Medicine Hat", "Mydycyny Hyt"), Arguments.of("Munich", "Mynych"), Arguments.of("Viewhill", "Vyywhyll"));
+    }
+
+//    @ParameterizedTest
+//    @MethodSource("inputProvider")
+//    public void replacesVowelsCorrectly(String city, String city2) throws InvalidClauseException {
+//        TestOption.incorrectStructureErrorMessage = "Your program does not correctly replace all the vowels in the city name";
+//        runWithInput(city, new Clause[]{
+//                new StringLiteral(city2)
+//        });
+//    }
+
+}
