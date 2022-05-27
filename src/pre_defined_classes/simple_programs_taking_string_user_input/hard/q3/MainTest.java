7d45b7f60fd4eb03083f414f57ba71f2c066962d
Hedgemon4 <sethlinc4@gmail.com>
2022-05-27 09:35:00 -0600

    finished JUnit for q3

---
+++ src/pre_defined_classes/simple_programs_taking_string_user_input/hard/q3/MainTest.java
@@ -0,0 +1,46 @@
+package pre_defined_classes.simple_programs_taking_string_user_input.hard.q3;
+
+import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
+import global.variables.Clause;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.PlaceHolder;
+import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+
+public class MainTest extends BaseTest {
+    // Parsons with distractors
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "Pearl";
+        return new Clause[]{
+                new StringLiteral("Enter a word:"),
+                new NewLine(),
+                new StringLiteral("The word is changed to: "),
+                new PlaceHolder()
+        };
+    }
+
+    public void runMain() {
+        removeVowel.main(new String[0]);
+    }
+
+    public static Stream<Arguments> inputProvider() {
+        return Stream.of(Arguments.of("pearl", "peerl"), Arguments.of("suspicious", "sespeceees"),
+                Arguments.of("incomprehensibility", "encemprehensebelety"), Arguments.of(System.lineSeparator(), ""),
+                Arguments.of("tiara", "teere"), Arguments.of("aeiou", "eeeee"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void replacesVowelCorrectly(String word, String changedWord) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not correctly replace all the vowels in a word with e.";
+        runWithInput(word, new Clause[]{
+                new StringLiteral(changedWord)
+        });
+    }
+}
