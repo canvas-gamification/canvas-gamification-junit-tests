fffd1a7f35f39402b59a74d151d338c832ad1e83
Seth <sethlinc4@gmail.com>
2022-05-30 16:30:20 -0700

    Updated question text

--- src/conditionals/programs_with_decision_points/medium/q4/ForeGolf.java
+++ src/conditionals/programs_with_decision_points/medium/q4/ForeGolf.java
@@ -3,9 +3,9 @@ package conditionals.programs_with_decision_points.medium.q4;
 Write a program that will take in the number of strokes taken by the user to complete the current hole in their golf
 game, as well as the number of strokes needed for "par" (the expected number of strokes to be considered average). Use
 this information to display the scoring term for the current hole based on the following: "Eagle" when the number of
-strokes is 2 strokes under par, "Birdie" when the number of strokes is 1 stroke under par, "Par" when the number of
-strokes is equal to par, "Bogey" when the number of strokes is 1 stroke above par, "Double Bogey" when the number of
-strokes is 2 strokes above par, and “No Scoring Term” for any score differences from par. Assume the user provides valid
+strokes is 2 under par, "Birdie" when the number of strokes is 1 under par, "Par" when the number of
+strokes is equal to par, "Bogey" when the number of strokes is 1 above par, "Double Bogey" when the number of
+strokes is 2 above par, and “No Scoring Term” for any other score differences from par. Assume the user provides valid
 input.
 
 Sample Output;
