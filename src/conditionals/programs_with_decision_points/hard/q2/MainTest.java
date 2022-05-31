797e386e7dd194ad3b10fa51b9d63274b2853342
Hedgemon4 <sethlinc4@gmail.com>
2022-05-31 10:12:52 -0600

    Updated question text and implemented more test cases for better test coverage.

--- src/conditionals/programs_with_decision_points/hard/q2/MainTest.java
+++ src/conditionals/programs_with_decision_points/hard/q2/MainTest.java
@@ -37,31 +37,32 @@ public class MainTest extends BaseTest {
         Calculator.main(new String[0]);
     }
 
-    public static Stream<Arguments> inputProviderValid(){
+    public static Stream<Arguments> inputProviderValid() {
         return Stream.of(Arguments.of(2.5, 9.5, "+", 12.0), Arguments.of(-8.4, 9.7, "-", -18.1), Arguments.of(47.9, 7, "*", 335.3),
                 Arguments.of(22, 7, "/", 3.142857142857143), Arguments.of(34.95, -96, "*utdhtd", -3355.2), Arguments.of(-87, 88, "+hellothere", 1));
     }
 
-    public static Stream<Arguments> inputProviderInvalid(){
+    public static Stream<Arguments> inputProviderInvalid() {
         return Stream.of(Arguments.of(48, 70.259, "h", "Invalid operator!"), Arguments.of(48.0259, 7846, "^", "Invalid operator!"),
-                Arguments.of(465.258, 845.3589, "1*jgfyuuui", "Invalid operator!"));
+                Arguments.of(48.256, 54.478, "%", "Invalid operator!"), Arguments.of(465.258, 845.3589, "1*jgfyuuui", "Invalid operator!"),
+                Arguments.of(1, 2, "a+b*c-d/e", "Invalid operator!"));
     }
 
     @ParameterizedTest
     @MethodSource("inputProviderValid")
     public void worksWithValidOperators(double num1, double num2, String operator, double answer) throws InvalidClauseException {
-            TestOption.incorrectStructureErrorMessage = "Your program does not correctly print out the calculator operation.";
-            runWithInput(num1 + " " + num2 + " " + operator, new Clause[]{
-                    new StringLiteral(num1 + " \\" + operator.charAt(0) + " " + num2 + " = "),
-                    new DoubleLiteral("answer")
-            });
-            assertEquals(Double.parseDouble(getItemByName("answer")), answer, 0.0001, "Your program does not correctly perform one of the calculator operations.");
+        TestOption.incorrectStructureErrorMessage = "Your program does not correctly print out the calculator operation.";
+        runWithInput(String.join(" ", String.valueOf(num1), String.valueOf(num2), operator), new Clause[]{
+                new StringLiteral(num1 + " \\" + operator.charAt(0) + " " + num2 + " = "),
+                new DoubleLiteral("answer")
+        });
+        assertEquals(Double.parseDouble(getItemByName("answer")), answer, 0.0001, "Your program does not correctly perform one of the calculator operations.");
     }
 
     @ParameterizedTest
     @MethodSource("inputProviderInvalid")
-    public void worksWithInvalidOperators(double num1, double num2, String operator, String message) throws InvalidClauseException{
-        TestOption.incorrectStructureErrorMessage = "Your program does not correctly print out an error message for an invalid operations.";
+    public void worksWithInvalidOperators(double num1, double num2, String operator, String message) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not print out an error message for an invalid input.";
         runWithInput(num1 + " " + num2 + " " + operator, new Clause[]{
                 new StringLiteral("Invalid operator!"),
                 new StringLiteral("")
