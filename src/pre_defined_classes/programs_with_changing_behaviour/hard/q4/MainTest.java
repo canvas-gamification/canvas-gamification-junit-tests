200c601cfc9b29c91a516ddf15e0e73e0779c61a
Hedgemon4 <sethlinc4@gmail.com>
2022-05-31 14:41:57 -0600

    Changed how independent randomness is tested

--- src/pre_defined_classes/programs_with_changing_behaviour/hard/q4/MainTest.java
+++ src/pre_defined_classes/programs_with_changing_behaviour/hard/q4/MainTest.java
@@ -5,11 +5,13 @@ import global.variables.Clause;
 import global.variables.clauses.RandomInteger;
 import global.variables.clauses.StringLiteral;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MainTest extends BaseRandomTest {
     // Parsons with Distractors
-    static int independentNumbers;      // Keeps track of how many times all digits of the passcode are equal to each other
+    private static int independentNumbers;      // Keeps track of how many times all digits of the passcode are equal to each other
 
     public Clause[] testSentence() {
         return new Clause[]{
@@ -32,20 +34,37 @@ public class MainTest extends BaseRandomTest {
         PassCode.main(new String[0]);
     }
 
-    @RepeatedTest(50)
-    public void generatedNumbersAreIndependent(){           // Independent Numbers Workaround
-        int n1 = Integer.parseInt(getItemByName("n1"));
-        int n2 = Integer.parseInt(getItemByName("n2"));
-        int n3 = Integer.parseInt(getItemByName("n3"));
-        int n4 = Integer.parseInt(getItemByName("n4"));
-        int n5 = Integer.parseInt(getItemByName("n5"));
-        int n6 = Integer.parseInt(getItemByName("n6"));
-        if(n1 == n2 && n1 == n3 && n1 == n4 && n1 == n5 && n1 == n6) {
+//    @RepeatedTest(50)
+//    public void generatedNumbersAreIndependentOLD() {           // Independent Numbers Workaround
+//        int n1 = Integer.parseInt(getItemByName("n1"));
+//        int n2 = Integer.parseInt(getItemByName("n2"));
+//        int n3 = Integer.parseInt(getItemByName("n3"));
+//        int n4 = Integer.parseInt(getItemByName("n4"));
+//        int n5 = Integer.parseInt(getItemByName("n5"));
+//        int n6 = Integer.parseInt(getItemByName("n6"));
+//        if (n1 == n2 && n1 == n3 && n1 == n4 && n1 == n5 && n1 == n6) {
+//            independentNumbers++;
+//        }
+//        if (independentNumbers >= 30) {
+//            fail("Make sure your randomly generated numbers are independent (not always equal).");
+//        }
+//    }
+
+    @Test
+    public void generatedNumbersAreIndependent() {
+        int n1, n2, n3, n4, n5, n6;
+        do {
+            runMain();
+            n1 = Integer.parseInt(getItemByName("n1"));
+            n2 = Integer.parseInt(getItemByName("n2"));
+            n3 = Integer.parseInt(getItemByName("n3"));
+            n4 = Integer.parseInt(getItemByName("n4"));
+            n5 = Integer.parseInt(getItemByName("n5"));
+            n6 = Integer.parseInt(getItemByName("n6"));
             independentNumbers++;
-        }
-        if(independentNumbers >= 30) {
-            fail("Make sure your randomly generated numbers are independent (not always equal).");
-        }
+            if (independentNumbers >= 15)
+                fail("Make sure your randomly generated numbers are independent (not always equal).");
+        } while (n1 == n2 && n1 == n3 && n1 == n4 && n1 == n5 && n1 == n6);
     }
 
 }
