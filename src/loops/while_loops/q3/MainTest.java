1e6874a80958cd2b09c537dc39ebb8b0df726f98
Hedgemon4 <sethlinc4@gmail.com>
2022-06-07 09:48:09 -0600

    wrote JUnit for q3

---
+++ src/loops/while_loops/q3/MainTest.java
@@ -0,0 +1,45 @@
+package loops.while_loops.q3;
+
+import global.BaseTest;
+import global.tools.TestOption;
+import global.variables.Clause;
+import global.variables.clauses.DoubleLiteral;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest extends BaseTest {
+    // Parsons with distractors
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "39";
+        return new Clause[]{
+                new StringLiteral("Enter the number of terms: "),
+                new NewLine(),
+                new StringLiteral("Sum of the series = "),
+                new DoubleLiteral("sum")
+        };
+    }
+
+    public void runMain() {
+        Seriesly.main(new String[0]);
+    }
+
+    static Stream<Arguments> inputProvider(){
+        return Stream.of(Arguments.of(39, 4.2535430389363755), Arguments.of(55783, 11.506449070632035), Arguments.of(0, 0.0),
+                Arguments.of(-483, 0.0), Arguments.of(1, 1.0));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void computesSumOfSeriesCorrectly(int numTerms, double sum){
+        runWithInput(numTerms + System.lineSeparator());
+        assertEquals(Double.parseDouble(getItemByName("sum")), sum, 0.0000000001, "Your program does not correctly compute the sum of the series up to n terms.");
+    }
+}
---
+++ src/loops/while_loops/q3/Seriesly.java
@@ -0,0 +1,38 @@
+package loops.while_loops.q3;
+/*
+Write a program that will ask the user for a number n and find the sum of the following series, using a while loop, up
+to n terms: 1 + ½ + ⅓ + ¼ + ⅕ + … + 1 / n. Assume the user inputs an integer. If the user does not input a positive
+number, the program should print that the sum of the series is 0.
+
+Sample Output:
+    Enter number of terms:
+    39
+    Sum of the series = 4.2535430389363755
+
+Distractors:
+    i--;
+    while( i <= 10 )
+    while( i <= n );
+    while  i <= n
+    while(int i = 1; i <= n; i--);
+    while( i < n )
+    while( i < n );
+
+ */
+
+import java.util.Scanner;
+
+public class Seriesly {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of terms: ");
+        int n = input.nextInt();
+        double sum = 0;
+        int i = 1;
+        while (i <= n) {
+            sum += (double) 1 / i;
+            i++;
+        }
+        System.out.println("Sum of the series = " + sum);
+    }
+}
