7f6ce15e3a8576488667c639dbde215101112f93
Hedgemon4 <sethlinc4@gmail.com>
2022-06-11 16:04:36 -0600

    Redid JUnit for easier variation on the website

--- src/loops/nested_loops/hard/q2/MainTest.java
+++ src/loops/nested_loops/hard/q2/MainTest.java
@@ -11,29 +11,26 @@ import org.junit.jupiter.api.Test;
 public class MainTest extends BaseTest {
     // Parsons
     public Clause[] testSentence() {
-        return new Clause[]{
-                new PlaceHolder()
-        };
+        return repeatPatternClause(4);
     }
 
     public void runMain() {
         RepeatTheRepeat.main(new String[0]);
     }
 
-    @Test
-    void printsPatternCorrectly() throws InvalidClauseException {
-        int n = 4;
-        Clause[][] c = new Clause[1][n * 2];
+    private Clause[] repeatPatternClause(int n){
+        Clause[] c = new Clause[n * 2];
         int x = 0;
         for (int i = 0; i < n; i++) {
             StringBuilder sb = new StringBuilder();
             for (int j = 1; j <= n; j++) {
                 sb.append(j).append(" ");
             }
-            c[0][x] = new StringLiteral(sb.toString());
-            c[0][x + 1] = new NewLine();
+            c[x] = new StringLiteral(sb.toString());
+            c[x + 1] = new NewLine();
             x += 2;
         }
-        runWithInput("", c);
+        return c;
     }
+
 }
