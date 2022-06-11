801d2adb6f1e361c75fa09f8fa87d296ddefd988
Hedgemon4 <sethlinc4@gmail.com>
2022-06-11 16:39:47 -0600

    Updated test cases, question text, and error messages for better feedback

--- src/loops/simple_programs_with_repitition/easy/q1/MainTest.java
+++ src/loops/simple_programs_with_repitition/easy/q1/MainTest.java
@@ -29,24 +29,24 @@ public class MainTest extends BaseTest {
         PunishmentLoop.main(new String[0]);
     }
 
-    static Stream<Arguments> inputProvider(){
+    static Stream<Arguments> inputProvider() {
         return Stream.of(Arguments.of("Hello there.", 10),
                 Arguments.of("So do all who live to see such times, but that is not for them to decide. All we have to decide is what to do with the time that is given to us.", 7),
-                Arguments.of("Among us", 0), Arguments.of("Never gonna give you up.", 1), Arguments.of("This should not work.", -1));
+                Arguments.of("Never gonna give you up.", 0), Arguments.of("Never gonna let you down.", 1), Arguments.of("Never gonna run around and desert you.", -1));
     }
 
     @ParameterizedTest
     @MethodSource("inputProvider")
     void printsSentenceCorrectNumberOfTimes(String sentence, int n) throws InvalidClauseException {
-        TestOption.incorrectStructureErrorMessage = "Your program does not print the input sentence n times.";
+        TestOption.incorrectStructureErrorMessage = "Your program does not correctly print the input sentence n times.";
         runWithInput(sentence + System.lineSeparator() + n, sentenceRepetition(sentence, n));
     }
 
-    Clause[][] sentenceRepetition(String sentence, int n){
-        if(n < 1)
+    Clause[][] sentenceRepetition(String sentence, int n) {
+        if (n < 1)
             return new Clause[][]{{new StringLiteral("")}};
         Clause[][] c = new Clause[1][n * 2];
-        for(int i = 0; i < c[0].length; i+=2){
+        for (int i = 0; i < c[0].length; i += 2) {
             c[0][i] = new StringLiteral(sentence);
             c[0][i + 1] = new NewLine();
         }
