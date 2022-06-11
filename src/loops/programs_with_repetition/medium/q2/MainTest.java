9572a2431056643eae58293cf9b93edcfdbfd5df
Hedgemon4 <sethlinc4@gmail.com>
2022-06-11 09:16:41 -0600

    Finished JUnit for q2

--- src/loops/programs_with_repetition/medium/q2/MainTest.java
+++ src/loops/programs_with_repetition/medium/q2/MainTest.java
@@ -1,21 +1,31 @@
 package loops.programs_with_repetition.medium.q2;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
 import global.tools.TestOption;
 import global.variables.Clause;
+import global.variables.clauses.IntegerLiteral;
+import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
+import global.variables.clauses.StringLiteral;
+import global.variables.wrappers.Optional;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
+    // Java
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
         TestOption.defaultInput = "2";
         return new Clause[]{
+                new StringLiteral("Enter an integer:"),
+                new Optional(new StringLiteral(" ")),
+                new NewLine(),
                 new PlaceHolder()
         };
     }
@@ -25,22 +35,56 @@ public class MainTest extends BaseTest {
     }
 
     static Stream<Integer> validInputProvider() {
-        return Stream.of(267, 2, 3, 10589);
+        return Stream.of(267, 2, 3, 1059);
     }
 
     static Stream<Arguments> invalidInputProvider() {
-        return Stream.of();
+        return Stream.of(Arguments.of(new int[]{0}, 6), Arguments.of(new int[]{1}, 38), Arguments.of(new int[]{0, 1, -1, -4, -5 -11 ,-57, -2, -558}, 471));
     }
 
     @ParameterizedTest
     @MethodSource("validInputProvider")
-    void worksWithValidInputs(int input) {
+    void worksWithValidInputs(int input) throws InvalidClauseException {
         TestOption.incorrectStructureErrorMessage = "Your program does not print all the composite numbers between 2 and the input n.";
         ArrayList<Integer> compositeNumbers = compositeNumberList(input);
+        Iterator<Integer> it = compositeNumbers.iterator();
+        Clause [][] c = new Clause[1][compositeNumbers.size() * 2];
+        for(int i = 0; i < c[0].length; i += 2){
+            c[0][i] = new IntegerLiteral(it.next());
+            c[0][i + 1] = new NewLine();
+        }
+        runWithInput(String.valueOf(input), c);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidInputProvider")
+    void worksWithInvalidInputs(int [] invalidInput, int validInput) throws InvalidClauseException{
+        TestOption.incorrectStructureErrorMessage = "Your program does not keep asking the user for a number if they enter invalid input.";
+        ArrayList<Integer> compositeNumbers = compositeNumberList(validInput);
+        Iterator<Integer> it = compositeNumbers.iterator();
+        // numberOfClauses is the number of invalid inputs, the valid input, and the number of composite numbers, plus new lines
+        int numberOfClauses = 2 * (invalidInput.length + compositeNumbers.size());
+        Clause [][] c = new Clause[1][numberOfClauses];
+        int i;
+        for(i = 0; i < invalidInput.length * 2; i+= 2 ){
+            c[0][i] = new StringLiteral("Enter an integer:");
+            c[0][i + 1] = new NewLine();
+        }
+        for(int j = i; j < c[0].length; j += 2){
+            c[0][j] = new IntegerLiteral(it.next());
+            c[0][j + 1] = new NewLine();
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int j : invalidInput) {
+            sb.append(j).append(System.lineSeparator());
+        }
+        sb.append(validInput);
+        runWithInput(sb.toString(), c);
     }
 
     private static ArrayList<Integer> compositeNumberList(int n) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        // Returns the composite numbers from 2 to n (inclusive) as an ArrayList
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i = 2; i <= n; i++) {
             int c = 0;
             for (int j = 1; j <= i; j++) {
--- src/loops/programs_with_repetition/medium/q2/VerifiedComposite.java
+++ src/loops/programs_with_repetition/medium/q2/VerifiedComposite.java
@@ -2,9 +2,14 @@ package loops.programs_with_repetition.medium.q2;
 /*
 Write a program that will ask the user for a number 'n' greater than 1 and list all composite numbers between 2 and 'n'
 (inclusive). Composite Numbers are those that have more than 2 factors, i.e. the opposite of prime numbers. Make sure
-that the program will keep asking for input until it receives valid input from the user, i.e. a number greater than 0.
+that the program will keep asking for input until it receives valid input from the user, i.e. a number greater than 0,
+and that your output matches the sample.
 Sample Output:
 	Enter an integer:
+    -1
+    Enter an integer:
+    0
+    Enter an integer:
     10
     4
     6
@@ -19,7 +24,7 @@ public class VerifiedComposite {
         Scanner input = new Scanner(System.in);
         int n;
         while (true) {
-            System.out.println("Enter an integer: ");
+            System.out.println("Enter an integer:");
             n = input.nextInt();
             if (n > 1)
                 break;
