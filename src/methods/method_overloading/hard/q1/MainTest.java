21f61b96ed7087a38a7a63924aefdbd3f5f2290a
Hedgemon4 <sethlinc4@gmail.com>
2022-06-14 14:56:56 -0600

    Started writing test but need to wait to make sure the framework will function without a main method

---
+++ src/methods/method_overloading/hard/q1/MainTest.java
@@ -0,0 +1,17 @@
+package methods.method_overloading.hard.q1;
+
+import global.BaseTest;
+import global.variables.Clause;
+import global.variables.clauses.StringLiteral;
+
+public class MainTest extends BaseTest {
+
+    public Clause[] testSentence() {
+        return new Clause[]{
+            new StringLiteral("")
+        };
+    }
+
+    public void runMain() {
+    }
+}
---
+++ src/methods/method_overloading/hard/q1/OneToThree.java
@@ -0,0 +1,21 @@
+package methods.method_overloading.hard.q1;
+/*
+Create a class with a static method called lengthString() that returns an integer. Overload this method twice so that it
+can take either one string, two strings, or three strings. Return the length of the string when there is just one input,
+and the length of all the strings added together otherwise.
+ */
+public class OneToThree {
+    public static int lengthString(String a) {
+        return a.length();
+    }
+
+    public static int lengthString(String a, String b) {
+        String x = a + b;
+        return x.length();
+    }
+
+    public static int lengthString(String a, String b, String c) {
+        String x = a + b + c;
+        return x.length();
+    }
+}
