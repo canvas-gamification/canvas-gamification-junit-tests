e0319051d96f0ba1f3a5d2e11e8c7bdfc5a8dd2e
Hedgemon4 <sethlinc4@gmail.com>
2022-06-14 16:26:00 -0600

    Finished writing JUnit for q1

--- src/methods/method_overloading/hard/q1/OneToThree.java
+++ src/methods/method_overloading/hard/q1/OneToThree.java
@@ -1,20 +1,20 @@
 package methods.method_overloading.hard.q1;
 /*
-Create a class with a static method called lengthString() that returns an integer. Overload this method twice so that it
+Create a class with a static method called "stringLength" that returns an integer. Overload this method twice so that it
 can take either one string, two strings, or three strings. Return the length of the string when there is just one input,
 and the length of all the strings added together otherwise.
  */
 public class OneToThree {
-    public static int lengthString(String a) {
+    public static int stringLength(String a) {
         return a.length();
     }
 
-    public static int lengthString(String a, String b) {
+    public static int stringLength(String a, String b) {
         String x = a + b;
         return x.length();
     }
 
-    public static int lengthString(String a, String b, String c) {
+    public static int stringLength(String a, String b, String c) {
         String x = a + b + c;
         return x.length();
     }
