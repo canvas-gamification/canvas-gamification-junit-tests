a7dc6a6e35275c62b60f5c189ad7d24957bfcdf7
yuqi88 <snow34545@gmail.com>
2022-06-23 11:47:51 -0700

    add class and its test

---
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/medium/q7/AbsoluteNumber.java
@@ -0,0 +1,28 @@
+package pre_defined_classes.simple_programs_taking_numeric_user_input.medium.q7;
+
+/*
+Make a program that asks the user for a score, takes the absolute value of it, multiplies that by a random number,
+ and displays the product as the user's goal.
+
+Sample output:
+Enter your score:
+-5
+Your goal is 45. Keep up the good work!
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class AbsoluteNumber {
+    public static void main( String[] args )
+    {
+        Scanner in = new Scanner( System.in );
+        Random gen = new Random();
+        System.out.println( "Enter your score: " );
+        int number = in.nextInt();
+        number = Math.abs( number );
+        number = number * gen.nextInt( 10 );
+        System.out.print( "Your goal is " +  number + ". Keep up the good work!" );
+    }
+
+}
---
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/medium/q7/MainTest.java
@@ -0,0 +1,57 @@
+package pre_defined_classes.simple_programs_taking_numeric_user_input.medium.q7;
+
+
+/*
+Sample output:
+Enter your score:
+-5
+Your goal is 45. Keep up the good work!
+ */
+
+import global.BaseRandomTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
+import global.variables.Clause;
+import global.variables.clauses.IntegerLiteral;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.PlaceHolder;
+import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class MainTest extends BaseRandomTest {
+    //Parsons
+    public Clause[] testSentence(){
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "5";
+        return new Clause[]{
+                new StringLiteral("Enter your score: "),
+                new NewLine(),
+                new StringLiteral("Your goal is "),
+                new PlaceHolder(),
+                new StringLiteral(". Keep up the good work!")
+        };
+    }
+
+    public void runMain(){ AbsoluteNumber.main(new String[0]); }
+
+    public static Stream<Integer> inputProvider(){
+        return Stream.of( 3 , 0 , -5 );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void num(int input) throws InvalidClauseException{
+        int goal = Math.abs(input);
+        goal *= new Random().nextInt( 10 );
+
+        runWithInput(String.valueOf(input),new Clause[]{
+                new IntegerLiteral(goal)
+        });
+
+
+    }
+}
