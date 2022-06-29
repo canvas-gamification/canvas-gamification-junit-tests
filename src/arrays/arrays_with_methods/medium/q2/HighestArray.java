f4488171d75b00e9914dfb501dcc0fba5e4b5172
Hedgemon4 <sethlinc4@gmail.com>
2022-06-29 15:40:52 -0700

    Formatted code

--- src/arrays/arrays_with_methods/medium/q2/HighestArray.java
+++ src/arrays/arrays_with_methods/medium/q2/HighestArray.java
@@ -1,4 +1,5 @@
 package arrays.arrays_with_methods.medium.q2;
+
 /*
 Create a method called highest that takes as input two arrays of type double and returns the array that has the highest
 number in it. For example, suppose the two input arrays are [ 2.6, 11.9, 5.3] and [ 3.2, 99.9], the method returns [3.2, 99.9].
