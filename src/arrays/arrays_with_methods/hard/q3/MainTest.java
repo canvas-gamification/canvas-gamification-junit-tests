ba466226140ce4eafad6a709a122338b0d9a416a
Hedgemon4 <sethlinc4@gmail.com>
2022-06-29 16:41:46 -0700

    Formatted code and added test cases

--- src/arrays/arrays_with_methods/hard/q3/MainTest.java
+++ src/arrays/arrays_with_methods/hard/q3/MainTest.java
@@ -11,19 +11,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
     // Java
-    static Stream<Arguments> inputProvider(){
-        return Stream.of(Arguments.of(new int[]{2, 4, 6, 8, 10, 12}, 7, 30),
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(Arguments.of(new int[]{2, 4, -6, -50, -8, 10, 12}, -49, 14),
                 Arguments.of(new int[]{}, 11, 0),
                 Arguments.of(new int[]{387, 956, 23, 57, 28, -111, 48576}, 28, 50004),
                 Arguments.of(new int[]{-11, -57, -124, -125, 85, -37, 0, 1, -11}, -124, -154),
-                Arguments.of(new int[]{-11, -1, -1, -457, -56, -3442, 0, 0 ,0, 1}, 0, 1));
+                Arguments.of(new int[]{1, 4, 2, -11, 4, 1, 55, -45, 12, 395}, 396, 0),
+                Arguments.of(new int[]{-11, -1, -1, -457, -56, -3442, 0, 0, 0, 1}, 0, 1));
     }
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void correctSelectiveSumMethod(int [] input, int n, int sum){
+    void correctSelectiveSumMethod(int[] input, int n, int sum) {
         int output = (int) MethodUtil.invokeIfMethodExists(SumTimes.class, "selectiveSum", new Object[]{input, n},
                 int[].class, int.class);
-        assertEquals(sum, output, "Your method does not correctly calculate the sum of the values in an array greater than a given n.");
+        assertEquals(sum, output, "Your method does not correctly calculate the sum of the values in an array greater than or equal to a given number.");
     }
 }
