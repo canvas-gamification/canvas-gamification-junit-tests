18c04ef7cd8173a7499637244cc369f2e4902911
Hedgemon4 <sethlinc4@gmail.com>
2022-06-28 17:43:39 -0700

    Finished JUnit for q1

---
+++ src/arrays/arrays_with_methods/hard/q1/IncrementThis.java
@@ -0,0 +1,14 @@
+package arrays.arrays_with_methods.hard.q1;
+/*
+Write a method called upByOne(int[] arr) that takes an integer array as input and increments every element of that array
+by 1. The method should then return this array. For example, if the method takes in the array [1, 2, 3, 4], it should
+return the array [2, 3, 4, 5].
+ */
+public class IncrementThis {
+    public static int[] upByOne( int[] arr )
+    {
+        for( int i = 0; i < arr.length; i++ )
+            arr[i] = arr[i] + 1;
+        return arr;
+    }
+}
