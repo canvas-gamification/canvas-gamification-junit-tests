764355787dea4aa4d6f8a9c89a94af17d635aa8d
Hedgemon4 <sethlinc4@gmail.com>
2022-06-29 16:22:09 -0700

    Formatted code

--- src/arrays/arrays_with_methods/hard/q1/MainTest.java
+++ src/arrays/arrays_with_methods/hard/q1/MainTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MainTest {
     // Java
-    static Stream<Arguments> inputProvider(){
+    static Stream<Arguments> inputProvider() {
         return Stream.of(Arguments.of(new int[]{1, 4, -1, 13, -67, 4, 0}, new int[]{2, 5, 0, 14, -66, 5, 1}),
                 Arguments.of(new int[]{}, new int[]{}),
                 Arguments.of(new int[]{11, -69, 29758, 0, 0, 1111111, 234, -5682, 0, 1, -1, -3453},
@@ -20,9 +20,9 @@ public class MainTest {
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void correctUpByOneMethod(int [] input, int [] incremented){
-        int [] output = (int[]) MethodUtil.invokeIfMethodExists(IncrementThis.class, "upByOne", new Object[]{input},
+    void correctUpByOneMethod(int[] input, int[] incremented) {
+        int[] output = (int[]) MethodUtil.invokeIfMethodExists(IncrementThis.class, "upByOne", new Object[]{input},
                 int[].class);
-        assertArrayEquals(incremented, output, "Your method does not increment all the values in an input array.");
+        assertArrayEquals(incremented, output, "Your method does not correctly increment all the values in an array.");
     }
 }
