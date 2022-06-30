36270a323cfdc735b6075dc4b427e8efd65eba11
Hedgemon4 <sethlinc4@gmail.com>
2022-06-29 17:20:34 -0700

    Formatted code, added test cases, and updated question

--- src/arrays/arrays_with_methods/hard/q5/MainTest.java
+++ src/arrays/arrays_with_methods/hard/q5/MainTest.java
@@ -11,19 +11,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
     // Java
-    static Stream<Arguments> inputProvider(){
+    static Stream<Arguments> inputProvider() {
         return Stream.of(Arguments.of(new int[]{112, 113, 7, 2, 4, 9, 1}, 1),
                 Arguments.of(new int[]{1, 76, 2, -457, 34, 4543, -235, -345, -329}, 16),
                 Arguments.of(new int[]{0, 0, 0, 0, 0}, 0),
                 Arguments.of(new int[]{-334, 241, -89, -3352, -24341, -12, 245, 511, 155, -7, 349, 12, -21412, 0, 1245}, 162),
-                Arguments.of(new int[]{-9, 34, 764, 87, 42, -11, -75, 0}, 43));
+                Arguments.of(new int[]{-9, 34, 764, 87, 42, -11, -75, 0}, 43),
+                Arguments.of(new int[]{}, 0),
+                Arguments.of(new int[]{49823}, 0),
+                Arguments.of(new int[]{155, 67, 349, 342, 11, -6434}, 7));
     }
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void correctSmallestDiffMethod(int [] input, int difference){
+    void correctSmallestDiffMethod(int[] input, int difference) {
         int output = (int) MethodUtil.invokeIfMethodExists(SmallestDifference.class, "smallestDiff", new Object[]{input},
                 int[].class);
-        assertEquals(difference, output, "Your method does not correctly calculate the smallest difference between items in an array.");
+        assertEquals(difference, output, "Your method does not correctly calculate the smallest difference between the items in an array.");
     }
 }
