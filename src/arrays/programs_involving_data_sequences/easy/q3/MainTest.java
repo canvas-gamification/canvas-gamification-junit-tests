dc748dcace4750f869bf1d3b621be5b38a26b870
Hedgemon4 <sethlinc4@gmail.com>
2022-07-01 08:26:09 -0700

    Finished JUnit for q3

--- src/arrays/programs_involving_data_sequences/easy/q3/MainTest.java
+++ src/arrays/programs_involving_data_sequences/easy/q3/MainTest.java
@@ -1,11 +1,13 @@
 package arrays.programs_involving_data_sequences.easy.q3;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
 import global.utils.MethodUtil;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
+    // Parsons
     public Clause[] testSentence() {
         return new Clause[]{
                 new StringLiteral("The name removed from the queue is: "),
@@ -30,21 +33,30 @@ public class MainTest extends BaseTest {
         StringyMovement.main(new String[0]);
     }
 
-    static Stream<Arguments> inputProvider(){
+    static Stream<Arguments> methodInputProvider() {
         return Stream.of(Arguments.of(new String[]{"Travis", "Laura", "Liam", "Matthew", "Sam", "Marisha"},
-                new String[]{"Laura", "Liam", "Matthew", "Sam", "Marisha", ""}, "Travis", "Laura"),
-                Arguments.of(new String[]{}, new String[]{}, "", ""));
+                        new String[]{"Laura", "Liam", "Matthew", "Sam", "Marisha", ""}, "The name removed from the queue is: Travis"),
+                Arguments.of(new String[]{"Seth", "Steve", "Spencer", "Stephanie", "Shirley"},
+                        new String[]{"Steve", "Spencer", "Stephanie", "Shirley", ""}, "The name removed from the queue is: Seth"),
+                Arguments.of(new String[]{}, new String[]{"The queue is empty."}, ""),
+                Arguments.of(new String[]{"Theophania"}, new String[]{""}, "The name removed from the queue is: Theophania"));
     }
 
     @ParameterizedTest
-    @MethodSource("inputProvider")
-    void correctNextPleaseMethod(String [] names, String [] nextName, String removed, String front) throws Throwable{
-        runWithInput("", new Clause[]{
-                new StringLiteral(removed),
-                new StringLiteral(front)
-        });
-        String [] output = (String[]) MethodUtil.invokeIfMethodExists(StringyMovement.class, "nextPlease", new Object[]{names}, String[].class);
+    @MethodSource("methodInputProvider")
+    void correctNextPleaseMethod(String[] names, String[] nextName, String stringOutput) throws Throwable {
+        String[] output = (String[]) MethodUtil.invokeIfMethodExists(StringyMovement.class, "nextPlease", new Object[]{names}, String[].class);
+        String removedName = MethodUtil.getMethodOutput();
+        assertEquals(stringOutput, removedName, "Your method does not correctly print the name removed from the queue.");
         assertArrayEquals(nextName, output, "Your method does not correctly remove the individual at the front of the queue.");
+    }
+
+    @Test
+    void printsCorrectOutput() throws InvalidClauseException {
+        runWithInput("", new Clause[]{
+                new StringLiteral("Travis"),
+                new StringLiteral("Laura")
+        });
     }
 
 }
--- src/arrays/programs_involving_data_sequences/easy/q3/StringyMovement.java
+++ src/arrays/programs_involving_data_sequences/easy/q3/StringyMovement.java
@@ -2,10 +2,12 @@ package arrays.programs_involving_data_sequences.easy.q3;
 
 /*
 Make a program that calls a method named nextPlease that takes an array of people's first names as input and
-removes the first name and shifts the remaining names to mimic a waiting list. Specifically, the method removes the
+removes the first name and shifts the remaining names to mimic a queue. Specifically, the method removes the
 first name from the array, shifts the remaining names one spot earlier (so the second element moves to the first, the
 third element moves to second, etc.), and makes the last name an empty string. The method should then print which name
 was removed. Finally, the method returns the shifted array as the output, with the new first element being printed.
+If the method input array is empty, the method should return an array of length one which contains the String
+"The queue is empty.".
  */
 public class StringyMovement {
     public static void main(String[] args) {
@@ -15,6 +17,8 @@ public class StringyMovement {
     }
 
     public static String[] nextPlease(String[] names) {
+        if (names.length == 0)
+            return new String[]{"The queue is empty."};
         String nextInLine = names[0];
         for (int i = 0; i < names.length - 1; i++)
             names[i] = names[i + 1];
