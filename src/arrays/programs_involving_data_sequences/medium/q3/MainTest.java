516ee037b561b0e2f0cf30e50f85f8a886a17136
Hedgemon4 <sethlinc4@gmail.com>
2022-07-01 17:34:14 -0700

    Finished JUnit for q3

--- src/arrays/programs_involving_data_sequences/medium/q3/MainTest.java
+++ src/arrays/programs_involving_data_sequences/medium/q3/MainTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
-
+    // Parsons with distractors
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
         TestOption.defaultInput = "1 2 3 4 5 6 7 8 9 10";
@@ -27,15 +27,28 @@ public class MainTest extends BaseTest {
         BuildAnArray.main(new String[0]);
     }
 
-//    static Stream<Arguments> inputProvider(){
-//        return Stream.of(Arguments.of("-1"));
-//    }
-//
-//    @ParameterizedTest
-//    @MethodSource("inputProvider")
-//    void printsArrayCorrectly(String input) throws InvalidClauseException {
-//        runWithInput(input, new Clause[]{
-//                new StringLiteral(input)
-//        });
-//    }
+    static Stream<Arguments> inputProvider(){
+        return Stream.of(Arguments.of("-1", "", 1),
+                Arguments.of("7 8 11 -56 0 3 -1", "7 8 11 -56 0 3 ", 7),
+                Arguments.of("7 8 11 -34 2 1 -2 0 9 486523", "7 8 11 -34 2 1 -2 0 9 486523 ", 10),
+                Arguments.of("1 2 3 4 5 6 7 8 9 10 11", "1 2 3 4 5 6 7 8 9 10 ", 10),
+                Arguments.of("-492 5 0 3542 -23 -23442 1 0 987 -1", "-492 5 0 3542 -23 -23442 1 0 987 ", 10));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void printsArrayCorrectly(String input, String output, int length) throws InvalidClauseException {
+        runWithInput(input, clauseBuilder(length, output));
+    }
+
+    private Clause[][] clauseBuilder(int size, String output){
+        Clause[][] c = new Clause[1][size * 2 + 1];
+        for (int i = 0; i < c[0].length - 1; i+= 2){
+            c[0][i] = new StringLiteral("Enter a value to insert into the array \\( -1 to exit \\): ");
+            c[0][i + 1] = new NewLine();
+        }
+        c[0][size * 2] = new StringLiteral(output);
+        return c;
+    }
+
 }
