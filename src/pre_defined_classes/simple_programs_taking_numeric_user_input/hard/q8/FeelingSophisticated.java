00b2daf8ab9ec595a998a840146e8fd95478ffa3
yuqi88 <snow34545@gmail.com>
2022-07-03 00:32:01 -0700

    update question

--- src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q8/FeelingSophisticated.java
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q8/FeelingSophisticated.java
@@ -3,23 +3,23 @@ package pre_defined_classes.simple_programs_taking_numeric_user_input.hard.q8;
 /*
 Sometimes it's just fun to be able to determine the square root of any given number.
 Make a program that asks the user for a numeric value, then determines its square root and prints it out.
+Assuming all user inputs are valid, which means that inputs can only be real numbers larger or equal to 0.
 
 Sample output:
     Enter a number:
     4
     The square root value of the given number is:2.0
+
 Sample output:
     Enter a number:
     5
     The square root value of the given number is:2.23606797749979
-Sample output:
-    Enter a number:
-    -1
-    The square root value of the given number is:NaN
+
 Sample output:
     Enter a number:
     0
     The square root value of the given number is:0.0
+
  */
 
 import java.util.Scanner;
@@ -31,6 +31,6 @@ public class FeelingSophisticated {
         System.out.println( "Enter a number:" );
         double inputVal = input.nextDouble();
         double sqrt = Math.sqrt( inputVal );
-        System.out.print( "The square root value of the given number is:" + sqrt );
+        System.out.println( "The square root value of the given number is:" + sqrt );
     }
 }
