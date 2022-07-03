907dea5b477b91a3089ba1a831522c03426a870a
yuqi88 <snow34545@gmail.com>
2022-07-02 22:23:58 -0700

    fix wording

--- src/pre_defined_classes/simple_programs_taking_numeric_user_input/medium/q10/PiperSaratoga.java
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/medium/q10/PiperSaratoga.java
@@ -4,7 +4,7 @@ package pre_defined_classes.simple_programs_taking_numeric_user_input.medium.q10
 A Piper Saratoga is a hobby plane with six seats that has a capacity to hold 1200 pounds on a flight.
  Make a program that helps this plane's pilot determine how much luggage can be brought on the return flight from
  a vacation. The program needs to consider the weight of the travellers, the weight of the gas needed for the return
- flight, and the weight of the luggage for the trip. Finally, the program will display the weight allowable for
+ flight, and the total weight of the luggage from all travelers for the trip. Finally, the program will display the weight allowable for
  bringing souvenirs home on the return flight.
 
 Sample output:
@@ -47,7 +47,7 @@ public class PiperSaratoga {
         System.out.println( "How much does the gas weigh for the return flight?" );
         double gas = scan.nextDouble();
         gas = gas / 2;
-        System.out.println( "How much luggage are you bringing and what does it weigh?" );
+        System.out.println( "What is the total weight of luggage from all travelers?" );
         double bags = scan.nextDouble();
         double gifts = MAX_CAPACITY - people - gas - bags;
         System.out.print( "You can bring back " + gifts + " pounds of souvenirs!" );
