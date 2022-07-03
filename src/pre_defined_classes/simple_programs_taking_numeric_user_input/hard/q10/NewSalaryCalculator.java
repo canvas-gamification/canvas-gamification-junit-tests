d9857e28611bc145af63b2ede519d0172d12c1d7
yuqi88 <snow34545@gmail.com>
2022-07-03 12:59:04 -0700

    clarify the question

--- src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q10/NewSalaryCalculator.java
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q10/NewSalaryCalculator.java
@@ -1,8 +1,9 @@
 package pre_defined_classes.simple_programs_taking_numeric_user_input.hard.q10;
 /*
 A company called HappyWorkLife has decided to give everybody a raise with an increase of 5% for each year of
- their service. Make a program that asks the user for the salary and the number of years worked at the company,
- then display the increased salary amount.
+their service. "The raise only happen to those who have served at least a year or more,
+and the raise is calculated annually." Make a program that asks the user for the salary and the number of years worked
+at the company, then display the increased salary amount.
 
 Sample output:
 Enter your salary:
@@ -11,8 +12,7 @@ Enter years of service:
 6
 Your increased salary is 1560.0
 
-
-//what if sb work half an year? >> should change the int years to double?
+//what if sb work half a year? >> should change the int years to double? >> add the " " in the question to clarify.(added to google docs too)
  */
 
 import java.util.Scanner;
@@ -24,10 +24,10 @@ public class NewSalaryCalculator {
         System.out.println( "Enter your salary: " );
         double salary = input.nextDouble();
         System.out.println( "Enter years of service:" );
-        int years = input.nextInt();
+        double years = input.nextInt();
         double bonus = 0.05 * years * salary;
         salary = salary + bonus;
-        System.out.print( "Your increased salary is " + salary );
+        System.out.println( "Your increased salary is " + salary );
     }
 
 }
