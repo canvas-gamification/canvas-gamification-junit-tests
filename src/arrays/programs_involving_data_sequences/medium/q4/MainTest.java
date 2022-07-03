4f62d91cd450da9c858af23b234006f7d5f9ae49
Hedgemon4 <sethlinc4@gmail.com>
2022-07-03 12:49:54 -0700

    Finished JUnit for q4

--- src/arrays/programs_involving_data_sequences/medium/q4/MainTest.java
+++ src/arrays/programs_involving_data_sequences/medium/q4/MainTest.java
@@ -1,24 +1,28 @@
 package arrays.programs_involving_data_sequences.medium.q4;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
 import global.utils.MethodUtil;
 import global.variables.Clause;
 import global.variables.clauses.IntegerLiteral;
+import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MainTest extends BaseTest {
     // Parsons with distractors
+    private static final int[] array = new int[]{98, 3, 5, 7, 9};
     public Clause[] testSentence() {
         return new Clause[]{
-                new StringLiteral("The index the number was added at is: "),
-                new IntegerLiteral("index")
+                new StringLiteral("The array is now: "),
+                new PlaceHolder()
         };
     }
 
@@ -26,15 +30,35 @@ public class MainTest extends BaseTest {
         Replacements.main(new String[0]);
     }
 
-    static Stream<Arguments> inputProvider(){
-        return Stream.of(Arguments.of(new int[]{1, 3, 5, 7, 9}, 98, 0));
+    static Stream<Arguments> methodInputProvider(){
+        return Stream.of(Arguments.of(new int[]{1, 3, 5, 7, 9}, 98, new int[]{98, 3, 5, 7, 9}),
+                Arguments.of(new int[]{}, 11, new int[]{}),
+                Arguments.of(new int[]{9563}, -39, new int[]{-39}),
+                Arguments.of(new int[]{11, -34, 5, 0, -123, 245, 8876, 2, 1, 5, 4, -457, 10}, -11,
+                        new int[]{11, -34, 5, 0, -11, 245, 8876, 2, 1, 5, 4, -457, 10}));
     }
 
     @ParameterizedTest
-    @MethodSource("inputProvider")
-    void correctAddThisOneMethod(int[] array, int number, int index) throws Throwable {
-        int result = (int) MethodUtil.invokeIfMethodExists(Replacements.class, "addThisOne",
+    @MethodSource("methodInputProvider")
+    void correctAddThisOneMethod(int[] array, int number, int[] replaced) throws Throwable {
+        int[] result = (int[]) MethodUtil.invokeIfMethodExists(Replacements.class, "addThisOne",
                 new Object[]{array, number}, int[].class, int.class);
-        assertEquals(index, result, "Your method does not add the number at the correct index.");
+        assertArrayEquals(replaced, result, "Your method does not insert the number at the correct index.");
+    }
+
+    @Test
+    void printsCorrectOutput() throws InvalidClauseException {
+        runWithInput("", clauseBuilder(array));
+    }
+
+    private static Clause[][] clauseBuilder(int[] array){
+        Clause[][] output = new Clause[1][array.length * 2];
+        int i = 0;
+        for(int item : array){
+            output[0][i] = new IntegerLiteral(array[i/2]);
+            output[0][i + 1] = new StringLiteral(" ");
+            i += 2;
+        }
+        return output;
     }
 }
--- src/arrays/programs_involving_data_sequences/medium/q4/Replacements.java
+++ src/arrays/programs_involving_data_sequences/medium/q4/Replacements.java
@@ -3,35 +3,40 @@ package arrays.programs_involving_data_sequences.medium.q4;
 Make a program that calls a method that takes an integer array and a number as input. The idea is to replace one of the
 elements in the array with this number. To find out which element gets replaced, the method takes the sum of all the
 values in the array, and then takes the modulus of the sum by the array size (index = sum % size). The method should
-then return the index, which is printed out in the main method.
+then return the array, which is printed out in the main method. If an empty array is passed to the method, the method
+should return the empty array.
 
 Distractors:
     int size = arr.length - 1;
     double size = arr.length;
     arr[ num ] = index;
-    public static int[] addThisOne( int [] arr, int num )
-    return arr;
+    public static int addThisOne( int [] arr, int num )
+    return index;
     {
     }
     return num;
     int n = addThisOne(arr, 98);
-
+    System.out.print("The item at index " + n + "was replaced.")
  */
 public class Replacements {
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 7, 9};
-        int index = addThisOne(arr, 98);
-        System.out.println("The index the number was added at is: " + index);
+        int[] replacement = addThisOne(arr, 98);
+        System.out.print("The array is now: ");
+        for(int i = 0; i < replacement.length; i++){
+            System.out.print(replacement[i] + " ");
+        }
     }
 
-    public static int addThisOne(int[] arr, int num) {
+    public static int[] addThisOne(int[] arr, int num) {
         int size = arr.length;
+        if(size == 0)
+            return arr;
         int sum = 0;
         for (int i = 0; i < size; i++)
             sum += arr[i];
         int index = sum % size;
         arr[index] = num;
-        return index;
+        return arr;
     }
 }
-
