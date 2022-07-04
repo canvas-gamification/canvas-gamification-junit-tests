394a1baccd5144fadf29d346249f7aa7f7252244
Kiet Phan <tuankietvn@outlook.com>
2022-07-04 15:56:20 -0700

    conditionals.programs_with_decision_points.medium.q7

---
+++ src/conditionals/programs_with_decision_points/medium/q7/JavaTopia.java
@@ -0,0 +1,43 @@
+package conditionals.programs_with_decision_points.medium.q7;
+
+/*
+ * In the imaginary land of "Javatopia", the first two numbers of the 5-digit ZIP code determine
+ * which district a certain address falls under. Write a java program to accept a 5-digit Javatopia ZIP code,
+ * extract the first 2 digits and display which district it belongs to based on the following information:
+ * ZIP codes that start with "12" belong to "String Avenue", ones that start with "14" belong to "Character County",
+ * those that start with "22" belong to "Memory Front", and those that start with "24" belong to "RAM Ranch."
+ * Anything else is outside the territory of Javatopia. Make sure to verify that the ZIP code is exactly 5-digits long.
+ * 
+ * Example output:
+ *   Enter a Javatopia ZIP code:
+ *   12345
+ *   That ZIP code belongs to String Avenue
+ * 
+ *   Enter a Javatopia ZIP code:
+ *   00999
+ *   This ZIP code does not belong to Javatopia
+ */
+
+import java.util.Scanner;
+
+public class JavaTopia {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a Javatopia ZIP code:");
+    int zip = input.nextInt();
+    if (zip > 9999 && zip <= 99999) {
+      int z = zip / 1000; // Extract the first two digits
+      if (z == 12)
+        System.out.println("This ZIP code belongs to String Avenue");
+      else if (z == 14)
+        System.out.println("This ZIP code belongs to Character County");
+      else if (z == 22)
+        System.out.println("This ZIP code belongs to Memory Front");
+      else if (z == 24)
+        System.out.println("This ZIP code belongs to RAM Ranch");
+      else
+        System.out.println("This ZIP code does not belong to Javatopia");
+    } else
+      System.out.println("Invalid Input!");
+  }
+}
---
+++ src/conditionals/programs_with_decision_points/medium/q7/MainTest.java
@@ -0,0 +1,90 @@
+package conditionals.programs_with_decision_points.medium.q7;
+
+import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
+import global.variables.Clause;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.PlaceHolder;
+import global.variables.clauses.StringLiteral;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class MainTest extends BaseTest {
+  // Parsons
+  public Clause[] testSentence() {
+    TestOption.isInputTest = true;
+    TestOption.defaultInput = "12345";
+    return new Clause[] {
+        new StringLiteral("Enter a Javatopia ZIP code:"),
+        new NewLine(),
+        new PlaceHolder(),
+        new PlaceHolder(),
+    };
+  }
+
+  @Override
+  public void runMain() {
+    JavaTopia.main(new String[0]);
+  }
+
+  public static Stream<Arguments> inputProviderValidInput() {
+    return Stream.of(
+        Arguments.of(12345, "This ZIP code belongs to String Avenue"),
+        Arguments.of(14325, "This ZIP code belongs to Character County"),
+        Arguments.of(22111, "This ZIP code belongs to Memory Front"),
+        Arguments.of(24912, "This ZIP code belongs to RAM Ranch"));
+  }
+
+  public static Stream<Arguments> inputProviderOutsideTerritoryInput() {
+    return Stream.of(
+        Arguments.of(23123),
+        Arguments.of(29123),
+        Arguments.of(92123),
+        Arguments.of(18123));
+  }
+
+  public static Stream<Arguments> inputProviderInvalidInput() {
+    return Stream.of(
+        Arguments.of(01235),
+        Arguments.of(1),
+        Arguments.of(12),
+        Arguments.of(123),
+        Arguments.of(1234),
+        Arguments.of(123456));
+  }
+
+  @ParameterizedTest
+  @MethodSource("inputProviderValidInput")
+  public void printsCorrectArea(int zipCode, String area) throws InvalidClauseException {
+    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct area in Javatopia for the given ZIP code.";
+    runWithInput(zipCode + "", new Clause[] {
+        new StringLiteral(area),
+        new NewLine(),
+    });
+  }
+
+  @ParameterizedTest
+  @MethodSource("inputProviderOutsideTerritoryInput")
+  public void printsErrorMessageForOutsideTerritoryAreas(int zipCode) throws InvalidClauseException {
+    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct error message for the ZIP codes outside of Javatopia.";
+    runWithInput(zipCode + "", new Clause[] {
+      new StringLiteral("This ZIP code does not belong to Javatopia"),
+      new NewLine(),
+    });
+  }
+
+  @ParameterizedTest
+  @MethodSource("inputProviderInvalidInput")
+  public void printsErrorMessageForInvalidInputs(int zipCode) throws InvalidClauseException {
+    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct error message for the invalid ZIP codes.";
+    runWithInput(zipCode + "", new Clause[] {
+        new StringLiteral("Invalid Input!"),
+        new NewLine(),
+    });
+  }
+}
