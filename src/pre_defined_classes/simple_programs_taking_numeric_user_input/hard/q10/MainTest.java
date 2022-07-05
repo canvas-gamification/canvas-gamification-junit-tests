8bc5671d39fc37fdd42fc600ac428a9e2857fd8e
yuqi88 <snow34545@gmail.com>
2022-07-05 15:27:52 -0700

    add cases and error message & fix bugs

--- src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q10/MainTest.java
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q10/MainTest.java
@@ -36,16 +36,18 @@ public class MainTest extends BaseTest {
 
     static Stream<Arguments> inputProvider() {
 
-        return Stream.of(Arguments.of(1200, 6, 1560.0), Arguments.of(70000, 1, 73500.0), Arguments.of(73500, 2, 80850.0)
-                , Arguments.of(0, 2, 0.0), Arguments.of(120000, 5, 150000.0)
+        return Stream.of(Arguments.of(1200, 6, 1560.0), Arguments.of(70000, 1, 73500.0),
+                Arguments.of(0, 2, 0.0), Arguments.of(120000, 0, 120000.0),
+                Arguments.of(73500.125, 2, 80850.1375), Arguments.of(1000, 2.5, 1125.0)
         );
     }
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void calculateNewSalaryCorrectly(double salary, int years, double newSalary) throws InvalidClauseException {
+    void calculateNewSalaryCorrectly(double salary, double years, double newSalary) throws InvalidClauseException {
         runWithInput(salary + " " + years);
-        assertEquals(Double.parseDouble(getItemByName("newSalary")), newSalary);
+        assertEquals(Double.parseDouble(getItemByName("newSalary")), newSalary,
+                "Your program does not correctly calculate the new salary after raise.");
     }
 }
 
