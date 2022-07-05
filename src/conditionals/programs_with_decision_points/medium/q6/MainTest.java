0bfa2bf5181c9880f4b999e20f8eeb025a9efcf4
Kiet Phan <tuankietvn@outlook.com>
2022-07-05 11:35:03 -0700

    Fix grammatical error

--- src/conditionals/programs_with_decision_points/medium/q6/MainTest.java
+++ src/conditionals/programs_with_decision_points/medium/q6/MainTest.java
@@ -83,7 +83,7 @@ public class MainTest extends BaseTest {
   @ParameterizedTest
   @MethodSource("inputProviderInvalidInput")
   void printsErrorMessageForInvalidInput(String direction) throws InvalidClauseException {
-    TestOption.incorrectStructureErrorMessage = "Your program does not print an error message when user inputs an invalid character.";
+    TestOption.incorrectStructureErrorMessage = "Your program does not print an error message when the user inputs an invalid character.";
     runWithInput(direction, new Clause[] {
         new StringLiteral("Invalid Input!")
     });
