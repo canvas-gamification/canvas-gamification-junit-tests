a0f62cdb25dce71f46cf09f6170455875d7abfc4
yuqi88 <snow34545@gmail.com>
2022-07-08 14:26:13 -0700

    fix question

--- src/pre_defined_classes/simple_programs_taking_string_user_input/hard/q7/Syllable.java
+++ src/pre_defined_classes/simple_programs_taking_string_user_input/hard/q7/Syllable.java
@@ -3,7 +3,8 @@ package pre_defined_classes.simple_programs_taking_string_user_input.hard.q7;
 /*
 Syllables in Japanese follow a "CV structure", which means that every syllable is made up of a consonant followed by a
 vowel. Make a program that asks a user to enter a syllable that has two letters and print true or false depending on
-whether it has the CV structure. For simplicity, we consider a vowel to be one of a, e, i, o, u only.
+whether it has the CV structure. For simplicity, we consider a vowel to be one of a, e, i, o, u only. Assume all users
+always enter 2 characters as an input.
 
 Sample output:
 Enter a syllable:
