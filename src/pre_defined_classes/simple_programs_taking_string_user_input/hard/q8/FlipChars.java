2e7985409f8638567d35eb966bfa4ee85e65381d
yuqi88 <snow34545@gmail.com>
2022-07-09 12:28:52 -0700

    add class & its test

--- src/pre_defined_classes/simple_programs_taking_string_user_input/hard/q8/FlipChars.java
+++ src/pre_defined_classes/simple_programs_taking_string_user_input/hard/q8/FlipChars.java
@@ -1,5 +1,26 @@
 package pre_defined_classes.simple_programs_taking_string_user_input.hard.q8;
 
+/*
+Create a program that takes a country name and returns it with its first and last characters swapped.
+Assume all countries have at least 2 letter in their names.
+
+Sample output:
+Please enter favourite country:
+Canada
+aanadC
+
+Distractors:
+char first = word.charAt( 1 );
+char first, last, middle;
+char first, last;
+char middle = word.substring( 1, word.length()-1 );
+char last = word.charAt( word.length() );
+char first, last;
+String middle;
+String middle = word.substring( 1, word.length()-2 );
+
+ */
+
 import java.util.Scanner;
 
 public class FlipChars {
--- src/pre_defined_classes/simple_programs_taking_string_user_input/hard/q8/MainTest.java
+++ src/pre_defined_classes/simple_programs_taking_string_user_input/hard/q8/MainTest.java
@@ -1,4 +1,46 @@
 package pre_defined_classes.simple_programs_taking_string_user_input.hard.q8;
 
-public class MainTest {
+import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
+import global.variables.Clause;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.PlaceHolder;
+import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class MainTest extends BaseTest {
+    // Parsons with distractors
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "The French Dip with mushrooms, a side of fries, and gravy please.";
+        return new Clause[]{
+                new StringLiteral("Please enter favourite country:"),
+                new NewLine(),
+                new PlaceHolder()
+        };
+    }
+
+    public void runMain() {FlipChars.main(new String[0]);}
+
+    public static Stream<Arguments> inputProvider() {
+        return Stream.of(Arguments.of("Canada", "aanadC"), Arguments.of("Hungary", "yungarH"),
+                Arguments.of("netherlands", "setherlandn"),
+                Arguments.of("ab", "ba"),Arguments.of("123", "321")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void printsFlippedCountryNameCorrectly(String originalName, String flippedName) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage =
+                "Your program does not correctly print the flipped country name.";
+        runWithInput(originalName, new Clause[]{
+                new StringLiteral(flippedName)
+        });
+    }
 }
