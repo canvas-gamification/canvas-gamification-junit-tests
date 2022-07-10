a79018a59f60e349e6a8e62fe0d83f9d9addc868
Hedgemon4 <sethlinc4@gmail.com>
2022-07-10 13:38:05 -0700

    Redid Junit for q1

--- src/arrays/programs_involving_data_sequences/hard/q1/CountArray.java
+++ src/arrays/programs_involving_data_sequences/hard/q1/CountArray.java
@@ -1,28 +1,52 @@
 package arrays.programs_involving_data_sequences.hard.q1;
 
 /*
-Write a program that defines (in the main method) an array of positive numbers in the range of 0 and 9 with the
-following values : [ 1, 3, 4, 5, 4, 9, 4, 9, 9, 7, 7, 3 ]. This program will also have a method called countThisPlease()
-that counts the frequency of each number in that array. With the input array you've defined, the method would count that
-there are no 0's, one 1's, no 2's, two 3's, three 4's, etc., and store those counts into a new array
-[ 0, 1, 0, 2, 3, 1, 0, 2, 0, 3] for each possible value between 0 to 9. That new array of counts will be returned as the
-method's output, which the main method should print out, with one element on each line. Make sure you check if the number
-in the array is between 0 and 9 (inclusive) before adding it to the array. If it is invalid, just ignore the number.
+Write a program that prompts the user to enter an array of positive numbers in the range of 0 and 100  with length 14.
+This program will also have a method called countThisPlease() that takes the array and integer as input and counts the
+number of items in it which are below the pivot, equal to the pivot, and above the pivot. Assume for this question the pivot
+is equal to 45, but your method should be generalized to handle all cases. The values calculate in the method should be
+returned as a new array of length 3, in the order numbers below, numbers equal to, and then numbers above the pivot.
+Finally, the main method should print out this array, with one element on each line. Ensure your output matches the sample.
+
+Sample Output:
+    Please enter n numbers:
+    1
+    11
+    49
+    ...
+    n
+    The number of numbers below 45 is x
+    The number of numbers equal to 45 is y
+    The number of numbers above 45 is z
 
  */
+import java.util.Scanner;
+
 public class CountArray {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 4, 5, 4, 9, 4, 9, 9, 7, 7, 3};
-        int[] arrCounts = countThisPlease(arr);
-        for (int j = 0; j < arrCounts.length; j++)
-            System.out.println(arrCounts[j]);
+        Scanner input = new Scanner(System.in);
+        int [] arr = new int[14];
+        System.out.println("Please enter 14 numbers:");
+        for(int i = 0; i < 14; i++)
+            arr[i] = input.nextInt();
+        int[] arrCount = countThisPlease(arr, 45);
+        System.out.printf("The number of numbers below %d is: %d\n", 45, arrCount[0]);
+        System.out.printf("The number of numbers equal to %d is: %d\n", 45, arrCount[1]);
+        System.out.printf("The number of numbers above %d is: %d\n", 45, arrCount[2]);
     }
 
-    public static int[] countThisPlease(int[] nums) {
-        int[] counts = new int[10];
-        for (int i = 0; i < nums.length; i++)
-            if (nums[i] >= 0 && nums[i] <= 9)
-                counts[nums[i]]++;
-        return counts;
+    public static int[] countThisPlease(int[] nums, int pivot) {
+        int above = 0;
+        int equal = 0;
+        int below = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] < pivot)
+                below++;
+            else if(nums[i] == pivot)
+                equal++;
+            else
+                above++;
+        }
+        return new int[]{below, equal, above};
     }
 }
--- src/arrays/programs_involving_data_sequences/hard/q1/MainTest.java
+++ src/arrays/programs_involving_data_sequences/hard/q1/MainTest.java
@@ -1,10 +1,14 @@
 package arrays.programs_involving_data_sequences.hard.q1;
 
 import global.BaseTest;
+
+import global.tools.TestOption;
 import global.utils.MethodUtil;
 import global.variables.Clause;
 import global.variables.clauses.IntegerLiteral;
 import global.variables.clauses.NewLine;
+import global.variables.clauses.StringLiteral;
+import global.variables.wrappers.Optional;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -12,42 +16,59 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class MainTest extends BaseTest {
     // Java
-    private static final int[] mainMethodOutput = new int[]{0, 1, 0, 2, 3, 1, 0, 2, 0, 3};
-
     public Clause[] testSentence() {
-        return clauseBuilder();
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "14 15 45 45 45 16 18 19 49 33 0 12 100 45";
+        return new Clause[]{
+                new StringLiteral("Please enter 14 numbers:"),
+                new Optional(new StringLiteral(" ")),
+                new NewLine(),
+                new StringLiteral("The number of numbers below 45 is: "),
+                new IntegerLiteral("below"),
+                new NewLine(),
+                new StringLiteral("The number of numbers equal to 45 is: "),
+                new IntegerLiteral("equal"),
+                new NewLine(),
+                new StringLiteral("The number of numbers above 45 is: "),
+                new IntegerLiteral("above")
+        };
     }
 
     public void runMain() {
         CountArray.main(new String[0]);
     }
 
-    static Stream<Arguments> methodInputProvider() {
-        return Stream.of(Arguments.of(new int[]{1, 5, 5, 3, 9, 8, 1, 4, 3, 7, 6, 9, 0, 1, 4, 5, 5, 5, 5},
-                new int[]{1, 3, 0, 2, 2, 6, 1, 1, 1, 2}),
-                Arguments.of(new int[]{}, new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}),
-                Arguments.of(new int[]{0, -11, 0, 9, 10, 10, -12, -3, -1, 0, 7, 9, 10, -5, 5, 3},
-                        new int[]{3, 0, 0, 1, 0, 1, 0, 1, 0, 2}));
+    static Stream<Arguments> mainMethodInputProvider() {
+        return Stream.of(Arguments.of("1 14 45 46 44 0 100 87 1 11 14 77 45 9", 8, 2, 4),
+                Arguments.of("0 0 0 45 45 45 0 0 0 0 0 45 45 1", 9, 5, 0));
+    }
+
+    static Stream<Arguments> countThisInputProvider() {
+        return Stream.of(Arguments.of(new int[]{45, 2, 44, 46, 18, 29, 78, 87, 46, 1, 0, 100, 45, 3}, 45, new int[]{7, 2, 5}),
+                Arguments.of(new int[]{45, 68, 0, 69, 88, 56, 12, 59, 100, 45, 91, 2, 4, 70}, 69, new int[]{9, 1, 4}));
     }
 
     @ParameterizedTest
-    @MethodSource("methodInputProvider")
-    void correctCountThisPleaseMethod(int[] input, int[] count) throws Throwable {
+    @MethodSource("countThisInputProvider")
+    void correctCountThisPleaseMethod(int[] input, int pivot, int[] result) throws Throwable {
         int[] output = (int[]) MethodUtil.invokeIfMethodExists(CountArray.class, "countThisPlease",
-                new Object[]{input}, int[].class);
-        assertArrayEquals(count, output, "Your method does not correctly count the values in the array.");
+                new Object[]{input, pivot}, int[].class, int.class);
+        assertArrayEquals(result, output, "Your method does not correctly count the all the number below, equal to, and above the pivot.");
+        String consoleOutput = MethodUtil.getMethodOutput();
+        assertEquals("", consoleOutput, "Your method should not have console output.");
     }
 
-    private Clause[] clauseBuilder() {
-        Clause[] c = new Clause[mainMethodOutput.length * 2];
-        int j = 0;
-        for (int i = 0; i < mainMethodOutput.length; i++) {
-            c[j++] = new IntegerLiteral(mainMethodOutput[i]);
-            c[j++] = new NewLine();
-        }
-        return c;
+    @ParameterizedTest
+    @MethodSource("mainMethodInputProvider")
+    void printCorrectOutput(String input, int below, int equal, int above) {
+        runWithInput(input);
+        assertEquals(Integer.parseInt(getItemByName("below")), below, "Your output for the numbers below the pivot is incorrect.");
+        assertEquals(Integer.parseInt(getItemByName("equal")), equal, "Your output for the numbers equal to the pivot is incorrect.");
+        assertEquals(Integer.parseInt(getItemByName("above")), above, "Your output for the numbers above the pivot is incorrect.");
     }
 }
