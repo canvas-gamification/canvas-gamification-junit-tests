d9902a96670095ed5852485ca82bf23cea34f3b7
Samantha H <hodge1214@gmail.com>
2022-07-11 17:00:06 -0400

    Checking everything works

---
+++ src/loops/while_loops/hard/q6/MainTest.java
@@ -0,0 +1,17 @@
+package loops.while_loops.hard.q6;
+
+import global.BaseTest;
+import global.variables.Clause;
+
+public class MainTest extends BaseTest {
+    @Override
+    public Clause[] testSentence() {
+        return new Clause[0];
+    }
+
+    @Override
+    public void runMain() {
+
+    }
+    //test
+}
