3357367dfaa60e78625792248b1aeaefc48fa53e
Kiet Phan <tuankietvn@outlook.com>
2022-07-11 14:36:32 -0700

    conditionals-program_with_decision_points-hard-q7

---
+++ src/conditionals/programs_with_decision_points/hard/q7/MainTest.java
@@ -0,0 +1,72 @@
+package conditionals.programs_with_decision_points.hard.q7;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
+import global.variables.Clause;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.PlaceHolder;
+import global.variables.clauses.StringLiteral;
+
+public class MainTest extends BaseTest {
+  // Parsons with distractors
+  public Clause[] testSentence() {
+    TestOption.isInputTest = true;
+    TestOption.defaultInput = "1000";
+    return new Clause[] {
+        new StringLiteral("How many people are in your area\\?"),
+        new NewLine(),
+        new PlaceHolder(),
+        new PlaceHolder()
+    };
+  }
+
+  public void runMain() {
+    RiskyBusiness.main(new String[0]);
+  }
+
+  static Stream<Arguments> inputProviderValid() {
+    return Stream.of(
+        Arguments.of(1, "Low Risk"),
+        Arguments.of(100, "Low Risk"),
+        Arguments.of(999, "Low Risk"),
+        Arguments.of(1000, "Moderate Risk"),
+        Arguments.of(5000, "Moderate Risk"),
+        Arguments.of(9999, "Moderate Risk"),
+        Arguments.of(10000, "High Risk"),
+        Arguments.of(20000, "High Risk"),
+        Arguments.of(1000000, "High Risk"));
+  }
+
+  static Stream<Arguments> inputProviderInvalid() {
+    return Stream.of(
+        Arguments.of(0),
+        Arguments.of(-1));
+  }
+
+  @ParameterizedTest
+  @MethodSource("inputProviderValid")
+  public void printsCorrectRiskLevelWithValidInput(int population, String riskLevel) throws InvalidClauseException {
+    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct risk level with the given valid input.";
+    runWithInput(population + "", new Clause[] {
+        new StringLiteral(riskLevel),
+        new NewLine(),
+    });
+  }
+
+  @ParameterizedTest
+  @MethodSource("inputProviderInvalid")
+  public void printsErrorMessageWithInvalidInput(int population) throws InvalidClauseException {
+    TestOption.incorrectStructureErrorMessage = "Your program does not print the error message for the invalid input.";
+    runWithInput(population + "", new Clause[] {
+        new StringLiteral("Invalid Input!"),
+        new NewLine(),
+    });
+  }
+}
---
+++ src/conditionals/programs_with_decision_points/hard/q7/RiskyBusiness.java
@@ -0,0 +1,40 @@
+package conditionals.programs_with_decision_points.hard.q7;
+
+/*
+ * Write a program that asks the user to enter the number of people present in a certain area, and display the
+ * approximate risk of infection based on the following information: Less than 1000 people is "Low Risk", Between
+ * 1000 and 10000 people is "Moderate Risk", above 10000 people is "High Risk." Make sure to display an error
+ * message if the user provides an invalid input.
+ * 
+ * Sample Output:
+ *    How many people are in your area?
+ *    1000
+ *    Moderate Risk
+ * 
+ * Distractors:
+ *    if( popDen > 0 || popDen < 1000)
+ *    {
+ *    }
+ *    else if( popDen > 10000 )
+ *    else if(  popDen >= 1000 || popDen < 10000)
+ *    else if(  popDen > 1000 && popDen < 10000)
+ * 
+ */
+
+import java.util.Scanner;
+
+public class RiskyBusiness {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("How many people are in your area?");
+    int popDen = input.nextInt();
+    if (popDen > 0 && popDen < 1000)
+      System.out.println("Low Risk");
+    else if (popDen >= 1000 && popDen < 10000)
+      System.out.println("Moderate Risk");
+    else if (popDen >= 10000)
+      System.out.println("High Risk");
+    else
+      System.out.println("Invalid Input!");
+  }
+}
