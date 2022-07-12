5b687574bad7bdfbbc22892518c904996926f4f6
yuqi88 <snow34545@gmail.com>
2022-07-12 13:00:58 -0700

    add class & its test - has errors in the question

---
+++ src/pre_defined_classes/programs_with_changing_behaviour/hard/q8/MainTest.java
@@ -0,0 +1,20 @@
+package pre_defined_classes.programs_with_changing_behaviour.hard.q8;
+
+import global.BaseRandomTest;
+import global.variables.Clause;
+import global.variables.clauses.RandomInteger;
+import global.variables.clauses.StringLiteral;
+
+public class MainTest  extends BaseRandomTest {
+    // Parsons with Distractors
+    public Clause[] testSentence() {
+        return new Clause[]{
+                new StringLiteral("Your new position is: "),
+                new RandomInteger(1, 101, "songChoice")
+        };
+    }
+
+    public void runMain() {
+        SnakesAndLadders.main(new String[0]);
+    }
+}
