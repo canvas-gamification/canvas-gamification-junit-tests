d791362302a8703fcb5cde07256bb6d0194f6069
Samantha H <hodge1214@gmail.com>
2022-07-12 23:52:55 -0400

    wrote tests and modified question text to exclude negative numbers as factorial apparently doesn't exist for them.

--- src/loops/while_loops/hard/q6/MainTest.java
+++ src/loops/while_loops/hard/q6/MainTest.java
@@ -1,17 +1,42 @@
 package loops.while_loops.hard.q6;
 
 import global.BaseTest;
+import global.tools.TestOption;
 import global.variables.Clause;
+import global.variables.clauses.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
-    @Override
+    //Parsons
     public Clause[] testSentence() {
-        return new Clause[0];
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "39";
+        return new Clause[]{
+                new StringLiteral("Enter a number: "),
+                new NewLine(),
+                new IntegerLiteral("number"),
+                new StringLiteral("! = "),
+                new IntegerLiteral("factorial")
+        };
     }
 
-    @Override
-    public void runMain() {
+    public void runMain() { FactOrIal.main(new String[0]); }
 
+    static Stream<Arguments> inputProvider(){
+        return Stream.of(Arguments.of(0, 0), Arguments.of(10, 3628800), Arguments.of(5, 120), Arguments.of(1, 1), Arguments.of(12, 479001600));
     }
-    //test
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void calculatesFactorialCorrectly(int in, int factorial){
+        runWithInput(in + System.lineSeparator());
+        assertEquals(Integer.parseInt(getItemByName("number")), in, "Your program does not output the same number that was input");
+        assertEquals(Integer.parseInt(getItemByName("factorial")), factorial, "Your program does not correctly calculate the factorial");
+    }
 }
