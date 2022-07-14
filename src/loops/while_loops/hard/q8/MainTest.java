022ac2b34e3d995a787b1659d4871d1bd8ab0c82
Samantha H <hodge1214@gmail.com>
2022-07-13 20:47:15 -0400

    filled out Clause and wrote test

--- src/loops/while_loops/hard/q8/MainTest.java
+++ src/loops/while_loops/hard/q8/MainTest.java
@@ -3,7 +3,16 @@ package loops.while_loops.hard.q8;
 import global.BaseTest;
 import global.tools.TestOption;
 import global.variables.Clause;
+import global.variables.clauses.IntegerLiteral;
+import global.variables.clauses.NewLine;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
     //Parsons
@@ -11,9 +20,24 @@ public class MainTest extends BaseTest {
         TestOption.isInputTest = true;
         TestOption.defaultInput = "39";
         return new Clause[]{
-                new StringLiteral("")
+                new StringLiteral("Enter a sentence"),
+                new NewLine(),
+                new StringLiteral("Total number of Characters in the sentence: "),
+                new IntegerLiteral("number")
         };
     }
 
     public void runMain() {CounterChar.main(new String[0]);}
+
+    static Stream<Arguments> inputProvider(){
+        return Stream.of(Arguments.of("    ", 0), Arguments.of("hello world", 10), Arguments.of("You Know ThAt MOST OTHER character counters Count spaceS", 48), Arguments.of("10",2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void countsCorrectly(String in, int num){
+        runWithInput(in);
+        assertEquals(Integer.parseInt(getItemByName("number")), num, "Your program is not correctly counting the number of characters.");
+    }
+
 }
