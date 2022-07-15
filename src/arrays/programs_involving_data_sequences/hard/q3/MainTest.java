70b766fe1aadfcbdc9f23f34b529bfe65622f131
Hedgemon4 <sethlinc4@gmail.com>
2022-07-15 15:03:05 -0700

    Finished JUnit for q3

--- src/arrays/programs_involving_data_sequences/hard/q3/MainTest.java
+++ src/arrays/programs_involving_data_sequences/hard/q3/MainTest.java
@@ -1,17 +1,28 @@
 package arrays.programs_involving_data_sequences.hard.q3;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
 import global.tools.TestOption;
+import global.utils.MethodUtil;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Random;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
+    private static final int numChars = 10;
 
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
-        TestOption.defaultInput = "a p p l e i s b a d 0 4";
+        TestOption.defaultInput = randomCharInputGenerator(numChars) + "2 " + "7 ";
         return new Clause[]{
                 new StringLiteral("Enter the character array:"),
                 new NewLine(),
@@ -27,5 +38,55 @@ public class MainTest extends BaseTest {
 
     public void runMain() {
         SubThat.main(new String[0]);
+    }
+
+    private static String randomCharInputGenerator(int length) {
+        StringBuilder sb = new StringBuilder();
+        Random r = new Random();
+        for (int i = 0; i < length; i++) {
+            sb.append((char) (r.nextInt(26) + 97));
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+    static Stream<Arguments> methodInputProvider() {
+        return Stream.of(Arguments.of(new char[]{'a', 'p', 'p', 'l', 'e', 'i', 's', 'b', 'a', 'd'}, 0, 4, "appl"),
+                Arguments.of(new char[]{'h', 'f', 'j', 'z', 'w'}, 0, 8, ""),
+                Arguments.of(new char[]{}, 0, 4, ""),
+                Arguments.of(new char[]{'a', 'z', 'x', 'w', 'q', 's', 'p', 'e', 'f', 'e', 'e', 'g', 'k', 'z', 'a', 'e'}, 5, 4, ""),
+                Arguments.of(new char[]{'a', 'b', 'c', 'd', 'z', 'g', 'l'}, 4, 4, ""),
+                Arguments.of(new char[]{'f', 'd', 'z', 'q', 'e', 't', 'y', 'p', 'e', 'z', 'w', 'z', 'm', 'n'}, 7, 14, "pezwzmn"));
+    }
+
+    static Stream<Arguments> mainMethodInputProvider() {
+        Random r = new Random();
+        return Stream.of(Arguments.of(randomCharInputGenerator(numChars), numChars / 2, numChars),
+                Arguments.of(randomCharInputGenerator(numChars), 0, numChars),
+                Arguments.of(randomCharInputGenerator(numChars), numChars - 1, numChars + 1),
+                Arguments.of(randomCharInputGenerator(numChars), -1, 5),
+                Arguments.of(randomCharInputGenerator(numChars), r.nextInt(numChars), r.nextInt(numChars)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("methodInputProvider")
+    void correctSubCharacterStringMethod(char[] input, int start, int end, String subString) throws Throwable {
+        String methodOutput = (String) MethodUtil.invokeIfMethodExists(SubThat.class, "subCharacterString",
+                new Object[]{input, start, end}, char[].class, int.class, int.class);
+        assertEquals(subString, methodOutput, "Your method does not generate the correct substring based on the inputs provided.");
+        String consoleOutput = MethodUtil.getMethodOutput();
+        assertEquals("", consoleOutput, "Your method should not print anything to the console.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("mainMethodInputProvider")
+    void printsCorrectOutput(String charInput, int startIndex, int endIndex) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not print the substring correctly.";
+        String output = "";
+        if (startIndex < endIndex && startIndex >= 0 && endIndex <= numChars)
+            output = charInput.replaceAll(" ", "").substring(startIndex, endIndex);
+        runWithInput(charInput + startIndex + " " + endIndex, new Clause[]{
+                new StringLiteral(output)
+        });
     }
 }
--- src/arrays/programs_involving_data_sequences/hard/q3/SubThat.java
+++ src/arrays/programs_involving_data_sequences/hard/q3/SubThat.java
@@ -1,22 +1,22 @@
 package arrays.programs_involving_data_sequences.hard.q3;
 /*
-Write a program that calls a method called subCharacterString that takes as input a character array and two indexes to
-that array. The method will then copy the characters starting from the first index all the way to the characters ending
-at the second index into a String and return it as output. If the indices provided are invalid (e.g., if they exceed the
-length of the character array or if the second index is smaller than the first index), then the method returns an empty
-string. Print the resulting string in the main method.
-
+Write a program that calls a method called subCharacterString( char[] letters, int start, int end ) that takes as input
+a character array and two indexes to that array. The method will then copy the characters starting from the first index (inclusive)
+all the way to the characters ending at the second index (exclusive) into a String and return it as output. If the indices provided
+are invalid (e.g., if they exceed the length of the character array or if the second index is less than or equal to the first index),
+then the method returns an empty string. Your method should be general enough to work with any length of array.
+The main method should prompt the user to enter an array of length 10, a starting index, and an ending index, which are
+sent to subCharacterString. Print the resulting string in the main method.
  */
 
 import java.util.Scanner;
-public class SubThat
-{
-    public static void main( String [] args )
-    {
+
+public class SubThat {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the character array:");
         char[] arr = new char[10];
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = input.next().charAt(0);
         }
         System.out.println("Enter the starting index:");
@@ -26,14 +26,14 @@ public class SubThat
         System.out.println("Your string is:");
         System.out.println(subCharacterString(arr, start, end));
     }
-    public static String subCharacterString( char[] letters, int start, int end )
-    {
+
+    public static String subCharacterString(char[] letters, int start, int end) {
         String finalWord = "";
-        if( end < start )
+        if (end < start)
             return finalWord;
-        if( start < 0 || end > letters.length )
+        if (start < 0 || end > letters.length)
             return finalWord;
-        for( int i = start; i < end; i++ )
+        for (int i = start; i < end; i++)
             finalWord = finalWord + letters[i];
         return finalWord;
     }
