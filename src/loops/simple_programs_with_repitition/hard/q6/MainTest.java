e686a5af683a2067b29e9640bcd1118c50192793
Samantha H <hodge1214@gmail.com>
2022-07-16 13:11:13 -0400

    wrote paramaterized test.

--- src/loops/simple_programs_with_repitition/hard/q6/MainTest.java
+++ src/loops/simple_programs_with_repitition/hard/q6/MainTest.java
@@ -1,11 +1,17 @@
 package loops.simple_programs_with_repitition.hard.q6;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
 import global.tools.TestOption;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
     // Java
@@ -21,4 +27,17 @@ public class MainTest extends BaseTest {
     }
 
     public void runMain(){QueryStr.main(new String[0]);}
+
+    static Stream<Arguments> inputProvider(){
+        return Stream.of(Arguments.of(" hello darkness  my old friend ", "%20%hello%20%darkness%20%%20%my%20%old%20%friend%20%"), Arguments.of("I'mAtAPayphone", "I'mAtAPayphone"), Arguments.of("   ", "%20%%20%%20%"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void correctlyReplacesSpaces(String in, String replaced) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not correctly replace all spaces with %20%.";
+        runWithInput(in, new Clause[]{
+                new StringLiteral(replaced)
+        });
+    }
 }
