502f3272af4a46ddc22b69de518f35558255664b
Samantha H <hodge1214@gmail.com>
2022-07-16 20:43:23 -0400

    wrote paramaterized test

--- src/loops/programs_with_repetition/hard/q4/MainTest.java
+++ src/loops/programs_with_repetition/hard/q4/MainTest.java
@@ -1,12 +1,18 @@
 package loops.programs_with_repetition.hard.q4;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
 import global.tools.TestOption;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 import global.variables.wrappers.Optional;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
     // Java
@@ -17,9 +23,24 @@ public class MainTest extends BaseTest {
                 new StringLiteral("Enter a sentence:"),
                 new Optional(new StringLiteral(" ")),
                 new NewLine(),
-                new PlaceHolder()
+                new PlaceHolder(),
+                new Optional(new StringLiteral(" "))
         };
     }
 
     public void runMain(){ArtificialSelection.main(new String[0]);}
+
+    static Stream<Arguments> inputProvider(){
+        return Stream.of(Arguments.of("warning, they are coming to find you", ",gninraw they era coming ot find uoy"),
+                Arguments.of("hello", "olleh"), Arguments.of("if i put a bunch of palindromes in here it would print out the same thing, wouldn't that be so pointless", "fi i tup a hcnub of semordnilap in ereh it dluow print tuo the emas thing, t'ndluow that eb so sseltniop "));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void reversesEveryOtherCorrectly(String in, String reversed) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not correctly reverse every other word according to the instruction.";
+        runWithInput(in, new Clause[]{
+                new StringLiteral(reversed)
+        });
+    }
 }
