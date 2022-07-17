a25c3f940c82bac4c0184054e29102c78be66a18
Samantha H <hodge1214@gmail.com>
2022-07-17 19:43:43 -0400

    seperated invalid input test

--- src/loops/while_loops/hard/q6/MainTest.java
+++ src/loops/while_loops/hard/q6/MainTest.java
@@ -6,6 +6,7 @@ import global.tools.TestOption;
 import global.variables.Clause;
 import global.variables.clauses.*;
 import global.variables.wrappers.Optional;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -30,25 +31,26 @@ public class MainTest extends BaseTest {
     public void runMain() { Factorial.main(new String[0]); }
 
     static Stream<Arguments> inputProvider(){
-        return Stream.of(Arguments.of(0, 0, 3), Arguments.of(10, 3628800, 3), Arguments.of(5, 120, 3), Arguments.of(1, 1, 3), Arguments.of(12, 479001600, 3), Arguments.of(-6, -1, 1));
+        return Stream.of(Arguments.of(0, 0), Arguments.of(10, 3628800), Arguments.of(5, 120), Arguments.of(1, 1), Arguments.of(12, 479001600));
     }
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void calculatesFactorialCorrectly(int in, int factorial, int v) throws InvalidClauseException {
-        Clause[][] place = new Clause[1][v];
-        if (v == 3) {
-            place[0][0] =  new IntegerLiteral("number");
-            place[0][1] = new StringLiteral("! = ");
-            place[0][2] = new IntegerLiteral("factorial");
-            runWithInput(in + System.lineSeparator(), place);
-            assertEquals(Integer.parseInt(getItemByName("number")), in, "Your program does not output the input number in the final output.");
-            assertEquals(Integer.parseInt(getItemByName("factorial")), factorial, "Your program does not correctly calculate the factorial.");
-        }
-        else{
-            TestOption.incorrectStructureErrorMessage = "Your program does not identify an invalid input.";
-            place[0][0] = new StringLiteral("Invalid input");
-            runWithInput(in + System.lineSeparator(), place);
-        }
+    void calculatesFactorialCorrectly(int in, int factorial) throws InvalidClauseException {
+        Clause[][] place = new Clause[1][3];
+        place[0][0] =  new IntegerLiteral("number");
+        place[0][1] = new StringLiteral("! = ");
+        place[0][2] = new IntegerLiteral("factorial");
+        runWithInput(in + System.lineSeparator(), place);
+        assertEquals(Integer.parseInt(getItemByName("number")), in, "Your program does not output the input number in the final output.");
+        assertEquals(Integer.parseInt(getItemByName("factorial")), factorial, "Your program does not correctly calculate the factorial.");
+    }
+
+    @Test
+    void printsErrorMessageForInvalidInput() throws InvalidClauseException{
+        TestOption.incorrectStructureErrorMessage = "Your program does not identify an invalid input.";
+        runWithInput(-6 + System.lineSeparator(), new Clause[]{
+                new StringLiteral("Invalid input")
+        });
     }
 }
