a859ec473f43dc7cb94da41af7c2fce81a1885e2
yuqi88 <snow34545@gmail.com>
2022-07-17 15:09:23 -0700

    update branch

---
+++ src/arrays/arrays_with_methods/hard/q2/MainTest.java
@@ -0,0 +1,32 @@
+package arrays.arrays_with_methods.hard.q2;
+
+import global.utils.MethodUtil;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class MainTest {
+    // Java
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{0, -11, 4561254, 2}, new int[]{4, -12241, 1, 5, 1241, 0, 0, 0, 124}, new int[]{4, -12241, 1, 5, 1241, 0, 0, 0, 124}),
+                Arguments.of(new int[]{2, 5, 1, 7, -8, 2}, new int[]{2, 3, 4, 6, 9}, new int[]{2, 5, 1, 7, -8, 2}),
+                Arguments.of(new int[]{5, 8, -1}, new int[]{9, 1, 1}, new int[]{5, 8, -1}),
+                Arguments.of(new int[]{}, new int[]{}, new int[]{}),
+                Arguments.of(new int[]{5, 213, 12, -21, 254}, new int[]{34525, 12, 52}, new int[]{5, 213, 12, -21, 254}),
+                Arguments.of(new int[]{1, 2, 3, 4, 5, 1, 2, 3, 41112, 5}, new int[]{1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6},
+                        new int[]{1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6}));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void correctWhoIsLongerMethod(int[] a, int[] b, int[] longer) throws Throwable{
+        int[] output = (int[]) MethodUtil.invokeIfMethodExists(Longer.class, "whoIsLonger",
+                new Object[]{a, b}, int[].class, int[].class);
+        assertArrayEquals(longer, output, "Your method does not return the longer array.");
+    }
+}
---
+++ src/arrays/arrays_with_methods/hard/q4/MainTest.java
@@ -0,0 +1,32 @@
+package arrays.arrays_with_methods.hard.q4;
+
+import global.utils.MethodUtil;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    // Java
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(Arguments.of(new double[]{1.4, 1.5, 1.5, 1.6, 3.4, 99.5, 99.2}, false),
+                Arguments.of(new double[]{-134.56, -134.55, -15.78, -4.556355, 0, 1, 1.45, 3.1415926, 4445.63455}, true),
+                Arguments.of(new double[]{}, true),
+                Arguments.of(new double[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, true),
+                Arguments.of(new double[]{45, 11, 54.75, 232.564, -12442.3456, 123.456}, false),
+                Arguments.of(new double[]{1452, -45, 125, 356, 2566, 3497}, false),
+                Arguments.of(new double[]{-45, 1157, 2462, 8743, 8743, 8743, 8743, 8744, 478393}, true));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void correctIsAscendingMethod(double[] input, boolean isAscending) throws Throwable{
+        boolean output = (boolean) MethodUtil.invokeIfMethodExists(NumbersAscend.class, "isAscending",
+                new Object[]{input}, double[].class);
+        assertEquals(isAscending, output,
+                "Your method does not return the correct boolean value based on the input array.");
+    }
+}
