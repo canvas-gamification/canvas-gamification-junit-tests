9e94a3a7eb1bcf8875bd5376a19e88202e3a1b55
yuqi88 <snow34545@gmail.com>
2022-07-17 21:01:04 -0700

    change wording

--- src/pre_defined_classes/programs_with_changing_behaviour/medium/q9/PlayerCharacteristics.java
+++ src/pre_defined_classes/programs_with_changing_behaviour/medium/q9/PlayerCharacteristics.java
@@ -3,7 +3,7 @@ package pre_defined_classes.programs_with_changing_behaviour.medium.q9;
 /*
 You are playing a role-playing game (RPG)! You have to select the age and height of your character, but you aren't sure
 what values to use for them. Thankfully, this game has a randomizer built-in to obtain the character's age and height.
-You want to make sure the age is between 1 and 50 (inclusive of both values) and the height is between 50 and 200
+You want to make sure the age is between 1 and 50 (inclusive of both values), and the height is between 50 and 200
 (inclusive of both values). Make a Java Program that performs these functions.
 
 Sample output:
