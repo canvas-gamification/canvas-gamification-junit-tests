6094e72cc8cb4b19161f47006f2fcb6417bfdfcc
Kiet Phan <tuankietvn@outlook.com>
2022-07-17 19:18:45 -0700

    Update question

--- src/loops/programs_with_repetition/medium/q5/MainTest.java
+++ src/loops/programs_with_repetition/medium/q5/MainTest.java
@@ -1,34 +1,74 @@
 package loops.programs_with_repetition.medium.q5;
 
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
+import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 
 public class MainTest extends BaseTest {
   // Java
   public Clause[] testSentence() {
-    return trianglePatternClause(5);
+    TestOption.isInputTest = true;
+    TestOption.defaultInput = "5";
+    return new Clause[] {
+        new StringLiteral("How wide is your arrow\\?"),
+        new NewLine(),
+        new PlaceHolder()
+    };
   }
 
   public void runMain() {
     ThatWay.main(new String[0]);
   }
 
-  Clause[] trianglePatternClause(int n) {
-    Clause[] c = new Clause[n * 4 - 2];
+  static Stream<Arguments> inputProviderValid() {
+    return Stream.of(
+        Arguments.of(10, arrowPatternClause(10)),
+        Arguments.of(5, arrowPatternClause(5)),
+        Arguments.of(1, new Clause[][] { { new StringLiteral("\\* "), new NewLine() } }));
+  }
+
+  static Stream<Integer> inputProviderInvalid() {
+    return Stream.of(0, -1, -5, -10);
+  }
+
+  @ParameterizedTest
+  @MethodSource("inputProviderValid")
+  public void printsCorrectArrow(int width, Clause[][] arrow) throws InvalidClauseException {
+    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct arrow with the given width.";
+    runWithInput(width + "", arrow);
+  }
+
+  @ParameterizedTest
+  @MethodSource("inputProviderInvalid")
+  public void printsErrorMessageForInvalidInput(int width) throws InvalidClauseException {
+    TestOption.incorrectStructureErrorMessage = "Your program does not print the error message with an invalid width.";
+    runWithInput(width + "", new Clause[] { new StringLiteral("Invalid Input!") });
+  }
+
+  static Clause[][] arrowPatternClause(int n) {
+    Clause[][] c = new Clause[1][n * 4 - 2];
     int index = 0;
     for (int i = 1; i <= n; ++i) {
       StringBuilder sb = new StringBuilder();
       sb.append("\\* ".repeat(i));
-      c[index++] = new StringLiteral(sb.toString());
-      c[index++] = new NewLine();
+      c[0][index++] = new StringLiteral(sb.toString());
+      c[0][index++] = new NewLine();
     }
     for (int i = n - 1; i >= 1; --i) {
       StringBuilder sb = new StringBuilder();
       sb.append("\\* ".repeat(i));
-      c[index++] = new StringLiteral(sb.toString());
-      c[index++] = new NewLine();
+      c[0][index++] = new StringLiteral(sb.toString());
+      c[0][index++] = new NewLine();
     }
 
     return c;
--- src/loops/programs_with_repetition/medium/q5/ThatWay.java
+++ src/loops/programs_with_repetition/medium/q5/ThatWay.java
@@ -1,9 +1,12 @@
 package loops.programs_with_repetition.medium.q5;
 
 /*
- * Write a program to print the following pattern using nested loops : 
+ * Write a program to print an arrow using the char “*” with nested loops and a positive integer n representing the width of the arrow.
+ * If the user enters a negative integer or 0, the program should print an error message.
  * 
- * Sample output:
+ *    Sample output: 
+ *    How wide is your arrow?
+ *    5
  *    *
  *    * * 
  *    * * * 
@@ -13,17 +16,27 @@ package loops.programs_with_repetition.medium.q5;
  *    * * *
  *    * *
  *    *
+ * 
  */
+
+import java.util.Scanner;
 
 public class ThatWay {
   public static void main(String[] args) {
-    for (int i = 0; i < 5; i++) {
+    Scanner inp = new Scanner(System.in);
+    System.out.println("How wide is your arrow?");
+    int n = inp.nextInt();
+    if (n <= 0) {
+      System.out.println("Invalid Input!");
+      return;
+    }
+    for (int i = 0; i < n; i++) {
       for (int k = 0; k <= i; k++)
         System.out.print("* ");
       System.out.println();
     }
-    for (int i = 1; i < 5; i++) {
-      for (int k = 0; k < 5 - i; k++)
+    for (int i = 1; i < n; i++) {
+      for (int k = 0; k < n - i; k++)
         System.out.print("* ");
       System.out.println();
     }
