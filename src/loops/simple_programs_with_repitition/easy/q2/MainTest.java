808bb15113fcdf6c47d94de9cf33f90583a510b4
Samantha H <hodge1214@gmail.com>
2022-07-18 14:50:10 -0400

    pasted in question text and wrote clause and test

---
+++ src/loops/simple_programs_with_repitition/easy/q2/MainTest.java
@@ -0,0 +1,55 @@
+package loops.simple_programs_with_repitition.easy.q2;
+
+import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
+import global.variables.Clause;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.PlaceHolder;
+import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class MainTest extends BaseTest {
+    // Parsons
+    public Clause[] testSentence(){
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "2";
+        return new Clause[]{
+                new StringLiteral("Enter either 1, 2, 3, 4: "),
+                new NewLine(),
+                new PlaceHolder()
+        };
+    }
+
+    public void runMain(){VirtualBouncer.main(new String[0]);}
+
+    static Stream<int[]> inputProvider(){
+        return Stream.of(new int[]{5, 8, 10, 11, 6, 23, 1}, new int[]{-1, -2, -3, 2}, new int[]{2});
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void loopsUntilSpecifiedNumbersAreEntered(int[] n) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not loop until 1, 2, 3, or 4 is entered.";
+        StringBuilder s = new StringBuilder();
+        for(int x : n){
+            s.append(x).append(" ");
+        }
+        runWithInput(s.toString(), clauseBuilder(n));
+    }
+
+    Clause[] clauseBuilder(int[] n){
+        StringBuilder s = new StringBuilder();
+        for(int x = 0; x < n.length-1; x++){
+            s.append("Please only enter either 1, 2, 3, or 4: ");
+            s.append(System.lineSeparator());
+        }
+        s.append("You entered a valid number!");
+        return new Clause[]{
+                new StringLiteral(s.toString())
+        };
+    }
+}
---
+++ src/loops/simple_programs_with_repitition/easy/q2/VirtualBouncer.java
@@ -0,0 +1,24 @@
+package loops.simple_programs_with_repitition.easy.q2;
+
+/*
+Write a program that asks the user to enter either 1, 2, 3 , or 4, and keep asking them to enter a valid value
+until they enter one of these 4 values.
+ */
+
+import java.util.Scanner;
+public class VirtualBouncer
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner( System.in );
+        System.out.println( "Enter either 1, 2, 3, 4: " );
+        int n = input.nextInt();
+        while( n != 1 && n != 2 && n != 3 && n != 4 )
+        {
+            System.out.println( "Please only enter either 1, 2, 3, or 4: ");
+            n = input.nextInt();
+        }
+        System.out.println( "You entered a valid number!" );
+    }
+}
+
