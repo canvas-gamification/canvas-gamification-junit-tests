f8a314f7a88200a766f7a5008c77a527807062a9
Kiet Phan <35695324+ketphan02@users.noreply.github.com>
2022-07-22 14:45:39 -0700

    Update src/conditionals/programs_with_decision_points/hard/q7/RiskyBusiness.java
    Co-authored-by: Seth Akins <83678885+Hedgemon4@users.noreply.github.com>

--- src/conditionals/programs_with_decision_points/hard/q7/RiskyBusiness.java
+++ src/conditionals/programs_with_decision_points/hard/q7/RiskyBusiness.java
@@ -4,7 +4,7 @@ package conditionals.programs_with_decision_points.hard.q7;
  * Write a program that asks the user to enter the number of people present in a certain area, and display the
  * approximate risk of disease transmission based on the following information: Less than 1000 people is "Low Risk",
  * Between 1000 and 10000 people is "Moderate Risk", above 10000 people is "High Risk." Make sure to display an error
- * message if the user provides an invalid input.
+ * message if the user provides an invalid input (numbers less than or equal to 0).
  * 
  * Sample Output:
  *    How many people are in your area?
