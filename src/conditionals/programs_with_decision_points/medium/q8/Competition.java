47630eff45d62d6fcee0f3d3c69a8c899887c936
Kiet Phan <35695324+ketphan02@users.noreply.github.com>
2022-07-22 14:44:50 -0700

    Update src/conditionals/programs_with_decision_points/medium/q8/Competition.java
    Co-authored-by: Seth Akins <83678885+Hedgemon4@users.noreply.github.com>

--- src/conditionals/programs_with_decision_points/medium/q8/Competition.java
+++ src/conditionals/programs_with_decision_points/medium/q8/Competition.java
@@ -4,7 +4,7 @@ package conditionals.programs_with_decision_points.medium.q8;
  * Write a program that will ask the user for their placement and print out an "award" based on it using the following
  * information: 1st place gets a "Gold Trophy", 2nd place gets a "Silver Trophy", 3rd place  gets a "Bronze Trophy", and
  * everything below that receives a "Consolation Goodie Bag". The program should print "Invalid Input!" if the user enters
- * an invalid place.
+ * an invalid place (any number less than 1).
  * 
  * Sample output:
  *  Which place did you finish? (Ex. 1, 2, 3, etc): 
