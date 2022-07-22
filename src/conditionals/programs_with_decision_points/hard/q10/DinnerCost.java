41d7d0eaf26a080cb6d2d546a298d03c46c77026
Seth Akins <83678885+Hedgemon4@users.noreply.github.com>
2022-07-22 17:57:58 -0600

    Update src/conditionals/programs_with_decision_points/hard/q10/DinnerCost.java

--- src/conditionals/programs_with_decision_points/hard/q10/DinnerCost.java
+++ src/conditionals/programs_with_decision_points/hard/q10/DinnerCost.java
@@ -4,7 +4,7 @@ package conditionals.programs_with_decision_points.hard.q10;
  * Write a Java program that determines the cost of dining at a restaurant depending on the quality of service.
  * The program should prompt the user about the cost of the food and if the service is good. If the service was
  * good, then the user tips 20%. If the service was okay, then the user only tips 15%. The cost of dinner should
- * be rounded to the nearest integer. To decide if the service whether the service is good, the user should enter
+ * be rounded to the nearest integer. To determine if the service is good, the user should enter
  * 1 for Yes, and 0 for No. If the cost of food is less than 0, or the user does not enter 0 or 1 for if the service was good, the program should print "Invalid Input!" to the
  * user.
  * 
