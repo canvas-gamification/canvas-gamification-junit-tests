818a15d2e4cd2778b73f6b32fc21e3148698e560
Kiet Phan <tuankietvn@outlook.com>
2022-07-22 16:57:10 -0700

    Fix bugs and formating

--- src/loops/programs_with_repetition/easy/q4/MainTest.java
+++ src/loops/programs_with_repetition/easy/q4/MainTest.java
@@ -1,6 +1,7 @@
 package loops.programs_with_repetition.easy.q4;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.stream.Stream;
 
@@ -12,7 +13,6 @@ import global.BaseTest;
 import global.exceptions.InvalidClauseException;
 import global.tools.TestOption;
 import global.variables.Clause;
-import global.variables.clauses.IntegerLiteral;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
@@ -42,7 +42,18 @@ public class MainTest extends BaseTest {
     return allPrimeInputs;
   }
 
-  static Stream<Arguments> repetitiveInputs() {
+  private static Stream<Arguments> repetitiveHardcodedInputs() {
+    return Stream.of(
+        Arguments.of(new ArrayList<Integer>(Arrays.asList(15, -1, -9, -10, -11, -12, 28, 21, 99, 19))),
+        Arguments.of(new ArrayList<Integer>(Arrays.asList(1, 41))),
+        Arguments.of(new ArrayList<Integer>(Arrays.asList(50, 41))),
+        Arguments.of(new ArrayList<Integer>(Arrays.asList(-1, 2))),
+        Arguments.of(new ArrayList<Integer>(Arrays.asList(39, 20, 1010, 16, -5, 31))),
+        Arguments.of(new ArrayList<Integer>(Arrays.asList(15, -1, -9, -10, -11, -12, 28, 21, 99, 19))),
+        Arguments.of(new ArrayList<Integer>(Arrays.asList(-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -51, 47))));
+  }
+
+  private static Stream<Arguments> repetitiveRandomlyGeneratedInputs() {
     int[] allPrimesUnder50 = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
     int[] allCompositesUnder50AndNegatives = { 1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28,
         30, 32, 33, 34, 35, 36, 38, 39, 40, 42, 44, 45, 46, 48, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12 };
@@ -60,6 +71,10 @@ public class MainTest extends BaseTest {
     return allPrimeInputs;
   }
 
+  static Stream<Arguments> repetitiveInputs() {
+    return Stream.concat(repetitiveHardcodedInputs(), repetitiveRandomlyGeneratedInputs());
+  }
+
   @ParameterizedTest
   @MethodSource("allPrimeInputs")
   public void printsCorrectMessageWithOneInput(int input) throws InvalidClauseException {
@@ -73,14 +88,20 @@ public class MainTest extends BaseTest {
   @MethodSource("repetitiveInputs")
   public void printsCorrectMessageWithMultipleInputs(ArrayList<Integer> inputs) throws InvalidClauseException {
     TestOption.incorrectStructureErrorMessage = "Your program does not stop when it enters a prime number that is less than 50.";
-    Iterator<Integer> it = inputs.iterator();
-    Clause[][] outputStrings = new Clause[1][inputs.size() * 3 - 2];
-    for (int i = 0; i < inputs.size() * 3 - 3; i += 3) {
-      outputStrings[0][i + 0] = new IntegerLiteral(it.next());
-      outputStrings[0][i + 1] = new StringLiteral("Enter an integer\\: ");
-      outputStrings[0][i + 2] = new NewLine();
+    String formatedInput = inputs.stream().map(Object::toString).reduce("", (a, b) -> a + " " + b);
+    Clause[][] expectedOutput = getPrimeTimeOutputClause(inputs);
+
+    runWithInput(formatedInput, expectedOutput);
+  }
+
+  private Clause[][] getPrimeTimeOutputClause(ArrayList<Integer> inputs) {
+    Clause[][] outputClause = new Clause[1][inputs.size() * 2 - 1];
+    for (int i = 0; i < inputs.size() * 2 - 2; i += 2) {
+      outputClause[0][i + 0] = new StringLiteral("Enter an integer\\: ");
+      outputClause[0][i + 1] = new NewLine();
     }
-    outputStrings[0][inputs.size() * 3 - 3] = new StringLiteral(
+    outputClause[0][inputs.size() * 2 - 2] = new StringLiteral(
         "Entered a prime number that is less than 50. Ending Program. ");
+    return outputClause;
   }
 }
