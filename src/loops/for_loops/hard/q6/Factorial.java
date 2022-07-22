f4995c6ef23dbaf3f7e964a3ee30e9fd75e386c7
Kiet Phan <35695324+ketphan02@users.noreply.github.com>
2022-07-22 16:57:28 -0700

    Update src/loops/for_loops/hard/q6/Factorial.java
    Co-authored-by: Seth Akins <83678885+Hedgemon4@users.noreply.github.com>

--- src/loops/for_loops/hard/q6/Factorial.java
+++ src/loops/for_loops/hard/q6/Factorial.java
@@ -2,7 +2,7 @@ package loops.for_loops.hard.q6;
 
 /*
  * Write a program that will accept a number from the user and print out the factorial of that number. ( Hint: n factorial or n! = 
- * n x (n - 1) x (n - 2) x … x 2 x 1 )
+ * n x (n - 1) x (n - 2) x … x 2 x 1 ). If the user enters a number less than 0, print "Invalid Input!"
  * 
  * Sample output:
  *    Enter a number: 
