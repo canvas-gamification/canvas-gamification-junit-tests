9f736e21994e8a9ddb8b5aa050b81bcdb30bf150
Seth Akins <83678885+Hedgemon4@users.noreply.github.com>
2022-07-23 19:12:12 -0600

    Update src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q10/MainTest.java

--- src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q10/MainTest.java
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q10/MainTest.java
@@ -44,7 +44,7 @@ public class MainTest extends BaseTest {
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void calculateNewSalaryCorrectly(double salary, int years, double newSalary) throws InvalidClauseException {
+    void calculatesNewSalaryCorrectly(double salary, int years, double newSalary) throws InvalidClauseException {
         runWithInput(salary + " " + years);
         assertEquals(Double.parseDouble(getItemByName("newSalary")), newSalary, 0.001,
                 "Your program does not correctly calculate the increased salary amount.");
