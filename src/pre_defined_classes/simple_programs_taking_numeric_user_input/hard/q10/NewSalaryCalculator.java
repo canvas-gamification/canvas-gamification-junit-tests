ed8b58ad2a64129f1eac4b97f0ba91e7e556ff8e
Seth Akins <83678885+Hedgemon4@users.noreply.github.com>
2022-07-23 19:14:53 -0600

    Update src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q10/NewSalaryCalculator.java

--- src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q10/NewSalaryCalculator.java
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q10/NewSalaryCalculator.java
@@ -3,7 +3,7 @@ package pre_defined_classes.simple_programs_taking_numeric_user_input.hard.q10;
 A company called HappyWorkLife has decided to give everybody a raise with an increase of 5% for each year of
 their service. The raise is only given to those who have worked at least a year or more,
 and the raise is calculated annually. Make a program that asks the user for the salary (as a double) and the number of
-years worked at the company (as an integer), then display the increased salary amount. Assume the user enters non-negative values.
+years worked at the company (as an integer), then displays the increased salary amount. Assume the user enters non-negative values.
 
 Sample output:
 Enter your salary:
