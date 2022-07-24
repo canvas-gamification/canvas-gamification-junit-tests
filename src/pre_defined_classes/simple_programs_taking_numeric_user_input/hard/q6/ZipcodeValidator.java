95d9e29257e689573da93489e94dedb285625972
Seth Akins <83678885+Hedgemon4@users.noreply.github.com>
2022-07-23 19:04:45 -0600

    Update src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q6/ZipcodeValidator.java

--- src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q6/ZipcodeValidator.java
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/hard/q6/ZipcodeValidator.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 /*
 An online e-commerce website uses zip codes to determine the approximate delivery date for a shipment. Prior to doing so, it validates the
-zip code by asking the user to re-submit the same zip code and double confirm the zip-code. Make a program that asks
+zip code by asking the user to enter and re-enter the zip code to confirm it is correct. Make a program that asks
 the user to enter a zip code and confirms it, then displays both zip codes back to the user.
 Note that American zip codes consist of 5 digits. Assume the provider will not ship to zip codes that start with 0,
 so users should not enter zip codes that start with 0.
