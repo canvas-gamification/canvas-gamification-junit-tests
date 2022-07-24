54a2634a5c8866f39de9b5385069d634182a447c
Hedgemon4 <sethlinc4@gmail.com>
2022-07-24 11:00:43 -0600

    Added sample output

--- src/loops/simple_programs_with_repetition/medium/q4/SerieslyQuestionMark.java
+++ src/loops/simple_programs_with_repetition/medium/q4/SerieslyQuestionMark.java
@@ -3,6 +3,11 @@ package loops.simple_programs_with_repetition.medium.q4;
 /*
 Write a Java program to ask the user for a number and calculate the sum of the following series:
 1/ n + 2/ n + 3/ n + 4/ n + 5/ n + 6/ n + … (n - 1)/ n. If a negative value is entered, the sum of the series should be 0.
+
+Sample Output:
+    Enter a number for the upper bound of the series:
+    5
+    The sum of the series is 2.0
 */
 
 import java.util.Scanner;
