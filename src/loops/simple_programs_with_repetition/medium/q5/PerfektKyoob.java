b42ebdde75fd5681c270292f435833b8d88bf319
Hedgemon4 <sethlinc4@gmail.com>
2022-07-26 15:50:32 -0600

    Updated question and added cases

--- src/loops/simple_programs_with_repetition/medium/q5/PerfektKyoob.java
+++ src/loops/simple_programs_with_repetition/medium/q5/PerfektKyoob.java
@@ -1,7 +1,14 @@
-package loops.simple_programs_with_repitition.medium.q5;
+package loops.simple_programs_with_repetition.medium.q5;
 
 /*
-Write a program that asks the user for a number and determines if it is a "Perfect Cube", i.e the sum of its digits is equal to its cube root. (Eg: 512 = 8^3 , and 5 + 1 + 2 = 8)
+Write a program that asks the user for a number and determines if it is a "Perfect Cube". In this case, a number is a
+perfect cube if the sum of its digits is equal to its cube root. (Eg: 512 = 8^3 , and 5 + 1 + 2 = 8). If the number
+is a perfect cube, the program should print "x is a Prefect Cube" and "x is NOT a Perfect Cube!" if not.
+
+Sample Output:
+	Enter a number:
+	512
+	512 is a Perfect Cube!
 */
 
 import java.util.Scanner;
