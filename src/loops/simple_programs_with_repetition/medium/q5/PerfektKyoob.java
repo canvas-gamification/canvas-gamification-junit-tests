ae02b742d693da2eaefa338d75fb28331676c7c1
Hedgemon4 <sethlinc4@gmail.com>
2022-07-26 20:22:50 -0600

    Renamed files and moved q6 to correct folder

--- src/loops/simple_programs_with_repetition/medium/q5/PerfektKyoob.java
+++ src/loops/simple_programs_with_repetition/medium/q5/PerfektKyoob.java
@@ -12,23 +12,21 @@ Sample Output:
 */
 
 import java.util.Scanner;
-public class PerfektKyoob
-{
-	   public static void main( String[] args )
-	   {
-	      Scanner input = new Scanner( System.in );
-	      System.out.println( "Enter a number: " );
-	      int n = input.nextInt();
-	      int nn = n;  
-	      int result  = 0; 
-	      while( n > 0 )
-	      { 
-	         result += n % 10 ;
-	         n = n / 10;
-	      }
-	      if(nn == Math.pow(result, 3))
-	         System.out.println( nn + " is a Perfect Cube!" );
-	      else
-	         System.out.println( nn + " is NOT a Perfect Cube!" ); 
-	   }
+
+public class PerfektKyoob {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = input.nextInt();
+        int nn = n;
+        int result = 0;
+        while (n > 0) {
+            result += n % 10;
+            n = n / 10;
+        }
+        if (nn == Math.pow(result, 3))
+            System.out.println(nn + " is a Perfect Cube!");
+        else
+            System.out.println(nn + " is NOT a Perfect Cube!");
+    }
 }
