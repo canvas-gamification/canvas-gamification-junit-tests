6c2b78c45a468ed6b241f33ec72e84fbd45eb173
Hedgemon4 <sethlinc4@gmail.com>
2022-07-27 16:03:58 -0600

    Updated sample output, added check for invalid input, and formatted code.

--- src/loops/programs_with_repetition/medium/q3/MainTest.java
+++ src/loops/programs_with_repetition/medium/q3/MainTest.java
@@ -1,23 +1,62 @@
 package loops.programs_with_repetition.medium.q3;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
+import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
+import global.variables.wrappers.Optional;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
     // Java
     public Clause[] testSentence() {
-        return pantsPattern(5);
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "5";
+        return new Clause[]{
+                new StringLiteral("Enter an integer:"),
+                new Optional(new StringLiteral(" ")),
+                new NewLine(),
+                new PlaceHolder()
+        };
     }
 
     public void runMain() {
         ThisPatternLooksLikePants.main(new String[0]);
     }
 
-    private Clause[] pantsPattern(int n) {
-        Clause[] c = new Clause[n * 2];
-        int x = 0;
+    static Stream<Integer> validInput() {
+        return Stream.of(1, 5, 9, 10, 39, 0);
+    }
+
+    static Stream<Integer> invalidInput() {
+        return Stream.of(-1, -2, -59);
+    }
+
+    @ParameterizedTest
+    @MethodSource("validInput")
+    void printsPantsPatternCorrectly(int input) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not correctly print out the pants pattern based on the input integer.";
+        runWithInput(String.valueOf(input), clauseBuilder(input));
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidInput")
+    void printsErrorMessageForInvalidInput(int input) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not print an error message for invalid input.";
+        runWithInput(String.valueOf(input), new Clause[]{
+                new StringLiteral("Invalid input!")
+        });
+    }
+
+    private Clause[][] clauseBuilder(int n) {
+        Clause[][] c = new Clause[1][n * 2];
+        int index = 0;
         for (int i = 0; i < n; i++) {
             StringBuilder sb = new StringBuilder();
             for (int k = 1; k <= n - i; k++)
@@ -29,9 +68,8 @@ public class MainTest extends BaseTest {
                 o++;
             for (; o >= 1; o--)
                 sb.append(o);
-            c[x] = new StringLiteral(sb.toString());
-            c[x + 1] = new NewLine();
-            x += 2;
+            c[0][index++] = new StringLiteral(sb.toString());
+            c[0][index++] = new NewLine();
         }
         return c;
     }
