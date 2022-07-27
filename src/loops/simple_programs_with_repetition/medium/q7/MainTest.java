915ce585cd64a88e96c2bd857daaf9d640f997f4
Hedgemon4 <sethlinc4@gmail.com>
2022-07-26 20:48:25 -0600

    Renamed files and moved updated test from parsons question to Java

---
+++ src/loops/simple_programs_with_repetition/medium/q7/MainTest.java
@@ -0,0 +1,67 @@
+package loops.simple_programs_with_repetition.medium.q7;
+
+import global.variables.*;
+import global.variables.clauses.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
+
+public class MainTest extends BaseTest {
+    // Java
+
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "10";
+        return new Clause[]{
+                new StringLiteral("Enter a number for the upper bound of the series: "),
+                new NewLine(),
+                new PlaceHolder()
+        };
+    }
+
+    public void runMain() {
+        SeriesSeries.main(new String[0]);
+    }
+
+    static Stream<Arguments> validInputProvider() {
+        return Stream.of(
+                Arguments.of(0, 0),
+                Arguments.of(1, 1),
+                Arguments.of(12, 650),
+                Arguments.of(45, 31395),
+                Arguments.of(48975, 2.784029941164E13)
+        );
+    }
+
+    static Stream<Integer> invalidInputProvider() {
+        return Stream.of(-1, -2, -10, -46893);
+    }
+
+    @ParameterizedTest
+    @MethodSource("validInputProvider")
+    void calculatesSumOfSeriesCorrectly(int input, double sum) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not correctly output the sum of the series.";
+        runWithInput(String.valueOf(input), new Clause[][]{{
+                new StringLiteral("The sum of the series is: "),
+                new DoubleLiteral("seriesSum")
+        }});
+        assertEquals(Double.parseDouble(getItemByName("seriesSum")), sum, 0.0000001, "Your program does not correctly calculate the sum of the series.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidInputProvider")
+    void printsErrorMessageForInvalidInput(int input) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not print an error message for invalid input.";
+        runWithInput(String.valueOf(input), new Clause[]{
+                new StringLiteral("Invalid input!")
+        });
+    }
+}
--- src/loops/simple_programs_with_repetition/medium/q7/SeriesSeries.java
+++ src/loops/simple_programs_with_repetition/medium/q7/SeriesSeries.java
@@ -1,21 +1,30 @@
-package loops.simple_programs_with_repitition.medium.q7;
+package loops.simple_programs_with_repetition.medium.q7;
 
 /*
-Write a Java program to ask the user for a number and 
-calculate the sum of the following series: 1^2 + 2^2 + 3^2 + 4^2 + … n^2
+Write a Java program to ask the user for a number (as an integer) and calculates the sum of the following series:
+1^2 + 2^2 + 3^2 + 4^2 + … n^2. The program should print the sum of the series as a double. If the program receives an input less
+than 0, it should print the error message "Invalid input!".
+
+Sample Output:
+    Enter a number for the upper bound of the series:
+    39
+    The sum of the series is: 20540.0
 */
 
 import java.util.Scanner;
-public class SeriesSeries
-{
-   public static void main( String[] args )
-   {
-      Scanner input = new Scanner( System.in );
-      System.out.println( "Enter a number for the upper bound of the series: " );
-      int n = input.nextInt(); 
-      int sum  = 0; 
-      for(int i = 1; i <= n; i++) 
-         sum += i * i;
-      System.out.println("The sum of the series is " + sum ); 
-   }
+
+public class SeriesSeries {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number for the upper bound of the series: ");
+        int n = input.nextInt();
+        if (n < 0) {
+            System.out.println("Invalid input!");
+        } else {
+            double sum = 0;
+            for (int i = 1; i <= n; i++)
+                sum += i * i;
+            System.out.println("The sum of the series is: " + sum);
+        }
+    }
 }
