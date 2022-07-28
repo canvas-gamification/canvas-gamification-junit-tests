cb18d5febb99b237bc71f4f0987b7a63a625987f
Seth Akins <83678885+Hedgemon4@users.noreply.github.com>
2022-07-27 18:32:51 -0600

    Update src/loops/for_loops/hard/q8/MainTest.java

--- src/loops/for_loops/hard/q8/MainTest.java
+++ src/loops/for_loops/hard/q8/MainTest.java
@@ -42,7 +42,7 @@ public class MainTest extends BaseTest {
   @ParameterizedTest
   @MethodSource("inputProvider")
   public void countsNumberOfCharactersCorrectly(String sentence, int numChar) throws InvalidClauseException {
-    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct number of characters in the input sentence.";
+    TestOption.incorrectStructureErrorMessage = "Your program does correctly count the number of characters in the input sentence.";
     runWithInput(sentence, new Clause[][] { {
         new StringLiteral("Total number of characters in the sentence: "),
         new IntegerLiteral(numChar),
