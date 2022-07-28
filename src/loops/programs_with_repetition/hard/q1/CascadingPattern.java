f8892d089c53548fd250cca1e6fd96c94034597f
Hedgemon4 <sethlinc4@gmail.com>
2022-07-27 20:18:22 -0600

    Finished q1

--- src/loops/programs_with_repetition/hard/q1/CascadingPattern.java
+++ src/loops/programs_with_repetition/hard/q1/CascadingPattern.java
@@ -1,24 +1,41 @@
 package loops.programs_with_repetition.hard.q1;
 /*
-Write a program to print the following pattern using nested loops with n = 5, where n is the starting number and the
-number of lines. Make sure your output matches the sample.
-Sample Output (n = 5):
+Write a program to ask the user for an integer n, and print the following pattern using nested loops, where n is the
+starting number and the number of lines. Your program should also make sure the integer is greater than or equal to 0,
+and print the error message "Invalid input!" if it is not. Make sure your output matches the sample.
+Sample Output:
+    Enter an integer:
+    5
     55555
     54444
     54333
     54322
     54321
+
+    Enter an integer:
+    -1
+    Invalid input!
  */
+
+import java.util.Scanner;
+
 public class CascadingPattern {
     public static void main(String[] args) {
-        int n = 5;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 5; j >= 5 - i; j--)
-                System.out.print(j);
-            for (int k = 1; k < 5 - i; k++)
-                System.out.print(n);
-            System.out.println();
-            n--;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int length = input.nextInt();
+        int n = length;
+        if (n < 0) {
+            System.out.println("Invalid input!");
+        } else {
+            for (int i = 0; i < length; i++) {
+                for (int j = length; j >= length - i; j--)
+                    System.out.print(j);
+                for (int k = 1; k < length - i; k++)
+                    System.out.print(n);
+                System.out.println();
+                n--;
+            }
         }
     }
 }
