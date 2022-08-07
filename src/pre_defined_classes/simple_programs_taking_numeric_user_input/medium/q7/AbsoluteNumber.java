ffd1fe1e1f63dce385e7b0377ec2f9e3fcaa9f3d
Hedgemon4 <sethlinc4@gmail.com>
2022-08-07 09:10:19 -0600

    Fixed JUnit q7

--- src/pre_defined_classes/simple_programs_taking_numeric_user_input/medium/q7/AbsoluteNumber.java
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/medium/q7/AbsoluteNumber.java
@@ -1,13 +1,15 @@
 package pre_defined_classes.simple_programs_taking_numeric_user_input.medium.q7;
 
 /*
-Make a program that asks the user for a score between -10 and 10 (both ends included), and takes the absolute value of it,
-multiplies that by a random number, and displays the product as the user's goal.
+Make a program that asks the user for a score between -10 and 10 (both ends included), and takes the absolute value of it.
+Then, your program should generate a score modifier (a random number between 1 and 10 inclusive), print it out,
+multiply the score by the score modifier, and display the new score to the user.
 
 Sample output:
-Enter your score between -10 and 10 (both ends included):
--5
-Your goal is 20. Keep up the good work!
+    Enter your score between -10 and 10 (both ends included):
+    -4
+    Your score modifier is: 6
+    Your goal is 24. Keep up the good work!
 
  */
 
@@ -15,15 +17,16 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class AbsoluteNumber {
-    public static void main( String[] args )
-    {
-        Scanner in = new Scanner( System.in );
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         Random gen = new Random();
-        System.out.println( "Enter your score between -10 and 10 (both ends included): " );
+        System.out.println("Enter your score between -10 and 10 (both ends included): ");
         int number = in.nextInt();
-        number = Math.abs( number );
-        number = number * (gen.nextInt( 10 )+1);
-        System.out.println( "Your goal is " +  number + ". Keep up the good work!" );
+        number = Math.abs(number);
+        int scoreModifier = gen.nextInt(10) + 1;
+        System.out.println("Your score modifier is: " + scoreModifier);
+        number = number * scoreModifier;
+        System.out.println("Your goal is " + number + ". Keep up the good work!");
     }
 
 }
--- src/pre_defined_classes/simple_programs_taking_numeric_user_input/medium/q7/MainTest.java
+++ src/pre_defined_classes/simple_programs_taking_numeric_user_input/medium/q7/MainTest.java
@@ -1,13 +1,5 @@
 package pre_defined_classes.simple_programs_taking_numeric_user_input.medium.q7;
 
-
-/*
-Sample output:
-Enter your score:
--5
-Your goal is 45. Keep up the good work!
- */
-
 import global.BaseRandomTest;
 import global.exceptions.InvalidClauseException;
 import global.tools.TestOption;
@@ -16,32 +8,40 @@ import global.variables.clauses.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Random;
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseRandomTest {
     // Parsons
-    public Clause[] testSentence(){
+    public Clause[] testSentence() {
         TestOption.isInputTest = true;
         TestOption.defaultInput = "-5";
         return new Clause[]{
                 new StringLiteral("Enter your score between \\-10 and 10 \\(both ends included\\)\\: "),
                 new NewLine(),
+                new StringLiteral("Your score modifier is: "),
+                new RandomInteger(1, 11, "modifier"),
+                new NewLine(),
                 new StringLiteral("Your goal is "),
-                new RandomInteger(0, 101),
+                new IntegerLiteral("score"),
                 new StringLiteral(". Keep up the good work!")
         };
     }
 
-    public void runMain(){ AbsoluteNumber.main(new String[0]); }
+    public void runMain() {
+        AbsoluteNumber.main(new String[0]);
+    }
 
-    public static Stream<Integer> inputProvider(){
-        return Stream.of( 3 , 0 , -5);
+    public static Stream<Integer> inputProvider() {
+        return Stream.of(3, 0, -5, 10, -1);
     }
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    public void correctlyPrintsRandomAbsValue(int input) throws InvalidClauseException{
-        runWithInput(input + " ");
+    public void printsFinalScoreCorrectly(int input) throws InvalidClauseException {
+        runWithInput(String.valueOf(input));
+        int score = Math.abs(input) * Integer.parseInt(getItemByName("modifier"));
+        assertEquals(score, Integer.parseInt(getItemByName("score")), "Your program does not correctly multiply the input score by a random score modifier.");
     }
 }
