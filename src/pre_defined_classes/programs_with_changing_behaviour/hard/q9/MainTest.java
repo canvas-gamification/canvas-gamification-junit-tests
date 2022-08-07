d5972e9eff933ce3ec85d0822c17ced7b8f8808d
Hedgemon4 <sethlinc4@gmail.com>
2022-08-07 13:21:47 -0600

    Finished JUnit for q9

--- src/pre_defined_classes/programs_with_changing_behaviour/hard/q9/MainTest.java
+++ src/pre_defined_classes/programs_with_changing_behaviour/hard/q9/MainTest.java
@@ -2,17 +2,38 @@ package pre_defined_classes.programs_with_changing_behaviour.hard.q9;
 
 import global.BaseRandomTest;
 import global.variables.Clause;
+import global.variables.clauses.IntegerLiteral;
+import global.variables.clauses.NewLine;
 import global.variables.clauses.RandomInteger;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseRandomTest {
-    // Parsons with Distractors
+    // Parsons with distractors
     public Clause[] testSentence() {
         return new Clause[]{
+                new StringLiteral("The first die rolled is a "),
+                new RandomInteger(1, 7, "firstRoll"),
+                new StringLiteral("\\."),
+                new NewLine(),
+                new StringLiteral("The second die rolled is a "),
+                new RandomInteger(1, 7, "secondRoll"),
+                new StringLiteral("\\."),
+                new NewLine(),
                 new StringLiteral("Your new position is: "),
-                new RandomInteger(48, 58+1, "playerPosition")
+                new IntegerLiteral("position")
         };
     }
 
-    public void runMain() {Monopoly.main(new String[0]);}
+    public void runMain() {
+        Monopoly.main(new String[0]);
+    }
+
+    @RepeatedTest(10)
+    void calculatesNewPositionCorrectly() {
+        int position = 46 + Integer.parseInt(getItemByName("firstRoll")) + Integer.parseInt(getItemByName("secondRoll"));
+        assertEquals(position, Integer.parseInt(getItemByName("position")), "Your program does not correctly calculate your new position on the monopoly board based on the dice rolls.");
+    }
 }
--- src/pre_defined_classes/programs_with_changing_behaviour/hard/q9/Monopoly.java
+++ src/pre_defined_classes/programs_with_changing_behaviour/hard/q9/Monopoly.java
@@ -1,16 +1,20 @@
 package pre_defined_classes.programs_with_changing_behaviour.hard.q9;
 
 /*
-Make a Java program that rolls the dice for one round of Monopoly using two dice that has 6 possible values each.
+Make a Java program that rolls the dice for one round of Monopoly using two dice that has 6 possible values each. The
+program should print out both the values of the dice, then add them to your current position on the board, which is
+currently square 46, and print out your new position.
 
 Sample output:
-Your new position is: 49
+    The first die rolled is a 6.
+    The second die rolled is a 4.
+    Your new position is: 56
 
 Distractors:
 Random gen = Random();
 random gen = new Random;
-int firstDice = gen.nextInt( 5 ) +1;
-int secondDice = gen.nextint( 6 ) +1
+int firstDie = gen.nextInt( 5 ) +1;
+int secondDie = gen.nextint( 6 ) +1
 playerPosition = 46;
 System.out.println( "Your new position is: " + firstDice + secondDice );
 
@@ -19,13 +23,14 @@ System.out.println( "Your new position is: " + firstDice + secondDice );
 import java.util.Random;
 
 public class Monopoly {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         Random gen = new Random();
-        int firstDice = gen.nextInt( 6 ) +1;
-        int secondDice = gen.nextInt( 6 ) +1;
+        int firstDie = gen.nextInt(6) + 1;
+        System.out.println("The first die rolled is a " + firstDie + ".");
+        int secondDie = gen.nextInt(6) + 1;
+        System.out.println("The second die rolled is a " + secondDie + ".");
         int playerPosition = 46;
-        playerPosition = playerPosition + firstDice + secondDice;
-        System.out.println( "Your new position is: " + playerPosition );
+        playerPosition = playerPosition + firstDie + secondDie;
+        System.out.println("Your new position is: " + playerPosition);
     }
 }
