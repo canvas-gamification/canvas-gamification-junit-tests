0fa8a0572dd89261fc5c60d9934210a2078b7841
Hedgemon4 <sethlinc4@gmail.com>
2022-08-07 09:39:38 -0600

    Changed question wording

--- src/pre_defined_classes/simple_programs_taking_string_user_input/hard/q9/Origin.java
+++ src/pre_defined_classes/simple_programs_taking_string_user_input/hard/q9/Origin.java
@@ -1,9 +1,8 @@
 package pre_defined_classes.simple_programs_taking_string_user_input.hard.q9;
 
 /*
-People all around the world use last names to identify their origins. Common last names include:"Silva" from Portugal
-and "Johnson" meaning the son of John. Make a program that checks to see if a given name starts
-with "Sil" or ends with "son" and prints true or false to both questions about the name's origin.
+Make a program that asks the user for a sentence, and then searches the sentence to see if it contains the words
+"computer" or "science". The program should print true or false for if it contains these words.
 
 Sample output:
 Please enter your last name:
@@ -29,15 +28,14 @@ isSon = name.contains( "son" );
 import java.util.Scanner;
 
 public class Origin {
-    public static void main( String[] args )
-    {
-        Scanner input = new Scanner( System.in );
-        System.out.println( "Please enter your last name:" );
-        String name = input.nextLine();
-        name = name.toLowerCase();
-        boolean isPortugese = name.startsWith( "sil" );
-        boolean isSon = name.endsWith( "son" );
-        System.out.println( "Is the person from Portugal? " + isPortugese );
-        System.out.println( "Does the person's name contain his/her parent's name? " + isSon );
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a sentence:");
+        String sentence = input.nextLine();
+        sentence = sentence.toLowerCase();
+        boolean containsComputer = sentence.contains("computer");
+        boolean containsScience = sentence.contains("science");
+        System.out.println("Does the sentence contain the word computer? " + containsComputer);
+        System.out.println("Does the sentence contain the word science? " + containsScience);
     }
 }
