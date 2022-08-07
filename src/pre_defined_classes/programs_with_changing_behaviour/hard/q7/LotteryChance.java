51f82e04f0cb0bbb7782dbf174cbe018e65a5e1e
Hedgemon4 <sethlinc4@gmail.com>
2022-08-07 10:57:07 -0600

    Fixed JUnit q7

--- src/pre_defined_classes/programs_with_changing_behaviour/hard/q7/LotteryChance.java
+++ src/pre_defined_classes/programs_with_changing_behaviour/hard/q7/LotteryChance.java
@@ -5,14 +5,17 @@ Lotteries are events held to randomly select a winner, and then rewarding that w
 bought 4 lottery tickets, and now you want to determine your chances of winning the lottery. To do this, you need to
 divide the number of tickets you've bought by the total number of tickets that have been bought by everyone.
 In this problem, we don't know the exact total number of lotteries bought, but we know it is a random number
-between 10000 and 999999 (inclusive of both values). Make a Java program that determines that chance you have
-at winning the lottery and displays that as a percentage.
+between 10,000 and 999,999 (inclusive of both values). Make a Java program that prints the number of tickets you bought,
+generates and prints a random number of lottery participants, nad then determines that chance you have at winning the
+lottery and displays that as a percentage.
 
 Sample output:
-Your chance of winning is: 8.619073578876375E-4
+You bought 4 tickets.
+The number of participants in the lottery is: 410177.
+Your chance of winning is: 9.751887599743525E-4%.
 
 Distractors:
-int totalParticipants = 10000+ gen.nextInt( 999999 );
+int totalParticipants = 10000 + gen.nextInt( 999999 );
 int totalParticipants = gen.nextInt( 999999 );
 double chanceOfWinning = (yourtickets/totalParticipants)*100;
 int chanceOfWinning = yourtickets/totalParticipants;
@@ -25,13 +28,14 @@ System.out.println( "The chance you have of winning the lottery is: " + yourTick
 import java.util.Random;
 
 public class LotteryChance {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         int yourTickets = 4;
+        System.out.println("You bought " + yourTickets + " tickets.");
         Random gen = new Random();
-        int totalParticipants = 10000+ gen.nextInt( 990000 );
-        double chanceOfWinning = (double) yourTickets/totalParticipants;
+        int totalParticipants = 10000 + gen.nextInt(990000);
+        System.out.println("The number of participants in the lottery is: " + totalParticipants + ".");
+        double chanceOfWinning = (double) yourTickets / totalParticipants;
         chanceOfWinning = chanceOfWinning * 100;
-        System.out.println( "Your chance of winning is: " + chanceOfWinning );
+        System.out.println("Your chance of winning is: " + chanceOfWinning + "%.");
     }
 }
--- src/pre_defined_classes/programs_with_changing_behaviour/hard/q7/MainTest.java
+++ src/pre_defined_classes/programs_with_changing_behaviour/hard/q7/MainTest.java
@@ -2,19 +2,36 @@ package pre_defined_classes.programs_with_changing_behaviour.hard.q7;
 
 import global.BaseRandomTest;
 import global.variables.Clause;
-import global.variables.clauses.RandomDouble;
-import global.variables.clauses.StringLiteral;
+import global.variables.clauses.*;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseRandomTest {
-    // Parsons with Distractors
+    // Parsons with distractors
     public Clause[] testSentence() {
         return new Clause[]{
+                new StringLiteral("You bought "),
+                new IntegerLiteral(4, "tickets"),
+                new StringLiteral(" tickets\\."),
+                new NewLine(),
+                new StringLiteral("The number of participants in the lottery is: "),
+                new RandomInteger(10000, 1000000, "participants"),
+                new StringLiteral("\\."),
+                new NewLine(),
                 new StringLiteral("Your chance of winning is: "),
-                new RandomDouble(0, 100+1, "chanceOfWinning")
+                new DoubleLiteral("winChance"),
+                new StringLiteral("%.")
         };
     }
 
     public void runMain() {
         LotteryChance.main(new String[0]);
+    }
+
+    @RepeatedTest(10)
+    void calculatesWinChanceCorrectly() {
+        double chance = 4.0 / Double.parseDouble(getItemByName("participants")) * 100.00;
+        assertEquals(chance, Double.parseDouble(getItemByName("winChance")), 0.0000000000001, "Your program does not correctly calculate the chance of you winning the lottery.");
     }
 }
