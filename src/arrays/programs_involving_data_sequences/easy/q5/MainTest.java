bb1af07daec456fe9e159e472d583d0675c8b589
Hedgemon4 <sethlinc4@gmail.com>
2022-08-09 13:54:17 -0600

    Updated question text and formatted code

--- src/arrays/programs_involving_data_sequences/easy/q5/MainTest.java
+++ src/arrays/programs_involving_data_sequences/easy/q5/MainTest.java
@@ -15,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MainTest extends BaseTest {
     // Parsons
     private static final String output = "13 29 66 9 1 4 2 ";
+
     public Clause[] testSentence() {
         return new Clause[]{
                 new StringLiteral(output)
@@ -25,20 +26,20 @@ public class MainTest extends BaseTest {
         RevItUp.main(new String[0]);
     }
 
-    static Stream<Arguments> inputProvider(){
-        return Stream.of(Arguments.of(new int[]{11, 37, 84, -29, 14, 4, 9856}, "9856 4 14 -29 84 37 11 "),
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(Arguments.of(new int[]{11, 37, 84, -29, 14, 4, 9856}, "9856 4 14 -29 84 37 11"),
                 Arguments.of(new int[]{}, ""),
-                Arguments.of(new int[]{-47}, "-47 "),
-                Arguments.of(new int[]{11, -14, -36}, "-36 -14 11 "),
+                Arguments.of(new int[]{-47}, "-47"),
+                Arguments.of(new int[]{11, -14, -36}, "-36 -14 11"),
                 Arguments.of(new int[]{-2342, 26534, 462, 58, 4, 0, 0, 0, 235, 259, 4671, 2, 12},
                         "12 2 4671 259 235 0 0 0 4 58 462 26534 -2342"));
     }
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void correctReverseMethod(int[] input, String result){
+    void correctReverseMethod(int[] input, String result) {
         MethodUtil.invokeIfMethodExists(RevItUp.class, "reverse", new Object[]{input}, int[].class);
         String output = MethodUtil.getMethodOutput();
-        assertEquals(result, output, "Your method does not reverse the array correctly.");
+        assertEquals(result, output, "Your method does not reverse and print the array correctly.");
     }
 }
--- src/arrays/programs_involving_data_sequences/easy/q5/RevItUp.java
+++ src/arrays/programs_involving_data_sequences/easy/q5/RevItUp.java
@@ -1,5 +1,18 @@
 package arrays.programs_involving_data_sequences.easy.q5;
 
+/*
+Make a program that calls a method named reverse, which reverses an integer array without creating a new array. Assume
+the array has an odd number of elements, so there is always a center element. Then, the method can swap the elements
+left of the center element with those right of it. In particular, the method should swap the outermost elements and then
+move inwards toward the center (e.g., swap the first element with the last element, then the second element with the
+second to last element, etc.).
+
+For example, given [2, 4, 1, 9, 66, 29, 13], the method does the first swap to get [13, 4, 1, 9, 66, 29, 2], then it
+does the second swap to get [13,29,1,9,66,4,2], and eventually get to the final reversed array with
+[13, 29, 66, 9, 1, 4, 2]. The method should then print the array.
+
+ */
+
 public class RevItUp {
     public static void main(String[] args) {
         int[] arr = {2, 4, 1, 9, 66, 29, 13};
