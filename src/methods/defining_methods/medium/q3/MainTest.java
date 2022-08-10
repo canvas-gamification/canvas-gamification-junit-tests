87013a978862deb1742dafb15b64177d4c84bb48
Samantha H <hodge1214@gmail.com>
2022-08-09 23:26:51 -0400

    wrote out tests

--- src/methods/defining_methods/medium/q3/MainTest.java
+++ src/methods/defining_methods/medium/q3/MainTest.java
@@ -1,11 +1,20 @@
 package methods.defining_methods.medium.q3;
 
 import global.BaseTest;
+import global.tools.CustomAssertions;
 import global.tools.TestOption;
+import global.utils.MethodUtil;
 import global.variables.Clause;
 import global.variables.clauses.IntegerLiteral;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
     // Parsons
@@ -21,4 +30,26 @@ public class MainTest extends BaseTest {
     }
 
     public void runMain(){LengthOfTheString.main(new String[0]);}
+
+    static Stream<Arguments> stringLenCalcInputProvider(){
+        return Stream.of(Arguments.of("help", 4), Arguments.of("now with spaces", 15), Arguments.of("CAPITAL IDEA", 12), Arguments.of("s     p     a     c    e", 24), Arguments.of("~hi!", 4), Arguments.of("!-~-!", 5), Arguments.of("", 0));
+    }
+
+    static Stream<Arguments> mainMethodInputProvider(){
+        return Stream.of(Arguments.of("seventy bottles of student's tears on the wall", 46), Arguments.of(System.lineSeparator(), 0), Arguments.of("aa", 2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringLenCalcInputProvider")
+    void correctStringLenCalcMethod(String in, int length) throws Throwable {
+        Object output = MethodUtil.invokeIfMethodExists(LengthOfTheString.class, "stringLenCalc", new Object[]{in}, String.class);
+        CustomAssertions._assertEquals(length, output, "Your stringLenCalc method does not correctly count the length of the string.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("mainMethodInputProvider")
+    void printsOutputCorrectly(String in, int length){
+        runWithInput(in);
+        assertEquals(length, Integer.parseInt(getItemByName("length")), "Your program does not display the correct sentence length.");
+    }
 }
