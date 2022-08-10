7d3b342f713249564cc5ed5aa2b547d94394af4a
Samantha H <hodge1214@gmail.com>
2022-08-09 23:54:27 -0400

    wrote method test but something is going wrong

--- src/methods/defining_methods/medium/q4/MainTest.java
+++ src/methods/defining_methods/medium/q4/MainTest.java
@@ -1,17 +1,24 @@
 package methods.defining_methods.medium.q4;
 
 import global.BaseTest;
+import global.tools.CustomAssertions;
 import global.tools.TestOption;
+import global.utils.MethodUtil;
 import global.variables.Clause;
 import global.variables.clauses.DoubleLiteral;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
     // Parsons
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
-        TestOption.defaultInput = "2022 1998";
+        TestOption.defaultInput = "2.9 8";
         return new Clause[]{
                 new StringLiteral("Please enter the height of the cylinder: "),
                 new NewLine(),
@@ -23,4 +30,15 @@ public class MainTest extends BaseTest {
     }
 
     public void runMain(){CylinderTime.main(new String[0]);}
+
+    static Stream<Arguments> volumeCylinderInputProvider(){
+        return Stream.of(Arguments.of(3, 3, 84.82300164692441), Arguments.of(6.8, 2, 290.5344886039841), Arguments.of(2, 4.6, 57.805304826052186), Arguments.of(4.1, 10.9, 575.630880324604), Arguments.of(0.4, 0.7, 0.3518583772020568), Arguments.of(260, 841, 1.7860456890482548E8));
+    }
+
+    @ParameterizedTest
+    @MethodSource("volumeCylinderInputProvider")
+    void correctVolumeCylinderInputProvider(double h, double r, double vol) throws Throwable {
+        Object output = MethodUtil.invokeIfMethodExists(CylinderTime.class, "volumeCylinder", new Object[]{h, r}, double.class, double.class);
+        CustomAssertions._assertEquals(vol, output, 0.00001, "Your volumeCylinder method does not correctly calculate the volume.");
+    }
 }
