e1982e2799c707f76e75c7fb08b0e65c6b98bc89
Samantha H <hodge1214@gmail.com>
2022-08-11 17:54:49 -0400

    wrote tests

--- src/methods/defining_methods/medium/q7/MainTest.java
+++ src/methods/defining_methods/medium/q7/MainTest.java
@@ -1,4 +1,57 @@
 package methods.defining_methods.medium.q7;
 
-public class MainTest {
+import global.BaseTest;
+import global.tools.CustomAssertions;
+import global.tools.TestOption;
+import global.utils.MethodUtil;
+import global.variables.Clause;
+import global.variables.clauses.DoubleLiteral;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest extends BaseTest {
+    // Parsons
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "12 14";
+        return new Clause[]{
+                new StringLiteral("Please enter the size of the first Pizza: "),
+                new NewLine(),
+                new StringLiteral("Please enter the size of the second Pizza: "),
+                new NewLine(),
+                new StringLiteral("The pizza with the greatest area is the pizza with length: "),
+                new DoubleLiteral("greatest")
+        };
+    }
+
+    public void runMain(){PizzaPizza.main(new String[0]);}
+
+    static Stream<Arguments> greaterSizeInputProvider(){
+        return Stream.of(Arguments.of(1, 1, 1.0), Arguments.of(23, 22.9, 23.0), Arguments.of(4567, 7654, 7654.0), Arguments.of(61.8, 4488765.3, 4488765.3), Arguments.of(-45, 3, -1));
+    }
+
+    static Stream<Arguments> mainMethodInputProvider(){
+        return Stream.of(Arguments.of("34 56", 56.0), Arguments.of("36.2 54.1", 54.1), Arguments.of("-4 2", -1.0));
+    }
+
+    @ParameterizedTest
+    @MethodSource("greaterSizeInputProvider")
+    void correctGreaterSizeMethod(double first, double second, double greatest) throws Throwable {
+        Object output = MethodUtil.invokeIfMethodExists(PizzaPizza.class, "greaterSize", new Object[]{first, second}, double.class, double.class);
+        CustomAssertions._assertEquals(greatest, output, 0.0001, "Your greaterSize method does not identify the pizza with the greatest area.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("mainMethodInputProvider")
+    void printsOutputCorrectly(String in, double greatest){
+        runWithInput(in);
+        assertEquals(greatest, Double.parseDouble(getItemByName("greatest")), 0.0001, "Your program does not print out the correct pizza.");
+    }
 }
--- src/methods/defining_methods/medium/q7/PizzaPizza.java
+++ src/methods/defining_methods/medium/q7/PizzaPizza.java
@@ -1,25 +1,41 @@
 package methods.defining_methods.medium.q7;
 
+/*
+When ordering a Pizza, you always want the greatest value for your money spent. Write a program that asks users for
+two sizes of Pizza (e.g. the user can input a 12 inch. Pizza as 12, and a 14 inch pizza as 14), and then write a method
+that accepts both input values, and returns the length with the greatest area. If either size is less than 0, return -1.
+Sample output:
+Please enter the size of the first Pizza:
+5
+Please enter the size of the second Pizza:
+2
+The pizza with the greatest area is the pizza with length: 5.0
+ */
+
 import java.util.Scanner;
 public class PizzaPizza
 {
     public static void main( String[] args )
     {
         Scanner input = new Scanner (System.in);
-        System.out.println("Please enter the size of the first Pizza");
+        System.out.println("Please enter the size of the first Pizza: ");
         double firstPizza = input.nextDouble();
-        System.out.println("Please enter the size of the second Pizza");
+        System.out.println("Please enter the size of the second Pizza: ");
         double secondPizza = input.nextDouble();
         System.out.println("The pizza with the greatest area is the pizza with length: " + greaterSize(firstPizza, secondPizza));
     }
     public static double greaterSize(double a, double b)
     {
-        double firstArea = (a/2)*(a/2)*Math.PI;
-        double secondArea = (b/2)*(b/2)*Math.PI;
-        if (firstArea>secondArea)
-            return a;
-        else
-            return b;
+        if(a < 0 || b < 0)
+            return -1;
+        else {
+            double firstArea = (a / 2) * (a / 2) * Math.PI;
+            double secondArea = (b / 2) * (b / 2) * Math.PI;
+            if (firstArea > secondArea)
+                return a;
+            else
+                return b;
+        }
     }
 }
 
