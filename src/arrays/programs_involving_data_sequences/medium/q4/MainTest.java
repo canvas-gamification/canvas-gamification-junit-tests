b804b0ddc40bff671139c0511babf18ef6144034
Hedgemon4 <sethlinc4@gmail.com>
2022-08-11 12:05:07 -0600

    Fixed q4

--- src/arrays/programs_involving_data_sequences/medium/q4/MainTest.java
+++ src/arrays/programs_involving_data_sequences/medium/q4/MainTest.java
@@ -2,9 +2,11 @@ package arrays.programs_involving_data_sequences.medium.q4;
 
 import global.BaseTest;
 import global.exceptions.InvalidClauseException;
+import global.tools.CustomAssertions;
+import global.tools.TestOption;
+import global.utils.ArrayUtil;
 import global.utils.MethodUtil;
 import global.variables.Clause;
-import global.variables.clauses.IntegerLiteral;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 import org.junit.jupiter.api.Test;
@@ -14,11 +16,10 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
 public class MainTest extends BaseTest {
     // Parsons with distractors
-    private static final int[] array = new int[]{98, 3, 5, 7, 9};
+    private static final int[] outputArray = new int[]{98, 3, 5, 7, 9};
+
     public Clause[] testSentence() {
         return new Clause[]{
                 new StringLiteral("The array is now: "),
@@ -30,8 +31,8 @@ public class MainTest extends BaseTest {
         Replacements.main(new String[0]);
     }
 
-    static Stream<Arguments> methodInputProvider(){
-        return Stream.of(Arguments.of(new int[]{1, 3, 5, 7, 9}, 98, new int[]{98, 3, 5, 7, 9}),
+    static Stream<Arguments> methodInputProvider() {
+        return Stream.of(Arguments.of(new int[]{4, 11, 112, 452, 2, 245}, 39, new int[]{4, 11, 112, 452, 39, 245}),
                 Arguments.of(new int[]{}, 11, new int[]{}),
                 Arguments.of(new int[]{9563}, -39, new int[]{-39}),
                 Arguments.of(new int[]{11, -34, 5, 0, -123, 245, 8876, 2, 1, 5, 4, -457, 10}, -11,
@@ -41,24 +42,16 @@ public class MainTest extends BaseTest {
     @ParameterizedTest
     @MethodSource("methodInputProvider")
     void correctAddThisOneMethod(int[] array, int number, int[] replaced) throws Throwable {
-        int[] result = (int[]) MethodUtil.invokeIfMethodExists(Replacements.class, "addThisOne",
+        Object result = MethodUtil.invokeIfMethodExists(Replacements.class, "addThisOne",
                 new Object[]{array, number}, int[].class, int.class);
-        assertArrayEquals(replaced, result, "Your method does not insert the number at the correct index.");
+        CustomAssertions._assertArrayEquals(replaced, result, "Your addThisOne method does not insert the number at the correct index.");
     }
 
     @Test
     void printsCorrectOutput() throws InvalidClauseException {
-        runWithInput("", clauseBuilder(array));
-    }
-
-    private static Clause[][] clauseBuilder(int[] array){
-        Clause[][] output = new Clause[1][array.length * 2];
-        int i = 0;
-        for(int item : array){
-            output[0][i] = new IntegerLiteral(array[i/2]);
-            output[0][i + 1] = new StringLiteral(" ");
-            i += 2;
-        }
-        return output;
+        TestOption.incorrectStructureErrorMessage = "Your program does not correctly print the array with the replaced element.";
+        runWithInput("", new Clause[]{
+                new StringLiteral(ArrayUtil.intArrayToInput(outputArray))
+        });
     }
 }
--- src/arrays/programs_involving_data_sequences/medium/q4/Replacements.java
+++ src/arrays/programs_involving_data_sequences/medium/q4/Replacements.java
@@ -1,10 +1,14 @@
 package arrays.programs_involving_data_sequences.medium.q4;
+
 /*
-Make a program that calls a method that takes an integer array and a number as input. The idea is to replace one of the
-elements in the array with this number. To find out which element gets replaced, the method takes the sum of all the
-values in the array, and then takes the modulus of the sum by the array size (index = sum % size). The method should
-then return the array, which is printed out in the main method. If an empty array is passed to the method, the method
-should return the empty array.
+Write a java program with a method named add this one, which takes an array and an integer and replaces one of the
+elements in the array with the integer. The method should compute an index by summing the values in the array and taking
+the modulus of the sum by the array length (sum % length). Then, it replaces the item at this index with the integer and
+returns the array. Print the array with the new element in the main method. If the method receives an empty array,
+return the empty array.
+
+Sample Output:
+    The array is now: 98 3 5 7 9
 
 Distractors:
     int size = arr.length - 1;
@@ -15,22 +19,23 @@ Distractors:
     {
     }
     return num;
-    int n = addThisOne(arr, 98);
-    System.out.print("The item at index " + n + "was replaced.")
+    int replacement = addThisOne(arr, 98);
+    System.out.print(arr[i] + " ");
  */
+
 public class Replacements {
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 7, 9};
         int[] replacement = addThisOne(arr, 98);
         System.out.print("The array is now: ");
-        for(int i = 0; i < replacement.length; i++){
+        for (int i = 0; i < replacement.length; i++) {
             System.out.print(replacement[i] + " ");
         }
     }
 
     public static int[] addThisOne(int[] arr, int num) {
         int size = arr.length;
-        if(size == 0)
+        if (size == 0)
             return arr;
         int sum = 0;
         for (int i = 0; i < size; i++)
