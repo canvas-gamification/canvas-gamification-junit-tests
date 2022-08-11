d39758b4272dc3fa08214315b532ed0bc2700b54
Samantha H <hodge1214@gmail.com>
2022-08-11 01:28:21 -0400

    wrote out tests

--- src/methods/defining_methods/medium/q5/MainTest.java
+++ src/methods/defining_methods/medium/q5/MainTest.java
@@ -1,11 +1,19 @@
 package methods.defining_methods.medium.q5;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.CustomAssertions;
 import global.tools.TestOption;
+import global.utils.MethodUtil;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
     // Parsons
@@ -21,4 +29,23 @@ public class MainTest extends BaseTest {
     }
 
     public void runMain(){InverseMethod.main(new String[0]);}
+
+    static Stream<Arguments> inputProvider(){
+        return Stream.of(Arguments.of(true, "false"), Arguments.of(false, "true"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void correctInverseCalcMethod(boolean b, String s) throws Throwable {
+        Object output = MethodUtil.invokeIfMethodExists(InverseMethod.class, "inverseCalc", new Object[]{b}, boolean.class);
+        CustomAssertions._assertEquals(!b, output, "Your inverseCalc method does not return the opposite boolean.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void printsOutputCorrectly(boolean b, String s) throws InvalidClauseException {
+        runWithInput(String.valueOf(b), new Clause[]{
+                new StringLiteral(s)
+        });
+    }
 }
