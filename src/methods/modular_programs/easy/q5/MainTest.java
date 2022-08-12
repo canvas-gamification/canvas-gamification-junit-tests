f1a27573dc3aab54c1d6b60b1fb90eaf3e7efac8
Samantha H <hodge1214@gmail.com>
2022-08-12 14:51:29 -0400

    wrote tests

--- src/methods/modular_programs/easy/q5/MainTest.java
+++ src/methods/modular_programs/easy/q5/MainTest.java
@@ -1,11 +1,20 @@
 package methods.modular_programs.easy.q5;
 
 import global.BaseTest;
+import global.tools.CustomAssertions;
 import global.tools.TestOption;
+import global.utils.MethodUtil;
 import global.variables.Clause;
 import global.variables.clauses.DoubleLiteral;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
     // Parsons
@@ -16,9 +25,31 @@ public class MainTest extends BaseTest {
                 new StringLiteral("Please enter your height in cm: "),
                 new NewLine(),
                 new StringLiteral("Your height in feet is: "),
-                new DoubleLiteral("height")
+                new DoubleLiteral("feet")
         };
     }
 
     public void runMain(){HeightInFeet.main(new String[0]);}
+
+    static Stream<Arguments> conversionHeightInputProvider(){
+        return Stream.of(Arguments.of(161.5, 5.2972), Arguments.of(88.392, 2.9), Arguments.of(182.88, 6.0), Arguments.of(0.1, 0.00328084), Arguments.of(-0.1, -1.0), Arguments.of(-45, -1.0), Arguments.of(3629, 119.0617));
+    }
+
+    static Stream<Arguments> mainMethodInputProvider(){
+        return Stream.of(Arguments.of("1523", 49.96719), Arguments.of("23", 0.754593), Arguments.of("174.8264", 5.7357742782), Arguments.of("-345246.13", -1.0));
+    }
+
+    @ParameterizedTest
+    @MethodSource("conversionHeightInputProvider")
+    void correctConversionHeightMethod(double in, double feet) throws Throwable {
+        Object output = MethodUtil.invokeIfMethodExists(HeightInFeet.class, "conversionHeight", new Object[]{in}, double.class);
+        CustomAssertions._assertEquals(feet, output, 0.1, "Your conversionHeight method does not correctly convert from cm to feet.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("mainMethodInputProvider")
+    void printsOutputCorrectly(String in, double feet){
+        runWithInput(in);
+        assertEquals(feet, Double.parseDouble(getItemByName("feet")), 0.05, "Your program does not output the correct height in feet.");
+    }
 }
