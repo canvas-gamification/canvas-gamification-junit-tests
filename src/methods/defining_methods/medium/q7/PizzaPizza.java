e8e4f591ab1d37c5cebd1f5b7f196991264b5777
Hedgemon4 <sethlinc4@gmail.com>
2022-08-20 11:18:46 -0600

    Formatted code, changed question text, and added test case

--- src/methods/defining_methods/medium/q7/PizzaPizza.java
+++ src/methods/defining_methods/medium/q7/PizzaPizza.java
@@ -1,32 +1,32 @@
 package methods.defining_methods.medium.q7;
 
 /*
-When ordering a Pizza, you always want the greatest value for your money spent. Write a program that asks users for
-two sizes of Pizza (e.g. the user can input a 12 inch. Pizza as 12, and a 14 inch pizza as 14), and then write a method
-that accepts both input values, and returns the length with the greatest area. If either size is less than 0, return -1.
+When ordering a Pizza, you always want the best value for your money spent. Write a program that asks users for the
+diameter of two pizzas and write a method called greaterArea that accepts both input values and returns the diameter of
+the pizza with the largest area. If either diameter is less than 0, return -1.
+
 Sample output:
-Please enter the size of the first Pizza:
-5
-Please enter the size of the second Pizza:
-2
-The pizza with the greatest area is the pizza with length: 5.0
+    Please enter the diameter of the first pizza:
+    5
+    Please enter the diameter of the second pizza:
+    39.9
+    The pizza with the greatest area is the pizza with diameter: 39.9
  */
 
 import java.util.Scanner;
-public class PizzaPizza
-{
-    public static void main( String[] args )
-    {
-        Scanner input = new Scanner (System.in);
-        System.out.println("Please enter the size of the first Pizza: ");
+
+public class PizzaPizza {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the diameter of the first pizza: ");
         double firstPizza = input.nextDouble();
-        System.out.println("Please enter the size of the second Pizza: ");
+        System.out.println("Please enter the diameter of the second pizza: ");
         double secondPizza = input.nextDouble();
-        System.out.println("The pizza with the greatest area is the pizza with length: " + greaterSize(firstPizza, secondPizza));
+        System.out.println("The pizza with the greatest area is the pizza with diameter: " + greaterArea(firstPizza, secondPizza));
     }
-    public static double greaterSize(double a, double b)
-    {
-        if(a < 0 || b < 0)
+
+    public static double greaterArea(double a, double b) {
+        if (a < 0 || b < 0)
             return -1;
         else {
             double firstArea = (a / 2) * (a / 2) * Math.PI;
@@ -38,4 +38,3 @@ public class PizzaPizza
         }
     }
 }
-
