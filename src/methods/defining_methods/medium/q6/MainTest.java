55fc0dc348c9c5f16d722f5d1fe360bc847e81d1
Hedgemon4 <sethlinc4@gmail.com>
2022-08-20 10:23:49 -0600

    Formatted code and updated question text

--- src/methods/defining_methods/medium/q6/MainTest.java
+++ src/methods/defining_methods/medium/q6/MainTest.java
@@ -29,13 +29,22 @@ public class MainTest extends BaseTest {
         };
     }
 
-    public void runMain(){VolToLength.main(new String[0]);}
-
-    static Stream<Arguments> lengthCubeInputProvider(){
-        return Stream.of(Arguments.of(64, 4.0), Arguments.of(45.9, 3.5804495757820107), Arguments.of(1234567, 107.27657218553581), Arguments.of(-27, -1.0), Arguments.of(1, 1.0), Arguments.of(0.75, 0.9085602964160698));
+    public void runMain() {
+        VolToLength.main(new String[0]);
     }
 
-    static Stream<Arguments> mainMethodInputProvider(){
+    static Stream<Arguments> lengthCubeInputProvider() {
+        return Stream.of(
+                Arguments.of(64, 4.0),
+                Arguments.of(45.9, 3.5804495757820107),
+                Arguments.of(1234567, 107.27657218553581),
+                Arguments.of(-27, -1.0),
+                Arguments.of(1, 1.0),
+                Arguments.of(0.75, 0.9085602964160698),
+                Arguments.of(-0.0001, -1.0));
+    }
+
+    static Stream<Arguments> mainMethodInputProvider() {
         return Stream.of(Arguments.of("25", 2.924017738212866), Arguments.of("2.5", 1.3572088082974532), Arguments.of("-34", -1.0));
     }
 
@@ -43,12 +52,12 @@ public class MainTest extends BaseTest {
     @MethodSource("lengthCubeInputProvider")
     void correctLengthCubeMethod(double in, double side) throws Throwable {
         Object output = MethodUtil.invokeIfMethodExists(VolToLength.class, "lengthCube", new Object[]{in}, double.class);
-        CustomAssertions._assertEquals(side, output, 0.00001, "Your lengthCube method does not correctly calculate the side length.");
+        CustomAssertions._assertEquals(side, output, 0.00001, "Your lengthCube method does not correctly calculate the side length of a cube based on the volume.");
     }
 
     @ParameterizedTest
     @MethodSource("mainMethodInputProvider")
-    void printsOutputCorrectly(String in, double side){
+    void printsOutputCorrectly(String in, double side) {
         runWithInput(in);
         assertEquals(side, Double.parseDouble(getItemByName("side")), 0.00001, "Your program does not print the correct side length.");
     }
--- src/methods/defining_methods/medium/q6/VolToLength.java
+++ src/methods/defining_methods/medium/q6/VolToLength.java
@@ -2,29 +2,28 @@ package methods.defining_methods.medium.q6;
 
 /*
 Create a Java Program that asks the user for the volume of a cube. Then, using a method called lengthCube, calculate
-and return the length of a side of the cube. If the entered volume is negative, return -1.
+and return the length of a side of the cube. If the input volume is negative, return -1.
+
 Sample output:
-Please enter the volume of a cube:
-27
-The side length of the given cube is: 3.0
+    Please enter the volume of a cube:
+    27
+    The side length of the given cube is: 3.0
  */
 
 import java.util.Scanner;
-public class VolToLength
-{
-    public static void main( String[] args )
-    {
-        Scanner input = new Scanner (System.in);
+
+public class VolToLength {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Please enter the volume of a cube: ");
-        double volumeCube= input.nextDouble();
+        double volumeCube = input.nextDouble();
         System.out.println("The side length of the given cube is: " + lengthCube(volumeCube));
     }
-    public static double lengthCube(double volume)
-    {
-        if(volume < 0)
+
+    public static double lengthCube(double volume) {
+        if (volume < 0)
             return -1;
         else
             return Math.cbrt(volume);
     }
 }
-
