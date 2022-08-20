a49f4903a6d272df79e31938070cd0b3a4f52488
Hedgemon4 <sethlinc4@gmail.com>
2022-08-20 09:43:05 -0600

    Formatted code and updated question text

--- src/methods/using_methods/hard/q8/Celverter.java
+++ src/methods/using_methods/hard/q8/Celverter.java
@@ -1,27 +1,27 @@
 package methods.using_methods.hard.q8;
 
 /*
-Write a Java Program called Celverter that asks users for the current temperature in Celsius,
-then write a method called tempCalc that accepts that value as an input parameter and returns the temperature in Fahrenheit.
-The program should print the converted temperature. 
+Write a Java Program called Celverter that asks users for the current temperature in Celsius, then write a method called
+tempCalc that accepts that value as an input parameter and returns the temperature in Fahrenheit. The program should
+print the converted temperature.
+
 Sample output:
-What is the current temperature in Celsius?
-19
-The current temperature in Fahrenheit is 66.2
+    What is the current temperature in Celsius?
+    19
+    The current temperature in Fahrenheit is 66.2
  */
 
 import java.util.Scanner;
-public class Celverter
-{
-    public static void main( String[] args )
-    {
+
+public class Celverter {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("What is the current temperature in Celsius?");
-        double currentTemp= input.nextDouble();
+        double currentTemp = input.nextDouble();
         System.out.println("The current temperature in Fahrenheit is " + tempCalc(currentTemp));
     }
-    public static double tempCalc(double a)
-    {
-        return a*(9.0/5)+32;
+
+    public static double tempCalc(double a) {
+        return a * (9.0 / 5) + 32;
     }
 }
--- src/methods/using_methods/hard/q8/MainTest.java
+++ src/methods/using_methods/hard/q8/MainTest.java
@@ -29,13 +29,15 @@ public class MainTest extends BaseTest {
         };
     }
 
-    public void runMain(){Celverter.main(new String[0]);}
+    public void runMain() {
+        Celverter.main(new String[0]);
+    }
 
-    static Stream<Arguments> tempCalcInputProvider(){
+    static Stream<Arguments> tempCalcInputProvider() {
         return Stream.of(Arguments.of(0, 32.0), Arguments.of(20.7, 69.25999999999999), Arguments.of(45.3, 113.53999999999999), Arguments.of(2.2, 35.96), Arguments.of(-11, 12.2), Arguments.of(-24.9, -12.82), Arguments.of(-40, -40.0));
     }
 
-    static Stream<Arguments> mainMethodInputProvider(){
+    static Stream<Arguments> mainMethodInputProvider() {
         return Stream.of(Arguments.of("5", 41.0), Arguments.of("53", 127.4), Arguments.of("21.3", 70.34), Arguments.of("-31", -23.800000000000004));
     }
 
@@ -48,7 +50,7 @@ public class MainTest extends BaseTest {
 
     @ParameterizedTest
     @MethodSource("mainMethodInputProvider")
-    void printsOutputCorrectly(String in, double ans){
+    void printsOutputCorrectly(String in, double ans) {
         runWithInput(in);
         assertEquals(ans, Double.parseDouble(getItemByName("converted")), 0.01, "Your program does not print the correct message based on the input temperature.");
     }
