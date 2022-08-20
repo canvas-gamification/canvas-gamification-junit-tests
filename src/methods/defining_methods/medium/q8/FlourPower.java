e27dc40d05ffef8a5999d2f412af351e0c3ad6eb
Hedgemon4 <sethlinc4@gmail.com>
2022-08-20 10:46:44 -0600

    Formatted code and updated question text

--- src/methods/defining_methods/medium/q8/FlourPower.java
+++ src/methods/defining_methods/medium/q8/FlourPower.java
@@ -1,36 +1,35 @@
 package methods.defining_methods.medium.q8;
 
 /*
-Write a Java Program that determines the cost per kg for flour. The program will ask the user for the weight of the flour,
-and then the cost of the flour. Then, using a method called costPerCalc, return the cost per kg of the flour.
-If either weight or cost is less than 0, return -1.
+Write a program that determines the cost per kg of flour. The program will ask the user for the weight (kilograms) and
+the price (dollars) of the flour. Then, using a method called costPerCalc, calculate and return the cost per kg of
+flour. If either weight or cost is less than 0, return -1.
+
 Sample output:
-Please enter the weight of the flour:
-4.4
-Please enter the cost:
-8
-The cost per kg of the flour is: 1.8181818181818181
+    Please enter the weight of the flour:
+    4.4
+    Please enter the cost:
+    8
+    The cost per kg of the flour is: 1.8181818181818181
 
  */
 
 import java.util.Scanner;
-public class FlourPower
-{
-    public static void main( String[] args )
-    {
-        Scanner input = new Scanner (System.in);
+
+public class FlourPower {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Please enter the weight of the flour: ");
         double weightFlour = input.nextDouble();
         System.out.println("Please enter the cost: ");
         double costFlour = input.nextDouble();
         System.out.println("The cost per kg of the flour is: " + costPerCalc(weightFlour, costFlour));
     }
-    public static double costPerCalc(double weight, double cost)
-    {
-        if(weight < 0||cost < 0)
+
+    public static double costPerCalc(double weight, double cost) {
+        if (weight < 0 || cost < 0)
             return -1;
         else
-            return cost/weight;
+            return cost / weight;
     }
 }
-
--- src/methods/defining_methods/medium/q8/MainTest.java
+++ src/methods/defining_methods/medium/q8/MainTest.java
@@ -31,13 +31,24 @@ public class MainTest extends BaseTest {
         };
     }
 
-    public void runMain(){FlourPower.main(new String[0]);}
-
-    static Stream<Arguments> costPerCalcInputProvider(){
-        return Stream.of(Arguments.of(5, 5, 1.0), Arguments.of(2.5, 8, 3.2), Arguments.of(3, 15.2, 5.066666666666666), Arguments.of(6.2, 16.3, 2.629032258064516), Arguments.of(-5, 13, -1.0), Arguments.of(1234567, 87, 7.047005144313756E-5), Arguments.of(23, 456789, 19860.391304347828));
+    public void runMain() {
+        FlourPower.main(new String[0]);
     }
 
-    static Stream<Arguments> mainMethodInputProvider(){
+    static Stream<Arguments> costPerCalcInputProvider() {
+        return Stream.of(
+                Arguments.of(5, 5, 1.0),
+                Arguments.of(2.5, 8, 3.2),
+                Arguments.of(3, 15.2, 5.066666666666666),
+                Arguments.of(6.2, 16.3, 2.629032258064516),
+                Arguments.of(-5, 13, -1.0),
+                Arguments.of(1234567, 87, 7.047005144313756E-5),
+                Arguments.of(23, 456789, 19860.391304347828),
+                Arguments.of(4.5, -11.0, -1.0)
+        );
+    }
+
+    static Stream<Arguments> mainMethodInputProvider() {
         return Stream.of(Arguments.of("4.5 12", 2.6666666666666665), Arguments.of("3951 54", 0.01366742596810934), Arguments.of("67 -1234", -1.0));
     }
 
@@ -45,12 +56,12 @@ public class MainTest extends BaseTest {
     @MethodSource("costPerCalcInputProvider")
     void correctCostPerCalcMethod(double weight, double cost, double price) throws Throwable {
         Object output = MethodUtil.invokeIfMethodExists(FlourPower.class, "costPerCalc", new Object[]{weight, cost}, double.class, double.class);
-        CustomAssertions._assertEquals(price, output, 0.0001, "Your costPerCalc method does not calculate the correct cost per kg.");
+        CustomAssertions._assertEquals(price, output, 0.000001, "Your costPerCalc method does not calculate the correct price per kg.");
     }
 
     @ParameterizedTest
     @MethodSource("mainMethodInputProvider")
-    void printsOutputCorrectly(String in, double price){
+    void printsOutputCorrectly(String in, double price) {
         runWithInput(in);
         assertEquals(price, Double.parseDouble(getItemByName("price")), 0.00001, "Your program does not print the correct cost per kg.");
     }
