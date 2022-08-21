52d23e33ae8c2223ecf7ab6b7c87b927350d0786
Hedgemon4 <sethlinc4@gmail.com>
2022-08-21 16:33:24 -0600

    Updated question text, formatted code, added test cases, and updated tests to use new custom assertions

--- src/arrays/program_involving_data_sequences/hard/q4/MainTest.java
+++ src/arrays/program_involving_data_sequences/hard/q4/MainTest.java
@@ -2,39 +2,30 @@ package arrays.program_involving_data_sequences.hard.q4;
 
 import global.BaseTest;
 import global.exceptions.InvalidClauseException;
+import global.tools.CustomAssertions;
 import global.tools.TestOption;
+import global.utils.ArrayUtil;
 import global.utils.MethodUtil;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 import global.variables.wrappers.Optional;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Random;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
     // Java
-    private static final String[] wordBank = new String[]{
-            "Hello", "There", "Math", "Pneumonoultramicroscopicsilicovolcanoconiosis", "My", "guacamole", "Chaotic",
-            "Computer", "World", "NASA", "Coffee", "glass", "boat", "king", "Zenyatta", "Kelowna", "mouse", "novel",
-            "critical", "artificial", "snow", "pattern", "confounded", "visible", "confusion", "ready", "TO", "work",
-             "return", "of", "the"
-    };
-
-    private static final int wordBankLength = wordBank.length;
     private static final int inputLength = 10;
 
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
-        TestOption.defaultInput =  "Hello how are you today this is my test sentence 5";
+        TestOption.defaultInput = "Hello how are you today this is my test sentence 5";
         return new Clause[]{
                 new StringLiteral("Enter words for the array:"),
                 new Optional(new StringLiteral(" ")),
@@ -50,44 +41,55 @@ public class MainTest extends BaseTest {
         StringSlice.main(new String[0]);
     }
 
-    private String randomWordInput(){
-        StringBuilder sb = new StringBuilder();
-        Random r = new Random();
-        for (int i = 0; i < inputLength; i++)
-            sb.append(wordBank[r.nextInt(wordBankLength)]).append(" ");
-        return sb.toString();
-    }
-
-    static Stream<Arguments> methodInputProvider(){
-        return Stream.of(Arguments.of(new String[]{"word", "new", "york", "knight", "night", "height", "fight", "flight"}, 5, new String[]{"height", "fight", "flight"}),
-                Arguments.of(new String[]{"World", "NASA", "Coffee", "glass", "boat", "king"}, 6, new String[]{}),
+    static Stream<Arguments> methodInputProvider() {
+        return Stream.of(
+                Arguments.of(new String[]{"word", "new", "york", "knight", "night", "height", "fight", "flight"}, 5, new String[]{"height", "fight", "flight"}),
+                Arguments.of(new String[]{"World", "NASA", "Coffee", "glass", "boat", "king"}, 6, new String[]{"World", "NASA", "Coffee", "glass", "boat", "king"}),
                 Arguments.of(new String[]{"Hello", "World!", "How", "are", "you", "this", "fine", "summer", "day"}, -11, new String[]{"Hello", "World!", "How", "are", "you", "this", "fine", "summer", "day"}),
                 Arguments.of(new String[]{"Hi", "how", "are", "ya"}, -1, new String[]{"Hi", "how", "are", "ya"}),
                 Arguments.of(new String[]{"cheese", "intelligent", "farmer", "pork", "dragon", "dawn"}, 0, new String[]{"cheese", "intelligent", "farmer", "pork", "dragon", "dawn"}),
-                Arguments.of(new String[]{}, 0, new String[]{}));
+                Arguments.of(new String[]{"eagle", "muggle", "johnson", "tea", "tip", "hedge", "sword", "penguin", "picture", "spicy"}, 10, new String[]{"eagle", "muggle", "johnson", "tea", "tip", "hedge", "sword", "penguin", "picture", "spicy"}),
+                Arguments.of(new String[]{"why", "am", "I", "still", "getting", "shielded"}, 5, new String[]{"shielded"}),
+                Arguments.of(new String[0], 0, new String[0])
+        );
+    }
+
+    static Stream<Arguments> mainMethodInputProvider() {
+        return Stream.of(
+                Arguments.of(ArrayUtil.generateRandomWordArray(inputLength), inputLength / 2),
+                Arguments.of(ArrayUtil.generateRandomWordArray(inputLength), inputLength),
+                Arguments.of(ArrayUtil.generateRandomWordArray(inputLength), inputLength - 1),
+                Arguments.of(ArrayUtil.generateRandomWordArray(inputLength), -1),
+                Arguments.of(ArrayUtil.generateRandomWordArray(inputLength), 0)
+        );
     }
 
     @ParameterizedTest
     @MethodSource("methodInputProvider")
     void correctGetRestMethod(String[] words, int index, String[] result) throws Throwable {
-        String[] output = (String[]) MethodUtil.invokeIfMethodExists(StringSlice.class, "getRest", new Object[]{words, index},
+        Object output = MethodUtil.invokeIfMethodExists(StringSlice.class, "getRest", new Object[]{words, index},
                 String[].class, int.class);
-        assertArrayEquals(result, output, "Your method does not return an array with all the words after an index.");
+        CustomAssertions._assertArrayEquals(result, output, "Your getRest method does not return an array with all the strings after an index.");
         String methodConsoleOutput = MethodUtil.getMethodOutput();
-        assertEquals("", methodConsoleOutput, "Your method should not have any printed output.");
+        assertEquals("", methodConsoleOutput, "Your getRest method should not have printed output.");
     }
 
-    @Test
-    void printsCorrectOutput() throws InvalidClauseException {
-        String input = randomWordInput();
-        Random r = new Random();
-        String[] output = input.split(" ");
-        Clause[][] c = new Clause[1][output.length * 2];
-        for(int i = 0; i < c[0].length; i+=2){
-            c[0][i] = new StringLiteral(output[i/2]);
-            c[0][i + 1] = new NewLine();
+    @ParameterizedTest
+    @MethodSource("mainMethodInputProvider")
+    void printsCorrectOutput(String[] words, int index) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not correctly print the new array.";
+        int length = words.length * 2;
+        int count = 0;
+        if (index >= 0 && index < words.length) {
+            length = (words.length - index) * 2;
+            count = index;
         }
-        runWithInput(input + r.nextInt(inputLength), c);
+        Clause[][] c = new Clause[1][length];
+        for (int i = 0; i < c[0].length; i++) {
+            c[0][i++] = new StringLiteral(words[count++]);
+            c[0][i] = new NewLine();
+        }
+        runWithInput(ArrayUtil.arrayToInput(words) + index, c);
     }
 
 }
--- src/arrays/program_involving_data_sequences/hard/q4/StringSlice.java
+++ src/arrays/program_involving_data_sequences/hard/q4/StringSlice.java
@@ -1,15 +1,37 @@
 package arrays.program_involving_data_sequences.hard.q4;
 /*
-Write a program that calls a method called getRest that takes as input an array of words and an index, then returns a
-new array with all the words from that index onwards. The program should then print out all the elements of the returned
-array on separate lines.
+Write a program that calls a method named getRest(String[] words, int index) that takes as input an array of strings
+and an index. The getRest method should return a new string array containing the strings from the original array
+starting from the index (inclusive). Your getRest method should also check if the index is valid, with a valid index
+being a number greater than or equal to 0 and less than the length of the array. If the index is invalid, return the
+input array.
 
-Write a program that calls a method called getRest that takes as input an array of words and an index, then returns a
-new array with all the words from that index onwards. The program should prompt the user to enter 10 words for the array,
-and an index, which are sent to the method. Then, print out all the elements of the returned array on separate lines.
-Your getRest method should also check of the index is valid, with a valid index being a number greater than 0 and less
-than or equal to the length of the array. If the index is invalid, return the input array.
+The main method should prompt the user to enter 10 words (no spaces) for the array and an index and use them to call
+getRest. Then, print out all the elements of the returned array on separate lines. Ensure that your output matches the
+sample.
+
+Sample Output:
+    Enter words for the array:
+    lemon
+    grape
+    lamp
+    slot
+    ape
+    slither
+    tame
+    game
+    computer
+    tree
+    Enter an index:
+    5
+    slither
+    tame
+    game
+    computer
+    tree
+
  */
+
 import java.util.Scanner;
 
 public class StringSlice {
@@ -27,7 +49,7 @@ public class StringSlice {
     }
 
     public static String[] getRest(String[] words, int index) {
-        if(index < 0 || index > words.length)
+        if (index < 0 || index >= words.length)
             return words;
         String[] remaining = new String[words.length - index];
         int pos = 0;
