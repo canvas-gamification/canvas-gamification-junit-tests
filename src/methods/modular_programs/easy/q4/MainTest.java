a5320aca58d5f6a19bc3bb6acdd1b1b771ebe93c
Samantha H <hodge1214@gmail.com>
2022-08-21 19:56:54 -0400

    switched back off input test

--- src/methods/modular_programs/easy/q4/MainTest.java
+++ src/methods/modular_programs/easy/q4/MainTest.java
@@ -8,6 +8,7 @@ import global.variables.Clause;
 import global.variables.clauses.DoubleLiteral;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -19,15 +20,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MainTest extends BaseTest {
     // Parsons
     public Clause[] testSentence() {
-        TestOption.isInputTest = true;
-        TestOption.defaultInput = "345.32 1.2345 2.3";
+        TestOption.isInputTest = false;
         return new Clause[]{
-                new StringLiteral("Enter the first number: "),
-                new NewLine(),
-                new StringLiteral("Enter the second number: "),
-                new NewLine(),
-                new StringLiteral("Enter the third number: "),
-                new NewLine(),
                 new StringLiteral("The values after the decimal point for the first variable are "),
                 new DoubleLiteral("first"),
                 new NewLine(),
@@ -39,14 +33,12 @@ public class MainTest extends BaseTest {
         };
     }
 
-    public void runMain(){ReturnChange.main(new String[0]);}
-
-    static Stream<Arguments> valueAfterCalcInputProvider(){
-        return Stream.of(Arguments.of(3.1, 0.1), Arguments.of(67.2175396, 0.2175396), Arguments.of(0.34, 0.34), Arguments.of(0.0, 0.0), Arguments.of(-12.523, -0.523), Arguments.of(0.1, 0.1), Arguments.of(-0.1, -0.1));
+    public void runMain() {
+        ReturnChange.main(new String[0]);
     }
 
-    static Stream<Arguments> mainMethodInputProvider(){
-        return Stream.of(Arguments.of("5.78 3.2 12743.867", 0.78, 0.2, 0.867), Arguments.of("-34.213 -56.34 91.2", -0.213, -0.34, 0.2), Arguments.of("0.56 0.1 0.853245", 0.56, 0.1, 0.853245), Arguments.of("0.0 0.1 1.1", 0.0, 0.1, 0.1));
+    static Stream<Arguments> valueAfterCalcInputProvider() {
+        return Stream.of(Arguments.of(3.1, 0.1), Arguments.of(67.2175396, 0.2175396), Arguments.of(0.34, 0.34), Arguments.of(0.0, 0.0), Arguments.of(-12.523, -0.523), Arguments.of(0.1, 0.1), Arguments.of(-0.1, -0.1));
     }
 
     @ParameterizedTest
@@ -56,12 +48,10 @@ public class MainTest extends BaseTest {
         CustomAssertions._assertEquals(decimals, output, 0.1, "Your valueAfterCalc method does not correctly calculate the decimal values.");
     }
 
-    @ParameterizedTest
-    @MethodSource("mainMethodInputProvider")
-    void printsOutputCorrectly(String in, double d1, double d2, double d3){
-        runWithInput(in);
-        assertEquals(d1, Double.parseDouble(getItemByName("first")), 0.1, "Your program does not correctly display the first set of values.");
-        assertEquals(d2, Double.parseDouble(getItemByName("second")), 0.1, "Your program does not correctly display the second set of values.");
-        assertEquals(d3, Double.parseDouble(getItemByName("third")), 0.1, "Your program does not correctly display the third set of values.");
+    @Test
+    void printsOutputCorrectly() {
+        assertEquals(0.123, Double.parseDouble(getItemByName("first")), 0.1, "Your program does not correctly display the first set of values.");
+        assertEquals(-0.24, Double.parseDouble(getItemByName("second")), 0.1, "Your program does not correctly display the second set of values.");
+        assertEquals(0.79, Double.parseDouble(getItemByName("third")), 0.1, "Your program does not correctly display the third set of values.");
     }
 }
--- src/methods/modular_programs/easy/q4/ReturnChange.java
+++ src/methods/modular_programs/easy/q4/ReturnChange.java
@@ -1,40 +1,28 @@
 package methods.modular_programs.easy.q4;
 
 /*
-Write a Java Program that takes three double variables from the user, then write a method to return the number values
+Write a Java Program that initializes three double variables and passes them to a method to return the number values
 after the decimal point. (e.g. if the double number is 14.5673, return 0.5673). If the variables are negative, return
 the negative decimal values.
 Sample output:
-Enter the first number:
-34.123
-Enter the second number:
-54.24
-Enter the third number:
-1.39264
-The values after the decimal point for the first variable are 0.12299999999999756
-The values after the decimal point for the second variable are 0.240000000000002
-The values after the decimal point for the third variable are 0.3926400000000001
+The values after the decimal point for the first variable are 0.12300000000000111
+The values after the decimal point for the second variable are -0.2400000000000002
+The values after the decimal point for the third variable are 0.7899999999999991
+
  */
 
-import java.util.Scanner;
-public class ReturnChange
-{
-    public static void main( String[] args )
-    {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the first number: ");
-        double a = input.nextDouble();
-        System.out.println("Enter the second number: ");
-        double b = input.nextDouble();
-        System.out.println("Enter the third number: ");
-        double c = input.nextDouble();
+public class ReturnChange {
+    public static void main(String[] args) {
+        double a = 22.123;
+        double b = -6.24;
+        double c = 11.79;
         System.out.println("The values after the decimal point for the first variable are " + valueAfterCalc(a));
         System.out.println("The values after the decimal point for the second variable are " + valueAfterCalc(b));
         System.out.println("The values after the decimal point for the third variable are " + valueAfterCalc(c));
     }
-    public static double valueAfterCalc(double a)
-    {
-        if(a < 0)
+
+    public static double valueAfterCalc(double a) {
+        if (a < 0)
             return (a - Math.ceil(a));
         else
             return a - Math.floor(a);
