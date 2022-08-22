8b5111f62355fc9b39741f4792289291f342c444
Samantha H <hodge1214@gmail.com>
2022-08-21 20:19:27 -0400

    added extra centuries to the program

--- src/methods/modular_programs/easy/q6/Century.java
+++ src/methods/modular_programs/easy/q6/Century.java
@@ -1,8 +1,9 @@
 package methods.modular_programs.easy.q6;
 
 /*
-Write a Java Program that asks users for the year that they were born in, then returns what century they were born in.
-The program should not accept any year preceding the 1900s as no one from then is still alive.
+Write a Java Program that asks users for the year they were born, then returns what century that year is in. The program
+should have a method called centuryCalc to determine this, which works for any century from the 1500's to 2000's. If the
+input is in any other century, return "Unknown Century".
 Sample output:
 Please enter the year you were born in:
 2002
@@ -10,22 +11,30 @@ You're from the 21st Century
  */
 
 import java.util.Scanner;
-public class Century
-{
-    public static void main( String[] args )
-    {
+
+public class Century {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the year you were born in: ");
+        System.out.println("Please enter the year you were born in:");
         int userYear = input.nextInt();
         System.out.println("You're from the " + centuryCalc(userYear));
     }
-    public static String centuryCalc(int userYear)
-    {
-        if (userYear>=2000)
+
+    public static String centuryCalc(int userYear) {
+        if (userYear >= 2000)
             return "21st Century";
-        else if (userYear>1899)
+        else if (userYear > 1899)
             return "20th Century";
+        else if (userYear > 1799)
+            return "19th Century";
+        else if (userYear > 1699)
+            return "18th Century";
+        else if (userYear > 1599)
+            return "17th Century";
+        else if (userYear > 1499)
+            return "16th Century";
         else
-            return "Invalid user Year";
+            return "Unknown Century";
     }
 }
+
