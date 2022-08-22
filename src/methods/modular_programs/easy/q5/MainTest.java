31b4a48be9421767efc65e4d2f68be9d6aec7ae4
Samantha H <hodge1214@gmail.com>
2022-08-21 20:05:06 -0400

    made changes

--- src/methods/modular_programs/easy/q5/MainTest.java
+++ src/methods/modular_programs/easy/q5/MainTest.java
@@ -29,27 +29,41 @@ public class MainTest extends BaseTest {
         };
     }
 
-    public void runMain(){HeightInFeet.main(new String[0]);}
-
-    static Stream<Arguments> conversionHeightInputProvider(){
-        return Stream.of(Arguments.of(161.5, 5.2972), Arguments.of(88.392, 2.9), Arguments.of(182.88, 6.0), Arguments.of(0.1, 0.00328084), Arguments.of(-0.1, -1.0), Arguments.of(-45, -1.0), Arguments.of(3629, 119.0617));
+    public void runMain() {
+        HeightInFeet.main(new String[0]);
     }
 
-    static Stream<Arguments> mainMethodInputProvider(){
-        return Stream.of(Arguments.of("1523", 49.96719), Arguments.of("23", 0.754593), Arguments.of("174.8264", 5.7357742782), Arguments.of("-345246.13", -1.0));
+    static Stream<Arguments> heightConversionInputProvider() {
+        return Stream.of(
+                Arguments.of(161.5, 5.2972),
+                Arguments.of(88.392, 2.9),
+                Arguments.of(182.88, 6.0),
+                Arguments.of(0.1, 0.00328084),
+                Arguments.of(-0.1, -1.0),
+                Arguments.of(-45, -1.0),
+                Arguments.of(0, 0.0),
+                Arguments.of(3629, 119.0617));
+    }
+
+    static Stream<Arguments> mainMethodInputProvider() {
+        return Stream.of(
+                Arguments.of("1523", 49.96719),
+                Arguments.of("23", 0.754593),
+                Arguments.of("174.8264", 5.7357742782),
+                Arguments.of("-345246.13", -1.0));
     }
 
     @ParameterizedTest
-    @MethodSource("conversionHeightInputProvider")
-    void correctConversionHeightMethod(double in, double feet) throws Throwable {
-        Object output = MethodUtil.invokeIfMethodExists(HeightInFeet.class, "conversionHeight", new Object[]{in}, double.class);
-        CustomAssertions._assertEquals(feet, output, 0.1, "Your conversionHeight method does not correctly convert from cm to feet.");
+    @MethodSource("heightConversionInputProvider")
+    void correctHeightConversionMethod(double in, double feet) throws Throwable {
+        Object output = MethodUtil.invokeIfMethodExists(HeightInFeet.class, "heightConversion", new Object[]{in}, double.class);
+        CustomAssertions._assertEquals(feet, output, 0.1, "Your heightConversion method does not correctly convert from centimetres to feet.");
     }
 
     @ParameterizedTest
     @MethodSource("mainMethodInputProvider")
-    void printsOutputCorrectly(String in, double feet){
+    void printsOutputCorrectly(String in, double feet) {
         runWithInput(in);
-        assertEquals(feet, Double.parseDouble(getItemByName("feet")), 0.05, "Your program does not output the correct height in feet.");
+        assertEquals(feet, Double.parseDouble(getItemByName("feet")), 0.05, "Your program does not correctly output the height in feet.");
     }
 }
