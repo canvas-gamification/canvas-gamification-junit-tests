dab28c32efc4d9dbd813e0b74221e239e7dc8d9b
Samantha H <hodge1214@gmail.com>
2022-09-14 12:52:41 -0400

    added sample output

--- src/arrays/creating_arrays/medium/q2/RandomNum.java
+++ src/arrays/creating_arrays/medium/q2/RandomNum.java
@@ -4,7 +4,7 @@ package arrays.creating_arrays.medium.q2;
 Make a Java program that stores 26 lowercase alphabet characters from  'a' to  'z' in an array. Print the results of
 the array to the console.
 Sample output:
-a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
+a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z,
  */
 
 public class RandomNum
