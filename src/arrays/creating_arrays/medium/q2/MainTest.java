3e48aed83647a0c9e8990090d03e2b073001c315
Samantha H <hodge1214@gmail.com>
2022-09-14 01:25:26 -0400

    got rid of an auto generated override

--- src/arrays/creating_arrays/medium/q2/MainTest.java
+++ src/arrays/creating_arrays/medium/q2/MainTest.java
@@ -6,8 +6,6 @@ import global.variables.clauses.StringLiteral;
 
 public class MainTest extends BaseTest {
     // Parsons
-
-    @Override
     public Clause[] testSentence() {
         return new Clause[]{
                 new StringLiteral("a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z")
