08ab9b47676fee128a535fc55dd5d13aa032fa01
Samantha H <hodge1214@gmail.com>
2022-09-22 22:33:52 -0400

    formatted

--- src/arrays/creating_arrays/hard/q5/AlphabetRays.java
+++ src/arrays/creating_arrays/hard/q5/AlphabetRays.java
@@ -11,14 +11,12 @@ y
 z
  */
 
-public class AlphabetRays
-{
-    public static void main( String [] args )
-    {
+public class AlphabetRays {
+    public static void main(String[] args) {
         char[] arr = new char[26];
-        for( int i = 0; i < arr.length; i++ )
-            arr[i] = (char) ( 97 + i );
-        for( int i = 0; i < arr.length; i++ )
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = (char) (97 + i);
+        for (int i = 0; i < arr.length; i++)
             System.out.println(arr[i]);
     }
 }
--- src/arrays/creating_arrays/hard/q5/MainTest.java
+++ src/arrays/creating_arrays/hard/q5/MainTest.java
@@ -11,19 +11,19 @@ public class MainTest extends BaseTest {
         return ClauseBuilder();
     }
 
-    public void runMain(){
+    public void runMain() {
         AlphabetRays.main(new String[0]);
     }
 
-    Clause[] ClauseBuilder(){
+    Clause[] ClauseBuilder() {
         Clause[] c = new Clause[51];
 
         String[] s = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
                 "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
-        "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+                "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
         int count = 0;
-        for(int x = 0; x < 25; x++){
+        for (int x = 0; x < 25; x++) {
             c[count++] = new StringLiteral(s[x]);
             c[count++] = new NewLine();
         }
