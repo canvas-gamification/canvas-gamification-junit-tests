671ee2c49893efe7d3e3b30ff1ecb7f239692614
Samantha H <hodge1214@gmail.com>
2022-09-26 00:22:45 -0400

    wrote test

---
+++ src/arrays/arrays_with_methods/hard/q7/ArraySimilarity.java
@@ -0,0 +1,20 @@
+package arrays.arrays_with_methods.hard.q7;
+
+/*
+Write a method called isEqual that takes two integer arrays as input and checks each of their elements one at a time to
+see whether all of their elements are the same or not. The method returns true if all the elements of the two arrays are
+the same, and false otherwise. Note that if the lengths of the two arrays are different, then the method can just return
+false.
+ */
+
+public class ArraySimilarity {
+    public static boolean isEqual( int [] a, int [] b )
+    {
+        if( a.length != b.length )
+            return false;
+        for( int i = 0; i < a.length; i++ )
+            if( a[i] != b[i] )
+                return false;
+        return true;
+    }
+}
---
+++ src/arrays/arrays_with_methods/hard/q7/MainTest.java
@@ -0,0 +1,31 @@
+package arrays.arrays_with_methods.hard.q7;
+
+import global.tools.CustomAssertions;
+import global.utils.MethodUtil;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class MainTest {
+    // Java
+    static Stream<Arguments> inputProvider(){
+        return Stream.of(
+                Arguments.of(new int[]{}, new int[]{}, true),
+                Arguments.of(new int[]{1, 2, 3, 5, 4}, new int[]{1, 2, 3, 4, 5}, false),
+                Arguments.of(new int[]{2, 2, 2, 2, 2, 2}, new int[]{2, 2, 2, 2, 2, 2}, true),
+                Arguments.of(new int[]{4, 4, 4}, new int[]{4}, false),
+                Arguments.of(new int[]{6, 7}, new int[]{6, 7, 8}, false),
+                Arguments.of(new int[]{1, 2, 3}, new int[]{1, 2}, false),
+                Arguments.of(new int[]{352, 85326, 5372, 5893, 475642}, new int[]{352, 85326, 5372, 5893, 475642}, true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void correctIsEqualMethod(int[] one, int[] two, boolean equal) throws Throwable {
+        Object output = MethodUtil.invokeIfMethodExists(ArraySimilarity.class, "isEqual", new Object[]{one, two}, int[].class, int[].class);
+        CustomAssertions._assertEquals(equal, output, "Your isEqual method does not return the correct boolean.");
+    }
+}
