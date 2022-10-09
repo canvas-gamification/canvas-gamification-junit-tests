82552853a1a2aa8febddfc88279a63209e88a13b
Samantha H <hodge1214@gmail.com>
2022-10-08 23:40:49 -0400

    wrote main test and formatted

--- src/arrays/arrays_with_methods/hard/q13/MainTest.java
+++ src/arrays/arrays_with_methods/hard/q13/MainTest.java
@@ -1,14 +1,18 @@
 package arrays.arrays_with_methods.hard.q13;
 
 import global.BaseTest;
+import global.MethodTest;
+import global.exceptions.InvalidClauseException;
 import global.tools.CustomAssertions;
 import global.tools.TestOption;
-import global.utils.MethodUtil;
 import global.variables.Clause;
+import global.variables.clauses.IntegerLiteral;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 import global.variables.wrappers.Optional;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,6 +21,11 @@ import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
     // Java
+
+    int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
+            103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223,
+            227, 229};
+
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
         TestOption.defaultInput = "6";
@@ -28,11 +37,11 @@ public class MainTest extends BaseTest {
         };
     }
 
-    public void runMain(){
+    public void runMain() {
         ArrayngeOfPrimes.main(new String[0]);
     }
 
-    static Stream<Arguments> isPrimeInputProvider(){
+    static Stream<Arguments> isPrimeInputProvider() {
         return Stream.of(
                 Arguments.of(0, false),
                 Arguments.of(1, false),
@@ -52,12 +61,27 @@ public class MainTest extends BaseTest {
         );
     }
 
-    //need main test stream that involves arrays somehow, to construct placeholder
-
     @ParameterizedTest
     @MethodSource("isPrimeInputProvider")
     void correctIsPrimeMethod(int in, boolean expected) throws Throwable {
-        Object output = MethodUtil.invokeIfMethodExists(ArrayngeOfPrimes.class, "isPrime", new Object[]{in}, int.class);
+        Object[][] arguments = {
+                {in, int.class}
+        };
+        MethodTest m = new MethodTest(ArrayngeOfPrimes.class, "isPrime", arguments);
+        Object output = m.callMethod();
         CustomAssertions._assertEquals(expected, output, "Your isPrime method does not correctly identify primes.");
+    }
+
+    @RepeatedTest(50)
+    void correctMainMethodOutput(RepetitionInfo repetitionInfo) throws InvalidClauseException {
+        Clause[][] c = new Clause[1][repetitionInfo.getCurrentRepetition() * 2];
+        int count = 0;
+
+        for (int x = 0; x < repetitionInfo.getCurrentRepetition(); x++) {
+            c[0][count++] = new IntegerLiteral(primes[x]);
+            c[0][count++] = new NewLine();
+        }
+
+        runWithInput(String.valueOf(repetitionInfo.getCurrentRepetition()), c);
     }
 }
