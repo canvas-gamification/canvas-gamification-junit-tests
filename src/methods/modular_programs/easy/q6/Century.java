606a7045a4da1e913f44a4761b747e51526626dc
Samantha H <hodge1214@gmail.com>
2022-10-22 14:17:36 -0400

    fixed sample output formatting

--- src/methods/modular_programs/easy/q6/Century.java
+++ src/methods/modular_programs/easy/q6/Century.java
@@ -4,10 +4,11 @@ package methods.modular_programs.easy.q6;
 Write a Java Program that asks users for the year they were born, then prints the century the individual was born.
 The program should have a method called centuryCalc, which determines the century and works for any century from the
 1500’s (16th Century) to the 2000's (21st Century). If the input is in any other century, return "Unknown Century".
+
 Sample output:
-Please enter the year you were born in:
-2002
-You're from the 21st Century
+    Please enter the year you were born in:
+    2002
+    You're from the 21st Century
  */
 
 import java.util.Scanner;
