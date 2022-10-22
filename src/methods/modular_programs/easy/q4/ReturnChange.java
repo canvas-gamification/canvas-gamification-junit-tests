a8ac7d6f7ac455ff531a26e6459ba06ca93a3adb
Samantha H <hodge1214@gmail.com>
2022-10-22 14:13:51 -0400

    fixed sample output formatting

--- src/methods/modular_programs/easy/q4/ReturnChange.java
+++ src/methods/modular_programs/easy/q4/ReturnChange.java
@@ -4,11 +4,11 @@ package methods.modular_programs.easy.q4;
 Write a Java Program that initializes three double variables and passes them to a method called valueAfterCalc to
 return the number values after the decimal point. (e.g. if the number is 14.5673, return 0.5673). If the variables are
 negative, the returned value should be negative.
-Sample output:
-The values after the decimal point for the first variable are 0.12300000000000111
-The values after the decimal point for the second variable are -0.2400000000000002
-The values after the decimal point for the third variable are 0.7899999999999991
 
+Sample output:
+    The values after the decimal point for the first variable are 0.12300000000000111
+    The values after the decimal point for the second variable are -0.2400000000000002
+    The values after the decimal point for the third variable are 0.7899999999999991
  */
 
 public class ReturnChange {
