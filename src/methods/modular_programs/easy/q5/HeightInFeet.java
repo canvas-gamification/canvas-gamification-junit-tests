20281802ee2fab37ff39eeffded2e09eeb0af7c9
Samantha H <hodge1214@gmail.com>
2022-10-22 14:03:17 -0400

    reformatted question text

--- src/methods/modular_programs/easy/q5/HeightInFeet.java
+++ src/methods/modular_programs/easy/q5/HeightInFeet.java
@@ -4,10 +4,11 @@ package methods.modular_programs.easy.q5;
 Write a program that asks users for their height in cm, then converts it to feet by dividing the centimeters by 30.48.
 Create a method called heightConversion to do this, which should return the value in feet, or -1 of the entered height
 is less than zero.
+
 Sample output:
-Please enter your height in cm:
-138
-Your height in feet is: 4.526400000000001
+    Please enter your height in cm:
+    138
+    Your height in feet is: 4.526400000000001
  */
 
 import java.util.Scanner;
