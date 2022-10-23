097e7a076c859d00d5038e6a151c4b98c9262c6b
Samantha H <hodge1214@gmail.com>
2022-10-23 01:44:38 -0400

    fixed sample output formatting

--- src/arrays/creating_arrays/medium/q4/RandomNum.java
+++ src/arrays/creating_arrays/medium/q4/RandomNum.java
@@ -3,12 +3,13 @@ package arrays.creating_arrays.medium.q4;
 /*
 Make a Java program that initializes an array of 100 random integers from the range of 0-9, inclusive of both 0 and 9,
 using the Random class.
+
 Sample output:
-5
-7
-1
-...
-6
+    5
+    7
+    1
+    ...
+    6
  */
 
 import java.util.Random;
