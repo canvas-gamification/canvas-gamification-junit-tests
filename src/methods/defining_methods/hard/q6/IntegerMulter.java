3dc35278c560d68cb87836768f58fe5b737e67ea
Samantha H <hodge1214@gmail.com>
2022-10-27 19:07:00 -0400

    changed question text and formatted

--- src/methods/defining_methods/hard/q6/IntegerMulter.java
+++ src/methods/defining_methods/hard/q6/IntegerMulter.java
@@ -1,28 +1,28 @@
 package methods.defining_methods.hard.q6;
 
-/**
- * *[J] Write a Java Program that asks users for an integer between 0 and 10, then write a method
- * that takes an integer as an input parameter and returns that integer multiplied by 1000. Call
- * the method multiply1000. It’s guarantee that the input is in the range between 0 and 10.
- * 
- * Sample Output:
- * Enter a number:
- *    5
- *    The new number is: 5000
- * 
+/*
+Write a Java program that asks users for an integer between 0 and 10 (inclusive), which has a method called multiply1000
+that takes an integer as an input parameter and returns that integer multiplied by 1000. If the method receives an
+invalid integer ( less than 0 or greater than 10), return -1. Call the method with the user input and print the returned
+integer in the main method.
+
+Sample Output:
+	Enter a number:
+	5
+	The new number is: 5000
  */
 
 import java.util.Scanner;
 
 public class IntegerMulter {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter a number:");
-    int in = input.nextInt();
-    System.out.println("The new number is: " + multiply1000(in));
-  }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int in = input.nextInt();
+        System.out.println("The new number is: " + multiply1000(in));
+    }
 
-  public static int multiply1000(int a) {
-    return a * 1000;
-  }
+    public static int multiply1000(int a) {
+        return a * 1000;
+    }
 }
