9cc69d159428da686446d7901d73b34d97507e84
Samantha H <hodge1214@gmail.com>
2022-10-27 18:01:42 -0400

    switched to method test

--- src/methods/defining_methods/hard/q7/MainTest.java
+++ src/methods/defining_methods/hard/q7/MainTest.java
@@ -2,6 +2,7 @@ package methods.defining_methods.hard.q7;
 
 import java.util.stream.Stream;
 
+import global.MethodTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,56 +11,61 @@ import global.BaseTest;
 import global.exceptions.InvalidClauseException;
 import global.tools.CustomAssertions;
 import global.tools.TestOption;
-import global.utils.MethodUtil;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 
 public class MainTest extends BaseTest {
-  // Java
-  public Clause[] testSentence() {
-    TestOption.isInputTest = true;
-    TestOption.defaultInput = "366";
+    // Java
 
-    return new Clause[] {
-        new StringLiteral("Enter the number of days in this year:"),
-        new NewLine(),
-        new PlaceHolder(),
-    };
-  }
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "366";
 
-  public void runMain() {
-    CurrentYearMethod.main(new String[0]);
-  }
+        return new Clause[]{
+                new StringLiteral("Enter the number of days in this year:"),
+                new NewLine(),
+                new PlaceHolder(),
+        };
+    }
 
-  static Stream<Arguments> possibleNumDaysForMethod() {
-    return Stream.of(
-        Arguments.of(366, true),
-        Arguments.of(365, false));
-  }
+    public void runMain() {
+        CurrentYearMethod.main(new String[0]);
+    }
 
-  static Stream<Arguments> possibleNumDaysForOutput() {
-    return Stream.of(
-        Arguments.of(366, "This year is a leap year"),
-        Arguments.of(365, "This year isn't a leap year"));
-  }
+    static Stream<Arguments> possibleNumDaysForMethod() {
+        return Stream.of(
+                Arguments.of(366, true),
+                Arguments.of(365, false),
+                Arguments.of(34, false));
+    }
 
-  @ParameterizedTest
-  @MethodSource("possibleNumDaysForMethod")
-  void correctLeapCheckerMethod(int numDays, boolean isLeapYear) throws Throwable {
-    String errorMessage = "Your method \"leapChecker()\" does not return the correct result";
-    Object output = MethodUtil.invokeIfMethodExists(CurrentYearMethod.class, "leapChecker", new Object[] { numDays },
-        int.class);
-    CustomAssertions._assertEquals(output, isLeapYear, errorMessage);
-  }
+    static Stream<Arguments> possibleNumDaysForOutput() {
+        return Stream.of(
+                Arguments.of(366, "This year is a leap year"),
+                Arguments.of(365, "This year isn't a leap year"),
+                Arguments.of(18, "This year isn't a leap year"));
+    }
 
-  @ParameterizedTest
-  @MethodSource("possibleNumDaysForOutput")
-  void printsCorrectLeapYearMessage(int numDays, String isLeapYearStr) throws InvalidClauseException {
-    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct message for this year";
-    runWithInput(numDays + "", new Clause[] {
-        new StringLiteral(isLeapYearStr),
-    });
-  }
+    @ParameterizedTest
+    @MethodSource("possibleNumDaysForMethod")
+    void correctLeapCheckerMethod(int numDays, boolean isLeapYear) throws Throwable {
+        Object[][] arguments = {
+                {numDays, int.class}
+        };
+        MethodTest m = new MethodTest(CurrentYearMethod.class, "leapChecker", arguments);
+        Object output = m.callMethod();
+        String errorMessage = "Your leapChecker() method does not return the correct result";
+        CustomAssertions._assertEquals(isLeapYear, output, errorMessage);
+    }
+
+    @ParameterizedTest
+    @MethodSource("possibleNumDaysForOutput")
+    void printsCorrectLeapYearMessage(int numDays, String isLeapYearStr) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not print the correct message for this year";
+        runWithInput(numDays + "", new Clause[]{
+                new StringLiteral(isLeapYearStr),
+        });
+    }
 }
