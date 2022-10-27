8e140435c729976a72e95d376f96fdd0aec8b83a
Samantha H <hodge1214@gmail.com>
2022-10-27 18:16:51 -0400

    formatted

--- src/methods/defining_methods/hard/q4/MainTest.java
+++ src/methods/defining_methods/hard/q4/MainTest.java
@@ -17,55 +17,55 @@ import global.variables.clauses.StringLiteral;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
-  // Java
+    // Java
 
-  public Clause[] testSentence() {
-    TestOption.isInputTest = true;
-    TestOption.defaultInput = "5 10";
-    return new Clause[] {
-        new StringLiteral("Enter the first number: "),
-        new StringLiteral("Enter the second number: "),
-            new StringLiteral("The greater number is "),
-            new IntegerLiteral("number")
-    };
-  }
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "5 10";
+        return new Clause[]{
+                new StringLiteral("Enter the first number: "),
+                new StringLiteral("Enter the second number: "),
+                new StringLiteral("The greater number is "),
+                new IntegerLiteral("number")
+        };
+    }
 
-  public void runMain() {
-    GreaterThan.main(new String[0]);
-  }
+    public void runMain() {
+        GreaterThan.main(new String[0]);
+    }
 
-  static Stream<Arguments> correctInputProvider() {
-    return Stream.of(
-        Arguments.of(1, 2, 2),
-        Arguments.of(2, 1, 2),
-        Arguments.of(-1, -2, -1),
-        Arguments.of(-2, -1, -1),
-        Arguments.of(-1, 0, 0),
-        Arguments.of(0, -1, 0),
-        Arguments.of(0, 0, 0),
-        Arguments.of(-1, 1, 1),
-        Arguments.of(1, -1, 1),
-        Arguments.of(1000, 999, 1000),
-        Arguments.of(1, -1000, 1));
-  }
+    static Stream<Arguments> correctInputProvider() {
+        return Stream.of(
+                Arguments.of(1, 2, 2),
+                Arguments.of(2, 1, 2),
+                Arguments.of(-1, -2, -1),
+                Arguments.of(-2, -1, -1),
+                Arguments.of(-1, 0, 0),
+                Arguments.of(0, -1, 0),
+                Arguments.of(0, 0, 0),
+                Arguments.of(-1, 1, 1),
+                Arguments.of(1, -1, 1),
+                Arguments.of(1000, 999, 1000),
+                Arguments.of(1, -1000, 1));
+    }
 
-  @ParameterizedTest
-  @MethodSource("correctInputProvider")
-  void correctGreaterNumMethod(int a, int b, int greater) throws Throwable {
-    Object[][] arguments = {
-            {a, int.class},
-            {b, int.class}
-    };
-    MethodTest m = new MethodTest(GreaterThan.class, "greaterNum", arguments);
-    Object output = m.callMethod();
-    String errorMessage = "Your greaterNum() method does not return the greater number.";
-    CustomAssertions._assertEquals(greater, output, errorMessage);
-  }
+    @ParameterizedTest
+    @MethodSource("correctInputProvider")
+    void correctGreaterNumMethod(int a, int b, int greater) throws Throwable {
+        Object[][] arguments = {
+                {a, int.class},
+                {b, int.class}
+        };
+        MethodTest m = new MethodTest(GreaterThan.class, "greaterNum", arguments);
+        Object output = m.callMethod();
+        String errorMessage = "Your greaterNum() method does not return the greater number.";
+        CustomAssertions._assertEquals(greater, output, errorMessage);
+    }
 
-  @ParameterizedTest
-  @MethodSource("correctInputProvider")
-  void printsCorrectGreaterNum(int a, int b, int greater) {
-    runWithInput(a + " " + b);
-    assertEquals(greater, Integer.parseInt(getItemByName("number")), "Your program does not print the greater number.");
-  }
+    @ParameterizedTest
+    @MethodSource("correctInputProvider")
+    void printsCorrectGreaterNum(int a, int b, int greater) {
+        runWithInput(a + " " + b);
+        assertEquals(greater, Integer.parseInt(getItemByName("number")), "Your program does not print the greater number.");
+    }
 }
