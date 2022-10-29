7198d48917c0ef10dfaa571792a64373f7904e8c
Samantha H <hodge1214@gmail.com>
2022-10-29 15:56:55 -0400

    fixed solution code formatting and changed question text

--- src/methods/defining_methods/hard/q9/NumCoursesMethod.java
+++ src/methods/defining_methods/hard/q9/NumCoursesMethod.java
@@ -1,26 +1,28 @@
 package methods.defining_methods.hard.q9;
 
-/**
- * [J] Write a Java Program that asks users for the number of courses they are taking right now. A course will contain
- * 3 credits. Write a method named creditsCac that accepts the number of courses and returns the number of credits the student will receive.
- * 
- * Sample Output:
- *    Enter the number of courses you're taking this year:
- *    3
- *    The number of credits you'll receive is 9
+/*
+Write a Java program asking the user for the number of courses they are taking right now. Then, create a method named
+creditsCac that accepts the number of courses as an integer and returns an integer for credits the student will receive,
+assuming that each course consists of three credits. Print the number of credits the user will receive in the main
+method.
+
+Sample Output:
+  Enter the number of courses you're taking this year:
+  3
+  The number of credits you'll receive is 9
  */
 
 import java.util.Scanner;
 
 public class NumCoursesMethod {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter the number of courses you're taking this year:");
-    int courses = input.nextInt();
-    System.out.println("The number of credits you'll receive is " + creditsCalc(courses));
-  }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of courses you're taking this year:");
+        int courses = input.nextInt();
+        System.out.println("The number of credits you'll receive is " + creditsCalc(courses));
+    }
 
-  public static int creditsCalc(int coursesCount) {
-    return 3 * coursesCount;
-  }
+    public static int creditsCalc(int coursesCount) {
+        return 3 * coursesCount;
+    }
 }
