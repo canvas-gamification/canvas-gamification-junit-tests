65a751afbbb9123fc4153cbd8731f8322b8b453f
Samantha H <hodge1214@gmail.com>
2022-10-30 15:33:56 -0400

    switched to method test

--- src/methods/method_overloading/medium/q6/MainTest.java
+++ src/methods/method_overloading/medium/q6/MainTest.java
@@ -2,51 +2,59 @@ package methods.method_overloading.medium.q6;
 
 import java.util.stream.Stream;
 
+import global.MethodTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import global.tools.CustomAssertions;
-import global.utils.MethodUtil;
 
 public class MainTest {
-  // Parsons
-  static Stream<Arguments> inputDoubleFloatProvider() {
-    return Stream.of(
-        Arguments.of(1.0, 2.0, 0.5),
-        Arguments.of(2.0, 1.0, 2.0),
-        Arguments.of(1.0, 1.0, 1.0),
-        Arguments.of(0.0, 1.0, 0.0),
-        Arguments.of(2.55, 222.123, 0.01148),
-        Arguments.of(222.123, -2.55, -87.1071));
-  }
+    // Parsons
 
-  static Stream<Arguments> inputIntLongProvider() {
-    return Stream.of(
-        Arguments.of(1, 2, 0.0),
-        Arguments.of(2, 1, 2.0),
-        Arguments.of(1, 1, 1.0),
-        Arguments.of(0, 1, 0.0),
-        Arguments.of(2, 222, 0.0),
-        Arguments.of(222, -2, -111.0));
-  }
+    static Stream<Arguments> inputDoubleFloatProvider() {
+        return Stream.of(
+                Arguments.of(1.0, 2.0, 0.5),
+                Arguments.of(2.0, 1.0, 2.0),
+                Arguments.of(1.0, 1.0, 1.0),
+                Arguments.of(0.0, 1.0, 0.0),
+                Arguments.of(2.55, 222.123, 0.01148),
+                Arguments.of(222.123, -2.55, -87.1071));
+    }
 
-  @ParameterizedTest
-  @MethodSource("inputDoubleFloatProvider")
-  void correctDoubleFloatDivide(double a, double b, double divide) throws Throwable {
-    String errMsg = "Your method \"divide()\" does not return a correct value for double and float.";
-    float floatB = (float) b;
-    Object output = MethodUtil.invokeIfMethodExists(OverloadFloat.class, "divide",
-        new Object[] { a, floatB }, double.class, float.class);
-    CustomAssertions._assertEquals(output, divide, 0.0001, errMsg);
-  }
+    static Stream<Arguments> inputIntLongProvider() {
+        return Stream.of(
+                Arguments.of(1, 2, 0.0),
+                Arguments.of(2, 1, 2.0),
+                Arguments.of(1, 1, 1.0),
+                Arguments.of(0, 1, 0.0),
+                Arguments.of(2, 222, 0.0),
+                Arguments.of(222, -2, -111.0));
+    }
 
-  @ParameterizedTest
-  @MethodSource("inputIntLongProvider")
-  void correctIntLongDivide(int a, long b, double divide) throws Throwable {
-    String errMsg = "Your method \"divide()\" does not return a correct value for int and long.";
-    Object output = MethodUtil.invokeIfMethodExists(OverloadFloat.class, "divide", new Object[] { a, b }, int.class,
-        long.class);
-    CustomAssertions._assertEquals(output, divide, 0.0001, errMsg);
-  }
+    @ParameterizedTest
+    @MethodSource("inputDoubleFloatProvider")
+    void correctDoubleFloatDivide(double a, double b, double divide) throws Throwable {
+        Object[][] arguments = {
+                {a, double.class},
+                {(float) b, float.class}
+        };
+        MethodTest m = new MethodTest(OverloadFloat.class, "divide", arguments);
+        Object output = m.callMethod();
+        String errMsg = "Your divide() method does not return a correct value for double and float.";
+        CustomAssertions._assertEquals(divide, output, 0.0001, errMsg);
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputIntLongProvider")
+    void correctIntLongDivide(int a, long b, double divide) throws Throwable {
+        Object[][] arguments = {
+                {a, int.class},
+                {b, long.class}
+        };
+        MethodTest m = new MethodTest(OverloadFloat.class, "divide", arguments);
+        Object output = m.callMethod();
+        String errMsg = "Your divide() method does not return a correct value for int and long.";
+        CustomAssertions._assertEquals(divide, output, 0.0001, errMsg);
+    }
 }
--- src/methods/method_overloading/medium/q6/OverloadFloat.java
+++ src/methods/method_overloading/medium/q6/OverloadFloat.java
@@ -1,16 +1,13 @@
 package methods.method_overloading.medium.q6;
 
-/**
- * Overload a Java Method so that it can take a double variable and float
- * variable or it can take a int variable and long variable, then divide the
- * former variable by the latter variable.
+/*
+ Overload a Java Method so that it can take a double variable and float variable or it can take a int variable and long
+ variable, then divide the former variable by the latter variable.
  */
 public class OverloadFloat {
-  public static double divide(double a, float b) {
-    return a / b;
-  }
+    public static double divide(double a, float b) {
+        return a / b;
+    }
 
-  public static double divide(int a, long b) {
-    return a / b;
-  }
+    public static double divide(int a, long b) { return a / b; }
 }
