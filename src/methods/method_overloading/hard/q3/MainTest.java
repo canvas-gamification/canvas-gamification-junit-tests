59d7dd57555a7c2b300fbe43fdb5354efb6132b9
Samantha H <hodge1214@gmail.com>
2022-10-30 02:27:02 -0400

    switched clause to scanner

--- src/methods/method_overloading/hard/q3/MainTest.java
+++ src/methods/method_overloading/hard/q3/MainTest.java
@@ -2,6 +2,8 @@ package methods.method_overloading.hard.q3;
 
 import java.util.stream.Stream;
 
+import global.tools.TestOption;
+import global.variables.wrappers.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -9,7 +11,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import global.BaseTest;
 import global.tools.CustomAssertions;
-import global.tools.TestOption;
 import global.utils.MethodUtil;
 import global.variables.Clause;
 import global.variables.clauses.DoubleLiteral;
@@ -18,70 +19,81 @@ import global.variables.clauses.NewLine;
 import global.variables.clauses.StringLiteral;
 
 public class MainTest extends BaseTest {
-  // Java
-  public Clause[] testSentence() {
-    return new Clause[] {
-        new StringLiteral("The sum of the 2 numbers: "),
-        new IntegerLiteral("sumInt"),
-        new NewLine(),
-        new StringLiteral("The sum of the 2 numbers: "),
-        new DoubleLiteral("sumDouble"),
-        new NewLine()
-    };
-  }
+    // Java
 
-  public void runMain() {
-    Overload3.main(new String[0]);
-  }
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "3 9\n45.3 21.1";
+        return new Clause[]{
+                new StringLiteral("Enter two integers:"),
+                new Optional(new StringLiteral(" ")),
+                new NewLine(),
+                new StringLiteral("Enter two doubles:"),
+                new Optional(new StringLiteral(" ")),
+                new NewLine(),
+                new StringLiteral("The sum of the 2 numbers: "),
+                new IntegerLiteral("sumInt"),
+                new NewLine(),
+                new StringLiteral("The sum of the 2 numbers: "),
+                new DoubleLiteral("sumDouble"),
+                new NewLine()
+        };
+    }
 
-  static Stream<Arguments> integerInputProvider() {
-    return Stream.of(
-        Arguments.of(1, 2, 3),
-        Arguments.of(0, 0, 0),
-        Arguments.of(1, 0, 1),
-        Arguments.of(1, -1, 0),
-        Arguments.of(-99, 201, 102));
-  }
+    public void runMain() {
+        Overload3.main(new String[0]);
+    }
 
-  static Stream<Arguments> doubleInputProvider() {
-    return Stream.of(
-        Arguments.of(1.1, 2.5, 3.6),
-        Arguments.of(0.01, 0.01, 0.02),
-        Arguments.of(-1.0, 1.03, 0.03),
-        Arguments.of(1.5, -1.5, 0.0),
-        Arguments.of(-99.0, 201.0, 102.0),
-        Arguments.of(-99.0, 201.142, 102.142));
-  }
+    static Stream<Arguments> integerInputProvider() {
+        return Stream.of(
+                Arguments.of(1, 2, 3),
+                Arguments.of(0, 0, 0),
+                Arguments.of(1, 0, 1),
+                Arguments.of(1, -1, 0),
+                Arguments.of(-99, 201, 102));
+    }
 
-  @ParameterizedTest
-  @MethodSource("integerInputProvider")
-  public void methodIntegerCorrect(int a, int b, int sum) throws Throwable {
-    String errorMsg = "Your method did not calculate the correct sum for 2 integers.";
-    Object output = MethodUtil.invokeIfMethodExists(Overload3.class, "sum", new Object[] { a, b, }, int.class,
-        int.class);
-    CustomAssertions._assertEquals(output, sum, errorMsg);
-  }
+    static Stream<Arguments> doubleInputProvider() {
+        return Stream.of(
+                Arguments.of(1.1, 2.5, 3.6),
+                Arguments.of(0.01, 0.01, 0.02),
+                Arguments.of(-1.0, 1.03, 0.03),
+                Arguments.of(1.5, -1.5, 0.0),
+                Arguments.of(-99.0, 201.0, 102.0),
+                Arguments.of(-99.0, 201.142, 102.142),
+                Arguments.of(3, 1, 4.0)
+        );
+    }
 
-  @ParameterizedTest
-  @MethodSource("doubleInputProvider")
-  public void methodDoubleCorrect(double a, double b, double sum) throws Throwable {
-    String errorMsg = "Your method did not calculate the correct sum for 2 doubles.";
-    Object output = MethodUtil.invokeIfMethodExists(Overload3.class, "sum", new Object[] { a, b, }, double.class,
-        double.class);
-    CustomAssertions._assertEquals(output, sum, 0.0001, errorMsg);
-  }
+    @ParameterizedTest
+    @MethodSource("integerInputProvider")
+    public void methodIntegerCorrect(int a, int b, int sum) throws Throwable {
+        String errorMsg = "Your method did not calculate the correct sum for 2 integers.";
+        Object output = MethodUtil.invokeIfMethodExists(Overload3.class, "sum", new Object[]{a, b,}, int.class,
+                int.class);
+        CustomAssertions._assertEquals(output, sum, errorMsg);
+    }
 
-  @Test
-  public void printsCorrectMessage() {
-    String errorMessageInt = "Your program did not print the correct message for sum of 2 integers.";
-    String errorMessageDouble = "Your program did not print the correct message for sum of 2 integers.";
-    final int expectedSumInt = 99;
-    final double expectedSumDouble = 117.5;
+    @ParameterizedTest
+    @MethodSource("doubleInputProvider")
+    public void methodDoubleCorrect(double a, double b, double sum) throws Throwable {
+        String errorMsg = "Your method did not calculate the correct sum for 2 doubles.";
+        Object output = MethodUtil.invokeIfMethodExists(Overload3.class, "sum", new Object[]{a, b,}, double.class,
+                double.class);
+        CustomAssertions._assertEquals(output, sum, 0.0001, errorMsg);
+    }
 
-    int sumInt = Integer.parseInt(getItemByName("sumInt"));
-    double sumDouble = Double.parseDouble(getItemByName("sumDouble"));
+    @Test
+    public void printsCorrectMessage() {
+        String errorMessageInt = "Your program did not print the correct message for sum of 2 integers.";
+        String errorMessageDouble = "Your program did not print the correct message for sum of 2 integers.";
+        final int expectedSumInt = 99;
+        final double expectedSumDouble = 117.5;
 
-    CustomAssertions._assertEquals(sumInt, expectedSumInt, errorMessageInt);
-    CustomAssertions._assertEquals(sumDouble, expectedSumDouble, 0.0001, errorMessageDouble);
-  }
+        int sumInt = Integer.parseInt(getItemByName("sumInt"));
+        double sumDouble = Double.parseDouble(getItemByName("sumDouble"));
+
+        CustomAssertions._assertEquals(sumInt, expectedSumInt, errorMessageInt);
+        CustomAssertions._assertEquals(sumDouble, expectedSumDouble, 0.0001, errorMessageDouble);
+    }
 }
--- src/methods/method_overloading/hard/q3/Overload3.java
+++ src/methods/method_overloading/hard/q3/Overload3.java
@@ -1,22 +1,31 @@
 package methods.method_overloading.hard.q3;
 
-/**
- * [J] Write a Java Program that can handle the addition of the 2 following
- * number sets, then overload a method so that it can accept either a number
- * with or without decimals and return the sum of the 2 numbers. Let's call that
- * method "sum".
- * Number set #1: 43, 56
- * Number set #2: 91.4, 26.1
- * 
- * Sample Output:
- * The sum of the 2 numbers: 99
- * The sum of the 2 numbers: 117.5
- */
+/*
+Write a Java program that prompts the user to input two integers and two doubles. Then pass each pair of numbers to an
+overloaded method called sum which takes the two numbers as parameters and returns their sum.
+
+Sample Output:
+  Enter two integers:
+  43 56
+  Enter two doubles:
+  91.4 26.1
+  The sum of the 2 numbers: 99
+  The sum of the 2 numbers: 117.5
+*/
+
+import java.util.Scanner;
 
 public class Overload3 {
   public static void main(String[] args) {
-    System.out.println("The sum of the 2 numbers: " + sum(43, 56));
-    System.out.println("The sum of the 2 numbers: " + sum(91.4, 26.1));
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter two integers: ");
+    int one = input.nextInt();
+    int two = input.nextInt();
+    System.out.println("Enter two doubles: ");
+    double x = input.nextDouble();
+    double y = input.nextDouble();
+    System.out.println("The sum of the 2 numbers: " + sum(one, two));
+    System.out.println("The sum of the 2 numbers: " + sum(x, y));
   }
 
   public static int sum(int num1, int num2) {
