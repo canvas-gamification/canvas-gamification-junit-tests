3ea238f6640c489511bb171b7809786fd0cf0f7a
Samantha H <hodge1214@gmail.com>
2022-10-31 01:01:49 -0400

    changed to method test

--- src/methods/method_overloading/medium/q8/MainTest.java
+++ src/methods/method_overloading/medium/q8/MainTest.java
@@ -2,12 +2,12 @@ package methods.method_overloading.medium.q8;
 
 import java.util.stream.Stream;
 
+import global.MethodTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import global.tools.CustomAssertions;
-import global.utils.MethodUtil;
 
 public class MainTest {
   // Parsons
@@ -35,19 +35,27 @@ public class MainTest {
   @ParameterizedTest
   @MethodSource("inputTwoCharsProvider")
   void correctTwoCharGreater(char a, char b, char greater) throws Throwable {
-    String errMsg = "Your method \"greatest()\" does not print the greater character.";
-    Object output = MethodUtil.invokeIfMethodExists(OverloadChar.class, "greatest", new Object[] { a, b }, char.class,
-        char.class);
-    CustomAssertions._assertEquals(output, greater, errMsg);
+    Object[][] arguments = {
+            {a, char.class},
+            {b, char.class}
+    };
+    MethodTest m = new MethodTest(OverloadChar.class, "greatest", arguments);
+    Object output = m.callMethod();
+    String errMsg = "Your greatest() method does not print the greater character.";
+    CustomAssertions._assertEquals(greater, output, errMsg);
   }
 
   @ParameterizedTest
   @MethodSource("inputThreeIntsProvider")
   void correctThreeIntsGreatest(int a, int b, int c, int greatest) throws Throwable {
-    String errMsg = "Your method \"greatest()\" does not print the greatest character.";
-    Object output = MethodUtil.invokeIfMethodExists(OverloadChar.class, "greatest", new Object[] { a, b, c },
-        int.class,
-        int.class, int.class);
-    CustomAssertions._assertEquals(output, greatest, errMsg);
+    Object[][] arguments = {
+            {a, int.class},
+            {b, int.class},
+            {c, int.class}
+    };
+    MethodTest m = new MethodTest(OverloadChar.class, "greatest", arguments);
+    Object output = m.callMethod();
+    String errMsg = "Your greatest() method does not print the greatest character.";
+    CustomAssertions._assertEquals(greatest, output, errMsg);
   }
 }
