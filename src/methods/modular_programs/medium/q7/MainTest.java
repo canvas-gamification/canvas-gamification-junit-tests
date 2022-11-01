f6ae553e8dd7ec172d266b0cb643ee50710f4067
Samantha H <hodge1214@gmail.com>
2022-11-01 15:21:08 -0400

    formatted

--- src/methods/modular_programs/medium/q7/MainTest.java
+++ src/methods/modular_programs/medium/q7/MainTest.java
@@ -16,69 +16,69 @@ import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 
 public class MainTest extends BaseTest {
-  // Java
+    // Java
 
-  public Clause[] testSentence() {
-    TestOption.isInputTest = true;
-    TestOption.defaultInput = "6";
-    return new Clause[] {
-        new StringLiteral("Enter a number: "),
-        new NewLine(),
-        new PlaceHolder(),
-    };
-  }
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "6";
+        return new Clause[]{
+                new StringLiteral("Enter a number: "),
+                new NewLine(),
+                new PlaceHolder(),
+        };
+    }
 
-  public void runMain() {
-    NowThatsPerfect.main(new String[0]);
-  }
+    public void runMain() {
+        NowThatsPerfect.main(new String[0]);
+    }
 
-  static Stream<Integer> perfectNumberProvider() {
-    return Stream.of(6, 28, 496, 8128, 33550336);
-  }
+    static Stream<Integer> perfectNumberProvider() {
+        return Stream.of(6, 28, 496, 8128, 33550336);
+    }
 
-  static Stream<Integer> imperfectNumberProvider() {
-    return Stream.of(1, 2, -1, 27, 11, 9092);
-  }
+    static Stream<Integer> imperfectNumberProvider() {
+        return Stream.of(1, 2, -1, 27, 11, 9092);
+    }
 
-  @ParameterizedTest
-  @MethodSource("perfectNumberProvider")
-  public void correctsPerfectNumberMethod(int n) throws Throwable {
-    Object[][] arguments = {
-            {n, int.class}
-    };
-    MethodTest m = new MethodTest(NowThatsPerfect.class, "perfectOrNot", arguments);
-    Object output = m.callMethod();
-    String errMsg = "Your perfectOrNot() method does not return the correct result for perfect numbers.";
-    CustomAssertions._assertEquals(true, output, errMsg);
-  }
+    @ParameterizedTest
+    @MethodSource("perfectNumberProvider")
+    public void correctsPerfectNumberMethod(int n) throws Throwable {
+        Object[][] arguments = {
+                {n, int.class}
+        };
+        MethodTest m = new MethodTest(NowThatsPerfect.class, "perfectOrNot", arguments);
+        Object output = m.callMethod();
+        String errMsg = "Your perfectOrNot() method does not return the correct result for perfect numbers.";
+        CustomAssertions._assertEquals(true, output, errMsg);
+    }
 
-  @ParameterizedTest
-  @MethodSource("imperfectNumberProvider")
-  public void correctsImperfectNumberMethod(int n) throws Throwable {
-    Object[][] arguments = {
-            {n, int.class}
-    };
-    MethodTest m = new MethodTest(NowThatsPerfect.class, "perfectOrNot", arguments);
-    Object output = m.callMethod();
-    String errMsg = "Your perfectOrNot() method does not return the correct result for imperfect numbers.";
-    CustomAssertions._assertEquals(false, output, errMsg);
-  }
+    @ParameterizedTest
+    @MethodSource("imperfectNumberProvider")
+    public void correctsImperfectNumberMethod(int n) throws Throwable {
+        Object[][] arguments = {
+                {n, int.class}
+        };
+        MethodTest m = new MethodTest(NowThatsPerfect.class, "perfectOrNot", arguments);
+        Object output = m.callMethod();
+        String errMsg = "Your perfectOrNot() method does not return the correct result for imperfect numbers.";
+        CustomAssertions._assertEquals(false, output, errMsg);
+    }
 
-  @ParameterizedTest
-  @MethodSource("perfectNumberProvider")
-  public void printsPerfectNumberCorrects(int n) throws InvalidClauseException {
-    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct message for perfect number.";
-    runWithInput(n + "", new Clause[] {
-        new StringLiteral("perfect"),
-    });
-  }
+    @ParameterizedTest
+    @MethodSource("perfectNumberProvider")
+    public void printsPerfectNumberCorrects(int n) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not print the correct message for perfect number.";
+        runWithInput(n + "", new Clause[]{
+                new StringLiteral("perfect"),
+        });
+    }
 
-  @ParameterizedTest
-  @MethodSource("imperfectNumberProvider")
-  public void printsImperfectNumberCorrects(int n) throws InvalidClauseException {
-    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct message for imperfect number.";
-    runWithInput(n + "", new Clause[] {
-        new StringLiteral("imperfect"),
-    });
-  }
+    @ParameterizedTest
+    @MethodSource("imperfectNumberProvider")
+    public void printsImperfectNumberCorrects(int n) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not print the correct message for imperfect number.";
+        runWithInput(n + "", new Clause[]{
+                new StringLiteral("imperfect"),
+        });
+    }
 }
