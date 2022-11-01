05d135c0a7c50ff069e2b5a494f115558dc7f863
Samantha H <hodge1214@gmail.com>
2022-11-01 15:14:56 -0400

    formatted

--- src/methods/modular_programs/medium/q8/MainTest.java
+++ src/methods/modular_programs/medium/q8/MainTest.java
@@ -17,49 +17,49 @@ import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 
 public class MainTest extends BaseTest {
-  // Java
+    // Java
 
-  public Clause[] testSentence() {
-    TestOption.isInputTest = true;
-    TestOption.defaultInput = " Hello There ";
-    return new Clause[] {
-        new StringLiteral("Enter a sentence: "),
-        new NewLine(),
-        new PlaceHolder(),
-    };
-  }
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = " Hello There ";
+        return new Clause[]{
+                new StringLiteral("Enter a sentence: "),
+                new NewLine(),
+                new PlaceHolder(),
+        };
+    }
 
-  public void runMain() {
-    QueryStr.main(new String[0]);
-  }
+    public void runMain() {
+        QueryStr.main(new String[0]);
+    }
 
-  static Stream<Arguments> inputSentenceProvider() {
-    return Stream.of(
-        Arguments.of(" Hello There ", "%20%Hello%20%There%20%"),
-        Arguments.of(" ", "%20%"),
-        Arguments.of("World", "World"),
-        Arguments.of("The sun will come up tomorrow", "The%20%sun%20%will%20%come%20%up%20%tomorrow"));
-  }
+    static Stream<Arguments> inputSentenceProvider() {
+        return Stream.of(
+                Arguments.of(" Hello There ", "%20%Hello%20%There%20%"),
+                Arguments.of(" ", "%20%"),
+                Arguments.of("World", "World"),
+                Arguments.of("The sun will come up tomorrow", "The%20%sun%20%will%20%come%20%up%20%tomorrow"));
+    }
 
-  @ParameterizedTest
-  @MethodSource("inputSentenceProvider")
-  void methodCleanUpCorrect(String originalStr, String cleanedUpStr) throws Throwable {
-    Object[][] arguments = {
-            {originalStr, String.class}
-    };
-    MethodTest m = new MethodTest(QueryStr.class, "cleanUp", arguments);
-    Object output = m.callMethod();
-    String errMsg = "Your cleanUp() method does not return the correct cleaned up string.";
-    CustomAssertions._assertEquals(cleanedUpStr, output, errMsg);
-  }
+    @ParameterizedTest
+    @MethodSource("inputSentenceProvider")
+    void methodCleanUpCorrect(String originalStr, String cleanedUpStr) throws Throwable {
+        Object[][] arguments = {
+                {originalStr, String.class}
+        };
+        MethodTest m = new MethodTest(QueryStr.class, "cleanUp", arguments);
+        Object output = m.callMethod();
+        String errMsg = "Your cleanUp() method does not return the correct cleaned up string.";
+        CustomAssertions._assertEquals(cleanedUpStr, output, errMsg);
+    }
 
-  @ParameterizedTest
-  @MethodSource("inputSentenceProvider")
-  void printsCorrectOutput(String originalStr, String cleanedUpStr) throws InvalidClauseException {
-    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct cleanup up string for the given string.";
-    runWithInput(originalStr, new Clause[][] { {
-        new StringLiteral("The new query string is: "),
-        new StringLiteral(cleanedUpStr),
-    } });
-  }
+    @ParameterizedTest
+    @MethodSource("inputSentenceProvider")
+    void printsCorrectOutput(String originalStr, String cleanedUpStr) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not print the correct cleanup up string for the given string.";
+        runWithInput(originalStr, new Clause[][]{{
+                new StringLiteral("The new query string is: "),
+                new StringLiteral(cleanedUpStr),
+        }});
+    }
 }
