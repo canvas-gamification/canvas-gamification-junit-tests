741738bcefb3c1703767b7e86fd9b8f7e4aea628
Samantha H <hodge1214@gmail.com>
2022-11-01 15:31:16 -0400

    modified question text and formatted solution code

--- src/methods/modular_programs/hard/q9/TryAngleButReflected.java
+++ src/methods/modular_programs/hard/q9/TryAngleButReflected.java
@@ -1,53 +1,43 @@
 package methods.modular_programs.hard.q9;
 
-/**
- * [J]Write a program with a method called printPattern that takes in an integer
- * n, and print the following pattern using nested loops based on their input:
- * 1 2 3 4 5 … n
- * …
- * 1 2 3 4 5
- * 1 2 3 4
- * 1 2 3
- * 1 2
- * 1
- * 
- * Write a main method that asks the user for the number of lines, and then
- * calls the printPattern method.
- * 
- * Sample output:
- * Enter number of lines: 5
- * 1 2 3 4 5
- * 1 2 3 4
- * 1 2 3
- * 1 2
- * 1
- * 
- * Note: If the user enters a negative number, the program should print an error
- * message instead as seen in the sample
- * output below.
- * Sample output:
- * Enter number of lines: -3
- * Number must be positive
- * 
- */
+/*
+Write a program that calls a method called printPattern. The method takes an integer n, if the number is negative it
+should print "Number must be positive". Otherwise, it prints the following pattern using nested loops based on their input:
+
+1 2 3 4 5 … n
+…
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+
+Sample output:
+Enter number of lines:
+3
+1 2 3
+1 2
+1
+*/
+
 import java.util.Scanner;
 
 public class TryAngleButReflected {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter number of lines: ");
-    int n = input.nextInt();
-    printPattern(n);
-  }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of lines: ");
+        int n = input.nextInt();
+        printPattern(n);
+    }
 
-  public static void printPattern(int n) {
-    if (n <= 0) {
-      System.out.println("Number must be positive");
+    public static void printPattern(int n) {
+        if (n <= 0) {
+            System.out.println("Number must be positive");
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i + 1); j++)
+                System.out.print(j + " ");
+            System.out.println();
+        }
     }
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= (n - i + 1); j++)
-        System.out.print(j + " ");
-      System.out.println();
-    }
-  }
 }
