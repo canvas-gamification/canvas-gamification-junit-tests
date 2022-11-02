8d9c9bbfa6a41bd3ce964dd658ba96e943c86116
Kai <kiamehrkhademi2019@gmail.com>
2022-11-01 21:25:42 -0700

    Modified q10.

--- src/methods/modular_programs/medium/q10/MainTest.java
+++ src/methods/modular_programs/medium/q10/MainTest.java
@@ -41,30 +41,7 @@ public class MainTest extends BaseTest {
         GCDCalculator.main(new String[0]);
     }
 
-    static Stream<Arguments> mainInputProvider() {
-        return Stream.of(
-                Arguments.of("18", "30", "6"),
-                Arguments.of("30", "18", "6"),
-                Arguments.of("99999", "9", "9"),
-                Arguments.of("2", "1", "1"),
-                Arguments.of("29", "7", "1"),
-                Arguments.of("5", "5", "5"),
-                Arguments.of("12", "54", "6"),
-                Arguments.of("0", "1", "1"),
-                Arguments.of("0", "7", "7"),
-                Arguments.of("1", "0", "1")
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("mainInputProvider")
-    void testForMainMethod(String s1, String s2, String exAns) throws InvalidClauseException {
-        TestOption.incorrectStructureErrorMessage = "Your program did not display the correct structure";
-        runWithInput(s1 + " " + s2);
-        assertEquals(getItemByName("ans"), exAns, "Your program does not display the correct answer");
-    }
-
-    static Stream<Arguments> methodInputProvider() {
+    static Stream<Arguments> InputProvider() {
         return Stream.of(
                 Arguments.of(18, 30, 6),
                 Arguments.of(30, 18, 6),
@@ -75,13 +52,24 @@ public class MainTest extends BaseTest {
                 Arguments.of(12, 54, 6),
                 Arguments.of(0, 1, 1),
                 Arguments.of(0, 7, 7),
-                Arguments.of(1, 0, 1)
+                Arguments.of(1, 0, 1),
+                Arguments.of(45, 142, 1),
+                Arguments.of(248, 84, 4),
+                Arguments.of(10000007, 181232123, 1)
         );
     }
 
     @ParameterizedTest
-    @MethodSource("methodInputProvider")
-    void testForFindGCDMethod(int first, int second, int gcd) throws Throwable {
+    @MethodSource("InputProvider")
+    void printsCorrectOutput(int s1, int s2, int exAns) throws InvalidClauseException {
+        runWithInput(s1 + " " + s2);
+        assertEquals(getItemByName("ans"), String.valueOf(exAns),
+                "Your program does not correctly calculate and print the GCD of two positive integers.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("InputProvider")
+    void correctFindGCDMethod(int first, int second, int gcd) throws Throwable {
         Object[][] arguments = {
                 {first, int.class},
                 {second, int.class}
@@ -89,6 +77,6 @@ public class MainTest extends BaseTest {
         MethodTest m = new MethodTest(GCDCalculator.class, "findGCD", arguments);
         Object output = m.callMethod();
         CustomAssertions._assertEquals(gcd, output,
-                "Your findGCD method does not correctly return the gcd of the two input numbers.");
+                "Your findGCD method does not correctly return the gcd of the two input integers.");
     }
 }
