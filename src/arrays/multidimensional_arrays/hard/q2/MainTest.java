34e7ac3821b2dc382617e0f2a907f4de22f8ab25
Samantha H <hodge1214@gmail.com>
2022-11-02 20:44:46 -0400

    added clause

--- src/arrays/multidimensional_arrays/hard/q2/MainTest.java
+++ src/arrays/multidimensional_arrays/hard/q2/MainTest.java
@@ -1,4 +1,35 @@
 package arrays.multidimensional_arrays.hard.q2;
 
-public class MainTest {
+import global.BaseTest;
+import global.tools.TestOption;
+import global.variables.Clause;
+import global.variables.clauses.IntegerLiteral;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.StringLiteral;
+import global.variables.wrappers.Optional;
+
+public class MainTest extends BaseTest {
+    // Java
+
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "3 3 1 2 3 4 5 6 7 8 9";
+        return new Clause[]{
+                new StringLiteral("Enter the number of rows:"),
+                new Optional(new StringLiteral(" ")),
+                new NewLine(),
+                new StringLiteral("Enter the number of column:"),
+                new Optional(new StringLiteral(" ")),
+                new NewLine(),
+                new StringLiteral("Enter the values for the array:"),
+                new Optional(new StringLiteral(" ")),
+                new NewLine(),
+                new StringLiteral("count of even numbers in array = "),
+                new IntegerLiteral("even")
+        };
+    }
+
+    public void runMain(){
+        SimpleDisplay.main(new String[0]);
+    }
 }
