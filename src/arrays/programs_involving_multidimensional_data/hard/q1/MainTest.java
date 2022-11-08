475c623d0ff2728091051b0ceeea5b4136d9b4c5
Samantha H <hodge1214@gmail.com>
2022-11-07 21:46:31 -0500

    wrote clause

--- src/arrays/programs_involving_multidimensional_data/hard/q1/MainTest.java
+++ src/arrays/programs_involving_multidimensional_data/hard/q1/MainTest.java
@@ -1,4 +1,31 @@
 package arrays.programs_involving_multidimensional_data.hard.q1;
 
-public class MainTest {
+import global.BaseTest;
+import global.tools.TestOption;
+import global.variables.Clause;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.PlaceHolder;
+import global.variables.clauses.StringLiteral;
+import global.variables.wrappers.Optional;
+
+public class MainTest extends BaseTest {
+    // Java
+
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "3 2 4 32 4 2 12 32";
+        return new Clause[]{
+                new StringLiteral("Enter the number of rows:"),
+                new Optional(new StringLiteral(" ")),
+                new NewLine(),
+                new StringLiteral("Enter the number of columns:"),
+                new Optional(new StringLiteral(" ")),
+                new NewLine(),
+                new PlaceHolder()
+        };
+    }
+
+    public void runMain(){
+        Sums.main(new String[0]);
+    }
 }
