fbdfd775d91045f7aa28d9b692a66c0b4b764d02
Samantha H <hodge1214@gmail.com>
2022-11-09 14:04:09 -0500

    wrote inputs

--- src/arrays/programs_involving_multidimensional_data/easy/q1/MainTest.java
+++ src/arrays/programs_involving_multidimensional_data/easy/q1/MainTest.java
@@ -2,15 +2,18 @@ package arrays.programs_involving_multidimensional_data.easy.q1;
 
 import global.BaseTest;
 import global.tools.TestOption;
+import global.utils.ArrayUtil;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 
+import java.util.stream.Stream;
+
 public class MainTest extends BaseTest {
     // Parsons
 
-    final int size = 4;
+    static final int size = 4;
 
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
@@ -20,6 +23,36 @@ public class MainTest extends BaseTest {
 
     public void runMain(){
         AllSameSum.main(new String[0]);
+    }
+
+    static Stream<int[][]> inputProvider(){
+        int[][] one = new int[size][size];
+        for(int x = 0; x < size; x ++){
+            one[x] = ArrayUtil.generateRandomArray(0, 10, size);
+        }
+        int[][] two = new int[size][size];
+        for(int x = 0; x < size; x ++){
+            two[x] = ArrayUtil.generateRandomArray(5, 5, size);
+        }
+        int[][] three = new int[size][size];
+        for(int x = 0; x < size; x ++){
+            three[x] = ArrayUtil.generateRandomArray(10, 100, size);
+        }
+        int[][] four = new int[size][size];
+        for(int x = 0; x < size; x ++){
+            four[x] = ArrayUtil.generateRandomArray(0, 10, size);
+        }
+        int[][] five = new int[size][size];
+        for(int x = 0; x < size; x ++){
+            five[x] = ArrayUtil.generateRandomArray(10, 20, size);
+        }
+        for(int x = 0; x < size; x++){
+            for(int y = 0; y < size; y++){
+                five[x][y] *= -1;
+            }
+        }
+
+        return Stream.of(one, two, three, four, five);
     }
 
     public Clause[] clauseBuilder(){
