efc644af2f246bcc804c4b84f5a415ea52cc2045
Samantha H <hodge1214@gmail.com>
2022-11-10 12:18:41 -0500

    changed question text

--- src/arrays/programs_involving_multidimensional_data/easy/q2/LineUps.java
+++ src/arrays/programs_involving_multidimensional_data/easy/q2/LineUps.java
@@ -1,9 +1,10 @@
 package arrays.programs_involving_multidimensional_data.easy.q2;
 
 /*
-A 2D array is used to keep track of the order of the people who appeared in line-ups. In particular, each row represents
-the names of those who lined up for a certain musical event. Make a program that checks if the same name appears in the
-last spot of each line (i.e., each row), and prints "Match" each time a match is found.
+Create a Java program that uses a 2D array to keep track of the order of people who appear in a line. In particular,
+each row represents the names of those who lined up for a musical event. Then, your program should go through all the
+lines and check if the person in the last spot is the same. If it is the same, the program should print "Match". For
+example, if the name "Sam" is last in two of the lines, your program should print "Match" twice.
  */
 
 public class LineUps {
