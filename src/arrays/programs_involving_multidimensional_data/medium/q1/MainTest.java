13b24ec2cd9fca6e46f8895419cb90d1eae2c0b5
Samantha H <hodge1214@gmail.com>
2022-11-12 19:55:48 -0500

    wrote main test

--- src/arrays/programs_involving_multidimensional_data/medium/q1/MainTest.java
+++ src/arrays/programs_involving_multidimensional_data/medium/q1/MainTest.java
@@ -1,20 +1,27 @@
 package arrays.programs_involving_multidimensional_data.medium.q1;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
+import global.utils.ArrayUtil;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
     // Parsons with distractors
 
-    /*
-    Literally all of this now needs to be changed
-     */
     static final int size = 5;
 
     public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = ArrayUtil.arrayToInput(ArrayUtil.generateRandomArray(1, 11, size*size*2));
         return clauseBuilder();
     }
 
@@ -22,16 +29,77 @@ public class MainTest extends BaseTest {
         Identical.main(new String[0]);
     }
 
+    static Stream<Arguments> inputProvider(){
+        int[][] a1 = new int[size][size];
+        int[][] b1 = new int[size][size];
+
+        for(int x = 0; x < size; x++){
+            a1[x] = ArrayUtil.generateRandomArray(1, 11, size);
+            b1[x] = ArrayUtil.generateRandomArray(1, 11, size);
+        }
+
+        int[][] a2 = new int[size][size];
+        int[][] b2 = new int[size][size];
+
+        for(int x = 0; x < size; x++){
+            a2[x] = ArrayUtil.generateRandomArray(10, 1001, size);
+            b2[x] = ArrayUtil.generateRandomArray(10, 1001, size);
+        }
+
+        int[][] a3 = new int[size][size];
+        int[][] b3 = new int[size][size];
+
+        for(int x = 0; x < size; x++){
+            a3[x] = ArrayUtil.generateRandomArray(1, 2, size);
+            b3[x] = ArrayUtil.generateRandomArray(1, 2, size);
+        }
+
+        int[][] a4 = new int[size][size];
+        int[][] b4 = new int[size][size];
+
+        for(int x = 0; x < size; x++){
+            a4[x] = ArrayUtil.generateRandomArray(1, 101, size);
+            b4[x] = ArrayUtil.generateRandomArray(1, 101, size);
+            for(int y = 0; y < size; y++){
+                a4[x][y] *= -1;
+                b4[x][y] *= -1;
+            }
+        }
+
+        return Stream.of(
+                Arguments.of(a1, b1),
+                Arguments.of(a2, b2),
+                Arguments.of(a3, b3),
+                Arguments.of(a4, b4)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void correctMainMethodOutput(int[][] a, int[][] b) throws InvalidClauseException {
+        StringBuilder s = new StringBuilder();
+
+        for(int x = 0; x < size; x ++){
+            s.append(ArrayUtil.arrayToInput(a[x]));
+        }
+        s.append(" ");
+        for(int x = 0; x < size; x ++){
+            s.append(ArrayUtil.arrayToInput(b[x]));
+        }
+
+        TestOption.incorrectStructureErrorMessage = "Your program does not print out the correct number of repetitions of 'different'.";
+        runWithInput(s.toString(), placeHolderClauseBuilder(a, b));
+    }
+
     public Clause[] clauseBuilder() {
-        Clause[] c = new Clause[size * 2 + 1];
+        Clause[] c = new Clause[size * 4 + 1];
         int count = 0;
 
         for (int x = 0; x < size; x++) {
             c[count++] = new StringLiteral("Enter " + size + " values for array a row " + x + ": ");
             c[count++] = new NewLine();
         }
-        c[count++] = new NewLine();
-        for (int x = 0; x < 5; x++) {
+        for (int x = 0; x < size; x++) {
             c[count++] = new StringLiteral("Enter " + size + " values for array b row " + x + ": ");
             c[count++] = new NewLine();
         }
