f670f0a737a7aaf30e44febe10fbd3aeb0fd2d6a
Kai <kiamehrkhademi2019@gmail.com>
2022-11-12 19:20:51 -0800

    Modified q11

--- src/conditionals/programs_with_decision_points/hard/q11/MainTest.java
+++ src/conditionals/programs_with_decision_points/hard/q11/MainTest.java
@@ -8,6 +8,7 @@ import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -46,67 +47,32 @@ public class MainTest extends BaseTest {
             return false;
     }
 
-    static int[][] qualifieds = new int[diff * 2 + 1][2];
-    static int[][] unqualifieds = new int[360 - diff * 2 - 1][2];
-
-    static Stream<int[]> InputProviderQualified() {
-        int t = 0;
-        for (int i = 1; i <= 12; i++) {
-            if (calcAns(1, i))
-                qualifieds[t++] = new int[]{1, i};
-            if (calcAns(30, 12))
-                qualifieds[t++] = new int[]{30, i};
-        }
-        for (int i = 2; i < 30; i++) {
-            for (int j = 1; j <= 12; j++) {
-                if (calcAns(i, j) && !calcAns(i - 1, j))
-                    qualifieds[t++] = new int[]{i, j};
-                if (calcAns(i, j) && !calcAns(i + 1, j))
-                    qualifieds[t++] = new int[]{i, j};
-            }
-        }
-        int[][] ans = new int[t][2];
-        for (int i = 0; i < t; i++)
-            ans[i] = qualifieds[i];
-        return Stream.of(ans);
+    static Stream<Arguments> InputProvider() {
+        return Stream.of(
+                Arguments.of(1, 1),
+                Arguments.of(30, 12),
+                Arguments.of(2, 1),
+                Arguments.of(2, 30),
+                Arguments.of(currentDay, currentMonth),
+                Arguments.of(1, currentMonth),
+                Arguments.of(30, currentMonth),
+                Arguments.of((currentDay + diff) % 30, (currentMonth + diff / 30) % 12),
+                Arguments.of((currentDay + diff + 1) % 30, (currentMonth + diff / 30) % 12),
+                Arguments.of((currentDay - diff + 30) % 30, (currentMonth - diff / 30 + 12) % 12),
+                Arguments.of((currentDay - diff - 1 + 30) % 30, (currentMonth - diff / 30 + 12) % 12)
+        );
     }
 
     @ParameterizedTest
-    @MethodSource("InputProviderQualified")
-    void testWithInputQualified(int[] in) throws InvalidClauseException {
-        TestOption.incorrectStructureErrorMessage = "Your program did not display the correct answer";
-        runWithInput(in[0] + " " + in[1], new Clause[]{
-                new StringLiteral("You qualify for a 10% discount!")});
-    }
-
-    static Stream<int[]> InputProviderUnqualified() {
-        int t = 0;
-        for (int i = 1; i <= 12; i++) {
-            if (!calcAns(1, i))
-                unqualifieds[t++] = new int[]{1, i};
-            if (!calcAns(30, 12))
-                unqualifieds[t++] = new int[]{30, i};
+    @MethodSource("InputProvider")
+    void identifiesDiscountCorrectly(int a, int b) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program did not correctly identify the qualification for the discount.";
+        if (calcAns(a, b)) {
+            runWithInput(a + " " + b, new Clause[]{
+                    new StringLiteral("You qualify for a 10% discount!")});
+        } else {
+            runWithInput(a + " " + b, new Clause[]{
+                    new StringLiteral("You do not qualify for any discounts.")});
         }
-        for (int i = 2; i < 30; i++) {
-            for (int j = 1; j <= 12; j++) {
-                if (!calcAns(i, j) && calcAns(i - 1, j))
-                    unqualifieds[t++] = new int[]{i, j};
-                if (!calcAns(i, j) && calcAns(i + 1, j))
-                    unqualifieds[t++] = new int[]{i, j};
-            }
-        }
-        int[][] ans = new int[t][2];
-        for (int i = 0; i < t; i++)
-            ans[i] = qualifieds[i];
-        return Stream.of(ans);
     }
-
-    @ParameterizedTest
-    @MethodSource("InputProviderUnqualified")
-    void testWithInputUnqualified(int[] in) throws InvalidClauseException {
-        TestOption.incorrectStructureErrorMessage = "Your program did not display the correct answer";
-        runWithInput(in[0] + " " + in[1], new Clause[]{
-                new StringLiteral("You do not qualify for any discounts.")});
-    }
-
 }
