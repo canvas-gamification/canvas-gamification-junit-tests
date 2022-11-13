37b35bb189488a437305b112cef9d5a76c451b45
Samantha H <hodge1214@gmail.com>
2022-11-12 19:39:34 -0500

    adjusted clause builder and place holder builder

--- src/arrays/programs_involving_multidimensional_data/medium/q1/Identical.java
+++ src/arrays/programs_involving_multidimensional_data/medium/q1/Identical.java
@@ -1,30 +1,30 @@
 package arrays.programs_involving_multidimensional_data.medium.q1;
 
 /*
-Write a Java program that initializes two integers arrays of size 5 by 5and takes user input values for them. Then,
+Write a Java program that initializes two integers arrays of size 5 by 5 and takes user input values for them. Then,
 check to see if each of their elements in the respective positions are the same. If any pair of elements are not the
 same, print "different", otherwise do nothing.
 
 Sample output:
-    Enter five values for array a row 0:
+    Enter 5 values for array a row 0:
     1 2 3 4 5
-    Enter five values for array a row 1:
+    Enter 5 values for array a row 1:
     1 2 3 4 5
-    Enter five values for array a row 2:
+    Enter 5 values for array a row 2:
     1 2 3 4 5
-    Enter five values for array a row 3:
+    Enter 5 values for array a row 3:
     1 2 3 4 5
-    Enter five values for array a row 4:
+    Enter 5 values for array a row 4:
     1 2 3 4 5
-    Enter five values for array b row 0:
+    Enter 5 values for array b row 0:
     1 2 3 4 5
-    Enter five values for array b row 1:
+    Enter 5 values for array b row 1:
     1 2 3 4 5
-    Enter five values for array b row 2:
+    Enter 5 values for array b row 2:
     5 4 3 2 1
-    Enter five values for array b row 3:
+    Enter 5 values for array b row 3:
     1 2 3 4 5
-    Enter five values for array b row 4:
+    Enter 5 values for array b row 4:
     1 2 3 4 5
     different
     different
@@ -53,7 +53,7 @@ public class Identical {
         int[][] a = new int[5][5];
         int[][] b = new int[5][5];
         for (int i = 0; i < a.length; i++) {
-            System.out.println("Enter five values for array a row " + i + ": ");
+            System.out.println("Enter 5 values for array a row " + i + ": ");
             a[i][0] = input.nextInt();
             a[i][1] = input.nextInt();
             a[i][2] = input.nextInt();
@@ -61,7 +61,7 @@ public class Identical {
             a[i][4] = input.nextInt();
         }
         for (int i = 0; i < a.length; i++) {
-            System.out.println("Enter five values for array b row " + i + ": ");
+            System.out.println("Enter 5 values for array b row " + i + ": ");
             b[i][0] = input.nextInt();
             b[i][1] = input.nextInt();
             b[i][2] = input.nextInt();
--- src/arrays/programs_involving_multidimensional_data/medium/q1/MainTest.java
+++ src/arrays/programs_involving_multidimensional_data/medium/q1/MainTest.java
@@ -4,7 +4,6 @@ import global.BaseTest;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
-import global.variables.clauses.RandomInteger;
 import global.variables.clauses.StringLiteral;
 
 public class MainTest extends BaseTest {
@@ -13,45 +12,52 @@ public class MainTest extends BaseTest {
     /*
     Literally all of this now needs to be changed
      */
+    static final int size = 5;
+
     public Clause[] testSentence() {
         return clauseBuilder();
     }
 
-    public void runMain(){
+    public void runMain() {
         Identical.main(new String[0]);
     }
 
-    public Clause[] clauseBuilder(){
-        Clause[] c = new Clause[113];
+    public Clause[] clauseBuilder() {
+        Clause[] c = new Clause[size * 2 + 1];
         int count = 0;
 
-        for(int x = 0; x <5; x++){
-            for(int y = 0; y < 5; y++){
-                c[count++] = new RandomInteger(0, 6, "aR"+x+"C"+y);
-                c[count++] = new StringLiteral(" ");
-            }
+        for (int x = 0; x < size; x++) {
+            c[count++] = new StringLiteral("Enter " + size + " values for array a row " + x + ": ");
             c[count++] = new NewLine();
         }
         c[count++] = new NewLine();
-        for(int x = 0; x <5; x++){
-            for(int y = 0; y < 5; y++){
-                c[count++] = new RandomInteger(0, 6, "bR"+x+"C"+y);
-                c[count++] = new StringLiteral(" ");
-            }
+        for (int x = 0; x < 5; x++) {
+            c[count++] = new StringLiteral("Enter " + size + " values for array b row " + x + ": ");
             c[count++] = new NewLine();
         }
-        c[count++] = new NewLine();
         c[count] = new PlaceHolder();
 
         return c;
     }
 
-    public void placeHolderClauseBuilder(){
-        int[][] a = new int[5][5];
-        int[][] b = new int[5][5];
+    public Clause[][] placeHolderClauseBuilder(int[][] a, int[][] b) {
+        int differences = 0;
 
+        for(int x = 0; x < size; x++){
+            for(int y = 0; y < size; y++){
+                if(a[x][y] != b[x][y])
+                    differences++;
+            }
+        }
 
+        Clause[][] c = new Clause[1][differences*2];
+        int count = 0;
 
-        Clause[][] c = new Clause[1][];
+        for(int x = 0; x < differences; x++){
+            c[0][count++] = new StringLiteral("different");
+            c[0][count++] = new NewLine();
+        }
+
+        return c;
     }
 }
