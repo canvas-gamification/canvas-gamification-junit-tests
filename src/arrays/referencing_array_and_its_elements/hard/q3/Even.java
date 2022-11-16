30b689044fe63b863438a60b7ef3dbceb6f64844
Kai <kiamehrkhademi2019@gmail.com>
2022-11-16 12:41:10 -0800

    Modified q3

---
+++ src/arrays/referencing_array_and_its_elements/hard/q3/Even.java
@@ -0,0 +1,43 @@
+package arrays.referencing_array_and_its_elements.hard.q3;
+
+/*
+Write a java program that accepts an array of 10 numbers from the user and then creates a new array of only even numbers
+from the original array. To do this, create a method named even that accepts an integer array, counts the number of even
+integers in the array, and then transfers them to a new array of that length in the same order, note that you method
+must be general enough to work with any size of array. Then, return the new array to the main method, and print out its
+elements.
+
+Sample Output:
+    Enter ten positive integers:
+    5 7 6 8 4 1 2 3 10 9
+    The array with only even numbers is:
+    6 8 4 2 10
+ */
+
+import java.util.Scanner;
+
+public class Even {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 10 positive integers: ");
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = input.nextInt();
+        int[] evens = even(arr);
+        System.out.println("The array with only even numbers is: ");
+        for (int i = 0; i < evens.length; i++)
+            System.out.print(evens[i] + " ");
+    }
+
+    public static int[] even(int[] arr) {
+        int numEven = 0;
+        for (int i = 0; i < arr.length; i++)
+            numEven += arr[i] % 2 == 0 ? 1 : 0;
+        int[] evenNumbers = new int[numEven];
+        int evenIndices = 0;
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] % 2 == 0)
+                evenNumbers[evenIndices++] = arr[i];
+        return evenNumbers;
+    }
+}
--- src/arrays/referencing_array_and_its_elements/hard/q3/MainTest.java
+++ src/arrays/referencing_array_and_its_elements/hard/q3/MainTest.java
@@ -9,6 +9,7 @@ import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
+import global.variables.wrappers.Optional;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -25,31 +26,33 @@ public class MainTest extends BaseTest {
 
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
-        TestOption.defaultInput = "1 2 3 4 5 6 7 8 9 10";
+        TestOption.defaultInput = arrayToInput(generateRandomArray(-100, 100, n));
         return new Clause[]{
-                new StringLiteral("Enter 10 positive integers:"),
+                new StringLiteral("Enter " + n + " positive integers:"),
+                new Optional(new StringLiteral(" ")),
                 new NewLine(),
-                new StringLiteral("The separated array is:"),
+                new StringLiteral("The array with only even numbers is:"),
+                new Optional(new StringLiteral(" ")),
                 new NewLine(),
-                new PlaceHolder()
+                new PlaceHolder(),
+                new Optional(new StringLiteral(" ")),
         };
     }
 
     public void runMain() {
-        EvenorOdd.main(new String[0]);
+        Even.main(new String[0]);
     }
 
-    public static String separated(int[] a) {
-        String ans = "";
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] % 2 == 0)
-                ans += a[i] + " ";
-        }
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] % 2 != 0)
-                ans += a[i] + " ";
-        }
-        return ans.trim();
+    public static int[] getEven(int[] arr) {
+        int numEven = 0;
+        for (int i = 0; i < arr.length; i++)
+            numEven += arr[i] % 2 == 0 ? 1 : 0;
+        int[] evenNumbers = new int[numEven];
+        int evenIndices = 0;
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] % 2 == 0)
+                evenNumbers[evenIndices++] = arr[i];
+        return evenNumbers;
     }
 
     static Stream<Arguments> mainInputProvider() {
@@ -57,42 +60,43 @@ public class MainTest extends BaseTest {
         int[] a2 = generateRandomArray(1, 1000000000, n);
         int[] a3 = generateRandomArray(1, 1000000000, n);
         return Stream.of(
-                Arguments.of(a1,
-                        Arguments.of(a3),
-                        Arguments.of(a3)
-                ));
+                Arguments.of(a1),
+                Arguments.of(a2),
+                Arguments.of(a3)
+        );
     }
 
     @ParameterizedTest
     @MethodSource("mainInputProvider")
     public void correctMainMethod(int[] input) throws InvalidClauseException {
-        TestOption.incorrectStructureErrorMessage = "Your main method does not print the correct separated array";
+        TestOption.incorrectStructureErrorMessage = "Your main method does not print the correct even array";
         runWithInput(arrayToInput(input), new Clause[]{
-                new StringLiteral(separated(input))
+                new StringLiteral(arrayToInput(getEven(input)).trim())
         });
     }
 
     static Stream<Arguments> InputProvider() {
         return Stream.of(
-                Arguments.of(new int[]{3, 4, 6, 21, 48, 42, 89, 2, 5, 91}, "4 6 48 42 2 3 21 89 5 91"),
-                Arguments.of(new int[]{}, ""),
-                Arguments.of(new int[]{1, 3, 5}, "1 3 5"),
-                Arguments.of(new int[]{2, 4, 6}, "2 4 6"),
-                Arguments.of(new int[]{5, 3, 1, 6, 4, 2}, "6 4 2 5 3 1")
+                Arguments.of(new int[]{3, 4, 6, 21, 48, 42, 89, 2, 5, 91}),
+                Arguments.of(new int[]{}),
+                Arguments.of(new int[]{1, 3, 5}),
+                Arguments.of(new int[]{2, 4, 6}),
+                Arguments.of(new int[]{5, 3, 1, 6, 4, 2}),
+                Arguments.of(generateRandomArray(1, 1000000000, 100000))
         );
     }
 
 
     @ParameterizedTest
     @MethodSource("InputProvider")
-    void testForEvenorOddMethod(int[] input, String ans) throws Throwable {
+    void correctEvenMethod(int[] input) throws Throwable {
         Object[][] arguments = {
                 {input, int[].class},
         };
-        MethodTest m = new MethodTest(EvenorOdd.class, "EorO", arguments);
+        MethodTest m = new MethodTest(Even.class, "even", arguments);
         Object output = m.callMethod();
-        CustomAssertions._assertEquals(ans, output,
-                "Your EorO method does not return the correct string of numbers");
+        CustomAssertions._assertArrayEquals(getEven(input), output,
+                "Your even method does not return the correct even array");
 
 
     }
