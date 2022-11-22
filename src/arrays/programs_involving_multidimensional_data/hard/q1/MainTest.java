70f7ef130185decad18a46befba5ef6d55420a66
Samantha H <hodge1214@gmail.com>
2022-11-22 11:54:25 -0500

    made changes

--- src/arrays/programs_involving_multidimensional_data/hard/q1/MainTest.java
+++ src/arrays/programs_involving_multidimensional_data/hard/q1/MainTest.java
@@ -2,14 +2,12 @@ package arrays.programs_involving_multidimensional_data.hard.q1;
 
 import global.BaseTest;
 import global.MethodTest;
-import global.exceptions.InvalidClauseException;
 import global.tools.CustomAssertions;
 import global.tools.TestOption;
 import global.utils.ArrayUtil;
 import global.variables.Clause;
 import global.variables.clauses.IntegerLiteral;
 import global.variables.clauses.NewLine;
-import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 import global.variables.wrappers.Optional;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -22,18 +20,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MainTest extends BaseTest {
     // Java
 
+    final static int size = 4;
+
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
-        TestOption.defaultInput = "3 2 4 32 4 2 12 32";
-        return new Clause[]{
-                new StringLiteral("Enter the number of rows:"),
-                new Optional(new StringLiteral(" ")),
-                new NewLine(),
-                new StringLiteral("Enter the number of columns:"),
-                new Optional(new StringLiteral(" ")),
-                new NewLine(),
-                new PlaceHolder()
-        };
+        TestOption.defaultInput = ArrayUtil.arrayToInput(ArrayUtil.generateRandomArray(1, 11, size * size));
+        return clauseBuilder();
     }
 
     public void runMain() {
@@ -41,57 +33,54 @@ public class MainTest extends BaseTest {
     }
 
     static Stream<int[][]> mainMethodInputProvider() {
-        int[][] a = new int[8][3];
-
-        for (int x = 0; x < 8; x++) {
-            a[x] = ArrayUtil.generateRandomArray(1, 11, 3);
+        int[][] a = new int[size][size];
+        for (int x = 0; x < size; x++) {
+            a[x] = ArrayUtil.generateRandomArray(1, 11, size);
         }
 
-        int[][] b = new int[2][2];
-
-        for (int x = 0; x < 2; x++) {
-            b[x] = ArrayUtil.generateRandomArray(10, 1001, 2);
+        int[][] b = new int[size][size];
+        for (int x = 0; x < size; x++) {
+            b[x] = ArrayUtil.generateRandomArray(10, 1001, size);
         }
 
-        int[][] c = new int[5][10];
-
-        for (int x = 0; x < 5; x++) {
-            c[x] = ArrayUtil.generateRandomArray(1, 2, 10);
+        int[][] c = new int[size][size];
+        for (int x = 0; x < size; x++) {
+            c[x] = ArrayUtil.replicateArray(2, size);
         }
 
-        int[][] d = new int[15][2];
-
-        for (int x = 0; x < 15; x++) {
-            d[x] = ArrayUtil.generateRandomArray(1, 11, 3);
-            for (int y = 0; y < 2; y++) {
+        int[][] d = new int[size][size];
+        for (int x = 0; x < size; x++) {
+            d[x] = ArrayUtil.generateRandomArray(1, 11, size);
+            for (int y = 0; y < size; y++) {
                 d[x][y] *= -1;
             }
         }
 
-        return Stream.of(a, b, c, d);
+        int[][] e = new int[size][size];
+        for (int x = 0; x < size; x++) {
+            e[x] = ArrayUtil.generateAscendingArray(1, size, 3);
+        }
+
+        return Stream.of(a, b, c, d, e);
     }
 
     static Stream<int[][]> sumsInputProvider() {
         int[][] a = new int[7][3];
-
         for (int x = 0; x < 7; x++) {
             a[x] = ArrayUtil.generateRandomArray(0, 21, 3);
         }
 
         int[][] b = new int[4][4];
-
         for (int x = 0; x < 4; x++) {
             b[x] = ArrayUtil.generateRandomArray(1, 100001, 4);
         }
 
         int[][] c = new int[2][9];
-
         for (int x = 0; x < 2; x++) {
-            c[x] = ArrayUtil.generateRandomArray(15, 16, 9);
+            c[x] = ArrayUtil.replicateArray(15, 9);
         }
 
         int[][] d = new int[17][4];
-
         for (int x = 0; x < 17; x++) {
             d[x] = ArrayUtil.generateRandomArray(1, 16, 4);
             for (int y = 0; y < 4; y++) {
@@ -99,29 +88,34 @@ public class MainTest extends BaseTest {
             }
         }
 
-        return Stream.of(a, b, c, d);
+        int[][] e = new int[26][43];
+        for (int x = 0; x < 26; x++) {
+            e[x] = ArrayUtil.generateAscendingArray(1, 43, 5);
+        }
+
+        return Stream.of(a, b, c, d, e);
     }
 
     @ParameterizedTest
     @MethodSource("mainMethodInputProvider")
-    void correctMainMethodOutput(int[][] input) throws InvalidClauseException {
+    void correctMainMethodOutput(int[][] input) {
         StringBuilder s = new StringBuilder();
-        for (int x = 0; x < input.length; x++) {
-            s.append(ArrayUtil.arrayToInput(input[x]));
+        for (int[] ints : input) {
+            s.append(ArrayUtil.arrayToInput(ints));
             s.append(" ");
         }
-        runWithInput(input.length + " " + input[0].length + " " + s, clauseBuilder(input.length, input[0].length));
+        runWithInput(s.toString());
 
-        int[] sums = new int[input.length];
+        int[] sums = new int[size];
 
-        for (int x = 0; x < input.length; x++) {
-            for (int y = 0; y < input[0].length; y++) {
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
                 sums[x] += input[x][y];
             }
         }
 
-        for (int x = 0; x < input.length; x++) {
-            assertEquals(sums[x], Integer.parseInt(getItemByName(String.valueOf(x))), "Your program does not print the correct sum of rows.");
+        for (int x = 0; x < size; x++) {
+            assertEquals(sums[x], Integer.parseInt(getItemByName(String.valueOf(x))), "Your program does not print the correct sum of the elements in a row.");
         }
     }
 
@@ -142,21 +136,19 @@ public class MainTest extends BaseTest {
         CustomAssertions._assertArrayEquals(sums, output, "Your sums method does not correctly calculate the sum of each row.");
     }
 
-    public Clause[][] clauseBuilder(int rows, int cols) {
-        Clause[][] c = new Clause[1][(rows * cols * 3) + (rows * 2)];
+    public Clause[] clauseBuilder() {
+        Clause[] c = new Clause[size * 3 + size * 2];
         int count = 0;
 
-        for (int x = 0; x < rows; x++) {
-            for (int y = 0; y < cols; y++) {
-                c[0][count++] = new StringLiteral("Enter the number for row " + (x + 1) + " column " + (y + 1) + ":");
-                c[0][count++] = new Optional(new StringLiteral(" "));
-                c[0][count++] = new NewLine();
-            }
+        for (int x = 0; x < size; x++) {
+            c[count++] = new StringLiteral("Enter " + size + " numbers for row " + (x + 1) + ":");
+            c[count++] = new Optional(new StringLiteral(" "));
+            c[count++] = new NewLine();
         }
 
-        for (int x = 0; x < rows; x++) {
-            c[0][count++] = new IntegerLiteral(String.valueOf(x));
-            c[0][count++] = new StringLiteral(" ");
+        for (int x = 0; x < size; x++) {
+            c[count++] = new IntegerLiteral(String.valueOf(x));
+            c[count++] = new StringLiteral(" ");
         }
 
         return c;
--- src/arrays/programs_involving_multidimensional_data/hard/q1/Sums.java
+++ src/arrays/programs_involving_multidimensional_data/hard/q1/Sums.java
@@ -1,28 +1,20 @@
 package arrays.programs_involving_multidimensional_data.hard.q1;
 
 /*
-Write a Java program called Sums. It should have a main method which takes user input to create a 2D array and calls
-another method called sums. The sums method should take a 2D array as a parameter and return a 1D array that is the sum
-of each row in the 2D array. The main method should then print the 1D array.
+Write a Java program called Sums. It should have a main method which takes user input to create a 4 by 4 2D integer
+array and calls another method called sums. The sums method should take a 2D integer array as a parameter and return a
+1D integer array which is the sum of each row in the 2D array. The main method should then print the 1D array.
 
 Sample output:
-    Enter the number of rows:
-    2
-    Enter the number of columns:
-    3
-    Enter the number for row 1 column 1:
-    2
-    Enter the number for row 1 column 2:
-    3
-    Enter the number for row 1 column 3:
-    4
-    Enter the number for row 2 column 1:
-    2
-    Enter the number for row 2 column 2:
-    5
-    Enter the number for row 2 column 3:
-    8
-    9 15
+    Enter 4 numbers for row 1:
+    3 5 1 2
+    Enter 4 numbers for row 2:
+    6 5 3 10
+    Enter 4 numbers for row 3:
+    20 4 1 4
+    Enter 4 numbers for row 4:
+    7 15 6 2
+    11 24 29 30
  */
 
 import java.util.Scanner;
@@ -31,15 +23,11 @@ public class Sums {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter the number of rows: ");
-        int rows = input.nextInt();
-        System.out.println("Enter the number of columns: ");
-        int cols = input.nextInt();
-        int[][] arr = new int[rows][cols];
+        int[][] arr = new int[4][4];
 
-        for (int x = 0; x < rows; x++) {
-            for (int y = 0; y < cols; y++) {
-                System.out.println("Enter the number for row " + (x + 1) + " column " + (y + 1) + ": ");
+        for (int x = 0; x < 4; x++) {
+            System.out.println("Enter 4 numbers for row " + (x + 1) + ": ");
+            for (int y = 0; y < 4; y++) {
                 arr[x][y] = input.nextInt();
             }
         }
