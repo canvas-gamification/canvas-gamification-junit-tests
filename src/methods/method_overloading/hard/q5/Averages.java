e871a5a90cb267ce82c673ed7fbfe9b6368a3b6f
Samantha H <hodge1214@gmail.com>
2022-11-25 02:00:12 -0500

    made changes

--- src/methods/method_overloading/hard/q5/Averages.java
+++ src/methods/method_overloading/hard/q5/Averages.java
@@ -1,8 +1,10 @@
 package methods.method_overloading.hard.q5;
 
 /*
-Write a Java Program that uses a method called average to calculate the average of 3, 4 and 5 integers which are then
-displayed in the main method. Use method overloading in your solution.
+Write a Java program to calculate the average of sets of 3, 4, and 5 numbers. To do this, create a method called average
+and overload it to accept 3, 4, and integers, and then returns the average of the set of numbers as a double. Your
+program should prompt the user to enter a set of 3, 4, and 5 numbers, calculate the average for each set and print out
+the average in the main method.
 
 Sample Output:
   Enter three numbers:
@@ -11,9 +13,9 @@ Sample Output:
   6 31 3 51
   Enter five numbers:
   2 1 4 5 999
-  The average of the following numbers is: 2
-  The average of the following numbers is: 22
-  The average of the following numbers is: 202
+  The average of the following numbers is: 2.3333333333333335
+  The average of the following numbers is: 22.75
+  The average of the following numbers is: 202.2
  */
 
 import java.util.Scanner;
@@ -41,15 +43,15 @@ public class Averages {
         System.out.println("The average of the following numbers is: " + average(n8, n9, n10, n11, n12));
     }
 
-    public static int average(int num1, int num2, int num3) {
-        return (num1 + num2 + num3) / 3;
+    public static double average(int num1, int num2, int num3) {
+        return (num1 + num2 + num3) / 3.0;
     }
 
-    public static int average(int num1, int num2, int num3, int num4) {
-        return (num1 + num2 + num3 + num4) / 4;
+    public static double average(int num1, int num2, int num3, int num4) {
+        return (num1 + num2 + num3 + num4) / 4.0;
     }
 
-    public static int average(int num1, int num2, int num3, int num4, int num5) {
-        return (num1 + num2 + num3 + num4 + num5) / 5;
+    public static double average(int num1, int num2, int num3, int num4, int num5) {
+        return (num1 + num2 + num3 + num4 + num5) / 5.0;
     }
 }
--- src/methods/method_overloading/hard/q5/MainTest.java
+++ src/methods/method_overloading/hard/q5/MainTest.java
@@ -4,6 +4,7 @@ import java.util.stream.Stream;
 
 import global.MethodTest;
 import global.tools.TestOption;
+import global.variables.clauses.DoubleLiteral;
 import global.variables.wrappers.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,7 +14,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import global.BaseTest;
 import global.tools.CustomAssertions;
 import global.variables.Clause;
-import global.variables.clauses.IntegerLiteral;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.StringLiteral;
 
@@ -36,13 +36,13 @@ public class MainTest extends BaseTest {
                 new Optional(new StringLiteral(" ")),
                 new NewLine(),
                 new StringLiteral("The average of the following numbers is: "),
-                new IntegerLiteral("threeInt"),
+                new DoubleLiteral("threeDouble"),
                 new NewLine(),
                 new StringLiteral("The average of the following numbers is: "),
-                new IntegerLiteral("fourInt"),
+                new DoubleLiteral("fourDouble"),
                 new NewLine(),
                 new StringLiteral("The average of the following numbers is: "),
-                new IntegerLiteral("fiveInt"),
+                new DoubleLiteral("fiveDouble"),
         };
     }
 
@@ -52,29 +52,29 @@ public class MainTest extends BaseTest {
 
     static Stream<Arguments> threeNumbersAndTheirAverageProvider() {
         return Stream.of(
-                Arguments.of(2, 1, 4, 2),
-                Arguments.of(6, 31, 3, 13),
+                Arguments.of(2, 1, 4, 2.333333333333333),
+                Arguments.of(6, 31, 3, 13.33333333333333),
                 Arguments.of(76, 122, -123, 25),
-                Arguments.of(-2, -2, -1, -1));
+                Arguments.of(-2, -2, -1, -1.666666666666666));
     }
 
     static Stream<Arguments> fourNumbersAndTheirAverageProvider() {
         return Stream.of(
                 Arguments.of(2, 2, 4, 4, 3),
                 Arguments.of(6, 31, 3, 52, 23),
-                Arguments.of(-2, -2, -1, -2, -1));
+                Arguments.of(-2, -2, -1, -2, -1.75));
     }
 
     static Stream<Arguments> fiveNumbersAndTheirAverageProvider() {
         return Stream.of(
-                Arguments.of(5, 0, 1, 2, 3, 2),
-                Arguments.of(6, -31, 3, -52, 23, -10),
+                Arguments.of(5, 0, 1, 2, 3, 2.2),
+                Arguments.of(6, -31, 3, -52, 23, -10.2),
                 Arguments.of(-2, -2, -1, -2, -3, -2));
     }
 
     @ParameterizedTest
     @MethodSource("threeNumbersAndTheirAverageProvider")
-    void threeNumbersAverageCorrect(int a, int b, int c, int avg) throws Throwable {
+    void threeNumbersAverageCorrect(int a, int b, int c, double avg) throws Throwable {
         Object[][] arguments = {
                 {a, int.class},
                 {b, int.class},
@@ -83,12 +83,12 @@ public class MainTest extends BaseTest {
         MethodTest m = new MethodTest(Averages.class, "average", arguments);
         Object output = m.callMethod();
         String errMsg = "Your average method does not correctly calculate the average of three numbers";
-        CustomAssertions._assertEquals(avg, output, errMsg);
+        CustomAssertions._assertEquals(avg, output, 0.000001, errMsg);
     }
 
     @ParameterizedTest
     @MethodSource("fourNumbersAndTheirAverageProvider")
-    void fourNumbersAverageCorrect(int a, int b, int c, int d, int avg) throws Throwable {
+    void fourNumbersAverageCorrect(int a, int b, int c, int d, double avg) throws Throwable {
         Object[][] arguments = {
                 {a, int.class},
                 {b, int.class},
@@ -98,12 +98,12 @@ public class MainTest extends BaseTest {
         MethodTest m = new MethodTest(Averages.class, "average", arguments);
         Object output = m.callMethod();
         String errMsg = "Your average method does not correctly calculate the average of four numbers";
-        CustomAssertions._assertEquals(avg, output, errMsg);
+        CustomAssertions._assertEquals(avg, output, 0.000001, errMsg);
     }
 
     @ParameterizedTest
     @MethodSource("fiveNumbersAndTheirAverageProvider")
-    void fiveNumbersAverageCorrect(int a, int b, int c, int d, int e, int avg) throws Throwable {
+    void fiveNumbersAverageCorrect(int a, int b, int c, int d, int e, double avg) throws Throwable {
         Object[][] arguments = {
                 {a, int.class},
                 {b, int.class},
@@ -114,15 +114,15 @@ public class MainTest extends BaseTest {
         MethodTest m = new MethodTest(Averages.class, "average", arguments);
         Object output = m.callMethod();
         String errMsg = "Your average method does not correctly calculate the average of five numbers";
-        CustomAssertions._assertEquals(avg, output, errMsg);
+        CustomAssertions._assertEquals(avg, output, 0.000001, errMsg);
     }
 
     @Test
     void printsCorrectMessage() {
         runWithInput("32 593 213 493 593 23 12 3492 95 483 102 4");
 
-        assertEquals(279, Integer.parseInt(getItemByName("threeInt")), "Your program does not print the correct average for three numbers");
-        assertEquals(280, Integer.parseInt(getItemByName("fourInt")), "Your program does not print the correct average for four numbers");
-        assertEquals(835, Integer.parseInt(getItemByName("fiveInt")), "Your program does not print the correct average for five numbers");
+        assertEquals(279.3333333333333, Double.parseDouble(getItemByName("threeDouble")), 0.000001, "Your program does not print the correct average for three numbers");
+        assertEquals(280.25, Double.parseDouble(getItemByName("fourDouble")), 0.000001, "Your program does not print the correct average for four numbers");
+        assertEquals(835.2, Double.parseDouble(getItemByName("fiveDouble")), 0.000001, "Your program does not print the correct average for five numbers");
     }
 }
