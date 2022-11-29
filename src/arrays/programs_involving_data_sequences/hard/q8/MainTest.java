077aec69660b98d9f3ac9bb6168b0f16a15bf0fe
Kai <kiamehrkhademi2019@gmail.com>
2022-11-29 04:00:56 -0800

    Modified q8

--- src/arrays/programs_involving_data_sequences/hard/q8/MainTest.java
+++ src/arrays/programs_involving_data_sequences/hard/q8/MainTest.java
@@ -3,6 +3,7 @@ package arrays.programs_involving_data_sequences.hard.q8;
 import global.BaseTest;
 import global.MethodTest;
 import global.exceptions.InvalidClauseException;
+import global.tools.CustomAssertions;
 import global.tools.TestOption;
 import global.variables.Clause;
 import global.variables.clauses.*;
@@ -11,6 +12,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Currency;
 import java.util.stream.Stream;
 
 import static global.utils.ArrayUtil.*;
@@ -49,8 +51,8 @@ public class MainTest extends BaseTest {
 
     static Stream<Arguments> mainInputProvider() {
         return Stream.of(
-                Arguments.of(generateRandomArray(-10000, 10000, n)),
-                Arguments.of(generateRandomArray(-10000, 10000, n)),
+                Arguments.of(generateRandomArray(-100, 100, n)),
+                Arguments.of(generateRandomArray(-1000, 1000, n)),
                 Arguments.of(generateRandomArray(-10000, 10000, n)),
                 Arguments.of(generateRandomArray(-1, 2, n)),
                 Arguments.of(replicateArray(1, n)),
@@ -72,25 +74,31 @@ public class MainTest extends BaseTest {
 
     static Stream<Arguments> methodInputProvider() {
         return Stream.of(
-                Arguments.of(generateRandomArray(-10000, 10000, 10000)),
-                Arguments.of(generateRandomArray(-10000, 10000, 10000)),
+                Arguments.of(generateRandomArray(-100, 100, 100)),
+                Arguments.of(generateRandomArray(-1000, 1000, 1000)),
                 Arguments.of(generateRandomArray(-10000, 10000, 1000)),
                 Arguments.of(generateRandomArray(-1, 2, 10000)),
                 Arguments.of(replicateArray(1, 10000)),
                 Arguments.of(replicateArray(0, 10000)),
-                Arguments.of(replicateArray(10000, 10000))
+                Arguments.of(replicateArray(10000, 10000)),
+                Arguments.of(new int[] {}),
+                Arguments.of(new int[] {2, 1}),
+                Arguments.of(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}),
+                Arguments.of(new int[] {1, -1, 1, -1}),
+                Arguments.of(new int[] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1})
+
         );
     }
 
     @ParameterizedTest
     @MethodSource("methodInputProvider")
     void correctFibonacciMaker(int[] input) throws Throwable {
+        int[] ans = mul(input);
         Object[][] arguments = {
                 {input, int[].class},
         };
         MethodTest m = new MethodTest(MultItUp.class, "productMaker", arguments);
-        m.setIncorrectMethodStructureErrorMessage("Your productMaker method does not return the correct multiplied array");
         Object output = m.callMethod();
-        assertArrayEquals((int[]) output, mul(input));
+        CustomAssertions._assertArrayEquals(ans, output, "Your productMaker method does not return the correct multiplied array");
     }
 }
