f73a08e51cb7bb4b924281dfbd8a46edc306b0da
Samantha H <hodge1214@gmail.com>
2022-12-02 01:57:23 -0500

    made changes

--- src/methods/method_overloading/hard/q4/MainTest.java
+++ src/methods/method_overloading/hard/q4/MainTest.java
@@ -32,16 +32,16 @@ public class MainTest extends BaseTest {
                 new StringLiteral("Enter two doubles:"),
                 new Optional(new StringLiteral(" ")),
                 new NewLine(),
-                new StringLiteral("The area of the rectangle is: "),
+                new StringLiteral("The area of the first rectangle is: "),
                 new IntegerLiteral("int_int"),
                 new NewLine(),
-                new StringLiteral("The area of the rectangle is: "),
+                new StringLiteral("The area of the second rectangle is: "),
                 new DoubleLiteral("int_double"),
                 new NewLine(),
-                new StringLiteral("The area of the rectangle is: "),
+                new StringLiteral("The area of the third rectangle is: "),
                 new IntegerLiteral("int_only"),
                 new NewLine(),
-                new StringLiteral("The area of the rectangle is: "),
+                new StringLiteral("The area of the fourth rectangle is: "),
                 new DoubleLiteral("double_only"),
         };
     }
@@ -55,8 +55,7 @@ public class MainTest extends BaseTest {
                 Arguments.of(2, 4, 8),
                 Arguments.of(2, 5, 10),
                 Arguments.of(10, 100, 1000),
-                Arguments.of(-1, -100, 100),
-                Arguments.of(1, -1, -1));
+                Arguments.of(1, 1, 1));
     }
 
     static Stream<Arguments> oneIntegerInputProvider() {
@@ -64,93 +63,93 @@ public class MainTest extends BaseTest {
                 Arguments.of(2, 4),
                 Arguments.of(5, 25),
                 Arguments.of(10, 100),
-                Arguments.of(-1, 1),
+                Arguments.of(1, 1),
                 Arguments.of(1000, 1000000));
     }
 
     static Stream<Arguments> integerDoubleInputProvider() {
         return Stream.of(
                 Arguments.of(2, 2.2, 4.4),
-                Arguments.of(-1, 1.12, -1.12),
-                Arguments.of(-1, -1.0, 1.0),
+                Arguments.of(1, 1.12, 1.12),
+                Arguments.of(1, 1.0, 1.0),
                 Arguments.of(112, 2222.123, 248877.776));
     }
 
     static Stream<Arguments> oneDoubleInputProvider() {
         return Stream.of(
                 Arguments.of(1.2, 1.44),
-                Arguments.of(-1.0, 1.0),
+                Arguments.of(1.0, 1.0),
                 Arguments.of(2.7, 7.29),
-                Arguments.of(-112.1232, 12571.61197824));
+                Arguments.of(112.1232, 12571.61197824));
     }
 
     static Stream<Arguments> mainMethodInputProvider() {
         return Stream.of(
                 Arguments.of(4, 76, 23.4246, 648372.1, 304, 93.6984, 5776, 4.2038638005841E11),
                 Arguments.of(32917, 4, 57.3, 1.321, 131668, 1886144.1, 16, 1.745041),
-                Arguments.of(-43, -23, 43, 763, 989, -1849.0, 529, 582169.0),
-                Arguments.of(4738, -32, -4372.432, 584.432391, -151616, -2.0716582816E7, 1024, 341561.21964997693)
+                Arguments.of(43, 23, 43, 763, 989, 1849.0, 529, 582169.0),
+                Arguments.of(4738, 32, 4372.432, 584.432391, 151616, 2.0716582816E7, 1024, 341561.21964997693)
         );
     }
 
     @ParameterizedTest
     @MethodSource("twoIntegersInputProvider")
-    void twoIntegerCorrect(int a, int b, int area) throws Throwable {
+    void correctTwoIntegersRectangleAreaMethod(int a, int b, int area) throws Throwable {
         Object[][] arguments = {
                 {a, int.class},
                 {b, int.class}
         };
         MethodTest m = new MethodTest(RectangleOverload.class, "rectangleArea", arguments);
         Object output = m.callMethod();
-        String errorMsg = "Your rectangleArea() method does not calculate the correct area for two integers.";
+        String errorMsg = "Your rectangleArea method does not calculate the correct area using two integers.";
         CustomAssertions._assertEquals(area, output, errorMsg);
     }
 
     @ParameterizedTest
     @MethodSource("oneIntegerInputProvider")
-    void oneIntegerCorrect(int a, int area) throws Throwable {
+    void correctOneIntegerRectangleAreaMethod(int a, int area) throws Throwable {
         Object[][] arguments = {
                 {a, int.class}
         };
         MethodTest m = new MethodTest(RectangleOverload.class, "rectangleArea", arguments);
         Object output = m.callMethod();
-        String errorMsg = "Your rectangleArea() method does not calculate the correct area for one integer.";
+        String errorMsg = "Your rectangleArea method does not calculate the correct area using one integer.";
         CustomAssertions._assertEquals(area, output, errorMsg);
     }
 
     @ParameterizedTest
     @MethodSource("integerDoubleInputProvider")
-    void twoDoubleCorrect(int a, double b, double area) throws Throwable {
+    void correctTwoDoublesRectangleAreaMethod(int a, double b, double area) throws Throwable {
         Object[][] arguments = {
                 {a, int.class},
                 {b, double.class}
         };
         MethodTest m = new MethodTest(RectangleOverload.class, "rectangleArea", arguments);
         Object output = m.callMethod();
-        String errorMsg = "Your rectangleArea() method does not calculate the correct area for an integer and a double.";
+        String errorMsg = "Your rectangleArea method does not calculate the correct area using an integer and a double.";
         CustomAssertions._assertEquals(area, output, 0.0001, errorMsg);
     }
 
     @ParameterizedTest
     @MethodSource("oneDoubleInputProvider")
-    void oneDoubleCorrect(double a, double area) throws Throwable {
+    void correctOneDoubleRectangleAreaMethod(double a, double area) throws Throwable {
         Object[][] arguments = {
                 {a, double.class}
         };
         MethodTest m = new MethodTest(RectangleOverload.class, "rectangleArea", arguments);
         Object output = m.callMethod();
-        String errorMsg = "Your rectangleArea() method does not calculate the correct area for one double.";
+        String errorMsg = "Your rectangleArea method does not calculate the correct area using one double.";
         CustomAssertions._assertEquals(area, output, 0.0001, errorMsg);
     }
 
     @ParameterizedTest
     @MethodSource("mainMethodInputProvider")
-    void printsCorrectMessage(int a, int b, double x, double y, int twoInts, double intDouble, int oneInt, double oneDouble) {
+    void correctMainMethodOutput(int a, int b, double x, double y, int twoInts, double intDouble, int oneInt, double oneDouble) {
         runWithInput(a + " " + b + " " + x + " " + y);
 
-        assertEquals(twoInts, Integer.parseInt(getItemByName("int_int")), "Your program does not print the correct message for the first area.");
-        assertEquals(intDouble, Double.parseDouble(getItemByName("int_double")), 0.0001, "Your program does not print the correct message for the second area.");
-        assertEquals(oneInt, Integer.parseInt(getItemByName("int_only")), "Your program does not print the correct message for the third area.");
-        assertEquals(oneDouble, Double.parseDouble(getItemByName("double_only")), 0.0001, "Your program does not print the correct message for the fourth area.");
+        assertEquals(twoInts, Integer.parseInt(getItemByName("int_int")), "Your program does not print the correct output for the area of the first rectangle.");
+        assertEquals(intDouble, Double.parseDouble(getItemByName("int_double")), 0.0001, "Your program does not print the correct output for the area of the second rectangle.");
+        assertEquals(oneInt, Integer.parseInt(getItemByName("int_only")), "Your program does not print the correct output for the area of the third rectangle.");
+        assertEquals(oneDouble, Double.parseDouble(getItemByName("double_only")), 0.0001, "Your program does not print the correct output for the area of the fourth rectangle.");
     }
 }
--- src/methods/method_overloading/hard/q4/RectangleOverload.java
+++ src/methods/method_overloading/hard/q4/RectangleOverload.java
@@ -1,20 +1,21 @@
 package methods.method_overloading.hard.q4;
 
 /*
-Write a Java Program that prompts the user to enter two integers and two doubles. Then write four overloaded methods
-called rectangleArea. The first takes both integer values for each side of the rectangle. The second takes the first
-integer and the first double for each side of the rectangle. The third takes the second integer and uses it for all
-sides of the rectangle. The fourth takes the second double and uses it for all sides of the rectangle.
+Write a Java Program that prompts the user to enter two integers and two doubles which will be used to calculate
+rectangle areas. This calculation will be performed by four overloaded methods called rectangleArea. The first takes
+both integer values for the sides of the rectangle. The second takes the first integer and the first double for the sides
+of the rectangle. The third takes the second integer and uses it for all sides of the rectangle. The fourth takes the
+second double and uses it for all sides of the rectangle. Assume the user will only enter positive numbers.
 
 Sample Output:
   Enter two integers:
   2 4
   Enter two doubles:
   5.3 3.3
-  The area of the rectangle is: 8
-  The area of the rectangle is: 10.6
-  The area of the rectangle is: 16
-  The area of the rectangle is: 10.889999999999999
+  The area of the first rectangle is: 8
+  The area of the second rectangle is: 10.6
+  The area of the third rectangle is: 16
+  The area of the fourth rectangle is: 10.889999999999999
  */
 
 import java.util.Scanner;
@@ -28,10 +29,10 @@ public class RectangleOverload {
         System.out.println("Enter two doubles: ");
         double x = input.nextDouble();
         double y = input.nextDouble();
-        System.out.println("The area of the rectangle is: " + rectangleArea(a, b));
-        System.out.println("The area of the rectangle is: " + rectangleArea(a, x));
-        System.out.println("The area of the rectangle is: " + rectangleArea(b));
-        System.out.println("The area of the rectangle is: " + rectangleArea(y));
+        System.out.println("The area of the first rectangle is: " + rectangleArea(a, b));
+        System.out.println("The area of the second rectangle is: " + rectangleArea(a, x));
+        System.out.println("The area of the third rectangle is: " + rectangleArea(b));
+        System.out.println("The area of the fourth rectangle is: " + rectangleArea(y));
     }
 
     public static int rectangleArea(int num1, int num2) {
