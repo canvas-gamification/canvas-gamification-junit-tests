ef58c1fadc9cea606528399ccf23ce59d83b0cd0
Kai <kiamehrkhademi2019@gmail.com>
2022-12-02 15:23:14 -0800

    Wrote new tests for q5

--- src/methods/modular_programs/hard/q5/DrivingFines.java
+++ src/methods/modular_programs/hard/q5/DrivingFines.java
@@ -1,60 +1,68 @@
 package methods.modular_programs.hard.q5;
 
-/**
- * Write a Java Program with a method called fineCalculator that determines the
- * fine of a person caught speeding by asking the user how much above the speed
- * limit is the driver, and if they were in a school zone. If a person is
- * driving 0 - 10 km/h above the speed limit, their fine is $50. If a person is
- * driving 11 - 30 km/h above the speed limit, their fine is $75. If a person is
- * driving 31 - 50 km/h above the speed limit, their fine is $100. If a person
- * is driving 51 km/h above the speed limit, their fine is $150. If a person is
- * in a school zone, then their fine is doubled. The method should catch invalid
- * input and prints "Invalid Input!" as error message.
- * 
- * Sample Output:
- *  How fast were you above the speed limit?
- *  20
- *  Were you in a school zone? (Enter T/F)
- *  T 
- *  Your fine is $150!
+/*
+Write a Java Program with a method called fineCalculator(String speed, char schoolZone) that determines the fine of a
+person caught speeding by asking. You need to take the speed as a string to handle invalid cases.
+the user how much above the speed limit is the driver, and if they were in a school zone. If a person is driving under
+or equal to speed limit their fine is $0. If a person is driving 0 - 10 km/h above the speed limit, their fine is $50.
+If a person is driving 11 - 30 km/h above the speed limit, their fine is $75. If a person is driving 31 - 50 km/h above
+the speed limit, their fine is $100. If a person is driving 51 km/h above the speed limit, their fine is $150. If a
+person is in a school zone, then their fine is doubled. The method should catch invalid input and prints
+"Invalid Input!" as error message.
+
+Sample Output:
+    How fast were you above the speed limit?
+    20
+    Were you in a school zone? (Enter T/F)
+    T
+    Your fine is 150 dollars!
  */
+
 import java.util.Scanner;
 
 public class DrivingFines {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.println("How fast were you above the speed limit?");
-    int speed = input.nextInt();
+    String speed = input.next();
     System.out.println("Were you in a school zone? (Enter T/F)");
     String temp = input.next();
     char zone = temp.charAt(0);
     fineCalculator(speed, zone);
   }
 
-  public static void fineCalculator(int speed, char zone) {
-    if (zone != 'T' && zone != 'F') {
+  public static void fineCalculator(String speedSt, char zone) {
+    int speed;
+    try {
+      speed = Integer.parseInt(speedSt);
+    } catch (Exception e) {
+      System.out.println("Invalid Input!");
+      return;
+    }
+    if ((zone != 'T' && zone != 'F')) {
       System.out.println("Invalid Input!");
     } else if (speed > 50) {
       if (zone == 'T')
-        System.out.println("Your fine is $300!");
+        System.out.println("Your fine is 300 dollars!");
       else
-        System.out.println("Your fine is $150!");
+        System.out.println("Your fine is 150 dollars!");
     } else if (speed > 30 && speed <= 50) {
       if (zone == 'T')
-        System.out.println("Your fine is $200!");
+        System.out.println("Your fine is 200 dollars!");
       else
-        System.out.println("Your fine is $100!");
+        System.out.println("Your fine is 100 dollars!");
     } else if (speed > 10 && speed <= 30) {
       if (zone == 'T')
-        System.out.println("Your fine is $150!");
+        System.out.println("Your fine is 150 dollars!");
       else
-        System.out.println("Your fine is $75!");
+        System.out.println("Your fine is 75 dollars!");
     } else if (speed > 0 && speed <= 10) {
       if (zone == 'T')
-        System.out.println("Your fine is $100!");
+        System.out.println("Your fine is 100 dollars!");
       else
-        System.out.println("Your fine is $50!");
-    } else
-      System.out.println("Invalid Input!");
+        System.out.println("Your fine is 50 dollars!");
+    } else if (speed <= 0)
+      System.out.println("Your fine is 0 dollars!");
   }
 }
+
--- src/methods/modular_programs/hard/q5/MainTest.java
+++ src/methods/modular_programs/hard/q5/MainTest.java
@@ -1,34 +1,43 @@
 package methods.modular_programs.hard.q5;
 
-import java.util.Objects;
-import java.util.stream.Stream;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
 import global.BaseTest;
+import global.MethodTest;
 import global.exceptions.InvalidClauseException;
-import global.tools.CustomAssertions;
-import global.tools.Logger;
 import global.tools.TestOption;
-import global.utils.MethodUtil;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
+import global.variables.wrappers.Optional;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
   // Java
+
+  public static final int v1 = 10;
+  public static final int v2 = 30;
+  public static final int v3 = 50;
+  public static final int p1 = 50;
+  public static final int p2 = 75;
+  public static final int p3 = 100;
+  public static final int p4 = 150;
+
   public Clause[] testSentence() {
     TestOption.isInputTest = true;
     TestOption.defaultInput = "20 T";
-    return new Clause[] {
-        new StringLiteral("How fast were you above the speed limit\\?"),
-        new NewLine(),
-        new StringLiteral("Were you in a school zone\\? \\(Enter T\\/F\\)"),
-        new NewLine(),
-        new PlaceHolder(),
+    return new Clause[]{
+            new StringLiteral("How fast were you above the speed limit\\?"),
+            new Optional(new StringLiteral(" ")),
+            new NewLine(),
+            new StringLiteral("Were you in a school zone\\? \\(Enter T\\/F\\)"),
+            new Optional(new StringLiteral(" ")),
+            new NewLine(),
+            new PlaceHolder(),
+            new Optional(new StringLiteral(" ")),
     };
   }
 
@@ -36,98 +45,79 @@ public class MainTest extends BaseTest {
     DrivingFines.main(new String[0]);
   }
 
-  static Stream<Arguments> inputValidSpeedProvider() {
+  public static int findFor(int v, char c) {
+    int ans;
+    if (v > v3)
+      ans = p4;
+    else if (v > v2 && v <= v3)
+      ans = p3;
+    else if (v > v1 && v <= v2)
+      ans = p2;
+    else if (v > 0 && v <= v1)
+      ans = p1;
+    else
+      ans = 0;
+    if (c == 'T')
+      ans *= 2;
+    return ans;
+
+  }
+
+  static Stream<Arguments> inputProvider() {
     return Stream.of(
-        Arguments.of(1, 'T', "Your fine is \\$100\\!"),
-        Arguments.of(7, 'T', "Your fine is \\$100\\!"),
-        Arguments.of(10, 'T', "Your fine is \\$100\\!"),
-        Arguments.of(11, 'T', "Your fine is \\$150\\!"),
-        Arguments.of(24, 'T', "Your fine is \\$150\\!"),
-        Arguments.of(30, 'T', "Your fine is \\$150\\!"),
-        Arguments.of(31, 'T', "Your fine is \\$200\\!"),
-        Arguments.of(43, 'T', "Your fine is \\$200\\!"),
-        Arguments.of(50, 'T', "Your fine is \\$200\\!"),
-        Arguments.of(51, 'T', "Your fine is \\$300\\!"),
-        Arguments.of(950, 'T', "Your fine is \\$300\\!"),
-        Arguments.of(1, 'F', "Your fine is \\$50\\!"),
-        Arguments.of(7, 'F', "Your fine is \\$50\\!"),
-        Arguments.of(10, 'F', "Your fine is \\$50\\!"),
-        Arguments.of(11, 'F', "Your fine is \\$75\\!"),
-        Arguments.of(24, 'F', "Your fine is \\$75\\!"),
-        Arguments.of(30, 'F', "Your fine is \\$75\\!"),
-        Arguments.of(31, 'F', "Your fine is \\$100\\!"),
-        Arguments.of(43, 'F', "Your fine is \\$100\\!"),
-        Arguments.of(50, 'F', "Your fine is \\$100\\!"),
-        Arguments.of(51, 'F', "Your fine is \\$150\\!"),
-        Arguments.of(950, 'F', "Your fine is \\$150\\!"));
-  }
-
-  static Stream<Arguments> inputInvalidSpeedProvider() {
-    return Stream.of(
-        Arguments.of(0, 'F'),
-        Arguments.of(-1, 'F'),
-        Arguments.of(-2, 'F'),
-        Arguments.of(-5, 'F'),
-        Arguments.of(-10, 'F'),
-        Arguments.of(-1000, 'F'),
-        Arguments.of(0, 'T'),
-        Arguments.of(-1, 'T'),
-        Arguments.of(-2, 'T'),
-        Arguments.of(-5, 'T'),
-        Arguments.of(-10, 'T'),
-        Arguments.of(-1000, 'T'),
-        Arguments.of(12, 'D'),
-        Arguments.of(12, 'P'),
-        Arguments.of(12, '0'),
-        Arguments.of(12, '?'),
-        Arguments.of(0, 'E'));
+            Arguments.of("20", 'T', findFor(20, 'T')),
+            Arguments.of(Integer.toString(v1), 'T', findFor(v1, 'T')),
+            Arguments.of(Integer.toString(v2), 'T', findFor(v2, 'T')),
+            Arguments.of(Integer.toString(v3), 'T', findFor(v3, 'T')),
+            Arguments.of(Integer.toString(v1 + 1), 'T', findFor(v1 + 1, 'T')),
+            Arguments.of(Integer.toString(v2 + 1), 'T', findFor(v2 + 1, 'T')),
+            Arguments.of(Integer.toString(v3 + 1), 'T', findFor(v3 + 1, 'T')),
+            Arguments.of(Integer.toString(v1 - 1), 'T', findFor(v1 - 1, 'T')),
+            Arguments.of(Integer.toString(v2 - 1), 'T', findFor(v2 - 1, 'T')),
+            Arguments.of(Integer.toString(v3 - 1), 'T', findFor(v3 - 1, 'T')),
+            Arguments.of("0", 'T', findFor(0, 'T')),
+            Arguments.of("-1", 'T', findFor(0, 'T')),
+            Arguments.of("False Number Format", 'T', -1),
+            Arguments.of("10", 'R', -1),
+            Arguments.of("False Number Format", 'E', -1)
+    );
   }
 
   @ParameterizedTest
-  @MethodSource("inputValidSpeedProvider")
-  public void methodGiveValidFine(int speedAbove, char isSchoolZone, String fine) throws Throwable {
-    String errMsg = "Your method \"fineCalculator()\" does not return void for the given speed and area.";
-    String errMsgOut = "Your method \"fineCalculator()\" does not print the correct fine for the given speed and area.";
-
-    Object output = MethodUtil.invokeIfMethodExists(DrivingFines.class, "fineCalculator",
-        new Object[] { speedAbove, isSchoolZone }, int.class, char.class);
-    String consoleOutput = MethodUtil.getMethodOutput().trim();
-
-    CustomAssertions._assertTrue(Objects.isNull(output), errMsg, errMsg);
-    CustomAssertions._assertEquals(fine.replace("\\", ""), consoleOutput, errMsgOut);
+  @MethodSource("inputProvider")
+  public void printsCorrectOutput(String speedst, char isSchoolZone, int fine) throws InvalidClauseException {
+    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct amount of fine or the correct answer.";
+    if (fine != -1) {
+      runWithInput(speedst + " " + isSchoolZone, new Clause[]{
+              new StringLiteral("Your fine is " + fine + " dollars!")
+      });
+    } else {
+      runWithInput(speedst + " " + isSchoolZone, new Clause[]{
+              new StringLiteral("Invalid Input!")
+      });
+    }
   }
 
   @ParameterizedTest
-  @MethodSource("inputInvalidSpeedProvider")
-  public void methodGiveErrorMessage(int speedAbove, char isSchoolZone) throws Throwable {
-    String errMsg = "Your method \"fineCalculator()\" does not return the void for invalid inputs.";
-    String errMsgOut = "Your method \"fineCalculator()\" does not print the error message for invalid inputs.";
-
-    Object output = MethodUtil.invokeIfMethodExists(DrivingFines.class, "fineCalculator",
-        new Object[] { speedAbove, isSchoolZone }, int.class, char.class);
-    String consoleOutput = MethodUtil.getMethodOutput().trim();
-
-    CustomAssertions._assertTrue(Objects.isNull(output), errMsg, errMsg);
-    CustomAssertions._assertEquals("Invalid Input!", consoleOutput, errMsgOut);
-  }
-
-  @ParameterizedTest
-  @MethodSource("inputValidSpeedProvider")
-  public void printsCorrectOutputValidInput(int speedAbove, char isSchoolZone, String fine)
-      throws InvalidClauseException {
-    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct fine for the given inputs.";
-    runWithInput(speedAbove + " " + isSchoolZone, new Clause[] {
-        new StringLiteral(fine),
-    });
-  }
-
-  @ParameterizedTest
-  @MethodSource("inputInvalidSpeedProvider")
-  public void printsCorrectOutputInvalidInput(int speedAbove, char isSchoolZone)
-      throws InvalidClauseException {
-    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct error message for invalid inputs.";
-    runWithInput(speedAbove + " " + isSchoolZone, new Clause[] {
-        new StringLiteral("Invalid Input!"),
-    });
+  @MethodSource("inputProvider")
+  public void correctFineCalculatorMethod(String speedst, char isSchoolZone, int fine) throws Throwable {
+    TestOption.incorrectStructureErrorMessage = "Your fineCalculator method does not print the correct fine or answer.";
+    Clause[] methodSentence;
+    if (fine != -1) {
+      methodSentence = new Clause[]{
+              new StringLiteral("Your fine is " + fine + " dollars!"),
+      };
+    } else {
+      methodSentence = new Clause[]{
+              new StringLiteral("Invalid Input!"),
+      };
+    }
+    Object[][] arguments = {
+            {speedst, String.class},
+            {isSchoolZone, char.class}
+    };
+    MethodTest m = new MethodTest(DrivingFines.class, "fineCalculator", arguments, methodSentence);
+    m.callMethod();
   }
 }
+
