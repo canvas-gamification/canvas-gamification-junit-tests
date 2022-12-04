c2d0feaae01b503ec69473af882f37bc5c53e0c4
Seth Akins <83678885+Hedgemon4@users.noreply.github.com>
2022-12-03 17:46:40 -0800

    Update src/methods/method_overloading/medium/q7/OverloadDubDub.java

--- src/methods/method_overloading/medium/q7/OverloadDubDub.java
+++ src/methods/method_overloading/medium/q7/OverloadDubDub.java
@@ -3,7 +3,7 @@ package methods.method_overloading.medium.q7;
 /*
 Overload a Java Method so that it can take either a double variable, or a double variable and an integer variable. The
 method that only takes a double should multiply the input value by 1000, then return it. In the method with two inputs,
-return the multiplication of both values.
+return the product of both values.
  */
 public class OverloadDubDub {
     public static double multiply(double a) {
