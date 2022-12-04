d6fba2bf7c5e400e4b9a9cd5ddf547a90c168df5
Samantha H <hodge1214@gmail.com>
2022-12-04 12:45:49 -0500

    changed question for website variation

--- src/methods/method_overloading/hard/q7/Distances.java
+++ src/methods/method_overloading/hard/q7/Distances.java
@@ -1,18 +1,19 @@
 package methods.method_overloading.hard.q7;
 
 /*
-Write a Java Program that calculates the distance between 2 points given by the user. The first set of points are
-doubles and the second set integers. Pass each set of values to an overloaded method called calcDistance, one method
-for doubles and one for integers. The calculation should be done by subtracting the two points and returning the
+Write a Java Program that calculates the distance between 2 sets of points where one point is already specified and the
+other is given by the user. The first set of points are 7.32 and a double given by the user and the second set is 3 and
+an integer given by the user. Pass each set of values to  an overloaded method called calcDistance, one method takes two
+doubles and the other two integers. The calculation should be done by subtracting the two points and returning the
 absolute value of the answer.
 
 Sample Output:
-    Enter a set of doubles:
-    141.4 591.3
-    Enter a set of integers:
-    46 985
-    The distance between the set of doubles is: 449.9
-    The distance between the set of integers is: 939
+    Enter a double:
+    141.4
+    Enter an integer:
+    46
+    The distance between the set of doubles is: 133.78
+    The distance between the set of integers is: 43
  */
 
 import java.util.Scanner;
@@ -20,11 +21,11 @@ import java.util.Scanner;
 public class Distances {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a set of doubles: ");
-        double x = input.nextDouble();
+        System.out.println("Enter a double: ");
+        double x = 7.32;
         double y = input.nextDouble();
-        System.out.println("Enter a set of integers: ");
-        int a = input.nextInt();
+        System.out.println("Enter an integer: ");
+        int a = 3;
         int b = input.nextInt();
         System.out.println("The distance between the set of doubles is: " + calcDistance(x, y));
         System.out.println("The distance between the set of integers is: " + calcDistance(a, b));
--- src/methods/method_overloading/hard/q7/MainTest.java
+++ src/methods/method_overloading/hard/q7/MainTest.java
@@ -22,14 +22,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MainTest extends BaseTest {
     // Java
 
+    final double n1 = 7.32;
+    final int n2 = 3;
+
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
-        TestOption.defaultInput = "22.2 56.142 6 31";
+        TestOption.defaultInput = "56.142 31";
         return new Clause[]{
-                new StringLiteral("Enter a set of doubles:"),
+                new StringLiteral("Enter a double:"),
                 new Optional(new StringLiteral(" ")),
                 new NewLine(),
-                new StringLiteral("Enter a set of integers:"),
+                new StringLiteral("Enter an integer:"),
                 new Optional(new StringLiteral(" ")),
                 new NewLine(),
                 new StringLiteral("The distance between the set of doubles is: "),
@@ -64,19 +67,22 @@ public class MainTest extends BaseTest {
 
     static Stream<Arguments> mainMethodInputProvider() {
         return Stream.of(
-                Arguments.of(367.2, 1982.6, 56, 91, 1615.4, 35),
-                Arguments.of(1.1, 1, 564, 382194, 0.1, 381630),
-                Arguments.of(67.32781, 467.237, 38, 2, 399.90919, 36)
+                Arguments.of(367.2, 56),
+                Arguments.of(1982.6, 91),
+                Arguments.of(1.1, 564),
+                Arguments.of(1, 382194),
+                Arguments.of(67.32781, 38),
+                Arguments.of(467.237, 2)
         );
     }
 
     @ParameterizedTest
     @MethodSource("mainMethodInputProvider")
-    void correctMainMethodOutput(double x, double y, int a, int b, double ans1, int ans2) {
-        runWithInput(x + " " + y + " " + a + " " + b);
+    void correctMainMethodOutput(double x, int a) {
+        runWithInput(x + " " + a );
 
-        assertEquals(ans1, Double.parseDouble(getItemByName("a1")), 0.0000001, "Your program does not correctly print the distance between the set of doubles.");
-        assertEquals(ans2, Integer.parseInt(getItemByName("a2")), "Your program does not correctly print the distance between the set of integers.");
+        assertEquals(getAnsDouble(x), Double.parseDouble(getItemByName("a1")), 0.0000001, "Your program does not correctly print the distance between the set of doubles.");
+        assertEquals(getAnsInt(a), Integer.parseInt(getItemByName("a2")), "Your program does not correctly print the distance between the set of integers.");
     }
 
     @ParameterizedTest
@@ -103,5 +109,13 @@ public class MainTest extends BaseTest {
         Object output = m.callMethod();
         String errMsg = "Your calcDistance method does not correctly calculate the distance between two integers.";
         CustomAssertions._assertEquals(distance, output, errMsg);
+    }
+
+    int getAnsInt(int a){
+        return Math.abs(n2 - a);
+    }
+
+    double getAnsDouble(double a){
+        return Math.abs(n1 - a);
     }
 }
