041333ade4e3c2f640e7dd8979e80416b8d40779
Kai <kiamehrkhademi2019@gmail.com>
2022-12-03 18:33:09 -0800

    Wrote new tests for q8

--- src/methods/modular_programs/hard/q8/MainTest.java
+++ src/methods/modular_programs/hard/q8/MainTest.java
@@ -2,6 +2,8 @@ package methods.modular_programs.hard.q8;
 
 import java.util.stream.Stream;
 
+import global.MethodTest;
+import global.variables.wrappers.Optional;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -18,51 +20,48 @@ import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 
 public class MainTest extends BaseTest {
-  // Java
-  public Clause[] testSentence() {
-    TestOption.isInputTest = true;
-    TestOption.defaultInput = "Hello world";
-    return new Clause[] {
-        new StringLiteral("Enter a sentence"),
-        new NewLine(),
-        new PlaceHolder()
-    };
-  }
+    // Java
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "Hello world";
+        return new Clause[]{
+                new StringLiteral("Enter a sentence:"),
+                new Optional(new StringLiteral(" ")),
+                new NewLine(),
+                new PlaceHolder()
+        };
+    }
 
-  public void runMain() {
-    CounterChar.main(new String[0]);
-  }
+    public void runMain() {
+        CounterChar.main(new String[0]);
+    }
 
-  static Stream<Arguments> inputProvider() {
-    return Stream.of(
-        Arguments.of("Hello World", 10),
-        Arguments.of("Hello World!!!", 13),
-        Arguments.of("                      Hello                 ", 5),
-        Arguments.of("123HelloWorld!!!", 16),
-        Arguments.of("@@@@@@@@@@@@@@@@@@@@@@@@@@", 26));
-  }
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(
+                Arguments.of("Hello World", 10),
+                Arguments.of("Hello World!!!", 13),
+                Arguments.of("                      Hello                 ", 5),
+                Arguments.of("123HelloWorld!!!", 16),
+                Arguments.of("@@@@@@@@@@@@@@@@@@@@@@@@@@", 26));
+    }
 
-  @ParameterizedTest
-  @MethodSource("inputProvider")
-  public void methodReturnsCorrectNumberOfChars(String str, int numChars) throws Throwable {
-    String errMsg = "Your method \"charCounter()\" does not return the correct number of characters for the given input.";
-    String errMsgOut = "Your method \"charCounter()\" prints to console, which is not allowed.";
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void correctCharCounterMethod(String str, int numChars) throws Throwable {
+        Object[][] arguments = {
+                {str, String.class}
+        };
+        MethodTest m = new MethodTest(CounterChar.class, "charCounter", arguments);
+        Object output = m.callMethod();
+        CustomAssertions._assertEquals(numChars, output, "Your charCounter method does not return the correct number of characters in the sentence.");
+    }
 
-    Object output = MethodUtil.invokeIfMethodExists(CounterChar.class, "charCounter", new Object[] { str },
-        String.class);
-    Object consoleOutput = MethodUtil.getMethodOutput().trim();
-
-    CustomAssertions._assertEquals(numChars, output, errMsg);
-    CustomAssertions._assertEquals("", consoleOutput, errMsgOut);
-  }
-
-  @ParameterizedTest
-  @MethodSource("inputProvider")
-  public void printsCorrectNumberOfChars(String str, int numChars) throws InvalidClauseException {
-    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct number of characters for the given input.";
-    runWithInput(str, new Clause[][] { {
-        new StringLiteral("Total number of Characters in the sentence: "),
-        new IntegerLiteral(numChars),
-    } });
-  }
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void printsCorrectOutput(String str, int numChars) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not print the correct number of characters for the given input.";
+        runWithInput(str, new Clause[]{
+                new StringLiteral("Total number of Characters in the sentence: " + numChars),
+        });
+    }
 }
