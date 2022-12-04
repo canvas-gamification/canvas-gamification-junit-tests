267590770e287c94205901283eacea48081aef15
Kai <kiamehrkhademi2019@gmail.com>
2022-12-03 18:14:15 -0800

    Wrote new tests for q7

--- src/methods/modular_programs/hard/q7/MainTest.java
+++ src/methods/modular_programs/hard/q7/MainTest.java
@@ -2,6 +2,7 @@ package methods.modular_programs.hard.q7;
 
 import java.util.stream.Stream;
 
+import global.MethodTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,50 +18,46 @@ import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 
 public class MainTest extends BaseTest {
-  // Java
-  public Clause[] testSentence() {
-    TestOption.isInputTest = true;
-    TestOption.defaultInput = "Hello World";
-    return new Clause[] {
-        new StringLiteral("Enter a sentence"),
-        new NewLine(),
-        new PlaceHolder(),
-    };
-  }
+    // Java
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "Hello World";
+        return new Clause[]{
+                new StringLiteral("Enter a sentence:"),
+                new NewLine(),
+                new PlaceHolder(),
+        };
+    }
 
-  public void runMain() {
-    RevUp.main(new String[0]);
-  }
+    public void runMain() {
+        RevUp.main(new String[0]);
+    }
 
-  static Stream<Arguments> inputProvider() {
-    return Stream.of(
-        Arguments.of("Hello World", "dlroW olleH"),
-        Arguments.of("Hello World!", "\\!dlroW olleH"),
-        Arguments.of("!*#&@^@", "\\@\\^\\@\\&\\#\\*\\!"),
-        Arguments.of("1232222", "2222321"),
-        Arguments.of("12345@A67890", "09876A\\@54321"));
-  }
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(
+                Arguments.of("Hello World", "dlroW olleH"),
+                Arguments.of("Hello World!", "!dlroW olleH"),
+                Arguments.of("1232222", "2222321"),
+                Arguments.of("12345@A67890", "09876A@54321"));
+    }
 
-  @ParameterizedTest
-  @MethodSource("inputProvider")
-  public void methodReturnsCorrectRev(String str, String revStr) throws Throwable {
-    String errMsg = "Your method \"revString()\" does not return the correct reversed string for the given input.";
-    String errMsgOut = "Your method prints to console, which is not allowed.";
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void methodReturnsCorrectRev(String str, String revStr) throws Throwable {
+        Object[][] arguments = {
+                {str, String.class}
+        };
+        MethodTest m = new MethodTest(RevUp.class, "revString", arguments);
+        Object output = m.callMethod();
+        CustomAssertions._assertEquals(revStr, output, "Your revString method does not reverse the string correctly.");
+    }
 
-    Object output = MethodUtil.invokeIfMethodExists(RevUp.class, "revString", new Object[] { str }, String.class);
-    Object consoleOutput = MethodUtil.getMethodOutput().trim();
-
-    CustomAssertions._assertEquals(revStr.replace("\\", ""), output, errMsg);
-    CustomAssertions._assertEquals("", consoleOutput, errMsgOut);
-  }
-
-  @ParameterizedTest
-  @MethodSource("inputProvider")
-  public void printsCorrectOutput(String str, String revStr) throws InvalidClauseException {
-    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct reversed string for the given input.";
-    runWithInput(str, new Clause[][] { {
-        new StringLiteral("Your sentence in reverse is: "),
-        new StringLiteral(revStr),
-    } });
-  }
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void printsCorrectOutput(String str, String revStr) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not print the correct reversed string for the given input.";
+        runWithInput(str, new Clause[]{
+                new StringLiteral("Your sentence in reverse is: " + revStr)
+        });
+    }
 }
--- src/methods/modular_programs/hard/q7/RevUp.java
+++ src/methods/modular_programs/hard/q7/RevUp.java
@@ -1,29 +1,31 @@
 package methods.modular_programs.hard.q7;
 
-/**
- * [J] Write a Java Program with a method called revString that accepts a string
- * and returns it in reverse. Write a main method that asks the user to enter a
- * sentence and calls the revString method and prints the result.
- * Sample Output:
- *    Enter a sentence: Hello World
- *    Your sentence in reverse is: dlroW olleH
- * 
+/*
+Write a Java Program with a method called revString that accepts a string and returns it in reverse. Write a main method
+that asks the user to enter a sentence and calls the revString method and prints the result.
+
+Sample Output:
+    Enter a sentence:
+    Hello World
+    Your sentence in reverse is: dlroW olleH
+
  */
+
 import java.util.Scanner;
 
 public class RevUp {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter a sentence");
-    String sentence = input.nextLine();
-    String result = revString(sentence);
-    System.out.println("Your sentence in reverse is: " + result);
-  }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String sentence = input.nextLine();
+        String result = revString(sentence);
+        System.out.println("Your sentence in reverse is: " + result);
+    }
 
-  public static String revString(String sentence) {
-    String result = "";
-    for (int i = sentence.length() - 1; i >= 0; i--)
-      result += sentence.charAt(i);
-    return (result);
-  }
+    public static String revString(String sentence) {
+        String result = "";
+        for (int i = sentence.length() - 1; i >= 0; i--)
+            result += sentence.charAt(i);
+        return (result);
+    }
 }
