b22262c2fe873ac1e18182c28d35f9f650b69516
Kai <kiamehrkhademi2019@gmail.com>
2022-12-05 13:36:04 -0800

    Wrote new tests for q10

--- src/methods/method_overloading/hard/q10/MainTest.java
+++ src/methods/method_overloading/hard/q10/MainTest.java
@@ -2,6 +2,10 @@ package methods.method_overloading.hard.q10;
 
 import java.util.stream.Stream;
 
+import global.MethodTest;
+import global.tools.TestOption;
+import global.variables.clauses.PlaceHolder;
+import global.variables.wrappers.Optional;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -15,13 +19,18 @@ import global.variables.clauses.StringLiteral;
 
 public class MainTest extends BaseTest {
     // Java
+
+    public static int n = 5;
+
     public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "John false";
         return new Clause[]{
-                new StringLiteral("The baby's name is Carina and the gender is Female"),
+                new StringLiteral("Enter a name or a name with gender:"),
+                new Optional(new StringLiteral(" ")),
                 new NewLine(),
-                new StringLiteral("The baby's name is Alex and the gender is Male"),
-                new NewLine(),
-                new StringLiteral("The baby's name is Bred"),
+                new PlaceHolder(),
+                new Optional(new StringLiteral(" "))
         };
     }
 
@@ -29,13 +38,24 @@ public class MainTest extends BaseTest {
         Overload10.main(new String[0]);
     }
 
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(
+                Arguments.of("Carina true", "The baby's name is Carina and the gender is Female"),
+                Arguments.of("Carina false", "The baby's name is Carina and the gender is Male"),
+                Arguments.of("Carina", "The baby's name is Carina"),
+                Arguments.of("Alex true", "The baby's name is Alex and the gender is Female"),
+                Arguments.of("Alex false", "The baby's name is Alex and the gender is Male"),
+                Arguments.of("Alex", "The baby's name is Alex")
+        );
+    }
+
     static Stream<Arguments> inputBabyWithGenderProvider() {
         return Stream.of(
                 Arguments.of("Carina", true, "The baby's name is Carina and the gender is Female"),
                 Arguments.of("Carina", false, "The baby's name is Carina and the gender is Male"),
                 Arguments.of("Alex", true, "The baby's name is Alex and the gender is Female"),
-                Arguments.of("Alex", false, "The baby's name is Alex and the gender is Male"),
-                Arguments.of(" ", false, "The baby's name is   and the gender is Male"));
+                Arguments.of("Alex", false, "The baby's name is Alex and the gender is Male")
+        );
     }
 
     static Stream<String> inputBabyWithoutGenderProvider() {
@@ -43,18 +63,34 @@ public class MainTest extends BaseTest {
     }
 
     @ParameterizedTest
+    @MethodSource("inputProvider")
+    void printsCorrectOutput(String babyName, String ans) throws Throwable {
+        runWithInput(babyName, new Clause[]{
+                new StringLiteral(ans)
+        });
+    }
+
+    @ParameterizedTest
     @MethodSource("inputBabyWithGenderProvider")
     void babyWithGenderCorrect(String babyName, Boolean babyGender, String babyStr) throws Throwable {
-        String errMsg = "Your method \"baby()\" does not give the correct output for baby with gender.";
-        Object output = MethodUtil.invokeIfMethodExists(Overload10.class, "baby", new Object[]{babyName, babyGender}, String.class, Boolean.class);
-        CustomAssertions._assertEquals(output, babyStr, errMsg);
+        Object[][] arguments = {
+                {babyName, String.class},
+                {babyGender, Boolean.class}
+        };
+        MethodTest m = new MethodTest(Overload10.class, "baby", arguments);
+        Object output = m.callMethod();
+        CustomAssertions._assertEquals(babyStr, output, "Your baby method with gender does not print the correct output.");
     }
 
     @ParameterizedTest
     @MethodSource("inputBabyWithoutGenderProvider")
     void babyWithoutGenderCorrect(String babyName) throws Throwable {
-        String errMsg = "Your method \"baby()\" does not give the correct output for baby with gender.";
-        Object output = MethodUtil.invokeIfMethodExists(Overload10.class, "baby", new Object[]{babyName}, String.class);
-        CustomAssertions._assertEquals(output, "The baby's name is " + babyName, errMsg);
+        String ans = "The baby's name is " + babyName;
+        Object[][] arguments = {
+                {babyName, String.class},
+        };
+        MethodTest m = new MethodTest(Overload10.class, "baby", arguments);
+        Object output = m.callMethod();
+        CustomAssertions._assertEquals(ans, output, "Your baby method without gender does not print the correct output.");
     }
 }
--- src/methods/method_overloading/hard/q10/Overload10.java
+++ src/methods/method_overloading/hard/q10/Overload10.java
@@ -1,26 +1,33 @@
 package methods.method_overloading.hard.q10;
 
-/**
- * [J] Write a Java Program for a hospital that displays the name and the gender
- * of the baby to the parents. The method takes in either only the name or the
- * name and the gender ( boolean, if true = girl, false = boy) of the baby. Use
- * method overloading to only call one method to complete this.
- * <p>
- * Input Sets:
- * - Carina, true
- * - Alex, false
- * - Bred
- * <p>
- * Sample Output:
- * The baby's name is Carina and the gender is Female
- * The baby's name is Alex and the gender is Male
- * The baby's name is Bred
+/*
+Write a Java Program for a hospital that displays the name and the gender of the baby to the parents. The method takes
+in either only the name or the name and the gender ( boolean, if true = girl, false = boy) of the baby. Use method
+overloading to only call one method to complete this.
+
+Sample Output:
+		The baby's name is Carina and the gender is Female
+        The baby's name is Alex and the gender is Male
+        The baby's name is Bred
+
  */
+
+import java.util.Scanner;
+
 public class Overload10 {
     public static void main(String[] args) {
-        System.out.println(baby("Carina", true));
-        System.out.println(baby("Alex", false));
-        System.out.println(baby("Bred"));
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter a name or a name with gender:");
+        String st = inp.nextLine();
+        Scanner in = new Scanner(st);
+        String name = in.next();
+        if (in.hasNextBoolean()) {
+            boolean gen = in.nextBoolean();
+            System.out.println(baby(name, gen));
+        } else {
+            System.out.println(baby(name));
+        }
+
     }
 
     public static String baby(String name, Boolean gender) {
@@ -38,3 +45,4 @@ public class Overload10 {
         return "The baby's name is " + name;
     }
 }
+
