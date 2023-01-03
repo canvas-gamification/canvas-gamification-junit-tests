6666653e4001edf7471b8dd308a105316dae42ef
Hedgemon4 <sethlinc4@gmail.com>
2023-01-03 14:15:38 -0700

    Started redoing question to give main method

--- src/methods/method_overloading/hard/q9/GenerateUsername.java
+++ src/methods/method_overloading/hard/q9/GenerateUsername.java
@@ -5,6 +5,10 @@ Write a Java Program that receives two first names and ages from the user and pa
 overloaded method called userName. The method should take the String and int in either order, (name, age) or (age, name),
 and return their username as first_name.age. For example, if the first name is john and the age 34, the username is john.34.
 
+Write a Java program that calls an overloaded method named userName which generates a username from the user's age and
+name. The method should be overloaded so that it accepts a string for the username and an integer for the age in either
+order.
+
 Sample Output:
   Enter two names:
   Joe Madi
@@ -13,28 +17,23 @@ Sample Output:
   The username for Joe with the age of 21 is: Joe.21
   The username for  Madi with the age of 19 is:  Madi.19
  */
-import java.util.Scanner;
 
 public class GenerateUsername {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter two names: ");
-    String n1 = input.next();
-    String n2 = input.next();
-    input.nextLine();
-    System.out.println("Enter corresponding ages: ");
-    int a1 = input.nextInt();
-    int a2 = input.nextInt();
-    System.out.println("The username for "+n1+" with the age of "+a1+" is: " + userName(n1, a1));
-    System.out.println("The username for "+n2+" with the age of "+a2+" is: " + userName(a2, n2));
-  }
+    public static void main(String[] args) {
+        String n1 = "Steve";
+        String n2 = "Madi";
+        int a1 = 17;
+        int a2 = 49;
+        System.out.println("The username for " + n1 + " with the age of " + a1 + " is: " + userName(n1, a1));
+        System.out.println("The username for " + n2 + " with the age of " + a2 + " is: " + userName(a2, n2));
+    }
 
-  public static String userName(int age, String name) {
-    return name + "." + age;
+    public static String userName(int age, String name) {
+        return name + "." + age;
 
-  }
+    }
 
-  public static String userName(String name, int age) {
-    return name + "." + age;
-  }
+    public static String userName(String name, int age) {
+        return name + "." + age;
+    }
 }
