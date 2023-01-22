c87968bb4dbac0c3de88043b24e89f9151b20d6b
Kai <kiamehrkhademi2019@gmail.com>
2023-01-22 11:12:27 -0800

    Wrote tests for q4

--- src/arrays/programs_involving_multidimensional_data/easy/q4/MainTest.java
+++ src/arrays/programs_involving_multidimensional_data/easy/q4/MainTest.java
@@ -1,9 +1,16 @@
 package arrays.programs_involving_multidimensional_data.easy.q4;
 
 import global.BaseRandomTest;
+import global.MethodTest;
 import global.tools.CustomAssertions;
+import global.tools.TestOption;
 import global.variables.Clause;
 import global.variables.clauses.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class MainTest extends BaseRandomTest {
 
@@ -12,17 +19,18 @@ public class MainTest extends BaseRandomTest {
     public static int down = 0;
     public Clause[] testSentence() {
         Clause[] c = new Clause[4 * n * n + 4];
-        c[0] = new StringLiteral("Before the rotation:");
-        c[1] = new NewLine();
-        for(int i = 2; i < 2 * n * n + 2; i += 2){
-            c[i] = new RandomInteger(down, up, "" + ((i - 2)/2));
-            c[i + 1] = new NewLine();
+        int p = 0;
+        c[p++] = new StringLiteral("Before the rotation:");
+        c[p++] = new NewLine();
+        for(int i = 0; i < n * n; i++){
+            c[p++] = new RandomInteger(down, up, "" + i);
+            c[p++] = new NewLine();
         }
-        c[2 * n * n + 2] = new StringLiteral("After the rotation:");
-        c[2 * n * n + 3] = new NewLine();
-        for(int i = 2 * n * n + 4; i < 4 * n * n + 4; i += 2){
-            c[i] = new IntegerLiteral("x" + (((i - 2 * n * n + 4) - 2) / 2));
-            c[i + 1] = new NewLine();
+        c[p++] = new StringLiteral("After the rotation:");
+        c[p++] = new NewLine();
+        for(int i = 0; i < n * n; i++){
+            c[p++] = new IntegerLiteral("x" + i);
+            c[p++] = new NewLine();
         }
         return c;
     }
@@ -32,16 +40,18 @@ public class MainTest extends BaseRandomTest {
     }
 
     public static int[] answerFor(int[] arr) {
-        int a = arr[0], b = arr[n - 1], c = arr[n * n - 1], d = arr[n * n - n];
-        arr[n - 1] = a;
-        arr[n * n - 1] = b;
-        arr[n * n - n] = c;
+        int sz = (int)Math.sqrt(arr.length);
+        int a = arr[0], b = arr[sz - 1], c = arr[sz * sz - 1], d = arr[sz * sz - sz];
+        arr[sz - 1] = a;
+        arr[sz * sz - 1] = b;
+        arr[sz * sz - sz] = c;
         arr[0] = d;
         return arr;
 
     }
 
-    void printsCorrectOutput(){
+    @Test
+    public void printsCorrectOutput(){
         int[] arr = new int[n * n];
         for(int i = 0; i < n * n; i ++){
             arr[i] = Integer.parseInt(getItemByName("" + i));
@@ -52,7 +62,47 @@ public class MainTest extends BaseRandomTest {
             in[i] = Integer.parseInt(getItemByName("x" + i));
         }
         CustomAssertions._assertArrayEquals(ans, in, "Your program does not rotated the 2D array correctly.");
-
+    }
+    static Stream<int[][]> inputProvider() {
+        int[][] t1 = {
+                {0, 0, 1},
+                {0, 0, 0},
+                {1, 0, 0}
+        };
+        int[][] t2 = {
+                {1, 0},
+                {0, 1}
+        };
+        int[][] t3 = {
+                {1, 2, 3, 4, 5},
+                {2, 3, 4, 5, 1},
+                {3, 4, 5, 1, 2},
+                {4, 5, 1, 2, 3},
+                {5, 1, 2, 3, 4}
+        };
+        return Stream.of(
+            t1, t2, t3
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void correctRotateArrayMethod(int[][] arr) throws Throwable {
+        int[] input = new int[arr.length * arr[0].length];
+        for(int i = 0; i < arr.length; i ++){
+            for(int j = 0; j < arr[0].length; j ++)
+                input[i * arr[0].length + j] = arr[i][j];
+        }
+        int[] ans = answerFor(input);
+        Clause[] c = new Clause[2 * arr.length * arr[0].length];
+        for(int i = 0; i < 2 * arr.length * arr[0].length; i += 2){
+            c[i] = new StringLiteral("" + ans[i / 2]);
+            c[i + 1] = new NewLine();
+        }
+        Object[][] arguments = {
+                {arr, int[][].class}
+        };
+        TestOption.incorrectStructureErrorMessage = "Your rotateArray method does not rotated the 2D array correctly.";
+        MethodTest m = new MethodTest(Rotation.class, "rotateArray", arguments, c);
 
     }
 }
--- src/arrays/programs_involving_multidimensional_data/easy/q4/Rotation.java
+++ src/arrays/programs_involving_multidimensional_data/easy/q4/Rotation.java
@@ -3,7 +3,8 @@ package arrays.programs_involving_multidimensional_data.easy.q4;
 /*
 Write a Java program that initializes a 2D array of random integers of size 4 by 4, with the range of values from 1 - 99
 (inclusive of both values). Then, "rotate" the elements in the four corners of the array towards the clockwise direction
-using a method called rotateArray. You should print the 2D array before and after the rotation.
+using a method called rotateArray and print the result in the method. You should print the 2D array before and after the
+rotation.
  */
 
 public class Rotation {
