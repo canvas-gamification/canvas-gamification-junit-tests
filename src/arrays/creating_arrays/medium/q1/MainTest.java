937a7f93ae34dd1df6491e864c0ad62fe3c57e22
Samantha H <hodge1214@gmail.com>
2023-01-22 19:46:03 -0500

    added variation for starting at multiplication 0 or 1

--- src/arrays/creating_arrays/medium/q1/MainTest.java
+++ src/arrays/creating_arrays/medium/q1/MainTest.java
@@ -1,7 +1,6 @@
 package arrays.creating_arrays.medium.q1;
 
 import global.BaseTest;
-import global.utils.ArrayUtil;
 import global.variables.Clause;
 import global.variables.clauses.*;
 import org.junit.jupiter.api.RepeatedTest;
@@ -12,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MainTest extends BaseTest {
     // Parsons
     private static final int length = 100;
+    private static final int subtract = 1, add = 0; //swap two numbers for a starting multiplication of 1
 
     public Clause[] testSentence() {
         return clauseBuilder();
@@ -24,9 +24,9 @@ public class MainTest extends BaseTest {
     @RepeatedTest(length)
     void printsCorrectArrayOutput(RepetitionInfo repetitionInfo) {
         double expected = Double.parseDouble(getItemByName("rand" + (repetitionInfo.getCurrentRepetition() - 1)))
-                * (repetitionInfo.getCurrentRepetition() - 1);
+                * (repetitionInfo.getCurrentRepetition() - subtract);
         double actual = Double.parseDouble(getItemByName("num" + (repetitionInfo.getCurrentRepetition() - 1)));
-        assertEquals(expected, actual, 0.01, "Your program does not correctly calculate the final number on row " + repetitionInfo.getCurrentRepetition());
+        assertEquals(expected, actual, 0.00001, "Your program does not correctly calculate the final number on row " + repetitionInfo.getCurrentRepetition());
     }
 
     public Clause[] clauseBuilder() {
@@ -35,14 +35,14 @@ public class MainTest extends BaseTest {
         for (int x = 0; x < length - 1; x++) {
             c[count++] = new RandomDouble(0, 1, "rand" + x);
             c[count++] = new StringLiteral(" \\* ");
-            c[count++] = new IntegerLiteral(x);
+            c[count++] = new IntegerLiteral(x + add);
             c[count++] = new StringLiteral(" = ");
             c[count++] = new DoubleLiteral("num" + x);
             c[count++] = new NewLine();
         }
         c[count++] = new RandomDouble(0, 1, "rand" + (length - 1));
         c[count++] = new StringLiteral(" \\* ");
-        c[count++] = new IntegerLiteral(length - 1);
+        c[count++] = new IntegerLiteral(length - 1 + add);
         c[count++] = new StringLiteral(" = ");
         c[count] = new DoubleLiteral("num" + (length - 1));
         return c;
