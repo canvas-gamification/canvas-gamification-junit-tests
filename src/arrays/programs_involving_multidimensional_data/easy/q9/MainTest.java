c9b4e069c99d080b9274ca40126afeff6d97918b
Kai <kiamehrkhademi2019@gmail.com>
2023-01-26 13:59:57 -0800

    Wrote q9

---
+++ src/arrays/programs_involving_multidimensional_data/easy/q9/MainTest.java
@@ -0,0 +1,63 @@
+package arrays.programs_involving_multidimensional_data.easy.q9;
+
+import global.BaseRandomTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.CustomAssertions;
+import global.variables.Clause;
+import global.variables.clauses.*;
+import org.junit.jupiter.api.Test;
+
+public class MainTest extends BaseRandomTest {
+    // Parsons
+
+    public static int n = 5;
+    public static int div = 5;
+
+    public Clause[] testSentence() {
+        Clause[] c = new Clause[2 * n * n + 3 * n];
+        int t = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                c[t++] = new RandomInteger(100, 1001, i + " " + j);
+                c[t++] = new StringLiteral(" ");
+            }
+            c[t++] = new NewLine();
+        }
+        for (int i = 0; i < n; i++) {
+            c[t++] = new IntegerLiteral("" + i);
+            c[t++] = new StringLiteral(" ");
+        }
+        return c;
+    }
+
+    public void runMain() {
+        Sum.main(new String[0]);
+    }
+
+    public static int[] answerFor(int[][] arr) {
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] /= div;
+                ans[i] += arr[i][j];
+            }
+        }
+        return ans;
+    }
+
+    @Test
+    public void printsCorrectOutput() throws InvalidClauseException {
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(getItemByName(i + " " + j));
+            }
+        }
+        int[] ans = answerFor(arr);
+        int[] out = new int[n];
+        for (int i = 0; i < n; i++) {
+            out[i] = Integer.parseInt(getItemByName(i + ""));
+        }
+        CustomAssertions._assertArrayEquals(ans, out, "Your program does not correctly print the array including sum of each rows after division.");
+    }
+}
---
+++ src/arrays/programs_involving_multidimensional_data/easy/q9/Sum.java
@@ -0,0 +1,31 @@
+package arrays.programs_involving_multidimensional_data.easy.q9;
+
+/*
+Write a Java program that generates a 2D array of random integers of size 5 by 5, with the range of values from 100-1000
+(inclusive of both values), and print the 2D array each row in a line. Then, divide all the values by 5, then sum all
+the values in each row and store those sums into a 1D array which is printed to the console.
+ */
+
+public class Sum {
+    public static void main(String[] args) {
+        int[][] arr = new int[5][5];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = 100 + (int) (Math.random() * 901);
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int[] out = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = arr[i][j] / 5;
+                sum = sum + arr[i][j];
+            }
+            out[i] = sum;
+        }
+        for (int i = 0; i < out.length; i++)
+            System.out.print(out[i] + " ");
+    }
+}
