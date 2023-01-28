ad3cf49f0451e6048504cf2a148e29f03f1858f1
SamamthaH <79422004+SamamthaH@users.noreply.github.com>
2023-01-28 15:15:43 -0500

    Update src/arrays/programs_involving_multidimensional_data/easy/q5/ValueLock.java

--- src/arrays/programs_involving_multidimensional_data/easy/q5/ValueLock.java
+++ src/arrays/programs_involving_multidimensional_data/easy/q5/ValueLock.java
@@ -1,7 +1,7 @@
 package arrays.programs_involving_multidimensional_data.easy.q5;
 
 /*
-Write a Java program that generates a 2D array of size 5 by 5, with random integers in the range of 1 and 999
+Write a Java program that generates a 5 by 5 2D array, with random integers ranging from 1 to 999
 (inclusive of both values). Then, print out those values with each row on its own line and the numbers separated by a
 space.
  */
