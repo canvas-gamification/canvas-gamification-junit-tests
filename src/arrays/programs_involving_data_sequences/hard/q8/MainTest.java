7dd7d0c9a1513415fd38df897c0bc4bbe26b7c9b
Kai Khademi <99101819+KaiKhademi@users.noreply.github.com>
2023-01-29 02:03:47 -0800

    Update src/arrays/programs_involving_data_sequences/hard/q8/MainTest.java
    Co-authored-by: SamamthaH <79422004+SamamthaH@users.noreply.github.com>

--- src/arrays/programs_involving_data_sequences/hard/q8/MainTest.java
+++ src/arrays/programs_involving_data_sequences/hard/q8/MainTest.java
@@ -97,6 +97,6 @@ public class MainTest extends BaseTest {
         };
         MethodTest m = new MethodTest(MultItUp.class, "productMaker", arguments);
         m.callMethod();
-        CustomAssertions._assertArrayEquals(ans, input, "Your productMaker method does not return the correct multiplied array");
+        CustomAssertions._assertArrayEquals(ans, input, "Your productMaker method does not return a correctly multiplied array");
     }
 }
