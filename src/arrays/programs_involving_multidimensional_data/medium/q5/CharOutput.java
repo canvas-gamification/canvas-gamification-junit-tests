aabfcccd70e018cc8d02dc0b1576039f7c81397e
Kai <kiamehrkhademi2019@gmail.com>
2023-01-31 00:56:57 -0800

    Wrote q5

---
+++ src/arrays/programs_involving_multidimensional_data/medium/q5/CharOutput.java
@@ -0,0 +1,37 @@
+package arrays.programs_involving_multidimensional_data.medium.q5;
+
+/*
+Write a Java program that defines a 2D array of characters of size 2 by 13, initialized with the character corresponding
+to the ASCII numbers starting at 97, then 98, 99, and so on, all the way until 122. The program also prints out all the
+characters in each row of the array (without any space separator). This is repeated for each row printed on a separate
+line.
+
+Distractors:
+    char[][] arr = new char[13][2];
+    int index = 0;
+    index = 97;
+    char index = 97;
+    arr[i][j] = index;
+    {
+    }
+    char[][] arr = new char[3][14];
+    arr[i][j] = ( char ) 97;
+ */
+
+public class CharOutput {
+    public static void main(String[] args) {
+        char[][] arr = new char[2][13];
+        int index = 97;
+        for (int i = 0; i < arr.length; i++)
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = (char) index;
+                index++;
+            }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++)
+                System.out.print(arr[i][j]);
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/arrays/programs_involving_multidimensional_data/medium/q5/MainTest.java
@@ -0,0 +1,30 @@
+package arrays.programs_involving_multidimensional_data.medium.q5;
+
+import global.BaseRandomTest;
+import global.variables.Clause;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.StringLiteral;
+
+public class MainTest extends BaseRandomTest {
+    // Parsons with Distractors
+
+    public static final int n = 2;
+    public static final int m = 13;
+    public static final char start = 97;
+
+    public Clause[] testSentence() {
+        Clause[] c = new Clause[n * m + n];
+        int t = 0;
+        char ch = start;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                c[t++] = new StringLiteral("" + (ch++));
+            c[t++] = new NewLine();
+        }
+        return c;
+    }
+
+    public void runMain() {
+        CharOutput.main(new String[0]);
+    }
+}
