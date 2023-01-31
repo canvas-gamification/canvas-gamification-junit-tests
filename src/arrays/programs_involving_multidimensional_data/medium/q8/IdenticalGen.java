d68801e9f04b0c2484056e95145e0dc580f983eb
Kai <kiamehrkhademi2019@gmail.com>
2023-01-31 02:17:59 -0800

    Wrote q8

---
+++ src/arrays/programs_involving_multidimensional_data/medium/q8/IdenticalGen.java
@@ -0,0 +1,32 @@
+package arrays.programs_involving_multidimensional_data.medium.q8;
+
+/*
+Write a Java program that creates a random integer in the range of 1 to1000 (inclusive of both values), and print it.
+Then, the program will also create a 2D array of the size of the 5 by 5. Next, use a method called squareRoot to
+initialize all the elements of the given array with the square root of the value and print out the elements in the
+2D array.
+
+Distractors:
+    out[i] = Math.sqrt( size );
+    out[j] = Math.sqrt( size );
+    out[i][j] = Math.round( size );
+    out[i][j] = Math.ceil( size );
+    out[i][j] = Math.floor( size );
+ */
+
+public class IdenticalGen {
+    public static void main(String[] args) {
+        int num = (int) (Math.random() * 1000 + 1);
+        System.out.println(num);
+        double[][] out = new double[5][5];
+        for (int i = 0; i < out.length; i++)
+            for (int j = 0; j < out[i].length; j++)
+                out[i][j] = Math.sqrt(num);
+        for (int i = 0; i < out.length; i++) {
+            for (int j = 0; j < out[i].length; j++)
+                System.out.print(out[i][j] + " ");
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/arrays/programs_involving_multidimensional_data/medium/q8/MainTest.java
@@ -0,0 +1,44 @@
+package arrays.programs_involving_multidimensional_data.medium.q8;
+
+import global.BaseRandomTest;
+import global.tools.CustomAssertions;
+import global.variables.Clause;
+import global.variables.clauses.*;
+import org.junit.jupiter.api.Test;
+
+public class MainTest extends BaseRandomTest {
+    // Parsons with Distractors
+
+    public static final int n = 5;
+    public static final int up = 1000;
+    public static final int down = 1;
+
+    public Clause[] testSentence() {
+        Clause[] c = new Clause[2 * n * n + n + 2];
+        int t = 0;
+        c[t++] = new RandomInteger(down, up + 1, "num");
+        c[t++] = new NewLine();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                c[t++] = new DoubleLiteral(i + " " + j);
+                c[t++] = new StringLiteral(" ");
+            }
+            c[t++] = new NewLine();
+        }
+        return c;
+    }
+
+    public void runMain() {
+        IdenticalGen.main(new String[0]);
+    }
+
+    @Test
+    public void printsCorrectOutput() {
+        double num = Math.sqrt(Double.parseDouble(getItemByName("num")));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                CustomAssertions._assertEquals(num, Double.parseDouble(getItemByName(i + " " + j)), "Your program does not print the correct 2D array containing the sqrt of the generated number.");
+            }
+        }
+    }
+}
