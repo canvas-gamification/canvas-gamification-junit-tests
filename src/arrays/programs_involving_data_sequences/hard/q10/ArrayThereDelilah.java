09f4ee507b6f87eb520147b0b05c79693fa9ecfe
te-sa <teresa_saller@freenet.de>
2023-02-06 13:10:54 -0800

    finished test

---
+++ src/arrays/programs_involving_data_sequences/hard/q10/ArrayThereDelilah.java
@@ -0,0 +1,34 @@
+package arrays.programs_involving_data_sequences.hard.q10;
+
+/*
+Create a Java program that takes an array of strings, finds the middle character in each string, and prints it to the
+console. To do this, create a method called middle, which accepts a String array, finds the middle character in the
+string, and adds it to a character array. For this method, find the middle element by dividing the length by two,
+rounding the number, and subtracting one. After computing the array, return it and print each character in the main
+method. Use the provided main method to call your method and print the output.
+
+Sample Output:
+        The middle letter of the array are:
+        delilah
+
+ */
+
+public class ArrayThereDelilah {
+    public static void main(String[] args) {
+        String[] array = new String[] {"add", "here", "lilly", "president", "jelly", "advance", "archers"};
+        char[] middleCharacter = middle(array);
+        String output = "";
+        for(char item: middleCharacter)
+            output += item;
+        System.out.println("The middle letters of the array are: ");
+        System.out.println(output);
+    }
+
+    public static char[] middle(String[] arr){
+        char[] middle = new char[arr.length];
+        for (int i = 0; i < arr.length; i++)
+            middle[i] = arr[i].charAt((int) Math.round(arr[i].length() / 2.0) - 1);
+        return middle;
+    }
+
+}
---
+++ src/arrays/programs_involving_data_sequences/hard/q10/MainTest.java
@@ -0,0 +1,54 @@
+package arrays.programs_involving_data_sequences.hard.q10;
+
+import global.BaseTest;
+import global.MethodTest;
+import global.tools.CustomAssertions;
+import global.variables.Clause;
+import global.variables.clauses.NewLine;
+import global.variables.clauses.StringLiteral;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class MainTest extends BaseTest {
+    //Java
+    @Override
+    public Clause[] testSentence() {
+        return new Clause[]{
+                new StringLiteral("The middle letters of the array are\\: "),
+                new NewLine(),
+                new StringLiteral("delilah"),
+                new NewLine()
+        };
+    }
+
+    @Override
+    public void runMain() {
+        ArrayThereDelilah.main(new String[0]);
+    }
+
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(
+                Arguments.of(new String[]{"add", "here", "lilly", "president", "jelly", "advance", "archers"}, "delilah".toCharArray()),
+                Arguments.of(new String[]{"archers", "get", "hello", "island", "cloud"}, "hello".toCharArray()),
+                Arguments.of(new String[]{"fireworks", "acorn", "array", "pilot", "hedge"}, "world".toCharArray()),
+                Arguments.of(new String[]{"funky", "opera", "flowers"}, "new".toCharArray()),
+                Arguments.of(new String[]{"rhyme", "yolk", "corks", "bookcase"}, "york".toCharArray()),
+                Arguments.of(new String[]{"lucid", "doing", "retry", "thyme"}, "city".toCharArray()),
+                Arguments.of(new String[]{"lemon", "faith", "pylon", "field", "backslash"},"miles".toCharArray())
+                );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    void printCorrectOutput(String[] input, char[] word) throws Throwable {
+        Object[][] arguments = {
+                {input, String[].class},
+        };
+        MethodTest m = new MethodTest(ArrayThereDelilah.class, "middle", arguments);
+        Object output = m.callMethod();
+        CustomAssertions._assertArrayEquals(word, output, "");
+    }
+}
