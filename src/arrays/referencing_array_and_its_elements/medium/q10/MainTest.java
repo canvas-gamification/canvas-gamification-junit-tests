3bfb198b3c53bbfdc36545a330209fd4f57fd5b1
Kai <kiamehrkhademi2019@gmail.com>
2023-02-10 02:00:44 -0800

    Modified q10

--- src/arrays/referencing_array_and_its_elements/medium/q10/MainTest.java
+++ src/arrays/referencing_array_and_its_elements/medium/q10/MainTest.java
@@ -1,6 +1,7 @@
 package arrays.referencing_array_and_its_elements.medium.q10;
 
 import global.BaseTest;
+import global.exceptions.InvalidClauseException;
 import global.tools.CustomAssertions;
 import global.tools.TestOption;
 import global.variables.Clause;
@@ -9,7 +10,6 @@ import global.variables.wrappers.Optional;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Random;
 import java.util.stream.Stream;
 
 import static global.utils.ArrayUtil.*;
@@ -17,20 +17,23 @@ import static global.utils.ArrayUtil.*;
 public class MainTest extends BaseTest {
     // Java
 
-    public static int n = 6;
-    public static char c = 'B';
+    public static int n = 30;
+    public static char ch = 'B';
 
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
         TestOption.defaultInput = arrayToInput(generateRandomWordArray(n));
         return new Clause[]{
-                new StringLiteral("Enter " + n + " given names:"),
-                new Optional(new StringLiteral(" ")),
-                new NewLine(),
-                new StringLiteral("There are "),
-                new IntegerLiteral("ans"),
-                new StringLiteral(" names that start with the letter " + c),
-                new Optional(new StringLiteral("."))
+            new StringLiteral("Enter " + n + " given names:"),
+            new Optional(new StringLiteral(" ")),
+            new NewLine(),
+            new StringLiteral("These are the "),
+            new IntegerLiteral("ans"),
+            new StringLiteral(" names that start with the letter " + ch + ":"),
+            new Optional(new StringLiteral(" ")),
+            new NewLine(),
+            new PlaceHolder(),
+            new Optional(new StringLiteral(" "))
         };
     }
 
@@ -39,41 +42,54 @@ public class MainTest extends BaseTest {
     }
 
     public static String randStr() {
-        Random r = new Random();
-        String ans = "";
-        for (int i = 0; i < n; i++) {
-            int up = r.nextInt(20) + 1;
-            String st = "" + (char) ('A' + r.nextInt(26));
-            for (int j = 1; j < up; j++)
-                st += "" + (char) ('a' + r.nextInt(26));
-            ans += st + " ";
+        String[] st = generateRandomWordArray(n);
+        for(int i = 0; i < n; i ++){
+            st[i] = (char)(st[i].charAt(0) - 'a' + 'A') + st[i].substring(1);
         }
-
-        return ans;
+        return arrayToInput(st);
     }
 
     static Stream<String> inputProvider() {
+        String[] t1 = new String[n];
+        for(int i = 0; i < n; i ++){
+            t1[i] = ch + "";
+        }
+        String[] t2 = new String[n];
+        for(int i = 0; i < n; i ++){
+            t2[i] = (char)(ch + 1) + "";
+        }
         return Stream.of(
                 randStr(),
                 randStr(),
                 randStr(),
                 randStr(),
                 randStr(),
-                randStr(),
-                arrayToInput(replicateArray(c, n)),
-                arrayToInput(replicateArray((char) (c + 1), n))
+                arrayToInput(t1),
+                arrayToInput(t2)
 
         );
     }
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void printsCorrectOutput(String a) {
+    void printsCorrectOutput(String st) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not print the correct names starting with the lettter" + ch + ".";
         int t = 0;
-        for (int i = 0; i < a.length(); i++)
-            if (a.charAt(i) == c)
-                t++;
-        runWithInput(a);
-        CustomAssertions._assertEquals(t, Integer.parseInt(getItemByName("ans")), "Your program does not correctly print the number of names starting with " + c + ".");
+        String ans = "";
+        boolean flg = false;
+        for(int i = 0; i < st.length(); i ++){
+            if(st.charAt(i) == ch) {
+                t ++;
+                flg = true;
+            }
+            if(flg)
+                ans += st.charAt(i);
+            if(st.charAt(i) == ' ')
+                flg = false;
+        }
+        runWithInput(st, new Clause[]{
+                new StringLiteral(ans.trim())
+        });
+        CustomAssertions._assertEquals(t, Integer.parseInt(getItemByName("ans")), "Your program does not correctly print the number of names starting with " + ch + ".");
     }
 }
--- src/arrays/referencing_array_and_its_elements/medium/q10/VowelNames.java
+++ src/arrays/referencing_array_and_its_elements/medium/q10/VowelNames.java
@@ -1,30 +1,35 @@
 package arrays.referencing_array_and_its_elements.medium.q10;
 
 /*
-Write a Java program that asks the user for 6 names and stores them in an array of Strings. Then, count how many of the
-names start with the letter "B" and display the value.
+Write a Java program that asks the user for 30 names and stores them in an array of Strings. Then, count how many of the
+names start with the letter "B" and display the value, and the name in order that they appear.
 
 Sample Output:
-    Enter 6 given names:
-    Billy Bob Jacob Anthony Bartholomew Angus
-    There are 3 names that start with the letter B
-
+    Enter 30 given names:
+    Billy Bob Jacob ...  Anthony Bartholomew Angus
+    These are the 3 names that start with the letter B:
+    Billy Bob Bartholomew
  */
 
 import java.util.Scanner;
 
 public class VowelNames {
     public static void main(String[] args) {
+        int n = 30;
         Scanner in = new Scanner(System.in);
-        String[] arr = new String[6];
-        System.out.println("Enter 6 given names:");
-        for (int i = 0; i < 6; i++)
+        String[] arr = new String[n];
+        System.out.println("Enter " + n + " given names:");
+        for (int i = 0; i < n; i++)
             arr[i] = in.next();
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i].charAt(0) == 'B')
                 count++;
         }
-        System.out.println("There are " + count + " names that start with the letter B");
+        System.out.println("These are the " + count + " names that start with the letter B:");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].charAt(0) == 'B')
+                System.out.print(arr[i] + " ");
+        }
     }
 }
