3b18421efb7a3de6336902f939efad28dddb0a7d
Kai <kiamehrkhademi2019@gmail.com>
2023-02-11 03:31:27 -0800

    Modified q3

--- src/arrays/programs_involving_multidimensional_data/hard/q3/EightByEight.java
+++ src/arrays/programs_involving_multidimensional_data/hard/q3/EightByEight.java
@@ -1,14 +1,13 @@
 package arrays.programs_involving_multidimensional_data.hard.q3;
 
 /*
-Write a Java program that initializes a 2D array of random integers of size 8 by 8 with values in the range of 0 and 10
-(inclusive of both values) and print it each row in a line, then write a function called find that takes a 8X8 2d array
-and an integer as an index, to represent an index in the array that spans across both rows and columns, and print out
-the value at that index. Call the function to find the value at index 7 and print it.
+Write a Java program that initializes an 8 by 8 2D array with random values from 0 to 10 (inclusive of both values) and
+prints each row in a line. Then write a function called to find that takes a 2d array
+and an index, and prints out the value at that index. Using that, find the value of the element at index 7.
 Consider some examples with an 8 by 8 array. Index 2 points to the element in the first row and the third column. Index
 8 points to the element in the second row and the first column. Index 16 points to the element in the third row and the
 first column.
-Note that your find method should be general enough to work with any size of 2D array.
+Note that your find method should be general enough to work with any size of a 2D array.
 
 Sample Output:
     2 3 3 6 8 5 3 4
