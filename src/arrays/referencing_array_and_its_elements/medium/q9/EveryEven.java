f4ef5945d257aab8b21add968108ff2ff26e4e6a
Kai <kiamehrkhademi2019@gmail.com>
2023-02-11 01:49:20 -0800

    Modified q9

--- src/arrays/referencing_array_and_its_elements/medium/q9/EveryEven.java
+++ src/arrays/referencing_array_and_its_elements/medium/q9/EveryEven.java
@@ -1,9 +1,9 @@
 package arrays.referencing_array_and_its_elements.medium.q9;
 
 /*
-Write a Java program that initializes an array of 10 integers randomly generated between 1 and 1000 (inclusive on both ends)
-and then prints the array. Then initialize a second array to be half the size of the first. Then copy all the values at
-even indexes from the first array and store them in the second array. Then print out the second array.
+Write a Java program that initializes an array of 1000 randomly generated integers between 1 and 1000 (inclusive on both
+ends) and then prints the array. Then initialize a second array to be half the size of the first and copy all values at
+even indexes from the first array and store them in the second array starting at 0. Then print out the second array.
 
 Sample Output:
     The randomly generated array:
@@ -14,11 +14,11 @@ Sample Output:
 
 public class EveryEven {
     public static void main(String[] args) {
-        int[] arr = new int[10];
+        int[] arr = new int[100];
         for (int i = 0; i < arr.length; i++)
             arr[i] = (int) (Math.random() * 1000) + 1;
         System.out.println("The random generated array:");
-        for(int i = 0; i < 10; i ++)
+        for(int i = 0; i < 100; i ++)
             System.out.print(arr[i] + " ");
         System.out.println();
         int len = arr.length;
