fa3eef618d0153689c8446577f27a8407ceb4510
Samantha H <hodge1214@gmail.com>
2023-02-12 16:56:30 -0500

    made changes

--- src/arrays/arrays_with_methods/hard/q11/MainTest.java
+++ src/arrays/arrays_with_methods/hard/q11/MainTest.java
@@ -1,10 +1,11 @@
 package arrays.arrays_with_methods.hard.q11;
 
 import global.BaseTest;
+import global.MethodTest;
 import global.exceptions.InvalidClauseException;
 import global.tools.CustomAssertions;
 import global.tools.TestOption;
-import global.utils.MethodUtil;
+import global.utils.ArrayUtil;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
 import global.variables.clauses.PlaceHolder;
@@ -18,6 +19,7 @@ import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
     // Java
+
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
         TestOption.defaultInput = "Hello my old heart";
@@ -35,15 +37,18 @@ public class MainTest extends BaseTest {
 
     static Stream<Arguments> charFinderInputProvider() {
         return Stream.of(
-                Arguments.of(new String[]{"Hello", "World"}, new char[]{'H', 'W'}),
-                Arguments.of(new String[]{"Don't", "Shut", "Down", "On", "me", "Now"}, new char[]{'D', 'S', 'D', 'O', 'm', 'N'}),
-                Arguments.of(new String[]{"chairs", "eat", "hair"}, new char[]{'c', 'e', 'h'}),
-                Arguments.of(new String[]{"oneWord"}, new char[]{'o'}),
-                Arguments.of(new String[]{"nOt", "cAsE", "sEnSitiVe", "Please"}, new char[]{'n', 'c', 's', 'P'}),
-                Arguments.of(new String[]{"764", "583", "3"}, new char[]{'7', '5', '3'}),
-                Arguments.of(new String[]{"!@", "////%!", "#@#", "&<>/'"}, new char[]{'!', '/', '#', '&'}),
-                Arguments.of(new String[]{"+<3u", "m1x", "!t", "U9!"}, new char[]{'+', 'm', '!', 'U'}),
-                Arguments.of(new String[]{"a", "s", "i", "n", "g", "l", "e", "l", "e", "t", "t", "e", "r"}, new char[]{'a', 's', 'i', 'n', 'g', 'l', 'e', 'l', 'e', 't', 't', 'e', 'r'})
+                Arguments.of((Object) new String[]{"Hello", "World"}),
+                Arguments.of((Object) new String[]{"Don't", "Shut", "Down", "On", "me", "Now"}),
+                Arguments.of((Object) new String[]{"chairs", "eat", "hair"}),
+                Arguments.of((Object) new String[]{"oneWord"}),
+                Arguments.of((Object) new String[]{"nOt", "cAsE", "sEnSitiVe", "Please"}),
+                Arguments.of((Object) new String[]{"764", "583", "3"}),
+                Arguments.of((Object) new String[]{"!@", "////%!", "#@#", "&<>/'"}),
+                Arguments.of((Object) new String[]{"+<3u", "m1x", "!t", "U9!"}),
+                Arguments.of((Object) new String[]{"a", "s", "i", "n", "g", "l", "e", "l", "e", "t", "t", "e", "r"}),
+                Arguments.of((Object) ArrayUtil.generateRandomWordArray(150)),
+                Arguments.of((Object) ArrayUtil.generateRandomWordArray(300)),
+                Arguments.of((Object) ArrayUtil.generateRandomWordArray(500))
         );
     }
 
@@ -53,15 +58,26 @@ public class MainTest extends BaseTest {
                 Arguments.of("741 518 3", new String[]{"7", "5", "3"}),
                 Arguments.of("L3t's m!x 1t 0p!", new String[]{"L", "m", "1", "0"}),
                 Arguments.of("!@ ////%! #@# &<>/'", new String[]{"!", "/", "#", "&"}),
-                Arguments.of("Run down the rabbit hole, swore i would never go, back to bad habbits I'm thorugh.", new String[]{"R", "d", "t", "r", "h", "s", "i", "w", "n", "g", "b", "t", "b", "h", "I", "t"})
+                Arguments.of("Run down the rabbit hole, swore i would never go, back to bad habits I'm through.", new String[]{"R", "d", "t", "r", "h", "s", "i", "w", "n", "g", "b", "t", "b", "h", "I", "t"})
         );
+    }
+
+    public static char[] ans(String[] input) {
+        char[] arr = new char[input.length];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = input[i].charAt(0);
+        return arr;
     }
 
     @ParameterizedTest
     @MethodSource("charFinderInputProvider")
-    void correctCharFinderMethod(String[] words, char[] expected) throws Throwable {
-        Object ouput = MethodUtil.invokeIfMethodExists(ArrayofLetters.class, "charFinder", new Object[]{words}, String[].class);
-        CustomAssertions._assertArrayEquals(expected, ouput, "Your charFinder method does not return the char array with the correct characters.");
+    void correctCharFinderMethod(String[] words) throws Throwable {
+        Object[][] arguments = {
+                {words, String[].class}
+        };
+        MethodTest m = new MethodTest(ArrayofLetters.class, "charFinder", arguments);
+        Object output = m.callMethod();
+        CustomAssertions._assertArrayEquals(ans(words), output, "Your charFinder method does not return the char array with the correct characters.");
     }
 
     @ParameterizedTest
