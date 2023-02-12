53b8b736b0d4f43059440f6068eadcdc469b6f06
Kai <kiamehrkhademi2019@gmail.com>
2023-02-12 02:39:21 -0800

    Modified q7

--- src/arrays/programs_involving_multidimensional_data/hard/q7/MainTest.java
+++ src/arrays/programs_involving_multidimensional_data/hard/q7/MainTest.java
@@ -7,6 +7,7 @@ import global.variables.Clause;
 import global.variables.clauses.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -15,8 +16,8 @@ public class MainTest extends BaseRandomTest {
     // Java
 
     public static final int n = 5;
-    public static final int up = 100;
-    public static final int down = 0;
+    public static final int upperBound = 100;
+    public static final int lowerBound = 0;
 
     public Clause[] testSentence() {
         Clause[] c = new Clause[4 * n * n + 2 * n + 4];
@@ -25,7 +26,7 @@ public class MainTest extends BaseRandomTest {
         c[t++] = new NewLine();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                c[t++] = new RandomDouble(down, up + 1, i + " " + j);
+                c[t++] = new RandomDouble(lowerBound, upperBound + 1, i + " " + j);
                 c[t++] = new StringLiteral(" ");
             }
             c[t++] = new NewLine();
@@ -52,14 +53,14 @@ public class MainTest extends BaseRandomTest {
         int[][] out = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                ans[i][j] = (int) (Double.parseDouble(getItemByName(i + " " + j)));
+                ans[i][j] = (int) Math.round(Double.parseDouble(getItemByName(i + " " + j)));
                 out[i][j] = Integer.parseInt(getItemByName(i + "x" + j));
             }
         }
-        CustomAssertions._assertArrayEquals(ans, out, "Your program does not correctly round the grades in the 2D array.");
+        CustomAssertions._assertArrayEquals(ans, out, "Your program does not correctly round and print the grades in the 2D array.");
     }
 
-    static Stream<double[][]> inputProvider() {
+    static Stream<Arguments> inputProvider() {
         double[][] t1 = {
                 {1.5, 2.4, 3.3, 4.2, 5.1},
                 {2.4, 3.3, 4.2, 5.1, 1.5},
@@ -67,31 +68,47 @@ public class MainTest extends BaseRandomTest {
                 {4.2, 5.1, 1.5, 2.4, 3.3},
                 {5.1, 1.5, 2.4, 3.3, 4.2}
         };
+        int[][] a1 = {
+                {2, 2, 3, 4, 5},
+                {2, 3, 4, 5, 2},
+                {3, 4, 5, 2, 2},
+                {4, 5, 2, 2, 3},
+                {5, 2, 2, 3, 4}
+        };
         double[][] t2 = {
                 {1.0, 0.5},
                 {0.5, 1.0}
+        };
+        int[][] a2 = {
+                {1, 1},
+                {1, 1}
         };
         double[][] t3 = {
                 {0.1, 0.2},
                 {0.3, 0.4}
         };
+        int[][] a3 = {
+                {0, 0},
+                {0, 0}
+        };
         double[][] t4 = {
                 {2.5}
         };
+        int[][] a4 = {
+                {3}
+        };
 
         return Stream.of(
-                t1, t2, t3, t4
+                Arguments.of(t1, a1),
+                Arguments.of(t2, a2),
+                Arguments.of(t3, a3),
+                Arguments.of(t4, a4)
         );
     }
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    public void correctRoundingMethod(double[][] arr) throws Throwable {
-        int[][] ans = new int[arr.length][arr[0].length];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++)
-                ans[i][j] = (int) (arr[i][j]);
-        }
+    public void correctRoundingMethod(double[][] arr, int[][] ans) throws Throwable {
         Object[][] arguments = {
                 {arr, double[][].class}
         };
--- src/arrays/programs_involving_multidimensional_data/hard/q7/Rounding.java
+++ src/arrays/programs_involving_multidimensional_data/hard/q7/Rounding.java
@@ -1,26 +1,26 @@
 package arrays.programs_involving_multidimensional_data.hard.q7;
 
 /*
-Suppose you have a 2D array that stores student assignment grades. In particular, each row represents the grades for a
-different assignment from all the students. You have to enter the student grades into a database and suddenly realize
-that the database only takes integers, so you will need to round the student's grades.
+Suppose you have a 2D array that stores student assignment grades.  You have to enter the student grades into a database
+and suddenly realize that the database only takes integers, so you will need to round the student's grades.
 Write a Java Program that makes a 5 by 5 2D array, fills it with random
-numbers between 0 to 100, and prints it. Then create a called rounding function to round each of the grades in the
-array, and returns that integer array. Finally, print the rounded 2D array.
+numbers between 0 to 100, and prints it. Then create a method called rounding to round each of the grades in the
+array by thaking the double array, and returns that integer array. Finally, print the rounded 2D array.
+Note that your rounding method should be general enough to work with any size of 2D array.
 
 Sample Output:
     The random 2D array:
-    13.544899944021337 64.70135979770244 94.23929312655405 76.63092238189374 30.521198148351885
-    73.87021267607889 55.60484892629431 98.77330545415433 67.68255298571691 41.130201268970794
-    55.973958811385735 40.962744378988404 53.3910570924838 64.24435158238697 88.942819126096
-    6.243920961189118 19.73357192213495 51.13095170984741 78.86428587056578 22.313021248589447
-    26.246488602006227 30.86133848517529 3.8830410017390524 52.67181102841428 8.485511662236396
+    1.9338482384426119 46.59835249491862 81.01173638264179 63.70336265471519 26.896058098483472
+    65.98237391038224 79.72196744223793 56.33921258158885 23.213405222932785 60.21131681612218
+    100.34856192363331 47.33455010753071 42.184489896873785 20.145072072441497 91.68668195260074
+    8.313453517738312 61.67094917049669 54.11214691680937 5.907644791339368 68.3065090623183
+    78.1290800203377 89.60667150546287 13.015885961478071 46.95144084024514 21.19032689829607
     The rounded 2D array:
-    13 64 94 76 30
-    73 55 98 67 41
-    55 40 53 64 88
-    6 19 51 78 22
-    26 30 3 52 8
+    2 47 81 64 27
+    66 80 56 23 60
+    100 47 42 20 92
+    8 62 54 6 68
+    78 90 13 47 21
  */
 
 public class Rounding {
@@ -36,8 +36,8 @@ public class Rounding {
         }
         System.out.println("The rounded 2D array:");
         int[][] ans = rounding(arr);
-        for(int i = 0; i < ans.length; i ++){
-            for(int j = 0; j < ans[i].length; j ++)
+        for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans[i].length; j++)
                 System.out.print(ans[i][j] + " ");
             System.out.println();
         }
@@ -47,7 +47,7 @@ public class Rounding {
         int[][] ans = new int[arr.length][arr[0].length];
         for (int i = 0; i < arr.length; i++)
             for (int j = 0; j < arr[i].length; j++) {
-                ans[i][j] = (int)Math.floor(arr[i][j]);
+                ans[i][j] = (int) Math.round(arr[i][j]);
             }
         return ans;
     }
