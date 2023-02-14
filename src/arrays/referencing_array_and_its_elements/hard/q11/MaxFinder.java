5ccb8d28d5f4d81ce5eecb4d95af612474214d05
Kai <kiamehrkhademi2019@gmail.com>
2023-02-14 12:02:53 -0800

    Modified q10

--- src/arrays/referencing_array_and_its_elements/hard/q11/MaxFinder.java
+++ src/arrays/referencing_array_and_its_elements/hard/q11/MaxFinder.java
@@ -6,8 +6,8 @@ passed to a method called findMax which accepts an array of doubles of any size 
 which the main method then prints out.
 
 Sample Output:
-    Enter an array of 10 double values:
-    60.5 50.2 70.2 43.2 61.4 32.5 74.1 11.2 94.3 64.3
+    Enter an array of 100 double values:
+    60.5 50.2 70.2 43.2 ... 61.4 32.5 74.1 11.2 94.3 64.3
     The maximum value of the array is: 94.3
  */
 
@@ -16,9 +16,9 @@ import java.util.Scanner;
 public class MaxFinder {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double[] arr = new double[10];
-        System.out.println("Enter an array of 10 double values:");
-        for (int i = 0; i < 10; i++)
+        double[] arr = new double[100];
+        System.out.println("Enter an array of 100 double values:");
+        for (int i = 0; i < 100; i++)
             arr[i] = in.nextDouble();
         System.out.println("The maximum value of the array is: " + findMax(arr));
     }
