6155dc0cd1948e1133808a45e8b14705839b7621
te-sa <teresa_saller@freenet.de>
2023-02-14 13:09:28 -0800

    put two cases for each year

--- src/methods/modular_programs/hard/q10/MainTest.java
+++ src/methods/modular_programs/hard/q10/MainTest.java
@@ -40,16 +40,28 @@ public class MainTest extends BaseTest {
     static Stream<Arguments> inputProvider() {
         return Stream.of(
                 Arguments.of(1960, "Rat"),
+                Arguments.of(1972, "Rat"),
                 Arguments.of(1985, "Ox"),
+                Arguments.of(2033, "Ox"),
                 Arguments.of(1962, "Tiger"),
+                Arguments.of(1950, "Tiger"),
                 Arguments.of(1951, "Rabbit"),
+                Arguments.of(1963, "Rabbit"),
                 Arguments.of(2024, "Dragon"),
+                Arguments.of(2012, "Dragon"),
                 Arguments.of(2001, "Snake"),
+                Arguments.of(1641, "Snake"),
                 Arguments.of(1978, "Horse"),
+                Arguments.of(778, "Horse"),
                 Arguments.of(1955, "Goat"),
+                Arguments.of(95, "Goat"),
                 Arguments.of(2004, "Monkey"),
+                Arguments.of(2016, "Monkey"),
                 Arguments.of(1573, "Rooster"),
+                Arguments.of(1981, "Rooster"),
                 Arguments.of(14, "Dog"),
+                Arguments.of(1994, "Dog"),
+                Arguments.of(1959, "Pig"),
                 Arguments.of(603, "Pig")
         );
     }
