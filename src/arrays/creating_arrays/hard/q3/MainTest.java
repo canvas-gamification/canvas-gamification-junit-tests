59fd2917653347b64bc8373282e08d033a081c51
Samantha H <hodge1214@gmail.com>
2023-02-19 17:56:11 -0500

    changed question so that array got referenced at random indexes a bunch

--- src/arrays/creating_arrays/hard/q3/MainTest.java
+++ src/arrays/creating_arrays/hard/q3/MainTest.java
@@ -2,13 +2,14 @@ package arrays.creating_arrays.hard.q3;
 
 import global.BaseTest;
 import global.tools.TestOption;
+import global.utils.ArrayUtil;
 import global.variables.Clause;
 import global.variables.clauses.IntegerLiteral;
 import global.variables.clauses.NewLine;
+import global.variables.clauses.RandomInteger;
 import global.variables.clauses.StringLiteral;
 import global.variables.wrappers.Optional;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -18,47 +19,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MainTest extends BaseTest {
     // Java
 
+    private final static int size = 100;
+
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
-        TestOption.defaultInput = "34 1 5432 765 -12";
-        return new Clause[]{
-                new StringLiteral("Enter 5 integers:"),
-                new Optional(new StringLiteral(" ")),
-                new NewLine(),
-                new IntegerLiteral("one"),
-                new NewLine(),
-                new IntegerLiteral("two"),
-                new NewLine(),
-                new IntegerLiteral("three"),
-                new NewLine(),
-                new IntegerLiteral("four"),
-                new NewLine(),
-                new IntegerLiteral("five")
-        };
+        TestOption.defaultInput = ArrayUtil.arrayToInput(ArrayUtil.generateRandomArray(-10000, 10000, size));
+        return clauseBuilder();
     }
 
     public void runMain() {
         StoringInts.main(new String[0]);
     }
 
-    static Stream<Arguments> mainMethodInputProvider() {
+    static Stream<int[]> mainMethodInputProvider() {
         return Stream.of(
-                Arguments.of("25 32 102 7362 4738", 25, 32, 102, 7362, 4738),
-                Arguments.of("-45 -217 -2 -93 -12963", -45, -217, -2, -93, -12963),
-                Arguments.of("0 7 12 1 5", 0, 7, 12, 1, 5),
-                Arguments.of("743289 43824 73219 463 382787", 743289, 43824, 73219, 463, 382787),
-                Arguments.of("3728 -482 2193 -32 -1", 3728, -482, 2193, -32, -1)
+                ArrayUtil.generateRandomArray(-10000, 10000, size),
+                ArrayUtil.generateRandomArray(-10000, 10000, size),
+                ArrayUtil.generateRandomArray(-10000, 10000, size),
+                ArrayUtil.generateRandomArray(-10000, 10000, size)
         );
     }
 
     @ParameterizedTest
     @MethodSource("mainMethodInputProvider")
-    void printsOutputCorrectly(String in, int one, int two, int three, int four, int five) {
-        runWithInput(in);
-        assertEquals(one, Integer.parseInt(getItemByName("one")), "Your program does not print the first input value.");
-        assertEquals(two, Integer.parseInt(getItemByName("two")), "Your program does not print the second input value.");
-        assertEquals(three, Integer.parseInt(getItemByName("three")), "Your program does not print the third input value.");
-        assertEquals(four, Integer.parseInt(getItemByName("four")), "Your program does not print the fourth input value.");
-        assertEquals(five, Integer.parseInt(getItemByName("five")), "Your program does not print the fifth input value.");
+    void printsOutputCorrectly(int[] in) {
+        runWithInput(ArrayUtil.arrayToInput(in));
+        String m = "Your program does not print the correct value for the randomly generated index";
+        for (int x = 0; x < size; x++) {
+            assertEquals(in[Integer.parseInt(getItemByName("index" + x))], Integer.parseInt(getItemByName("num" + x)), m);
+        }
+    }
+
+    public Clause[] clauseBuilder() {
+        Clause[] c = new Clause[size * 5 + 3];
+
+        c[0] = new StringLiteral("Enter " + size + " integers:");
+        c[1] = new Optional(new StringLiteral(" "));
+        c[2] = new NewLine();
+
+        int count = 3;
+
+        for (int x = 0; x < size; x++) {
+            c[count++] = new StringLiteral("Number at index ");
+            c[count++] = new RandomInteger(0, size, "index" + x);
+            c[count++] = new StringLiteral(" is: ");
+            c[count++] = new IntegerLiteral("num" + x);
+            c[count++] = new NewLine();
+        }
+
+        return c;
     }
 }
--- src/arrays/creating_arrays/hard/q3/StoringInts.java
+++ src/arrays/creating_arrays/hard/q3/StoringInts.java
@@ -1,17 +1,17 @@
 package arrays.creating_arrays.hard.q3;
 
 /*
-Write a program that asks the user for five integers, stores those values into an array, and prints the array with each
-value on a new line back to the user.
+Write a program that asks the user for one hundred integers, stores those values into an array, and prints the values at
+one hundred randomly generated indexes of the array.
 
 Sample output:
     Enter 5 integers:
-    374 5 5482 76 -16
-    374
-    5
-    5482
-    76
-    16
+    374 5 ... 76 -16
+    Number at index 2 is: 34
+    Number at index 17 is: 5482
+    ...
+    Number at index 3 is: 76
+    Number at index 0 is: 374
  */
 
 import java.util.Scanner;
@@ -19,13 +19,14 @@ import java.util.Scanner;
 public class StoringInts {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter 5 integers: ");
-        int[] sent = new int[5];
+        System.out.println("Enter 100 integers: ");
+        int[] sent = new int[100];
         for (int i = 0; i < sent.length; i++) {
             sent[i] = input.nextInt();
         }
         for (int i = 0; i < sent.length; i++) {
-            System.out.println(sent[i]);
+            int index = (int) (Math.random()*sent.length);
+            System.out.println("Number at index " + index + " is: " + sent[index]);
         }
     }
 }
