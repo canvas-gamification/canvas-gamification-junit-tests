15b93557ab8ddf84255fac6fadff3544bf91723d
Seth Akins <83678885+Hedgemon4@users.noreply.github.com>
2023-02-20 10:37:57 -0800

    Update src/arrays/arrays_with_methods/hard/q7/ArraySimilarity.java

--- src/arrays/arrays_with_methods/hard/q7/ArraySimilarity.java
+++ src/arrays/arrays_with_methods/hard/q7/ArraySimilarity.java
@@ -3,7 +3,7 @@ package arrays.arrays_with_methods.hard.q7;
 /*
 Write a method called isEqual that takes two integer arrays of any length as input and checks each of their elements one
 at a time to see whether all of their elements are the same or not. The method returns true if all the elements of the
-two arrays are the same, and false otherwise.
+two arrays are the same, and false otherwise. Note that the arrays must be the same length to be equal.
  */
 
 public class ArraySimilarity {
