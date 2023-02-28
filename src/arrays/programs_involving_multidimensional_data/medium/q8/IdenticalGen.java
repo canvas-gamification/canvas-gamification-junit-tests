0444ba254123afd82c394392e942f287da0011bc
Kai <kiamehrkhademi2019@gmail.com>
2023-02-28 13:56:10 -0800

    Modified q8

--- src/arrays/programs_involving_multidimensional_data/medium/q8/IdenticalGen.java
+++ src/arrays/programs_involving_multidimensional_data/medium/q8/IdenticalGen.java
@@ -1,10 +1,20 @@
 package arrays.programs_involving_multidimensional_data.medium.q8;
 
 /*
-Write a Java program that creates a random integer in the range of 1 to 1000 (inclusive of both values), and print it.
-Then, the program will also create a 10 * 10 2D array. Next, use a method called squareRoot to
-initialize all the elements of the given array with the square root of the value and print out the elements in the
-2D array.
+Write a Java program that creates a random integer x in the range of 1 to 1000 (inclusive of both values), and print it.
+Then, the program will also create a 10 * 10 2D array, using a method called squareRoot that takes the number x, and
+two numbers n and m and return a 2D double array which all its elements are the square root of x, and finally print
+that 2D array in the main method. Your method should be general enough to work with any size of array.
+
+Sample Output:
+    Random number: 16
+    The 2D array of square root is:
+    4.0 4.0 4.0 ... 4.0
+    4.0 4.0 4.0 ... 4.0
+    .               .
+    .               .
+    .               .
+    4.0 4.0 4.0 ... 4.0
 
 Distractors:
     out[i] = Math.sqrt( size );
@@ -17,16 +27,24 @@ Distractors:
 public class IdenticalGen {
     public static void main(String[] args) {
         int num = (int) (Math.random() * 1000 + 1);
+        System.out.print("Random number: ");
         System.out.println(num);
-        double[][] out = new double[10][10];
-        for (int i = 0; i < out.length; i++)
-            for (int j = 0; j < out[i].length; j++)
-                out[i][j] = Math.sqrt(num);
+        double[][] out = squareRoot(num, 10, 10);
+        System.out.println("The 2D array of square root is:");
         for (int i = 0; i < out.length; i++) {
             for (int j = 0; j < out[i].length; j++)
                 System.out.print(out[i][j] + " ");
             System.out.println();
         }
+    }
+
+    public static double[][] squareRoot(int x, int n, int m) {
+        double[][] ans = new double[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                ans[i][j] = Math.sqrt(x);
+        }
+        return ans;
     }
 
 }
--- src/arrays/programs_involving_multidimensional_data/medium/q8/MainTest.java
+++ src/arrays/programs_involving_multidimensional_data/medium/q8/MainTest.java
@@ -1,25 +1,35 @@
 package arrays.programs_involving_multidimensional_data.medium.q8;
 
 import global.BaseRandomTest;
+import global.MethodTest;
 import global.tools.CustomAssertions;
 import global.variables.Clause;
 import global.variables.clauses.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class MainTest extends BaseRandomTest {
     // Parsons with Distractors
 
     public static final int n = 10;
+    public static final int m = 10;
     public static final int up = 1000;
     public static final int down = 1;
 
     public Clause[] testSentence() {
-        Clause[] c = new Clause[2 * n * n + n + 2];
+        Clause[] c = new Clause[2 * n * m + n + 5];
         int t = 0;
+        c[t++] = new StringLiteral("Random number: ");
         c[t++] = new RandomInteger(down, up + 1, "num");
         c[t++] = new NewLine();
+        c[t++] = new StringLiteral("The 2D array of square root is:");
+        c[t++] = new NewLine();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 c[t++] = new DoubleLiteral(i + " " + j);
                 c[t++] = new StringLiteral(" ");
             }
@@ -36,9 +46,39 @@ public class MainTest extends BaseRandomTest {
     public void printsCorrectOutput() {
         double num = Math.sqrt(Double.parseDouble(getItemByName("num")));
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                CustomAssertions._assertEquals(num, Double.parseDouble(getItemByName(i + " " + j)), "Your program does not print the correct 2D array containing the sqrt of the generated number.");
+            for (int j = 0; j < m; j++) {
+                CustomAssertions._assertEquals(num, Double.parseDouble(getItemByName(i + " " + j)),
+                        "Your program does not print the correct 2D array containing the sqrt of the generated number.");
             }
         }
+    }
+
+    static Stream<Arguments> inputProvider() {
+        return Stream.of(
+                Arguments.of(16, 5, 7),
+                Arguments.of(32, 2, 10),
+                Arguments.of(1000001, 10, 10),
+                Arguments.of(23, 24, 25),
+                Arguments.of(1, 1, 1),
+                Arguments.of(0, 0, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void correctSquareRootMethod(int num, int n, int m) throws Throwable {
+        Object[][] arguments = {
+                {num, int.class},
+                {n, int.class},
+                {m, int.class}
+        };
+        MethodTest test = new MethodTest(IdenticalGen.class, "squareRoot", arguments);
+        Object output = test.callMethod();
+        double[][] ans = new double[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                ans[i][j] = Math.sqrt(num);
+        }
+        CustomAssertions._assertArrayEquals(ans, output, "Your square Root method does not return the correct 2D array containing the sqrt of the generated number.");
     }
 }
