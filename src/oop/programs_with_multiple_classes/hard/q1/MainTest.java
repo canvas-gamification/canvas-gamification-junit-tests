8235a1e9e27bcd8856f8c5a46801dd1dc36660b0
Hedgemon4 <sethlinc4@gmail.com>
2023-02-28 14:01:41 -0800

    Started coding question

---
+++ src/oop/programs_with_multiple_classes/hard/q1/MainTest.java
@@ -0,0 +1,14 @@
+package oop.programs_with_multiple_classes.hard.q1;
+
+import global.BaseTest;
+import global.variables.Clause;
+
+public class MainTest extends BaseTest {
+    public Clause[] testSentence() {
+        return new Clause[0];
+    }
+
+    public void runMain() {
+
+    }
+}
---
+++ src/oop/programs_with_multiple_classes/hard/q1/TestGroup.java
@@ -0,0 +1,9 @@
+package oop.programs_with_multiple_classes.hard.q1;
+
+public class TestGroup {
+    public static void main(String[] args) {
+        Circus funGroup = new Circus(100);
+        Member owner = new Member("Nick");
+        Member juggler = new Member("Sarah");
+    }
+}
