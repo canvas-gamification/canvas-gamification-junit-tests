0b700c5e58079506207355c51dfb59b6a5495393
Hedgemon4 <sethlinc4@gmail.com>
2023-03-11 18:07:01 -0800

    Kept coding question

--- src/oop/creating_objects_from_classes/hard/q1/MainTest.java
+++ src/oop/creating_objects_from_classes/hard/q1/MainTest.java
@@ -2,6 +2,7 @@ package oop.creating_objects_from_classes.hard.q1;
 
 import global.ObjectTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -11,16 +12,26 @@ import static global.tools.CustomAssertions._assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest {
-    private final String pet = "pet";
-    private final String animalType = "dog";
+    private final String pet = "Pet";
+    private final String animalType = "Dog";
+    private final String methodName = "makePet";
     private ObjectTest petClass;
-    private ObjectTest animalClass;
 
     @BeforeEach
     public void setUp() {
         String petClassString = "oop.creating_objects_from_classes.hard.q1." + pet;
-        String animalClassString = "oop.creating_objects_from_classes.hard.q1." + animalType;
-        petClass =  new ObjectTest(petClassString);
+        petClass = new ObjectTest(petClassString);
+    }
+
+    @Test
+    public void petClassHasMakePetMethod() {
+        Class<?>[] classes = {String.class};
+        assertTrue(petClass.hasMethod("makePet", classes),
+                "Your " + pet + " class is missing the method" + methodName + ".");
+        assertTrue(petClass.hasReturnType("makePet", classes, Dog.class),
+                "Your " + methodName + " method does not have the correct return type.");
+        assertTrue(petClass.hasModifier("makePet",  classes, "public"),
+                "Your " + methodName + " method does not have the correct visibility modifier.");
     }
 
     private static Stream<String> petNameInputProvider() {
@@ -29,9 +40,14 @@ public class MainTest {
 
     @ParameterizedTest
     @MethodSource("petNameInputProvider")
-    public void petClassHasCorrectMakePetMethod() throws Throwable {
+    public void makePetClassReturnsCorrectValue(String name) throws Throwable {
         Object petInstance = petClass.createInstance();
-        Class<?>[] classes = {String.class};
-        assertTrue(petClass.hasMethod("makePet", classes), );
+        Object[][] arguments = {
+                {name, String.class}
+        };
+        Object makePetOutput = petClass.callMethod("makePet", arguments, petInstance);
+        Dog expectedOutput = new Dog(name);
+        _assertEquals(expectedOutput, makePetOutput,
+                "Your makePet method does not return the correct output.");
     }
 }
--- src/oop/creating_objects_from_classes/hard/q1/Pet.java
+++ src/oop/creating_objects_from_classes/hard/q1/Pet.java
@@ -2,6 +2,6 @@ package oop.creating_objects_from_classes.hard.q1;
 
 public class Pet {
     public Dog makePet(String name) {
-        return new Dog("name");
+        return new Dog(name);
     }
 }
