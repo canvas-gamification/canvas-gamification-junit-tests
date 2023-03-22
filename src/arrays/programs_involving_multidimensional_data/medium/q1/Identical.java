c1cdfec0ec9e6872f8bc379bb53be560c56478c4
Samantha H <hodge1214@gmail.com>
2023-03-21 23:02:38 -0400

    changed question so it isn't a scanner question

--- src/arrays/programs_involving_multidimensional_data/medium/q1/Identical.java
+++ src/arrays/programs_involving_multidimensional_data/medium/q1/Identical.java
@@ -1,36 +1,20 @@
 package arrays.programs_involving_multidimensional_data.medium.q1;
 
 /*
-Write a Java program that initializes two integer arrays of size 5 by 5 and prompts the user to input values for them.
-Then, check to see if each element in the first array matches the corresponding element in the second array. If any pair
-of elements are not the same, print "different", otherwise, do nothing. For example, if arr1[x][y] = 5, and arr2[x][y]
-= 6, you would print different.
+Make a Java program that initializes two integer arrays of the same size and checks if each element in the first array
+matches the corresponding element in the second array. If any pair of elements are not the same print the index of the
+differing elements. For example, if arr1[x][y] = 5, and arr2[x][y] = 6, you would print "The elements at index [x][y]
+are different".
 
 Sample output:
-    Enter 5 values for array a row 0:
-    1 2 3 4 5
-    Enter 5 values for array a row 1:
-    1 2 3 4 5
-    Enter 5 values for array a row 2:
-    1 2 3 4 5
-    Enter 5 values for array a row 3:
-    1 2 3 4 5
-    Enter 5 values for array a row 4:
-    1 2 3 4 5
-    Enter 5 values for array b row 0:
-    1 2 3 4 5
-    Enter 5 values for array b row 1:
-    1 2 3 4 5
-    Enter 5 values for array b row 2:
-    5 4 3 2 1
-    Enter 5 values for array b row 3:
-    1 2 3 4 5
-    Enter 5 values for array b row 4:
-    1 2 3 4 5
-    different
-    different
-    different
-    different
+    The elements at index [0][0] are different
+    The elements at index [0][4] are different
+    The elements at index [1][1] are different
+    The elements at index [2][2] are different
+    The elements at index [3][0] are different
+    The elements at index [3][3] are different
+    The elements at index [4][0] are different
+    The elements at index [4][3] are different
 
 Distractors:
 for (int i = 0; i <= a.length; i++)
@@ -38,41 +22,35 @@ for (int i = 0; i <= a.length; i++)
 for (int j = 0; j <= a[i].length; j++)
 if ( a[i][j] == b[i][j] )
 if( a[i] == b[i] )
-int[][] a = new int[][];
-int[][] b = new int[][];
-int[] a = new int[5];
-int[] b = new int[5];
+int[][] a = new int[][]{
+int[][] b = new int[][]{
+int[] a = {
+int[] b = {
 public static void main (String args)
-int[][] b = new int[5][5]
+int[][] b =
  */
-
-import java.util.Scanner;
 
 public class Identical {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int[][] a = new int[5][5];
-        int[][] b = new int[5][5];
-        for (int i = 0; i < a.length; i++) {
-            System.out.println("Enter 5 values for array a row " + i + ": ");
-            a[i][0] = input.nextInt();
-            a[i][1] = input.nextInt();
-            a[i][2] = input.nextInt();
-            a[i][3] = input.nextInt();
-            a[i][4] = input.nextInt();
-        }
-        for (int i = 0; i < a.length; i++) {
-            System.out.println("Enter 5 values for array b row " + i + ": ");
-            b[i][0] = input.nextInt();
-            b[i][1] = input.nextInt();
-            b[i][2] = input.nextInt();
-            b[i][3] = input.nextInt();
-            b[i][4] = input.nextInt();
-        }
+        int[][] a = {
+                {26, 4, 12, 67, 325}, //a1
+                {89, 448, 59, 32, 5}, //a2
+                {9, 53, 282, 3, 413}, //a3
+                {782, 24, 6, 462, 13}, //a4
+                {40, 12, 424, 404, 9} //a5
+        };
+        int[][] b = {
+                {27, 4, 12, 67, 31}, //b1
+                {89, 4, 59, 32, 5}, //b2
+                {9, 53, 281, 3, 413}, //b3
+                {72, 24, 6, 926, 13}, //b4
+                {42, 12, 424, 414, 9} //b5
+        };
+
         for (int i = 0; i < a.length; i++)
             for (int j = 0; j < a[i].length; j++)
                 if (a[i][j] != b[i][j])
-                    System.out.println("different");
+                    System.out.println("The elements at index [" + i + "][" + j + "] are different");
 
     }
 }
--- src/arrays/programs_involving_multidimensional_data/medium/q1/MainTest.java
+++ src/arrays/programs_involving_multidimensional_data/medium/q1/MainTest.java
@@ -1,27 +1,33 @@
 package arrays.programs_involving_multidimensional_data.medium.q1;
 
 import global.BaseTest;
-import global.exceptions.InvalidClauseException;
 import global.tools.TestOption;
-import global.utils.ArrayUtil;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
-import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
     // Parsons with distractors
 
     static final int size = 5;
 
+    static final int[][] a = {
+            {26, 4, 12, 67, 325},
+            {89, 448, 59, 32, 5},
+            {9, 53, 282, 3, 413},
+            {782, 24, 6, 462, 13},
+            {40, 12, 424, 404, 9}
+    };
+    static final int[][] b = {
+            {27, 4, 12, 67, 31},
+            {89, 4, 59, 32, 5},
+            {9, 53, 281, 3, 413},
+            {72, 24, 6, 926, 13},
+            {42, 12, 424, 414, 9}
+    };
+
     public Clause[] testSentence() {
-        TestOption.isInputTest = true;
-        TestOption.defaultInput = ArrayUtil.arrayToInput(ArrayUtil.generateRandomArray(1, 11, size * size * 2));
+        TestOption.incorrectStructureErrorMessage = "Your program does not correctly print out the indexes where the arrays differ.";
         return clauseBuilder();
     }
 
@@ -29,112 +35,36 @@ public class MainTest extends BaseTest {
         Identical.main(new String[0]);
     }
 
-    static Stream<Arguments> inputProvider() {
-        int[][] a1 = new int[size][size];
-        int[][] b1 = new int[size][size];
-
-        for (int x = 0; x < size; x++) {
-            a1[x] = ArrayUtil.generateRandomArray(1, 11, size);
-            b1[x] = ArrayUtil.generateRandomArray(1, 11, size);
-        }
-
-        int[][] a2 = new int[size][size];
-        int[][] b2 = new int[size][size];
-
-        for (int x = 0; x < size; x++) {
-            a2[x] = ArrayUtil.generateRandomArray(10, 1001, size);
-            b2[x] = ArrayUtil.generateRandomArray(10, 1001, size);
-        }
-
-        int[][] a3 = new int[size][size];
-        int[][] b3 = new int[size][size];
-
-        for (int x = 0; x < size; x++) {
-            a3[x] = ArrayUtil.replicateArray(1, size);
-            b3[x] = ArrayUtil.replicateArray(1, size);
-        }
-
-        int[][] a4 = new int[size][size];
-        int[][] b4 = new int[size][size];
-
-        for (int x = 0; x < size; x++) {
-            a4[x] = ArrayUtil.generateRandomArray(1, 101, size);
-            b4[x] = ArrayUtil.generateRandomArray(1, 101, size);
-            for (int y = 0; y < size; y++) {
-                a4[x][y] *= -1;
-                b4[x][y] *= -1;
-            }
-        }
-
-        int[][] a5 = new int[size][size];
-        int[][] b5 = new int[size][size];
-
-        for (int x = 0; x < size; x++) {
-            a5[x] = ArrayUtil.generateAscendingArray(6, size);
-            b5[x] = ArrayUtil.generateAscendingArray(6, size);
-        }
-
-        return Stream.of(
-                Arguments.of(a1, b1),
-                Arguments.of(a2, b2),
-                Arguments.of(a3, b3),
-                Arguments.of(a4, b4),
-                Arguments.of(a5, b5)
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("inputProvider")
-    void correctMainMethodOutput(int[][] a, int[][] b) throws InvalidClauseException {
-        StringBuilder s = new StringBuilder();
-
-        for (int x = 0; x < size; x++) {
-            s.append(ArrayUtil.arrayToInput(a[x]));
-        }
-        s.append(" ");
-        for (int x = 0; x < size; x++) {
-            s.append(ArrayUtil.arrayToInput(b[x]));
-        }
-
-        TestOption.incorrectStructureErrorMessage = "Your program does not print out the correct number of repetitions of 'different'.";
-        runWithInput(s.toString(), placeHolderClauseBuilder(a, b));
-    }
-
     public Clause[] clauseBuilder() {
-        Clause[] c = new Clause[size * 4 + 1];
+        String[] differences = collectDifferences();
+
+        Clause[] c = new Clause[differences.length * 2];
         int count = 0;
 
-        for (int x = 0; x < size; x++) {
-            c[count++] = new StringLiteral("Enter " + size + " values for array a row " + x + ": ");
+        for(int x = 0; x < differences.length; x++){
+            c[count++] = new StringLiteral(differences[x]);
             c[count++] = new NewLine();
         }
-        for (int x = 0; x < size; x++) {
-            c[count++] = new StringLiteral("Enter " + size + " values for array b row " + x + ": ");
-            c[count++] = new NewLine();
-        }
-        c[count] = new PlaceHolder();
 
         return c;
     }
 
-    public Clause[][] placeHolderClauseBuilder(int[][] a, int[][] b) {
-        int differences = 0;
+    public String[] collectDifferences(){
+        int c = 0;
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < a[i].length; j++)
+                if (a[i][j] != b[i][j])
+                    c++;
 
-        for (int x = 0; x < size; x++) {
-            for (int y = 0; y < size; y++) {
-                if (a[x][y] != b[x][y])
-                    differences++;
-            }
-        }
+        String[] dif = new String[c];
 
-        Clause[][] c = new Clause[1][differences * 2];
-        int count = 0;
+        c = 0;
 
-        for (int x = 0; x < differences; x++) {
-            c[0][count++] = new StringLiteral("different");
-            c[0][count++] = new NewLine();
-        }
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < a[i].length; j++)
+                if (a[i][j] != b[i][j])
+                    dif[c++] = "The elements at index \\[" + i + "\\]\\[" + j + "\\] are different";
 
-        return c;
+        return dif;
     }
 }
