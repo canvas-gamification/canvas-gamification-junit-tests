26c02d1c3eb90ea4c0b1f6760248c2d796b41007
Seth Akins <83678885+Hedgemon4@users.noreply.github.com>
2023-03-25 16:44:05 -0700

    Update EightByEight.java

--- src/arrays/programs_involving_multidimensional_data/hard/q3/EightByEight.java
+++ src/arrays/programs_involving_multidimensional_data/hard/q3/EightByEight.java
@@ -1,13 +1,12 @@
 package arrays.programs_involving_multidimensional_data.hard.q3;
 
 /*
-Write a Java program that initializes an 8 by 8 2D array with random values from 0 to 10 (inclusive of both values) and
-prints it. Then write a function called find that takes a 2D array
-and an index, and prints out the value at that index.
-Consider some examples with an 8 by 8 array. The index of an element in row i and column j in an n * m 2D array is
-i * n + j (We are using 0 based numbering).
-Generate a random index value from 0 to 64 and print it. Consider that the input is always valid.
-Note that your find method should be general enough to work with any size of a 2D array.
+Write a Java program that initializes an 8 by 8 2D array with random values from 0 to 10 (inclusive of both values) and 
+generates a random index between 0 and 63 (exclusive), printing both to the console. Then write a function called find 
+that takes a 2D array and an index and prints out the value at that index. Note that the index is some number from 0 to 
+n * m, where n is the number of rows and m is the number of columns in the passed 2D array. For example, if we want to 
+fetch the value at index 37, that would mean the value at row 3, column 7. Use this function to print the value in the 
+generated 2D array at the randomized index.
 
 Sample Output:
     Generated 2D array:
