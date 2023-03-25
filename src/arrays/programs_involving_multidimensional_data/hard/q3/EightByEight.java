868bdcd3fc1dd5bfdf4c39953009655ee9d832c1
Seth Akins <83678885+Hedgemon4@users.noreply.github.com>
2023-03-25 16:45:35 -0700

    Update src/arrays/programs_involving_multidimensional_data/hard/q3/EightByEight.java

--- src/arrays/programs_involving_multidimensional_data/hard/q3/EightByEight.java
+++ src/arrays/programs_involving_multidimensional_data/hard/q3/EightByEight.java
@@ -2,7 +2,7 @@ package arrays.programs_involving_multidimensional_data.hard.q3;
 
 /*
 Write a Java program that initializes an 8 by 8 2D array with random values from 0 to 10 (inclusive of both values) and 
-generates a random index between 0 and 63 (exclusive), printing both to the console. Then write a function called find 
+generates a random index between 0 and 63 (exclusive), printing both to the console. Then, write a function called find 
 that takes a 2D array and an index and prints out the value at that index. Note that the index is some number from 0 to 
 n * m, where n is the number of rows and m is the number of columns in the passed 2D array. For example, if we want to 
 fetch the value at index 37, that would mean the value at row 3, column 7. Use this function to print the value in the 
