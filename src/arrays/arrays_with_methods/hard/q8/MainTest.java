42e715e0ab64a58e615caca716ce2ecc22437117
Samantha H <hodge1214@gmail.com>
2023-04-02 14:03:23 -0400

    formatted

--- src/arrays/arrays_with_methods/hard/q8/MainTest.java
+++ src/arrays/arrays_with_methods/hard/q8/MainTest.java
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
 public class MainTest {
     // Java
 
-    static Stream<int[]> inputProvider(){
+    static Stream<int[]> inputProvider() {
         return Stream.of(
                 result(2),
                 result(3),
@@ -33,13 +33,12 @@ public class MainTest {
         CustomAssertions._assertArrayEquals(in, output, "Your fibbingArray method does not return the correct array of Fibonacci numbers.");
     }
 
-    private static int[] result( int size )
-    {
-        int [] result = new int[size];
+    private static int[] result(int size) {
+        int[] result = new int[size];
         result[0] = 0;
         result[1] = 1;
-        for( int i = 2; i < result.length; i++ )
-            result[i] = result[ i - 1 ] + result[ i - 2 ];
+        for (int i = 2; i < result.length; i++)
+            result[i] = result[i - 1] + result[i - 2];
         return result;
     }
 }
