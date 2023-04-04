1a74bb36b9d48303f89aff4796adf6daff1868aa
Kai Khademi <99101819+KaiKhademi@users.noreply.github.com>
2023-04-04 11:54:13 -0700

    Update src/arrays/programs_involving_multidimensional_data/hard/q6/MainTest.java
    Co-authored-by: Seth Akins <83678885+Hedgemon4@users.noreply.github.com>

--- src/arrays/programs_involving_multidimensional_data/hard/q6/MainTest.java
+++ src/arrays/programs_involving_multidimensional_data/hard/q6/MainTest.java
@@ -64,7 +64,7 @@ public class MainTest extends BaseRandomTest {
             }
         }
         int out = Integer.parseInt(getItemByName("max"));
-        CustomAssertions._assertEquals(max, out, "Your program does not correctly find the maximum value of the 2D array.");
+        CustomAssertions._assertEquals(max, out, "Your program does not correctly find and print the maximum value of the 2D array.");
     }
 
     public static int[][] gen(int n, int m){
