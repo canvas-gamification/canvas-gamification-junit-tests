3b49777a34e8be4f413646c8c7b3a3ffd560a884
Samantha H <hodge1214@gmail.com>
2023-04-04 16:14:08 -0400

    changed test

--- src/arrays/creating_arrays/hard/q5/MainTest.java
+++ src/arrays/creating_arrays/hard/q5/MainTest.java
@@ -3,10 +3,18 @@ package arrays.creating_arrays.hard.q5;
 import global.BaseTest;
 import global.variables.Clause;
 import global.variables.clauses.NewLine;
+import global.variables.clauses.RandomChar;
 import global.variables.clauses.StringLiteral;
+import global.variables.wrappers.Optional;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest extends BaseTest {
     // Java
+
+    static final int n = 500;
+
     public Clause[] testSentence() {
         return ClauseBuilder();
     }
@@ -15,19 +23,48 @@ public class MainTest extends BaseTest {
         AlphabetRays.main(new String[0]);
     }
 
-    Clause[] ClauseBuilder() {
-        Clause[] c = new Clause[51];
+    @Test
+    void reversesCorrectly(){
+        runMain();
 
-        String[] s = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-                "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
-                "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-
-        int count = 0;
-        for (int x = 0; x < 25; x++) {
-            c[count++] = new StringLiteral(s[x]);
-            c[count++] = new NewLine();
+        boolean b = true;
+        int rev = n-1;
+        for(int x = 0; x < n; x++){
+            if(!getItemByName("char"+x).equals(getItemByName("reversedChar"+rev--))){
+                b = false;
+            }
         }
-        c[count] = new StringLiteral(s[25]);
+
+        assertTrue(b, "Your program does not correctly print the array in reverse.");
+    }
+
+    Clause[] ClauseBuilder() {
+        Clause[] c = new Clause[n * 4 + 7];
+
+        c[0] = new StringLiteral("Array of random letters:");
+        c[1] = new Optional(new StringLiteral(" "));
+        c[2] = new NewLine();
+
+        int count = 3;
+        for(int x = 0; x < n-1; x++){
+            c[count++] = new RandomChar('a', 'z', "char" + x, true);
+            c[count++] = new StringLiteral(" ");
+        }
+        c[count++] = new RandomChar('a', 'z', "char" + (n-1), true);
+        c[count++] = new Optional(new StringLiteral(" "));
+        c[count++] = new NewLine();
+
+        c[count++] = new StringLiteral("Reversed array:");
+        c[count++] = new Optional(new StringLiteral(" "));
+        c[count++] = new NewLine();
+
+        //having to use randomChar because not charLiteral
+        for(int x = 0; x < n-1; x++){
+            c[count++] = new RandomChar('a', 'z', "reversedChar" + x, true);
+            c[count++] = new StringLiteral(" ");
+        }
+        c[count++] = new RandomChar('a', 'z', "reversedChar" + (n-1), true);
+        c[count] = new Optional(new StringLiteral(" "));
 
         return c;
     }
