c3a7ed40c5bb07e9e50a69f0f5a023f1b7b06bbd
Samantha H <hodge1214@gmail.com>
2023-04-10 15:13:51 -0400

    made randomization test clearer in function. Added test to check if other numbers are left alone.

--- src/arrays/arrays_with_methods/hard/q6/MainTest.java
+++ src/arrays/arrays_with_methods/hard/q6/MainTest.java
@@ -1,9 +1,11 @@
 package arrays.arrays_with_methods.hard.q6;
 
 import global.MethodTest;
+import global.tools.CustomAssertions;
 import global.utils.ArrayUtil;
 import global.variables.clauses.RandomInteger;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -15,55 +17,68 @@ import java.util.stream.Stream;
 public class MainTest {
     // Java
 
-    public static Stream<Integer> randomReplaceInput(){
+    public static Stream<Integer> randomReplaceInput() {
         return Stream.of(3, 4, 5, 6, 7, 50);
+    }
+
+    @Test
+    public void randomReplaceMethodKeepsOddIndexNumber() throws Throwable {
+        //this checks that the odd index numbers are not modified
+        int[] arr = {2, 43, 342, 5, 324, 543, 543, 6, 43, 634, 49, 236, 246, 2673653, 7653};
+        int[] solutions = {43, 5, 543, 6, 634, 236, 2673653};
+        Object[][] arguments = {
+                {arr, int[].class}
+        };
+        MethodTest m = new MethodTest(ShrinkItDown.class, "randomReplace", arguments);
+        if (m.getMethodReturnType() != int[].class) {
+            Assertions.fail("Your createRandomArray method does not return an array of integers.");
+        }
+        int[] output = (int[]) m.callMethod();
+        int[] oddNums = new int[7];
+        int count = 0;
+        for(int x = 0; x < output.length; x++){
+            if(x%2 == 1)
+                oddNums[count++] = output[x];
+        }
+        CustomAssertions._assertArrayEquals(solutions, oddNums, "Your program does not modify only every 2nd element.");
     }
 
     @ParameterizedTest
     @MethodSource("randomReplaceInput")
     public void correctRandomReplaceMethod(int size) throws Throwable {
+        //generates the input array
         int[] arr = ArrayUtil.generateAscendingArray(1, size);
-        int[][] storeRandoms = new int[(size+1)/2][1000];
+        //an array of the arrays storing the generated numbers at each even index
+        int[][] storeRandoms = new int[(size + 1) / 2][1000];
 
         Object[][] arguments = {
                 {arr, int[].class}
         };
         MethodTest m = new MethodTest(ShrinkItDown.class, "randomReplace", arguments);
-
-        Object output = m.callMethod();
-        int[] hold = new int[0];
-        try {
-            hold = (int[]) output;
-        } catch (Exception e) {
+        if (m.getMethodReturnType() != int[].class) {
             Assertions.fail("Your createRandomArray method does not return an array of integers.");
         }
 
-        int count = 0;
         int repetitions = 0;
-        //get first round of numbers
-        for(int x = 0; x < size; x++){
-            if( x % 2 == 0 )
-                storeRandoms[count++][repetitions] = hold[x];
-        }
-        repetitions++;
 
-        //now rest of numbers
-        for(int x = 0; x < 999; x ++){
+        //runs the program 1000 times and stores all the even index numbers generated. Each are in their own array
+        //because each index has its own range the variables can fall in (I'm using ascending array to control that)
+        for (int x = 0; x < 1000; x++) {
             arr = ArrayUtil.generateAscendingArray(1, size);
-            count = 0;
-            output = m.callMethod();
-            hold = (int[]) output;
-            for(int y = 0; y < size; y++){
-                if( y % 2 == 0 )
-                    storeRandoms[count++][repetitions] = hold[y];
+            int count = 0;
+            int[] output = (int[]) m.callMethod();
+            for (int y = 0; y < size; y++) {
+                if (y % 2 == 0)
+                    storeRandoms[count++][repetitions] = output[y];
             }
             repetitions++;
         }
 
+        //checks randomization
         int index = 0;
         for (int[] storeRandom : storeRandoms) {
             ArrayList<Integer> response = Arrays.stream(storeRandom).boxed().collect(Collectors.toCollection(ArrayList::new));
-            RandomInteger randomInteger = new RandomInteger(0, index+2);
+            RandomInteger randomInteger = new RandomInteger(0, index + 2);
             index += 2;
             randomInteger.validateRandom(response);
         }
--- src/arrays/arrays_with_methods/hard/q6/ShrinkItDown.java
+++ src/arrays/arrays_with_methods/hard/q6/ShrinkItDown.java
@@ -7,7 +7,7 @@ if the element at arr[6] = 88, replace it with a random number between 0 and 88 
  */
 
 public class ShrinkItDown {
-    public static int[] randomReplace( int [] arr )
+    public static int[] randomReplace( int[] arr )
     {
         for( int i = 0; i < arr.length; i++ )
             if( i % 2 == 0 )
