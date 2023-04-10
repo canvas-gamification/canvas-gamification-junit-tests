1cacb12014ce397d717e6d102da168656a8555f3
Seth Akins <83678885+Hedgemon4@users.noreply.github.com>
2023-04-10 09:15:20 -0700

    Changed question text

--- src/arrays/arrays_with_methods/hard/q8/MakeFibonacci.java
+++ src/arrays/arrays_with_methods/hard/q8/MakeFibonacci.java
@@ -1,10 +1,11 @@
 package arrays.arrays_with_methods.hard.q8;
 
 /*
-Write a method that takes an integer variable as input and returns an integer array of the same size populated with the
-terms of the Fibonacci sequence. Recall that the Fibonacci sequence starts with F0 = 0, F1 = 1, and then all future
-values are defined by the previous values using the formula: Fn= Fn-1+Fn-2. You can assume that the method's input
-variable will always be an integer greater than 1.
+Write a Java method called fibbingArray that takes an integer size variable as input and returns an integer array of length 
+size populated with the terms of the Fibonacci sequence. Recall that the Fibonacci sequence starts with F0 = 0, F1 = 1, and 
+then all future values are defined by the previous values using the formula: Fn= Fn-1+Fn-2. For example, if the integer 
+passed to your method was five, you would return the array {0, 1, 1, 2, 3}. You can assume that the method's input variable 
+will always be an integer greater than 1.
  */
 
 public class MakeFibonacci {
