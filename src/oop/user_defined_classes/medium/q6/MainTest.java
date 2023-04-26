2b24fd994b92e84db61e0be57b3582e2df7086fe
Kai <kiamehrkhademi2019@gmail.com>
2023-04-26 14:31:18 -0700

    Wrote q6

---
+++ src/oop/user_defined_classes/medium/q6/MainTest.java
@@ -0,0 +1,61 @@
+package oop.user_defined_classes.medium.q6;
+
+import global.ObjectTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static global.tools.CustomAssertions._assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    // Java
+    private final String className = "Homework";
+    private final String varMark = "marks";
+    private final String varNum = "studentNumber";
+
+    private ObjectTest classInstance;
+
+    @BeforeEach
+    public void setUp() throws Throwable {
+        String classString = "oop.user_defined_classes.medium.q6." + className;
+        classInstance = new ObjectTest(classString);
+        String modifiedClassMessage =
+                "You have modified the class fields in the " + className + " class. Please revert them back to the original state they were provided in.";
+        Class<?>[] classArguments = {double.class, int.class};
+        assertTrue(classInstance.hasConstructor(classArguments), modifiedClassMessage);
+        assertTrue(classInstance.hasModifier(classArguments, "public"), modifiedClassMessage);
+        Object[][] tests = new Object[][]{
+                {67.5, 34443},
+                {34.7, 33212},
+                {95.6, 74832}
+        };
+        for(int num = 0; num < tests.length; num ++){
+            double mark = (double)tests[num][0];
+            int id = (int)tests[num][1];
+            Object[][] arguments = {
+                    {mark, double.class},
+                    {id, int.class}
+            };
+            Object instance = classInstance.createInstance(arguments);
+            _assertEquals(mark, classInstance.getFieldValue(instance, varMark),
+                    "Your " + className + " constructor does not correctly initialize the " + varMark + " field.");
+            _assertEquals(id, classInstance.getFieldValue(instance, varNum),
+                    "Your " + className + " constructor does not correctly initialize the " + varNum + " field.");
+        }
+    }
+
+    @Test
+    public void cupboardClassHasCorrectAttributes() {
+        String incorrectFieldMessage = "Your " + className + " class is missing a required field.";
+        String incorrectModifierMessage = "One of your " + className + " class attributes does not have the correct modifier.";
+        assertTrue(classInstance.hasField(varMark, double.class), incorrectFieldMessage);
+        assertTrue(classInstance.hasModifier(varMark, "private"), incorrectModifierMessage);
+        assertTrue(classInstance.hasField(varNum, int.class), incorrectFieldMessage);
+        assertTrue(classInstance.hasModifier(varNum, "private"), incorrectModifierMessage);
+    }
+}
---
+++ src/oop/user_defined_classes/medium/q6/QuestionText.md
@@ -0,0 +1,20 @@
+# oop-user_defined_classes-medium-q6
+
+## Java
+
+Teachers need to have an efficient way to keep track of each student and how they do in 
+their course work. Make a Java class called Homework that has two attributes called marks
+studentNumber that are initialized by the constructor.
+
+
+
+
+## Homework
+
+The `Homework` class you are given has the following items:
+
+- Constructors:
+    - A constructor that initializes the **marks** and **studentNumber** fields
+
+Write the required attributes according to the initialization and parameters of
+the constructor.
