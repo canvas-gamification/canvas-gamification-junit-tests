29e36cffd70e69d7a867135f050516e4f244f0c4
Kai <kiamehrkhademi2019@gmail.com>
2023-05-01 02:11:14 -0700

    Wrote q7

---
+++ src/oop/user_defined_classes/medium/q7/MainTest.java
@@ -0,0 +1,81 @@
+package oop.user_defined_classes.medium.q7;
+
+import global.ObjectTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static global.tools.CustomAssertions._assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    // Java
+    private final String className = "Highway";
+    private final String var1 = "speedLimit";
+    private final String var2 = "fine";
+    private final String methodName = "issueTicket";
+    private static final int initialFee = 250;
+
+    private ObjectTest classInstance;
+
+    @BeforeEach
+    public void setUp() throws Throwable {
+        String classString = "oop.user_defined_classes.medium.q7." + className;
+        classInstance = new ObjectTest(classString);
+        String modifiedClassMessage =
+                "You have modified the class fields in the " + className + " class. Please revert them back to the original state they were provided in.";
+        assertTrue(classInstance.hasField(var1, int.class), modifiedClassMessage);
+        assertTrue(classInstance.hasModifier(var1, "private"), modifiedClassMessage);
+        assertTrue(classInstance.hasField(var2, int.class), modifiedClassMessage);
+        assertTrue(classInstance.hasModifier(var2, "private"), modifiedClassMessage);
+        Class<?>[] classArguments = {int.class};
+        assertTrue(classInstance.hasConstructor(classArguments), modifiedClassMessage);
+        assertTrue(classInstance.hasModifier(classArguments, "public"), modifiedClassMessage);
+        Object[][] tests = new Object[][]{
+                {120},
+                {60},
+                {30}
+        };
+        for (int num = 0; num < tests.length; num++) {
+            int speed = (int) tests[num][0];
+            Object[][] arguments = {
+                    {speed, int.class}
+            };
+            Object instance = classInstance.createInstance(arguments);
+            _assertEquals(speed, classInstance.getFieldValue(instance, var1),
+                    "Your " + className + " constructor does not correctly initialize the " + var1 + " field.");
+            _assertEquals(initialFee, classInstance.getFieldValue(instance, var2),
+                    "Your " + className + " constructor does not correctly initialize the " + var2 + " field.");
+        }
+
+
+    }
+
+    private static Stream<Arguments> methodInputProvider() {
+        return Stream.of(
+                Arguments.of(120, 40, 0),
+                Arguments.of(60, 70, initialFee),
+                Arguments.of(30, 40, initialFee),
+                Arguments.of(100, 100, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("methodInputProvider")
+    public void bankCorrectIncreaseMethod(int speedLim, int current, int ans) throws Throwable {
+        Object[][] arguments1 = {
+                {speedLim, int.class}
+        };
+        Object[][] arguments2 = {
+                {current, int.class}
+        };
+        Object instance = classInstance.createInstance(arguments1);
+        Object output = classInstance.callMethod(methodName, arguments2, (new String[]{"public"}), instance);
+        _assertEquals(ans, output,
+                "Your " + className + " " + methodName + " does not correctly calculate the fine.");
+
+    }
+}
---
+++ src/oop/user_defined_classes/medium/q7/QuestionText.md
@@ -0,0 +1,22 @@
+# oop-user_defined_classes-medium-q7
+
+## Java
+
+Civil engineers design roads and recommend reasonable speed limits to ensure everyone's 
+safety. Make a Java class called Highway that has two attributes called speedLimit and 
+fine whose values are initialized by the constructor. This class has a method called 
+issueTicket that takes as input a driving speed and returns a fine if it is more 
+than the speedLimit.
+
+## Highway
+
+The `Highway` class you are given has the following items:
+
+- Attributes:
+    - An `int` called **speedLimit**
+    - An `int` called **fine**
+- Constructor:
+    - A constructor to initialize the **speedLimit** and initialize **fine** to 250.
+
+Create the method **issueTicket** that takes an int which is the current speed and
+return an `int` that is the fine needed to be paid for the input speed.
