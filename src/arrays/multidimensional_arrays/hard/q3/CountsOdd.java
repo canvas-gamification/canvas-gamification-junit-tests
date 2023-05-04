1aba46bb2e90ddc56e6b676cbef81f5711b63bbe
Kai <kiamehrkhademi2019@gmail.com>
2023-05-03 20:18:27 -0700

    Modified q3

--- src/arrays/multidimensional_arrays/hard/q3/CountsOdd.java
+++ src/arrays/multidimensional_arrays/hard/q3/CountsOdd.java
@@ -2,17 +2,20 @@ package arrays.multidimensional_arrays.hard.q3;
 
 /*
 Write a Java program that randomly initialize a 3 by 3 multidimensional array, with integers between 1 and 100
-(inclusive of both ends) in the main method and prints it. Then write a countsOdd method that accept a multidimensional
-array of integers and print the number of odd integers in it.
-Note that the last line in the sample output should be printed in the method.
+(inclusive of both ends) and prints it. Then gets two integers a and b and count the number of odd number for all
+the integers in the array whose indices are i and j so that 0 <= i < a and 0 <= j < b.
 
 Sample output:
     Generated array:
     42 84 85
     45 50 63
     14 7 17
-    count of odd numbers in array = 5
+    Enter two limits for the indices:
+    2 3
+    count of odd numbers in array = 3
  */
+
+import java.util.Scanner;
 
 public class CountsOdd {
     public static void main(String[] args) {
@@ -25,14 +28,14 @@ public class CountsOdd {
             }
             System.out.println();
         }
-        countsOdd(arr);
-    }
-
-    public static void countsOdd(int[][] nums) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter two limits for the indices:");
+        int a = in.nextInt();
+        int b = in.nextInt();
         int count = 0;
-        for (int i = 0; i < nums.length; i++)
-            for (int j = 0; j < nums[i].length; j++)
-                if (nums[i][j] % 2 != 0)
+        for (int i = 0; i < a; i++)
+            for (int j = 0; j < b; j++)
+                if (arr[i][j] % 2 != 0)
                     count++;
         System.out.println("count of odd numbers in array = " + count);
     }
