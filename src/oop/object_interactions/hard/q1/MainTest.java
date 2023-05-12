b541660ba8efed77bf4c11e4ff4e0154dd0d0a76
Hedgemon4 <sethlinc4@gmail.com>
2023-05-11 20:40:55 -0700

    Started coding question 1

--- src/oop/object_interactions/hard/q1/MainTest.java
+++ src/oop/object_interactions/hard/q1/MainTest.java
@@ -1,55 +1,60 @@
 package oop.object_interactions.hard.q1;
-/*
-Identity theft is a serious crime. You are given the following class called Passport, which has attributes called name
-and dob (date of birth). The attributes are defined in a constructor that takes input parameters for initializing these
-attributes. Currently, the class has accessor, mutator, and toString() methods. Your job is to write a method in this
-class called steal() that takes another Passport object as input and copies the values of the attributes.
- */
 
-import global.MethodTest;
-import global.utils.ClassUtil;
+import global.ObjectTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static global.tools.CustomAssertions._assertEquals;
-import static global.tools.CustomAssertions._fail;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest {
-    // Java
+    private ObjectTest passport;
+    private final String passportName = "Passport";
+    private final String nameAttribute = "name";
+    private final String dobAttribute = "dob";
 
-    static Stream<Arguments> stealInputProvider() {
+
+    @BeforeEach
+    public void setup() {
+        String passportString = "oop.object_interactions.hard.q1." + passportName;
+        passport = new ObjectTest(passportString);
+    }
+
+    @Test
+    public void passportClassHasRequiredAttributes() {
+        assertTrue(passport.hasField(nameAttribute, String.class),
+                String.format("We expected an attribute named %s in your code but it was not found or was misspelled.", nameAttribute));
+        assertTrue(passport.hasField(dobAttribute, String.class),
+                String.format("We expected an attribute named %s in your code but it was not found or was misspelled.", dobAttribute));
+        assertTrue(passport.hasModifier(nameAttribute, "private"),
+                String.format("The %s field does not have the correct visibility modifier.", nameAttribute));
+        assertTrue(passport.hasModifier(dobAttribute, "private"),
+                String.format("The %s field does not have the correct visibility modifier.", dobAttribute));
+    }
+
+    private static Stream<Arguments> passportConstructorInputProvider() {
         return Stream.of(
-                Arguments.of("Steve", "11/06/1997"),
-                Arguments.of("Jasmine Gregson", "04/04/1934"),
-                Arguments.of("No Name", "No Birthdate")
+                Arguments.of("John Cena", "April 23, 1977"),
+                Arguments.of("null", "null"),
+                Arguments.of("One longer name with other sumbols&&@$#$", "mnsaidawkd2@#")
         );
     }
 
     @ParameterizedTest
-    @MethodSource("stealInputProvider")
-    public void correctStealMethod(String name, String birthdate) throws Throwable {
-        // The problem for this test is that we need to call a method in a class the changes the values of the methods,
-        // and we might not know if that method is defined.
-        // Should probably create a ClassTest object for this that will only attempt to call methods if they exist.
-
-        // Should also be able to use forName to make sure that the class actually exists
-        // Class.forName can also be used to initialize objects
-        // https://www.geeksforgeeks.org/different-ways-create-objects-java/?ref=lbp
-        // https://www.baeldung.com/java-check-class-exists
-        // Maybe we can define a comparator for easier comparison?
-        Passport stolen = new Passport("Test Name", "Test DOB");
-        Passport victim = new Passport(name, birthdate);
-        Passport expected = new Passport(name, birthdate);
-        if (ClassUtil.classHasMethod(Passport.class, "public void Passport)"))
-            stolen.steal(victim);
-        else
-            _fail("Your Passport class does not have a steal method.", "No steal method found.");
-        _assertEquals(expected.getDob(), victim.getDob(), "Your program does not correctly steal the values from the input passport.");
-        _assertEquals(expected.getName(), victim.getName(), "Your program does not correctly steal the values from the input passport.");
+    @MethodSource("passportConstructorInputProvider")
+    public void passportClassHasCorrectConstructor(String name, String birthday) throws Throwable {
+        Object[][] arguments = {
+                {name, String.class},
+                {birthday, String.class}
+        };
+        assertTrue(passport.hasConstructor(new Class[]{String.class, String.class}, new String[]{"public"}),
+                String.format("Your %s class is missing a required constructor or has the incorrect visibility modifier.", passportName));
+        Object passportInstance = passport.createInstance(arguments);
 
     }
+
 }
---
+++ src/oop/object_interactions/hard/q1/questionTest.md
@@ -0,0 +1,34 @@
+# oop-object_interactions-hard-q1
+
+- Parsons question
+
+## Question Text
+
+Identity theft is a serious crime. Design a Passport class using the requirements below, and create a steal method that
+takes the attributes from one passport and copies them to another.
+
+### Passport Class:
+
+Your `Passport` class must have the following items:
+
+- Attributes:
+    - A `String` attribute called **name**
+    - A `String` attribute called **dob**
+    - Both of these attributes should not be accessible from another class
+
+- Constructor:
+    - A constructor visible from another class that accepts two `String` parameters, and initializes the **name** field
+      with the first `String` and the **dob** field with the second `String`
+    - This constructor should be visible from another class
+
+- Methods
+    - Note that unless otherwise specified, the following methods should be visible from another class
+    - A `getName` method which returns the **name** attribute of the passport
+    - A `getDob` method which returns the **dob** attribute of the passport
+    - A `setName` method which takes a `String` parameter and updates the **name** field to that value
+    - A `setDob` method which takes a `String` parameter and updates the **dob** field to that value
+    - A `toString` method which returns a `String` describing the Passport object. It should be of the format "My name
+      is x, and I was born on y." where x is the **name** attribute and y is the **dob** attribute of the passport
+    - A `steal` method that accepts a `Passport` as a parameter and copies the passed passports **name** and **dob**
+      values to the passport the method was called on
+    
