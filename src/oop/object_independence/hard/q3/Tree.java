ae180b1c0d08699061d66f730b078f236a5edfae
Samantha H <hodge1214@gmail.com>
2023-05-12 00:46:16 -0400

    formatted

--- src/oop/object_independence/hard/q3/Tree.java
+++ src/oop/object_independence/hard/q3/Tree.java
@@ -1,25 +1,36 @@
 package oop.object_independence.hard.q3;
 
-public class Tree
-{
+public class Tree {
 
     private boolean isRare;
     private double height;
-    public Tree( boolean rarity, double hght )
-    {
+
+    public Tree(boolean rarity, double hght) {
         isRare = rarity;
         height = hght;
     }
-    public boolean getIsRare(){ return isRare; }
-    public double getHeight(){ return height; }
-    public void setIsRare( boolean a ){ isRare = a; }
-    public void setHeight( double gen ){ height = gen; }
-    public void deforestation()
-    {
+
+    public boolean getIsRare() {
+        return isRare;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setIsRare(boolean a) {
+        isRare = a;
+    }
+
+    public void setHeight(double gen) {
+        height = gen;
+    }
+
+    public void deforestation() {
         height = 0;
     }
-    public String toString()
-    {
-        return "This tree’s rarity is " + isRare+ ", and is " + height+ "m tall";
+
+    public String toString() {
+        return "This tree’s rarity is " + isRare + ", and is " + height + "m tall";
     }
 }
