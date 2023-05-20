af728add30af68c6498c2d46237b975dbdf3036a
Kai <kiamehrkhademi2019@gmail.com>
2023-05-19 19:43:14 -0700

    Wrote q10

---
+++ src/oop/special_class_method/hard/q10/MainTest.java
@@ -0,0 +1,192 @@
+package oop.special_class_method.hard.q10;
+
+import global.ObjectTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static global.tools.CustomAssertions._assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    // Parsons with Distractors
+    private final String className = "Award";
+    private final String firstFieldName = "awardRecipient";
+    private final String secondFieldName = "awardCategory";
+    private final String setSecondMethodName = "setAwardCategory";
+    private final String getSecondMethodName = "getAwardCategory";
+    private final String getFirstMethodName = "getAwardRecipient";
+    private final String setFirstMethodName = "setAwardRecipient";
+    private ObjectTest testClass;
+
+    @BeforeEach
+    public void setUp() {
+        String classString = "oop.special_class_method.hard.q10." + className;
+        testClass = new ObjectTest(classString);
+    }
+
+    @Test
+    public void awardClassHasCorrectAttributes() {
+        assertTrue(testClass.hasField(firstFieldName, String.class),
+                "Your " + className + " class is missing the " + firstFieldName + " attributes.");
+        assertTrue(testClass.hasModifier(firstFieldName, "private"),
+                "Your " + firstFieldName + " does not have the correct visibility modifier.");
+        assertTrue(testClass.hasField(secondFieldName, String.class),
+                "Your " + className + " class is missing the " + secondFieldName + " attributes.");
+        assertTrue(testClass.hasModifier(secondFieldName, "private"),
+                "Your " + secondFieldName + " does not have the correct visibility modifier.");
+    }
+
+    private static Stream<Arguments> inputProvider() {
+        return Stream.of(
+                Arguments.of("First", "Second"),
+                Arguments.of("Science", "Anna"),
+                Arguments.of("Jack", "History"),
+                Arguments.of("Finn", "Fiction")
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void awardClassHasCorrectConstructor(String value1, String value2) throws Throwable {
+        Object[][] arguments = {
+                {value1, String.class},
+                {value2, String.class}
+        };
+        Class<?>[] constructorClasses = {String.class, String.class};
+        Object classInstance = testClass.createInstance(arguments);
+        String incorrectDoubleValueMessage =
+                "Your " + className + " constructor does not initialize the " + firstFieldName + " attribute to the correct value.";
+        String incorrectStringValueMessage =
+                "Your " + className + " constructor does not initialize the " + secondFieldName + " attribute to the correct value.";
+        String incorrectConstructorVisibilityModifier =
+                "Your " + className + " constructor does not have the correct visibility modifier.";
+        _assertEquals(value1, testClass.getFieldValue(classInstance, firstFieldName),
+                incorrectDoubleValueMessage);
+        _assertEquals(value2, testClass.getFieldValue(classInstance, secondFieldName),
+                incorrectStringValueMessage);
+        assertTrue(testClass.hasModifier(constructorClasses, "public"), incorrectConstructorVisibilityModifier);
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void awardClassHasCorrectSetAwardRecipientMethod(String initialValue, String setValue) throws Throwable {
+        Object[][] arguments = {
+                {initialValue, String.class},
+                {"", String.class}
+        };
+        Object classInstance = testClass.createInstance(arguments);
+        Object[][] setSizeArguments = {
+                {setValue, String.class}
+        };
+        String[] setMethodModifiers = {"public"};
+        String incorrectSetMethodMessage = String.join(" ",
+                "Your", setFirstMethodName, "does not correctly set the value of the", firstFieldName, "attribute.");
+        Object setMethodOutput = testClass.callMethod(setFirstMethodName, setSizeArguments, setMethodModifiers, classInstance);
+        _assertEquals(setValue, testClass.getFieldValue(classInstance, firstFieldName), incorrectSetMethodMessage);
+        assertNull(setMethodOutput, String.join(" ", "Your", setFirstMethodName, "should not return any output"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void awardClassHasCorrectGetAwardRecipientMethod(String value1, String value2) throws Throwable {
+        Object[][] arguments = {
+                {value1, String.class},
+                {value2, String.class}
+        };
+        Object classInstance = testClass.createInstance(arguments);
+        String[] getMethodModifiers = {"public"};
+        String incorrectGetMethodMessage = String.join(" ",
+                "Your", getFirstMethodName, "does not correctly get the value of the", firstFieldName, "attribute.");
+        Object getMethodOutput = testClass.callMethod(getFirstMethodName, getMethodModifiers, classInstance);
+        _assertEquals(value1, getMethodOutput, incorrectGetMethodMessage);
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void awardClassHasCorrectSetAwardCategoryMethod(String initialValue, String updatedValue) throws Throwable {
+        Object[][] arguments = {
+                {"", String.class},
+                {initialValue, String.class}
+        };
+        Object classInstance = testClass.createInstance(arguments);
+        testClass.setFieldValue(classInstance, initialValue, secondFieldName);
+        Object[][] setMethodArguments = {
+                {updatedValue, String.class}
+        };
+        String[] setMethodModifiers = {"public"};
+        String incorrectSetMethodMessage = String.join(" ",
+                "Your", setSecondMethodName, "does not correctly set the value of the", secondFieldName, "attribute.");
+        Object setMethodOutput = testClass.callMethod(setSecondMethodName, setMethodArguments, setMethodModifiers, classInstance);
+        _assertEquals(updatedValue, testClass.getFieldValue(classInstance, secondFieldName), incorrectSetMethodMessage);
+        assertNull(setMethodOutput, String.join(" ", "Your", setSecondMethodName, "should not return any output"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void awardClassHasCorrectGetAwardCategoryMethod(String value1, String value2) throws Throwable {
+        Object[][] arguments = {
+                {value1, String.class},
+                {value2, String.class}
+        };
+        Object classInstance = testClass.createInstance(arguments);
+        testClass.setFieldValue(classInstance, value2, secondFieldName);
+        String[] getMethodModifiers = {"public"};
+        String incorrectGetMethodMessage = String.join(" ",
+                "Your", getSecondMethodName, "does not correctly get the value of the", secondFieldName, "attribute.");
+        Object getMethodOutput = testClass.callMethod(getSecondMethodName, getMethodModifiers, classInstance);
+        _assertEquals(value2, getMethodOutput, incorrectGetMethodMessage);
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void awardClassHasCorrectToStringMethod(String value, String b) throws Throwable {
+        Object[][] arguments = {
+                {value, String.class},
+                {b, String.class}
+        };
+        Object classInstance = testClass.createInstance(arguments);
+        String[] methodModifiers = {"public"};
+        String expected = "The award for " + b + " category goes to " + value;
+        String incorrectToStringMessage = String.join(" ",
+                "Your", className, " toString method does not return the correct String.");
+        Object output = testClass.callMethod("toString", methodModifiers, classInstance);
+        _assertEquals(expected, output, incorrectToStringMessage);
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void awardClassMethodsWorkTogether(String value, String b) throws Throwable {
+        Object[][] arguments = {
+                {"initialValue", String.class},
+                {"secondValue", String.class}
+        };
+        Object classInstance = testClass.createInstance(arguments);
+        _assertEquals("initialValue", testClass.callMethod(getFirstMethodName, classInstance),
+                "Your " + getFirstMethodName + " method does not return the correct value.");
+        _assertEquals("secondValue", testClass.callMethod(getSecondMethodName, classInstance),
+                "Your " + getSecondMethodName + " method does not return the correct value.");
+        Object[][] setDoubleArguments = {
+                {value, String.class}
+        };
+        testClass.callMethod(setFirstMethodName, setDoubleArguments, classInstance);
+        _assertEquals(value, testClass.callMethod(getFirstMethodName, classInstance),
+                "Your " + getFirstMethodName + " method does not return the correct value calling the " + setFirstMethodName + " method.");
+        Object[][] setBooleanArguments = {
+                {b, String.class}
+        };
+        testClass.callMethod(setSecondMethodName, setBooleanArguments, classInstance);
+        _assertEquals(b, testClass.callMethod(getSecondMethodName, classInstance),
+                "Your " + getSecondMethodName + " method does not return the correct value calling the " + setSecondMethodName + " method.");
+        String expectedToString = "The award for " + b + " category goes to " + value;
+        String incorrectToStringMessage = String.join(" ",
+                "Your", className, " toString method does not return the correct String after updating the values of its attributes using its setter methods.");
+        Object output = testClass.callMethod("toString", classInstance);
+        _assertEquals(expectedToString, output, incorrectToStringMessage);
+    }
+}
---
+++ src/oop/special_class_method/hard/q10/QuestionText.md
@@ -0,0 +1,49 @@
+# oop-special_class_method-hard-q10
+
+## Question Text
+
+You've been chosen to make plans for an award show! Your first task is to model the awards being received, so create a
+java class called **Award** that has two `String` attributes **awardRecipient** and **awardCategory** that are
+initialized by the constructor. You will need to compose the necessary methods so that these attributes can be accessed 
+and modified from outside the class.
+
+### Award Class:
+
+Your `Award` class must have the following items:
+
+- Attributes:
+    - A `String` attribute named **awardRecipient** which is not accessible from other classes
+    - An `String` attribute named **awardCategory** which is not accessible from other classes
+
+- Constructors:
+    - A constructor which is accessible from other classes and accepts two `String` parameter to initialize the
+      **awardRecipient** and **awardCategory** attributes in that order.
+
+- Methods:
+    - a `setAwardRecipient` method which accepts a `String` value, updates the **awardRecipient** attribute with it, and
+      is
+      accessible
+      from other classes
+    - a `getAwardRecipient` method which is accessible from other classes and returns the value of the **
+      awardRecipient** attribute
+    - a `setAwardCategory` method which accepts a `String` value, updates the **awardCategory** attribute with it, and
+      is
+      accessible
+      from other classes
+    - a `getAwardCategory` method which is accessible from other classes and returns the value of the **awardCategory**
+      attribute
+    - a `toString` method which returns a String of the information about the `Award` class of the form
+      `"The award for " + awardCategory + " category goes to " + awardRecipient`.
+
+### Distractors:
+
+- setAwardRecipient(awardRecipient);
+- setAwardCategory(awardCategory);
+- return AwardCategory;
+- return awardCategory = awardCategory;
+- public String awardCategory;
+- public String awardRecipient;
+- private class Award
+- public Award()
+- public String toString()
+- public void setAwardRecipient(String)	
