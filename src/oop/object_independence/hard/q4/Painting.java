55b2546b28bcb711ddeb8cb1b0bfea316fbb1a18
Samantha H <hodge1214@gmail.com>
2023-05-21 17:53:17 -0400

    wrote test

--- src/oop/object_independence/hard/q4/Painting.java
+++ src/oop/object_independence/hard/q4/Painting.java
@@ -1,4 +1,31 @@
 package oop.object_independence.hard.q4;
 
 public class Painting {
+    private String name;
+    private String quality;
+
+    public Painting(String n, String q) {
+        name = n;
+        quality = q;
+    }
+
+    public void setQuality(String q) {
+        quality = q;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return "The item " + name + " is of " + quality + " quality";
+    }
 }
--- src/oop/object_independence/hard/q4/TestMuseum.java
+++ src/oop/object_independence/hard/q4/TestMuseum.java
@@ -1,4 +1,20 @@
 package oop.object_independence.hard.q4;
 
 public class TestMuseum {
+    public static void main(String[] args) {
+        Painting p1 = new Painting("The Starry Night", "good");
+        Painting p2 = new Painting("The Scream", "poor");
+        Painting[] inventory = {p1, p2};
+        for (int i = 0; i < inventory.length; i++)
+            System.out.println(inventory[i].toString());
+        System.out.println("Some time passed...");
+        p1.setQuality("fair");
+        for (int i = 0; i < inventory.length; i++) {
+            Painting item = inventory[i];
+            System.out.println(item.toString());
+            if (item.getQuality().equals("poor"))
+                System.out.println("This needs cleaning.");
+        }
+    }
 }
+
