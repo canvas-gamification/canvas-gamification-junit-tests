e2a01a2d7e5f2cebd359e9cb02e2b7fcc341e255
Samantha H <hodge1214@gmail.com>
2023-05-21 23:13:42 -0400

    set up rest of question

--- src/oop/object_independence/hard/q5/Glass.java
+++ src/oop/object_independence/hard/q5/Glass.java
@@ -1,4 +1,26 @@
 package oop.object_independence.hard.q5;
 
 public class Glass {
+    private String condition;
+    public boolean isBroken;
+    private double weight;
+
+    public Glass(String s, boolean b, double w) {
+        condition = s;
+        isBroken = b;
+        weight = w;
+    }
+
+    public void broken() {
+        isBroken = true;
+    }
+
+    public void fix() {
+        isBroken = false;
+    }
+
+    public String toString() {
+        return "The item is " + condition + ", and its weight is " + weight + " and is it broken? " + isBroken;
+    }
 }
+
--- src/oop/object_independence/hard/q5/TestFragile.java
+++ src/oop/object_independence/hard/q5/TestFragile.java
@@ -1,4 +1,22 @@
 package oop.object_independence.hard.q5;
 
 public class TestFragile {
+    public static void main(String[] args) {
+        Glass g1 = new Glass("New", false, 40.0);
+        Glass g2 = new Glass("Worn", false, 32.9);
+        Glass g3 = new Glass("Old", false, 813.25);
+        System.out.println(g1.toString());
+        System.out.println(g2.toString());
+        System.out.println(g3.toString());
+        g1.broken();
+        g3.broken();
+        Glass[] homeInventory = {g1, g2, g3};
+        for (int i = 0; i < homeInventory.length; i++) {
+            if (homeInventory[i].isBroken)
+                homeInventory[i].fix();
+        }
+        System.out.println(g1.toString());
+        System.out.println(g2.toString());
+        System.out.println(g3.toString());
+    }
 }
