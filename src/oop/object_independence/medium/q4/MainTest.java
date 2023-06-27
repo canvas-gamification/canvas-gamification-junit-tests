7678f622138eff54d715968301a4f9a0895bfc9d
Kai Khademi <99101819+KaiKhademi@users.noreply.github.com>
2023-06-27 05:31:56 -0700

    Update src/oop/object_independence/medium/q4/MainTest.java
    Co-authored-by: SamamthaH <79422004+SamamthaH@users.noreply.github.com>

--- src/oop/object_independence/medium/q4/MainTest.java
+++ src/oop/object_independence/medium/q4/MainTest.java
@@ -93,7 +93,7 @@ public class MainTest {
                         new StringLiteral("Now Playing : " + playlist[current])
                 },
                 String.join(" ", "Your", firstMethod, "method does not print the correct output."));
-        assertNull(output, String.join(" ", "Your", firstMethod, "should not return any output"));
+        assertNull(output, String.join(" ", "Your", firstMethod, "method should not return any output"));
         _assertEquals(current, testClass.getFieldValue(classInstance, secondFieldName),
                 String.join(" ", "Your", className, firstMethod, "method should not increase the", secondFieldName));
     }
