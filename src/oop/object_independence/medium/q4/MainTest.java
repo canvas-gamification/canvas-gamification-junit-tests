39bc319de1d99ff8898471e2e5f89f1b54df46d1
Kai Khademi <99101819+KaiKhademi@users.noreply.github.com>
2023-06-27 05:33:02 -0700

    Update src/oop/object_independence/medium/q4/MainTest.java
    Co-authored-by: SamamthaH <79422004+SamamthaH@users.noreply.github.com>

--- src/oop/object_independence/medium/q4/MainTest.java
+++ src/oop/object_independence/medium/q4/MainTest.java
@@ -133,7 +133,7 @@ public class MainTest {
                 String.join(" ", "Your", firstMethod, "method does not print the correct output."));
         assertNull(output, String.join(" ", "Your", firstMethod, "method should not return any output"));
         _assertEquals((current - 1 + playlist.length) % playlist.length, testClass.getFieldValue(classInstance, secondFieldName),
-                String.join(" ", "Your", className, firstMethod, "method does not decrease the", secondFieldName));
+                String.join(" ", "Your", firstMethod, "method does not decrease the", secondFieldName, "attribute"));
     }
 
 }
