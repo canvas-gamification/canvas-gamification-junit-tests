6900e753c1205f56a4edfd8f51cecbc7050f52b3
Kai Khademi <99101819+KaiKhademi@users.noreply.github.com>
2023-06-27 03:17:16 -0700

    Update src/oop/special_class_method/medium/q7/MainTest.java
    Co-authored-by: SamamthaH <79422004+SamamthaH@users.noreply.github.com>

--- src/oop/special_class_method/medium/q7/MainTest.java
+++ src/oop/special_class_method/medium/q7/MainTest.java
@@ -54,7 +54,7 @@ public class MainTest {
         assertTrue(testClass.hasModifier(getAttributeMethodName1, null, "public"),
                 "Your " + getAttributeMethodName1 + " method does not have the correct visibility modifier.");
         _assertEquals(0, getMethodOutput,
-                "Your " + getAttributeMethodName1 + " method does not return the value of the size field.");
+                "Your " + getAttributeMethodName1 + " method does not return the value of the " + attributeName1+ " attribute.");
     }
 
     @Test
