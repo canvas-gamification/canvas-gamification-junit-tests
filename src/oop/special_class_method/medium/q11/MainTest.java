825db9002d74fc0e6d3bbd3a7d4598920481fe5b
Kai <kiamehrkhademi2019@gmail.com>
2023-07-19 16:51:27 -0700

    Wrote q11

---
+++ src/oop/special_class_method/medium/q11/MainTest.java
@@ -0,0 +1,187 @@
+package oop.special_class_method.medium.q11;
+
+import global.ObjectTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static global.tools.CustomAssertions._assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    // Java
+
+    private final String className = "Oven";
+    private final String attributeName0 = "currTemp";
+    private final String getAttributeMethodName0 = "getCurrTemp";
+    private final String setAttributeMethodName0 = "setCurrTemp";
+    private final String attributeName1 = "size";
+    private final String getAttributeMethodName1 = "getSize";
+    private final String attributeName2 = "colour";
+    private final String getAttributeMethodName2 = "getColour";
+    private final String methodName1 = "powerDecrease";
+    private final String methodName2 = "powerIncrease";
+    private final String methodName3 = "changePower";
+    private final int changeValue = 10;
+    private final int changeLimit = 100;
+
+    private ObjectTest testClass;
+
+    @BeforeEach
+    public void setUp() throws Throwable {
+        String classString = "oop.special_class_method.medium.q11." + className;
+        this.testClass = new ObjectTest(classString);
+        String modifiedClassMessage =
+                "You have modified the provided portions of class " + className + ". Please revert them to the original state.";
+        assertTrue(testClass.hasField(attributeName0, double.class), modifiedClassMessage);
+        assertTrue(testClass.hasModifier(attributeName0, "private"), modifiedClassMessage);
+        assertTrue(testClass.hasField(attributeName1, double.class), modifiedClassMessage);
+        assertTrue(testClass.hasModifier(attributeName1, "private"), modifiedClassMessage);
+        assertTrue(testClass.hasField(attributeName2, String.class), modifiedClassMessage);
+        assertTrue(testClass.hasModifier(attributeName2, "private"), modifiedClassMessage);
+        Object[][] tests = new Object[][]{
+                {3.2, 3.4, "Red", 43243.3},
+                {432.3, 10.2, "Black", 432.2},
+                {1023.4, 11.0, "Blue", 565.4},
+                {434.3, 43.4, "White", 543.3},
+                {43.34, 7.2, "Yellow", 5555.4}
+        };
+        for (int i = 0; i < tests.length; i++) {
+            double temp = (double) tests[i][0];
+            double size = (double) tests[i][1];
+            String colour = (String) tests[i][2];
+            double updatedValue = (double) tests[i][3];
+            Object[][] arguments = {
+                    {temp, double.class},
+                    {size, double.class},
+                    {colour, String.class}
+            };
+            Class<?>[] classes = {double.class, double.class, String.class};
+            Object testInstance = testClass.createInstance(arguments);
+            _assertEquals(temp, testClass.getFieldValue(testInstance, attributeName0), modifiedClassMessage);
+            _assertEquals(size, testClass.getFieldValue(testInstance, attributeName1), modifiedClassMessage);
+            _assertEquals(colour, testClass.getFieldValue(testInstance, attributeName2), modifiedClassMessage);
+            assertTrue(testClass.hasModifier(classes, "public"), modifiedClassMessage);
+
+            Object getMethodOutput = testClass.callMethod(getAttributeMethodName0, testInstance);
+            assertTrue(testClass.hasModifier(getAttributeMethodName0, null, "public"), modifiedClassMessage);
+            _assertEquals(temp, getMethodOutput, modifiedClassMessage);
+
+            getMethodOutput = testClass.callMethod(getAttributeMethodName1, testInstance);
+            assertTrue(testClass.hasModifier(getAttributeMethodName1, null, "public"), modifiedClassMessage);
+            _assertEquals(size, getMethodOutput, modifiedClassMessage);
+
+            getMethodOutput = testClass.callMethod(getAttributeMethodName2, testInstance);
+            assertTrue(testClass.hasModifier(getAttributeMethodName2, null, "public"), modifiedClassMessage);
+            _assertEquals(colour, getMethodOutput, modifiedClassMessage);
+
+            Object[][] setMethodArguments = {
+                    {updatedValue, double.class}
+            };
+            Class<?>[] methodModifierClasses = {
+                    double.class
+            };
+            assertTrue(testClass.hasMethod(setAttributeMethodName0, methodModifierClasses, Void.TYPE), modifiedClassMessage);
+            testClass.callMethod(setAttributeMethodName0, setMethodArguments, testInstance);
+            assertTrue(testClass.hasModifier(setAttributeMethodName0, methodModifierClasses, "public"), modifiedClassMessage);
+
+            _assertEquals(updatedValue, testClass.getFieldValue(testInstance, attributeName0), modifiedClassMessage);
+        }
+    }
+
+
+    private static Stream<Arguments> inputProvider() {
+        return Stream.of(
+                Arguments.of(3.2, 3.4, "Red"),
+                Arguments.of(432.3, 10.2, "Black"),
+                Arguments.of(1023.4, 11.0, "Blue"),
+                Arguments.of(434.3, 43.4, "White"),
+                Arguments.of(43.34, 7, "Yellow"),
+                Arguments.of(100, 7, "Yellow")
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void correctToStringMethod(double value1, double value2, String value3) throws Throwable {
+        Object[][] arguments = {
+                {value1, double.class},
+                {value2, double.class},
+                {value3, String.class}
+        };
+        String incorrectToStringMessage = "Your toString method does not return the correct String.";
+        Object testInstance = testClass.createInstance(arguments);
+        String expectedOutput = "The " + value3 + " Oven with size of " + value2 + " has the tempeture of " + value1;
+        Object toStringOutput = testClass.callMethod("toString", testInstance);
+        _assertEquals(expectedOutput, toStringOutput, incorrectToStringMessage);
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void correctPowerDecreaseMethod(double value1, double value2, String value3) throws Throwable {
+        Object[][] arguments = {
+                {value1, double.class},
+                {value2, double.class},
+                {value3, String.class}
+        };
+        Object testInstance = testClass.createInstance(arguments);
+        assertTrue(testClass.hasMethod(methodName1, null),
+                "Your " + className + " class does not include a " + methodName1 + " method.");
+        assertTrue(testClass.hasMethod(methodName1, null, Void.TYPE),
+                "Your " + methodName1 + " method should not return anything.");
+        assertTrue(testClass.hasMethod(methodName1, null, Void.TYPE, new String[]{"public"}),
+                "Your " + methodName1 + " method does not have the correct visibility modifier.");
+        testClass.callMethod(methodName1, testInstance);
+        _assertEquals(value1 - changeValue, testClass.getFieldValue(testInstance, attributeName0),
+                "Your " + methodName1 + " method does not decrease the value of " + attributeName0 + " correctly.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void correctPowerIncreaseeMethod(double value1, double value2, String value3) throws Throwable {
+        Object[][] arguments = {
+                {value1, double.class},
+                {value2, double.class},
+                {value3, String.class}
+        };
+        Object testInstance = testClass.createInstance(arguments);
+        assertTrue(testClass.hasMethod(methodName2, null),
+                "Your " + className + " class does not include a " + methodName2 + " method.");
+        assertTrue(testClass.hasMethod(methodName2, null, Void.TYPE),
+                "Your " + methodName2 + " method should not return anything.");
+        assertTrue(testClass.hasMethod(methodName2, null, Void.TYPE, new String[]{"public"}),
+                "Your " + methodName2 + " method does not have the correct visibility modifier.");
+        testClass.callMethod(methodName2, testInstance);
+        _assertEquals(value1 + changeValue, testClass.getFieldValue(testInstance, attributeName0),
+                "Your " + methodName2 + " method does not increase the value of " + attributeName0 + " correctly.");
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputProvider")
+    public void correctChangePowerMethod(double value1, double value2, String value3) throws Throwable {
+        Object[][] arguments = {
+                {value1, double.class},
+                {value2, double.class},
+                {value3, String.class}
+        };
+        Object testInstance = testClass.createInstance(arguments);
+        assertTrue(testClass.hasMethod(methodName3, null),
+                "Your " + className + " class does not include a " + methodName3 + " method.");
+        assertTrue(testClass.hasMethod(methodName3, null, Void.TYPE),
+                "Your " + methodName3 + " method should not return anything.");
+        assertTrue(testClass.hasMethod(methodName3, null, Void.TYPE, new String[]{"public"}),
+                "Your " + methodName3 + " method does not have the correct visibility modifier.");
+        testClass.callMethod(methodName3, testInstance);
+        if (value1 > changeLimit) {
+            _assertEquals(value1 - changeValue, testClass.getFieldValue(testInstance, attributeName0),
+                    "Your " + methodName3 + " method does not change the value of " + attributeName0 + " correctly.");
+        } else {
+            _assertEquals(value1 + changeValue, testClass.getFieldValue(testInstance, attributeName0),
+                    "Your " + methodName3 + " method does not change the value of " + attributeName0 + " correctly.");
+        }
+    }
+}
---
+++ src/oop/special_class_method/medium/q11/QuestionText.md
@@ -0,0 +1,33 @@
+# oop-special_class_method-medium-q11
+
+## Question Text
+
+Many appliances and electronic devices have an internal mechanism to moderate temperature. Make a Java class called
+**Oven** that has attributes **currTemp**, **size**, and **colour** that specify different properties of the **Oven**.
+
+### Oven Class:
+
+Your `Oven` class already has the following items:
+
+- Attributes:
+    - A `double` called **currTemp**
+    - A `double` called **size**
+    - A `String` called **colour**
+    - These are not visible from other classes
+
+- Constructor:
+    - An `Oven` constructor that is visible to other classes and accepts two `double` and a `String` and uses it to
+      initialize the **currTemp**, **size**, and **colour** attributes.
+- Methods:
+    - Getter methods for **size** and **colour** attributes, and getter and setter method for **currTemp** attribute.
+
+You need to add the following items:
+
+- Methods:
+    - a `toString` method which returns the string `"The z Oven with size of y has the tempeture of x` where x is
+      the value of **currTemp**, y is the value of **size**, and z is the value of **colour**.
+    - Two `powerDecrease` and `powerIncrease` methods where the `powerDecrease` decrease the value of **currTemp** by 10
+      and the `powerIncrease` method increase the value of **currTemp** by 10.
+    - A `changePower` method which call the `powerDecrease` if the value of **currTemp** attribute is more than 100, and
+      call the `powerIncrease` otherwise.
+    - These methods must be visible from other classes.
