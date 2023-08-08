4eb2b9c758877facf77b3d5486ba62f4063032d1
Kai <kiamehrkhademi2019@gmail.com>
2023-08-07 20:25:08 -0700

    Modified q11

--- src/oop/special_class_method/hard/q11/MainTest.java
+++ src/oop/special_class_method/hard/q11/MainTest.java
@@ -31,13 +31,13 @@ public class MainTest {
     @Test
     public void alienClassHasCorrectAttributes() {
         assertTrue(testClass.hasField(attributeName1),
-                "Your " + className + " class is missing the " + attributeName1 + " attributes, or it spelled incorrectly.");
+                "Your " + className + " class is missing the " + attributeName1 + " attribute, or it spelled incorrectly.");
         assertTrue(testClass.hasField(attributeName1, int.class),
                 "Your " + attributeName1 + " does not have the correct type.");
         assertTrue(testClass.hasModifier(attributeName1, "private"),
                 "Your " + attributeName1 + " does not have the correct visibility modifier.");
         assertTrue(testClass.hasField(attributeName2),
-                "Your " + className + " class is missing the " + attributeName2 + " attributes, or it is spelled incorrectly.");
+                "Your " + className + " class is missing the " + attributeName2 + " attribute, or it is spelled incorrectly.");
         assertTrue(testClass.hasField(attributeName2, String.class),
                 "Your " + attributeName2 + " does not have the correct type.");
         assertTrue(testClass.hasModifier(attributeName2, "private"),
