dedb7d8ca1cf4323e72a3220d1e294a79bf18f90
Kai <kiamehrkhademi2019@gmail.com>
2023-08-07 20:46:36 -0700

    Modified q3

--- src/methods/modular_programs/medium/q3/MainTest.java
+++ src/methods/modular_programs/medium/q3/MainTest.java
@@ -2,6 +2,7 @@ package methods.modular_programs.medium.q3;
 
 import java.util.stream.Stream;
 
+import global.MethodTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,39 +17,52 @@ import global.variables.clauses.PlaceHolder;
 import global.variables.clauses.StringLiteral;
 
 public class MainTest extends BaseTest {
-  // Parsons
-  public Clause[] testSentence() {
-    TestOption.isInputTest = true;
-    TestOption.defaultInput = "12";
-    return new Clause[] {
-        new StringLiteral("Enter a number for the upper bound of the series: "),
-        new NewLine(),
-        new PlaceHolder(),
-    };
-  }
+    // Parsons
+    public Clause[] testSentence() {
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "12";
+        return new Clause[]{
+                new StringLiteral("Enter a number for the upper bound of the series: "),
+                new NewLine(),
+                new PlaceHolder(),
+        };
+    }
 
-  public void runMain() {
-    AlternateIt.main(new String[0]);
-  }
+    public void runMain() {
+        AlternateIt.main(new String[0]);
+    }
 
-  static Stream<Arguments> inputUpperBoundProvider() {
-    return Stream.of(
-        Arguments.of(0, 0),
-        Arguments.of(1, 1),
-        Arguments.of(12, -6),
-        Arguments.of(13, 7),
-        Arguments.of(2, -1),
-        Arguments.of(1233, 617),
-        Arguments.of(992, -496));
-  }
+    static Stream<Arguments> inputUpperBoundProvider() {
+        return Stream.of(
+                Arguments.of(0, 0),
+                Arguments.of(1, 1),
+                Arguments.of(12, -6),
+                Arguments.of(13, 7),
+                Arguments.of(2, -1),
+                Arguments.of(1233, 617),
+                Arguments.of(992, -496));
+    }
 
-  @ParameterizedTest
-  @MethodSource("inputUpperBoundProvider")
-  public void printsCorrectOutput(int upperBound, int expectedSum) throws InvalidClauseException {
-    TestOption.incorrectStructureErrorMessage = "Your program does not print the correct message of the series sum.";
-    runWithInput(upperBound + "", new Clause[][] { {
-        new StringLiteral("The sum of the series is: "),
-        new IntegerLiteral(expectedSum),
-    } });
-  }
+    @ParameterizedTest
+    @MethodSource("inputUpperBoundProvider")
+    public void printsCorrectOutput(int upperBound, int expectedSum) throws InvalidClauseException {
+        TestOption.incorrectStructureErrorMessage = "Your program does not print the correct message of the series sum.";
+        runWithInput(upperBound + "", new Clause[][]{{
+                new StringLiteral("The sum of the series is: "),
+                new IntegerLiteral(expectedSum),
+        }});
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputUpperBoundProvider")
+    public void correctSumCalcMethod(int n, int ans) throws Throwable {
+        Clause[] methodSentence = new Clause[]{
+                new StringLiteral("The sum of the series is: " + ans)
+        };
+        Object[][] arguments = {
+                {n, int.class}
+        };
+        MethodTest m = new MethodTest(AlternateIt.class, "sumCalc", arguments, methodSentence);
+        m.callMethod();
+    }
 }
