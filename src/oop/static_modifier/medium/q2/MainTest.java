279f3d86e241bc58b1a8a44e2ea607e84b88ea0e
Hedgemon4 <sethlinc4@gmail.com>
2023-08-26 17:59:08 -0700

    Added test method order annoation

--- src/oop/static_modifier/medium/q2/MainTest.java
+++ src/oop/static_modifier/medium/q2/MainTest.java
@@ -14,6 +14,7 @@ import java.util.stream.Stream;
 import static global.tools.CustomAssertions._assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MainTest {
     private ObjectTest classInstance;
     private ObjectTest testInstance;
