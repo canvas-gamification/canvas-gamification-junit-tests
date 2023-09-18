437c06c68a0b220152c68646683a221344da00e6
Kai <kiamehrkhademi2019@gmail.com>
2023-09-17 17:33:30 -0700

    Modified q2

--- src/oop/special_class_method/hard/q2/MainTest.java
+++ src/oop/special_class_method/hard/q2/MainTest.java
@@ -50,10 +50,11 @@ public class MainTest {
         Object[][] arguments = {
                 {new String[0], String[].class}
         };
+        String incorrectMainMethodOutput = "Your " + testClassName + " does not print the correct output in its main method.";
         outputClass.callMethod("main", arguments, methodModifiers, classInstance, new Clause[]{
                 new StringLiteral("This " + className + " class have a size of " + outputSize * 10 + ", and are type " + outputType),
                 new Optional(new StringLiteral(" "))
-        });
+        }, incorrectMainMethodOutput);
     }
 
     @Test
