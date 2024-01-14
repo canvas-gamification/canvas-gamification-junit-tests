1893b64dde684a773f24af825693790da14ef0bf
Samantha H <hodge1214@gmail.com>
2024-01-14 02:11:41 -0500

    updated question and test

--- src/oop/creating_objects_from_classes/hard/q3/MainTest.java
+++ src/oop/creating_objects_from_classes/hard/q3/MainTest.java
@@ -3,100 +3,65 @@ package oop.creating_objects_from_classes.hard.q3;
 import global.ObjectTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
-
-import static global.tools.CustomAssertions._assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
     // Java
 
-    private final String tomato = "Tomato";
-    private final String methodName = "plantSeed";
-    private ObjectTest tomatoClass;
-
-    private final String usedFor = "usedFor";
-    private final String flavour = "flavour";
-    private final String name = "name";
+    private ObjectTest testTomato;
+    private final String testClass = "TomatoTest";
+    private final String methodName = "produceSeed";
+    private final String objectName = "Tomato";
+    private final String type = "type";
+    private final String nickname = "nickname";
+    private final String[] t1ExpectedValues = {null, null};
+    private final String[] t2ExpectedValues = {"cherry", null};
+    private final String[] t3ExpectedValues = {"plum", "Sherry"};
+    private final String[] t4ExpectedValues = {"cherry", "Delilah"};
 
     @BeforeEach
     public void setUp() {
-        String tomatoClassString = "oop.creating_objects_from_classes.hard.q3." + tomato;
-        tomatoClass = new ObjectTest(tomatoClassString);
+        String tomatoClassString = "oop.creating_objects_from_classes.hard.q3." + testClass;
+        testTomato = new ObjectTest(tomatoClassString);
     }
 
     @Test
-    public void tomatoClassHasPlantSeedMethod() {
-        Class<?>[] classes = {String[].class};
-        assertTrue(tomatoClass.hasMethod(methodName, classes),
-                "Your " + tomato + " class is missing the method " + methodName + ".");
-        assertTrue(tomatoClass.hasReturnType(methodName, classes, Tomato.class),
-                "Your " + methodName + " method does not return the correct type.");
-        assertTrue(tomatoClass.hasModifier(methodName, classes, "public"),
-                "Your " + methodName + " method does not have the correct visibility modifier.");
-    }
+    public void correctTomatoTestClass() throws Throwable {
+        String missingMain = "Your %s class is missing the %s method. Make sure it has been declared, it is spelt correctly, and has the correct parameters.";
+        String incorrectVisibility = "Your main method does not have the correct visibility modifier.";
+        String missingStatic = "Your main method is missing the static modifier.";
+        String wrongReturnType = "Your main method does not have the correct return type.";
+        Class<?>[] mainArgs = new Class[]{String[].class};
+        /* Check Method Definitions */
+        assertTrue(testTomato.hasMethod("main", mainArgs), String.format(missingMain, testClass, "main"));
+        assertTrue(testTomato.hasModifier("main", mainArgs, "public"), incorrectVisibility);
+        assertTrue(testTomato.hasModifier("main", mainArgs, "static"), missingStatic);
+        assertTrue(testTomato.hasReturnType("main", mainArgs, Void.TYPE), wrongReturnType);
 
-    private static Stream<Arguments> tomatoNamesArrayInputProvider() {
-        return Stream.of(
-                Arguments.of((Object) new String[]{"Grape Tomato"}),
-                Arguments.of((Object) new String[]{"Red Beefsteak Tomato", "juicy", "salsa"}),
-                Arguments.of((Object) new String[]{"Green Beefsteak Tomato"}),
-                Arguments.of((Object) new String[]{"Cherry Tomatoes", "sweet", "salads"}),
-                Arguments.of((Object) new String[]{"Cocktail Tomatoes"}),
-                Arguments.of((Object) new String[]{}),
-                Arguments.of((Object) new String[]{"Heirloom Tomatoes", "meaty", "roasting", "deep, rich colour"})
-        );
-    }
+        Object[][] mainParameters = {{new String[0], String[].class}};
+        testTomato.callMethod("main", mainParameters);
 
-    @ParameterizedTest
-    @MethodSource("tomatoNamesArrayInputProvider")
-    public void plantSeedClassReturnsCorrectValue(String[] names) throws Throwable {
-        Object tomatoInstance = tomatoClass.createInstance();
-        Object[][] arguments = {
-                {names, String[].class}
-        };
-        Object plantSeedOutput = tomatoClass.callMethod(methodName, arguments, tomatoInstance);
-        assertTrue(plantSeedOutput instanceof Tomato, "Your " + methodName + " method does not return the correct type.");
+        /* Check that the constructor was called and with the correct values */
+        String noConstructorCall = "Your main method did not initialize the %s %s object.";
+        String incorrectValue = "Your main method did not initialize the %s %s with the correct %s value.";
 
-        if (names.length == 1) {
-            // check that only name was initialized (correct constructor was called)
-            _assertEquals(names[0], tomatoClass.getFieldValue(plantSeedOutput, this.name),
-                    "Your " + methodName + " method does not return the correct output. " +
-                            "Check that you are calling the correct constructor for the input given.");
-            assertNull(tomatoClass.getFieldValue(plantSeedOutput, this.flavour),
-                    "Your " + methodName + " method does not return the correct output. " +
-                            "Check that you are calling the correct constructor for the input given.");
-            assertNull(tomatoClass.getFieldValue(plantSeedOutput, this.usedFor),
-                    "Your " + methodName + " method does not return the correct output. " +
-                            "Check that you are calling the correct constructor for the input given.");
-        } else if (names.length == 3) {
-            // check that all fields were initialized
-            _assertEquals(names[0], tomatoClass.getFieldValue(plantSeedOutput, this.name),
-                    "Your " + methodName + " method does not return the correct output. " +
-                            "Check that you are calling the correct constructor for the input given.");
-            _assertEquals(names[1], tomatoClass.getFieldValue(plantSeedOutput, this.flavour),
-                    "Your " + methodName + " method does not return the correct output. " +
-                            "Check that you are calling the correct constructor for the input given.");
-            _assertEquals(names[2], tomatoClass.getFieldValue(plantSeedOutput, this.usedFor),
-                    "Your " + methodName + " method does not return the correct output. " +
-                            "Check that you are calling the correct constructor for the input given.");
-        } else {
-            // check that no fields were initialized (default constructor)
-            assertNull(tomatoClass.getFieldValue(plantSeedOutput, this.name),
-                    "Your " + methodName + " method does not return the correct output. " +
-                            "Check that you are calling the correct constructor for the input given.");
-            assertNull(tomatoClass.getFieldValue(plantSeedOutput, this.flavour),
-                    "Your " + methodName + " method does not return the correct output. " +
-                            "Check that you are calling the correct constructor for the input given.");
-            assertNull(tomatoClass.getFieldValue(plantSeedOutput, this.usedFor),
-                    "Your " + methodName + " method does not return the correct output. " +
-                            "Check that you are calling the correct constructor for the input given.");
-        }
+        assertTrue(Tomato.isConstructor1Called(), String.format(noConstructorCall, "first", objectName));
+        assertEquals(t1ExpectedValues[0], Tomato.getTomato1().getType(), String.format(incorrectValue, "first", objectName, type));
+        assertEquals(t1ExpectedValues[1], Tomato.getTomato1().getNickname(), String.format(incorrectValue, "first", objectName, nickname));
 
+        assertTrue(Tomato.isConstructor2Called(), String.format(noConstructorCall, "second", objectName));
+        assertEquals(t2ExpectedValues[0], Tomato.getTomato2().getType(), String.format(incorrectValue, "second", objectName, type));
+        assertEquals(t2ExpectedValues[1], Tomato.getTomato2().getNickname(), String.format(incorrectValue, "second", objectName, nickname));
+
+        assertTrue(Tomato.isConstructor3Called(), String.format(noConstructorCall, "third", objectName));
+        assertEquals(t3ExpectedValues[0], Tomato.getTomato3().getType(), String.format(incorrectValue, "third", objectName, type));
+        assertEquals(t3ExpectedValues[1], Tomato.getTomato3().getNickname(), String.format(incorrectValue, "third", objectName, nickname));
+
+        assertTrue(Tomato.isMethodCalled(), String.format(noConstructorCall, "fourth", objectName));
+        assertEquals(t4ExpectedValues[0], Tomato.getTomato4().getType(), String.format(incorrectValue, "fourth", objectName, type));
+        assertEquals(t4ExpectedValues[1], Tomato.getTomato4().getNickname(), String.format(incorrectValue, "fourth", objectName, nickname));
     }
 }
+
+// On website place Tomato class here
--- src/oop/creating_objects_from_classes/hard/q3/QuestionText.md
+++ src/oop/creating_objects_from_classes/hard/q3/QuestionText.md
@@ -1,24 +1,50 @@
 # oop-creating_objects_from_classes-hard-q3
 
-Note: for this question give the student the entire Tomato file, except for the `plantSeed(String[] names)` method
-You can just leave a comment saying something like // add your plantSeed(String[] names) method here
+## Java questions
 
 ## Question Text
 
-Suppose you have defined a `Tomato` class, and now you want to add a method called `plantSeed` that takes in a String
-array called `names`.
-The method initializes and returns a `Tomato` object based on the length of the `names` array. The names array contains
-values for the class attributes in the following order: name, flavour, usedFor. It may contain either 0, 1, 3, or more
-than 3 of the attributes.
+Suppose you have defined a `Tomato` class to model your tomato plants, and now you want to test it to make sure that it 
+works. Given the tomato class below, create a test class called `TomatoTest` with a main method and initialize four 
+`Tomato` objects. The first tomato should be initialized using the default constructor, with no attributes. The second
+tomato should be initialized just with the attribute:
+-Type: cherry
+The third should be initialized with the attributes:
+-Type: plum
+-Nickname: Sherry
+The last should be initialized using the `produceSeed` method with:
+-Nickname: Delilah 
+and type the copy of the second tomato which it comes from. 
 
-E.g.
+Note: You do not need to place the tomato class in your submission code, the website will handle this for you. It is just
+for you to reference.
 
+### Tomato Class:
+
+```java
+public class Tomato {
+    private String type;
+    private String nickname;
+
+    public Tomato() {
+    }
+
+    public Tomato(String t) {
+        type = t;
+    }
+
+    public Tomato(String t, String n) {
+        type = t;
+        nickname = n;
+    }
+
+    public Tomato produceSeed(String[] parameters) {
+        if (parameters.length == 1)
+            return new Tomato(parameters[0]);
+        else if (parameters.length == 2)
+            return new Tomato(parameters[0], parameters[1]);
+        else
+            return new Tomato();
+    }
+}
 ```
-String[] names = new String[]{"Grape Tomato"};
-Tomato parentTomato = new Tomato();
-Tomato childTomato = parentTomato.plantSeed(names);
-```
-
-In this example, childTomato will have name "Grape Tomato," and none of its other fields will be initialized.
-
-You should use the given constructors to perform the initialization.
