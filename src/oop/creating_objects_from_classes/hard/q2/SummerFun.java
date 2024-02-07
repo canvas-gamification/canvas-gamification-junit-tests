b1c7f13dbe0db641b9f3cf7be2d9e214131c8317
Teresa <77898527+te-sa@users.noreply.github.com>
2024-02-07 13:08:11 -0800

    oop-creating_objects_from_classes-hard-q2 (#600)
    * wip
    
    * fixed tests
    
    * updated question and test
    
    * implemented CI fix
    
    * cleaned up copy paste
    
    ---------
    
    Co-authored-by: Seth Akins <83678885+Hedgemon4@users.noreply.github.com>
    Co-authored-by: Samantha H <hodge1214@gmail.com>
    Co-authored-by: Seth Akins <sethlinc4@gmail.com>

---
+++ src/oop/creating_objects_from_classes/hard/q2/SummerFun.java
@@ -0,0 +1,10 @@
+package oop.creating_objects_from_classes.hard.q2;
+
+public class SummerFun {
+    public static void main(String[] args) {
+        WaterToy w = new WaterToy("yellow", 5.2, false);
+        w.inflate();
+        w.throwInWater();
+    }
+}
+
