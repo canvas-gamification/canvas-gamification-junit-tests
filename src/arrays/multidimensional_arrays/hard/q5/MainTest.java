d8da75171afeb35c7b7526af631e1021d1e8d762
Samantha H <hodge1214@gmail.com>
2024-06-22 22:45:01 -0400

    reworked q5

--- src/arrays/multidimensional_arrays/hard/q5/MainTest.java
+++ src/arrays/multidimensional_arrays/hard/q5/MainTest.java
@@ -1,17 +1,8 @@
 package arrays.multidimensional_arrays.hard.q5;
 
 import global.BaseTest;
-import global.MethodTest;
-import global.tools.CustomAssertions;
 import global.variables.Clause;
 import global.variables.clauses.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
-
-import static global.utils.ArrayUtil.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
     // Parsons
@@ -41,7 +32,7 @@ public class MainTest extends BaseTest {
     }
 
     public static double avg(int[][] nums) {
-        int count = 0;
+        double count = 0;
         int sum = 0;
         for (int i = 0; i < nums.length; i++)
             for (int j = 0; j < nums[i].length; j++) {
@@ -50,43 +41,5 @@ public class MainTest extends BaseTest {
             }
         double avg = sum / count;
         return avg;
-    }
-
-    static Stream<int[][]> InputProvider() {
-        int[][] a1 = new int[10][20];
-        for (int i = 0; i < 10; i++)
-            a1[i] = generateRandomArray(1, 100000000, 20);
-        int[][] a2 = new int[40][30];
-        for (int i = 0; i < 40; i++)
-            a2[i] = generateRandomArray(1, 100000000, 30);
-        int[][] a3 = new int[70][70];
-        for (int i = 0; i < 70; i++)
-            a3[i] = generateRandomArray(1, 100000000, 70);
-        return Stream.of(
-                a1, a2, a3
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("InputProvider")
-    void correctSumArrayMethod(int[][] input) throws Throwable {
-        Object[][] arguments = {
-                {input, int[][].class},
-        };
-        MethodTest m = new MethodTest(SumAverageProgram.class, "SumArray", arguments);
-        Object output = m.callMethod();
-        CustomAssertions._assertEquals(sum(input), output, "Your SumArray method does not return the correct sum of elements of a 2D array");
-    }
-
-    @ParameterizedTest
-    @MethodSource("InputProvider")
-    void correctAvgArrayMethod(int[][] input) throws Throwable {
-        Object[][] arguments = {
-                {input, int[][].class},
-        };
-        MethodTest m = new MethodTest(SumAverageProgram.class, "AvgArray", arguments);
-        m.setIncorrectMethodStructureErrorMessage("Your AvgArray method does not return the correct average of elements of a 2D array");
-        Object output = m.callMethod();
-        CustomAssertions._assertEquals(avg(input), output, "Your AvgArray method does not return the correct average of elements of a 2D array");
     }
 }
