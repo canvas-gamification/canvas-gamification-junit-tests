2ef99325351c4d05fd579527e0a66900e98bad2a
Samantha H <hodge1214@gmail.com>
2024-06-24 22:50:58 -0400

    reworked test to fit in category

--- src/arrays/multidimensional_arrays/hard/q9/ImAWholeDimensionAhead.java
+++ src/arrays/multidimensional_arrays/hard/q9/ImAWholeDimensionAhead.java
@@ -1,35 +1,41 @@
 package arrays.multidimensional_arrays.hard.q9;
 
 /*
-Make a Java program that randomly initialize a 3 * 3 * 3 3D array with integers ranging from 1 to 100
-(Inclusive of both ends), and then print it, in one line. For that you need to make a method that takes a 3D integer
-array and the print it in one line. For example if the 3D array is {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}} you should print
-1 2 3 4 5 6 7 8. Note that your method should be general enough to work with any size of array.
+Make a Java program that initializes a 3 * 3 * 3 3D array with integer input from the user, then ask the user which
+2-d arrays to swap and then swap them (the indexes given will refer to the first index slot). Finally, print the array
+using the Arrays.deepToString() method from the java.util.Arrays import.
 
-Sample output;
-    12 84 9 8 6 28 46 35 77 91 22 21 24 38 35 40 74 90 43 11 68 58 24 41 46 17 25
-
+Sample output:
+    Enter the array values:
+    1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
+    Enter the array indexes to swap:
+    2 0
+    [[[19, 20, 21], [22, 23, 24], [25, 26, 27]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]], [[1, 2, 3], [4, 5, 6], [7, 8, 9]]]
  */
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class ImAWholeDimensionAhead {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         int[][][] nums = new int[3][3][3];
+
+        System.out.println("Enter the array values:");
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums[i].length; j++) {
                 for (int k = 0; k < nums[i][j].length; k++)
-                    nums[i][j][k] = (int) (Math.random() * 100);
+                    nums[i][j][k] = in.nextInt();
             }
         }
-        imAWholeDimensionAhead(nums);
-    }
+        System.out.println("Enter the array indexes to swap:");
+        int i1 = in.nextInt();
+        int i2 = in.nextInt();
 
-    public static void imAWholeDimensionAhead(int[][][] nums) {
+        int[][] hold = nums[i1];
+        nums[i1] = nums[i2];
+        nums[i2] = hold;
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums[i].length; j++) {
-                for (int k = 0; k < nums[i][j].length; k++)
-                    System.out.print(nums[i][j][k] + " ");
-            }
-        }
+        System.out.println(Arrays.deepToString(nums));
     }
 }
--- src/arrays/multidimensional_arrays/hard/q9/MainTest.java
+++ src/arrays/multidimensional_arrays/hard/q9/MainTest.java
@@ -1,16 +1,22 @@
 package arrays.multidimensional_arrays.hard.q9;
 
 import global.BaseTest;
-import global.MethodTest;
+import global.exceptions.InvalidClauseException;
+import global.tools.TestOption;
 import global.variables.Clause;
-import global.variables.clauses.RandomInteger;
+import global.variables.clauses.IntegerLiteral;
+import global.variables.clauses.NewLine;
 import global.variables.clauses.StringLiteral;
+import global.variables.wrappers.Optional;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static global.utils.ArrayUtil.arrayToInput;
 import static global.utils.ArrayUtil.generateRandomArray;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest extends BaseTest {
     // Java
@@ -18,20 +24,42 @@ public class MainTest extends BaseTest {
     public static final int n = 3;
     public static final int m = 3;
     public static final int l = 3;
-    public static final int up = 100;
-    public static final int down = 1;
 
     public Clause[] testSentence() {
-        Clause[] c = new Clause[2 * n * m * l];
+        TestOption.isInputTest = true;
+        TestOption.defaultInput = "1 2 3 4 5 6 7 8 18 19 20 21 22 23 24 25 26 9 10 11 12 13 14 15 16 17 27 2 0";
+        Clause[] c = new Clause[8 + (n * 2) + (m * 2 * n) + (((l * 2) - 1) * m * n)];
         int t = 0;
+        c[t++] = new StringLiteral("Enter the array values:");
+        c[t++] = new Optional(new StringLiteral(" "));
+        c[t++] = new NewLine();
+        c[t++] = new StringLiteral("Enter the array indexes to swap:");
+        c[t++] = new Optional(new StringLiteral(" "));
+        c[t++] = new NewLine();
+        c[t++] = new StringLiteral("\\[");
         for (int i = 0; i < n; i++) {
+            c[t++] = new StringLiteral("\\[");
             for (int j = 0; j < m; j++) {
+                c[t++] = new StringLiteral("\\[");
                 for (int k = 0; k < l; k++) {
-                    c[t++] = new RandomInteger(down, up + 1, i + " " + j + " " + k);
-                    c[t++] = new StringLiteral(" ");
+                    c[t++] = new IntegerLiteral(i + " " + j + " " + k);
+                    if (k == l - 1) {
+                        if (j == m - 1)
+                            c[t++] = new StringLiteral("\\]");
+                        else
+                            c[t++] = new StringLiteral("\\], ");
+                    } else {
+                        c[t++] = new StringLiteral(", ");
+                    }
                 }
             }
+            if (i == n - 1) {
+                c[t++] = new StringLiteral("]");
+            } else {
+                c[t++] = new StringLiteral("], ");
+            }
         }
+        c[t] = new StringLiteral("]");
         return c;
     }
 
@@ -39,37 +67,51 @@ public class MainTest extends BaseTest {
         ImAWholeDimensionAhead.main(new String[0]);
     }
 
-    static Stream<int[][][]> inputProvider() {
-        int[][][] test = new int[5][15][10];
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 15; j++)
-                test[i][j] = generateRandomArray(-1000000000, 1000000000, 10);
+    static Stream<Arguments> inputProvider() {
+        int[][][][] inputs = new int[n][n][m][l];
+        int[][] swaps = {{0, 1}, {0, 2}, {2, 1}};
+        for(int x = 0; x < n; x ++){
+            for(int y = 0; y < n; y ++){
+                for(int z = 0; z < m; z++){
+                    inputs[x][y][z] = generateRandomArray(0, 100, l);
+                }
+            }
         }
+
         return Stream.of(
-                new int[][][]{{{1, 2}, {3, 4}, {5, 6}}, {{2, 1}, {4, 4}, {11, 0}}, {{12, 24}, {31, 44}, {51, 60}}},
-                new int[][][]{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}, {{9, 10}, {11, 12}}},
-                test
+                Arguments.of(inputs[0], swaps[0]),
+                Arguments.of(inputs[1], swaps[1]),
+                Arguments.of(inputs[2], swaps[2])
         );
     }
 
     @ParameterizedTest
     @MethodSource("inputProvider")
-    void correctImAWholeDimensionAheadMethod(int[][][] a) throws Throwable {
-        String ans = "";
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                for (int k = 0; k < a[i][j].length; k++)
-                    ans += a[i][j][k] + " ";
+    public void printsCorrectOutput(int[][][] input, int[] swap) throws InvalidClauseException {
+        StringBuilder s = new StringBuilder();
+        int[][][] answer = new int[n][m][l];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < l; k++) {
+                    s.append(input[i][j][k]).append(" ");
+                    answer[i][j][k] = input[i][j][k];
+                }
             }
         }
-        Clause[] methodSentence = new Clause[]{
-                new StringLiteral(ans)
-        };
-        Object[][] arguments = {
-                {a, int[][][].class}
-        };
-        MethodTest m = new MethodTest(ImAWholeDimensionAhead.class, "imAWholeDimensionAhead", arguments, methodSentence);
-        m.setIncorrectMethodStructureErrorMessage("Your imAWholeDimensionAhead does not print the elements of the 3D array correctly.");
-        m.callMethod();
+
+        int[][] hold = answer[swap[0]];
+        answer[swap[0]] = answer[swap[1]];
+        answer[swap[1]] = hold;
+
+
+        runWithInput(s + " " + arrayToInput(swap));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < l; k++) {
+                    assertEquals(answer[i][j][k], Integer.parseInt(getItemByName(i + " " + j + " " + k)),
+                            "Your program does not correctly print the output based on the input provided.");
+                }
+            }
+        }
     }
 }
