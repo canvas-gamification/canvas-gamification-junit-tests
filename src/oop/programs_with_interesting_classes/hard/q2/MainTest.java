c22e51b61ed89591419b30760becf1a0912893f7
SamamthaH <79422004+SamamthaH@users.noreply.github.com>
2024-08-15 16:09:57 -0400

    Update src/oop/programs_with_interesting_classes/hard/q2/MainTest.java

--- src/oop/programs_with_interesting_classes/hard/q2/MainTest.java
+++ src/oop/programs_with_interesting_classes/hard/q2/MainTest.java
@@ -15,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
     private ObjectTest player;
+    // Java
     private ObjectTest leaderBoard;
     private final String playerClassName = "Player";
     private final String leaderBoardClassName = "LeaderBoard";
