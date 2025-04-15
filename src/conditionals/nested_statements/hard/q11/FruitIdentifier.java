188cec22b5068bef48dc719907e3bd29d596ec76
SamamthaH <79422004+SamamthaH@users.noreply.github.com>
2025-04-15 01:17:30 -0400

    Update src/conditionals/nested_statements/hard/q11/FruitIdentifier.java

--- src/conditionals/nested_statements/hard/q11/FruitIdentifier.java
+++ src/conditionals/nested_statements/hard/q11/FruitIdentifier.java
@@ -25,7 +25,6 @@ public class FruitIdentifier {
         String color = scanner.nextLine();
         System.out.println("Enter taste: ");
         String taste = scanner.nextLine();
-        scanner.close();
 
         switch (color) {
             case "Red":
