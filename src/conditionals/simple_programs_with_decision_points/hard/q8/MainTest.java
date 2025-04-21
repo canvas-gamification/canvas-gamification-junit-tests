750ff44659258db2af83c90f173013684f6730f8
Jayati <jae186@gmail.com>
2025-04-21 13:03:13 -0700

    made changes from Sam's comments

--- src/conditionals/simple_programs_with_decision_points/hard/q8/MainTest.java
+++ src/conditionals/simple_programs_with_decision_points/hard/q8/MainTest.java
@@ -15,39 +15,43 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 public class MainTest extends BaseTest {
-    
+
+    static final int THRESHOLD = 50;
+
     @Override
     public Clause[] testSentence() {
         TestOption.isInputTest = true;
         TestOption.defaultInput = "1\n2";
-        
+
         return new Clause[]{
-            new StringLiteral("Enter x "),
+            new StringLiteral("Enter x: "),
             new Optional(new StringLiteral(" ")),
             new NewLine(),
-            new StringLiteral("Enter y "),
+            new StringLiteral("Enter y: "),
             new Optional(new StringLiteral(" ")),
             new NewLine(),
             new PlaceHolder()
         };
     }
-    
+
     static Stream<Arguments> inputProvider() {
         return Stream.of(
-            // Case 1: 1 + 10 = 11, which is less than 50.
-            Arguments.of(1, 10, "Sum is less than 50"),
-            // Case 2: 25 + 25 = 50, exactly 50.
-            Arguments.of(25, 25, "Sum is exactly 50"),
-            // Case 3: 30 + 25 = 55, which is more than 50.
-            Arguments.of(30, 25, "Sum is more than 50")
+            // Case 1: Sum is less than the threshold
+            Arguments.of(1, 10, "Sum is less than " + THRESHOLD),
+            // Case 2: Sum is less than the threshold
+            Arguments.of(25, 24, "Sum is less than " + THRESHOLD),
+            // Case 3: Sum is exactly the threshold
+            Arguments.of(25, 25, "Sum is exactly " + THRESHOLD),
+            // Case 4: Sum is more than the threshold
+            Arguments.of(26, 25, "Sum is more than " + THRESHOLD)
         );
     }
-    
+
     @Override
     public void runMain() {
-        sumToFifty.main(new String[0]);
+        SumToFifty.main(new String[0]);
     }
-    
+
     @ParameterizedTest
     @MethodSource("inputProvider")
     void printsCorrectClassification(int x, int y, String expectedFinalMessage) throws InvalidClauseException {
--- src/conditionals/simple_programs_with_decision_points/hard/q8/sumToFifty.java
+++ src/conditionals/simple_programs_with_decision_points/hard/q8/sumToFifty.java
@@ -11,13 +11,13 @@ Sum is less than 50.
  */
 import java.util.Scanner;
 
-public class sumToFifty {
+public class SumToFifty {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter x ");
+        System.out.println("Enter x: ");
         int x = input.nextInt();
-        System.out.println("Enter y ");
+        System.out.println("Enter y: ");
         int y = input.nextInt();
 
         int sum = x + y; 
